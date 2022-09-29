d934a1ff859d1674070ae6d45aad75197ded8c8b
Hamza Bakri <bakri@demos-deutschland.de>
2022-09-29 10:32:08 +0200

    Feat : add constructor to inject procedure repository

--- demosplan/DemosPlanCoreBundle/Validator/ProcedureTemplateMaillaneConnectionConstraintValidator.php
+++ demosplan/DemosPlanCoreBundle/Validator/ProcedureTemplateMaillaneConnectionConstraintValidator.php
@@ -12,14 +12,22 @@ use demosplan\DemosPlanProcedureBundle\Repository\ProcedureRepository;
 
 class ProcedureTemplateMaillaneConnectionConstraintValidator extends ConstraintValidator
 {
+    /**
+     * @var ProcedureRepository
+     */
+    private $procedureRepository;
+    public function __construct(ProcedureRepository $procedureRepository)
+    {
+        $this->procedureRepository = $procedureRepository;
+    }
     public function validate($value, Constraint $constraint): void
     {
         $this->validateTyped($value, $constraint);
     }
 
-    private function validateTyped(Procedure $procedure, ProcedureTemplateMaillaneConnectionConstraint $constraint, ProcedureRepository $procedureRepository): void
+    private function validateTyped(Procedure $procedure, ProcedureTemplateMaillaneConnectionConstraint $constraint): void
     {
-        if ($procedure->getMaster() && null !== $procedureRepository->getMaillaneConnection($procedure->getId())) {
+        if ($procedure->getMaster() && null !== $this->procedureRepository->getMaillaneConnection($procedure->getId())) {
             $this->context->buildViolation($constraint->message)->addViolation();
         }
     }
