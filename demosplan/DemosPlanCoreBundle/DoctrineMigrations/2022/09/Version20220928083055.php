18372f5b9966d59978a778f4a6891cc78a4ee7dc
Hamza Bakri <bakri@demos-deutschland.de>
2022-09-29 09:52:48 +0200

    Feat : adjust migration

--- demosplan/DemosPlanCoreBundle/DoctrineMigrations/2022/09/Version20220928083055.php
+++ demosplan/DemosPlanCoreBundle/DoctrineMigrations/2022/09/Version20220928083055.php
@@ -16,6 +16,7 @@ class Version20220928083055 extends AbstractMigration implements ContainerAwareI
     private $container;
 
     private const RESTRICT_TO_PROJECT = 'blp';
+
     public function getDescription(): string
     {
         return 'refs T: test Migration';
@@ -30,15 +31,24 @@ class Version20220928083055 extends AbstractMigration implements ContainerAwareI
         if (self::RESTRICT_TO_PROJECT !== $this->container->getParameter('project_folder')){
             return;
         }
-        $this->abortIfNotMysql();
-        $this->addSql('ALTER TABLE _procedure DROP FOREIGN KEY FK_D1A01D02AC0C069A');
-        $this->addSql('DROP INDEX UNIQ_D1A01D02AC0C069A ON _procedure');
 
-        $this->addSql('ALTER TABLE _procedure DROP COLUMN maillane_connection_id');
+        $matchingRecords = $this->getMatchingRecordsToUp();
+        $this->abortIfNotMysql();
         $this->addSql('ALTER TABLE maillane_connection ADD procedure_id CHAR(36) DEFAULT NULL');
         $this->addSql('ALTER TABLE maillane_connection ADD CONSTRAINT FK_71C04D1D1624BCD2 FOREIGN KEY (procedure_id) REFERENCES _procedure (_p_id)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_71C04D1D1624BCD2 ON maillane_connection (procedure_id)');
-
+        foreach ($matchingRecords as $matchingRecord) {
+            $this->addSql('UPDATE maillane_connection SET procedure_id = :procedureId
+                        WHERE id = :maillaneConnectionId' ,
+                [
+                    'procedureId' => $matchingRecord['_p_id'],
+                    'maillaneConnectionId' => $matchingRecord['maillane_connection_id']
+                ]);
+        }
+        $this->addSql('ALTER TABLE _procedure DROP FOREIGN KEY FK_D1A01D02AC0C069A');
+        $this->addSql('DROP INDEX UNIQ_D1A01D02AC0C069A ON _procedure');
+        $this->addSql('ALTER TABLE _procedure DROP COLUMN maillane_connection_id');
+        //$this->addSql('ALTER TABLE maillane_connection ALTER COLUMN procedure_id CHAR(36) NOT NULL');
     }
 
     /**
@@ -51,15 +61,21 @@ class Version20220928083055 extends AbstractMigration implements ContainerAwareI
         }
         $this->abortIfNotMysql();
 
+        $matchingRecords = $this->getMatchingRecordsToDown();
+        $this->addSql('ALTER TABLE _procedure ADD maillane_connection_id CHAR(36) DEFAULT NULL');
+        $this->addSql('ALTER TABLE _procedure ADD CONSTRAINT FK_D1A01D02AC0C069A FOREIGN KEY (maillane_connection_id) REFERENCES maillane_connection (id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_D1A01D02AC0C069A ON _procedure (maillane_connection_id)');
+        foreach ($matchingRecords as $matchingRecord) {
+            $this->addSql('UPDATE _procedure SET maillane_connection_id = :maillaneConnectionId
+                        WHERE _p_id = :procedureId',
+                [
+                    'procedureId' => $matchingRecord['procedure_id'],
+                    'maillaneConnectionId' => $matchingRecord['id']
+                ]);
+        }
         $this->addSql('ALTER TABLE maillane_connection DROP FOREIGN KEY FK_71C04D1D1624BCD2');
         $this->addSql('DROP INDEX UNIQ_71C04D1D1624BCD2 ON maillane_connection');
         $this->addSql('ALTER TABLE maillane_connection DROP COLUMN procedure_id');
-        $this->addSql('ALTER TABLE _procedure ADD maillane_connection_id CHAR(36) DEFAULT NULL');
-        $this->addSql('ALTER TABLE _procedure ADD CONSTRAINT FK_D1A01D02AC0C069A FOREIGN KEY (maillane_connection_id) REFERENCES maillane_connection (id)');
-
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_D1A01D02AC0C069A ON _procedure (maillane_connection_id)');
-
-
     }
 
     /**
@@ -75,5 +91,17 @@ class Version20220928083055 extends AbstractMigration implements ContainerAwareI
     public function setContainer(ContainerInterface $container = null)
     {
         $this->container = $container;
+    }
+
+    public function getMatchingRecordsToUp()
+    {
+        return $this->connection->fetchAllAssociative('SELECT _p_id,maillane_connection_id  
+                                                            FROM maillane_connection
+                                                            INNER JOIN _procedure ON maillane_connection.id = _procedure.maillane_connection_id
+                                                            ');
+    }
+    public function getMatchingRecordsToDown()
+    {
+        return $this->connection->fetchAllAssociative('SELECT id, procedure_id FROM maillane_connection');
     }
 }
