a11b673bdbaef85627af54b8f78fab76b4f52327
AlexMenschoid <meyer-schlichtmann@demos-deutschland.de>
2022-10-11 14:20:25 +0200

    feat: Use array_key_exists instead of try-catch
    This has the benefit of being more restrictive but also having a better performance as we cut out the need to generate an exception that we don't even really use.
    
    Also a bit of a refactoring to improve readability.

--- demosplan/DemosPlanCoreBundle/Logic/MenuBuilder/MenuBuilder.php
+++ demosplan/DemosPlanCoreBundle/Logic/MenuBuilder/MenuBuilder.php
@@ -87,7 +87,7 @@ class MenuBuilder
      */
     private function setAvailableRouteParameters(Request $request): void
     {
-        # get orgaId
+        // get orgaId
         try {
             $currentUser = $this->currentUserService->getUser();
             $orgaId = $currentUser->getOrganisationId();
@@ -100,16 +100,7 @@ class MenuBuilder
         if (null !== $this->currentProcedure) {
             // If there is a procedure, get the procedureId
             $procedureId = $this->currentProcedure->getId();
-            if ($request->hasSession() && $request->getSession()->has('hashList')) {
-                // If there is a hashlist in the current session, get the hash
-                // I thought an empty catch is fine here as it clearly represents what the software supposed to do:
-                // Try to get the correct hash list for the current procedure. If you can't, that's fine, do nothing.
-                try {
-                    $filterHash = $request->getSession()->get('hashList')[$procedureId]['assessment']['hash'];
-                } catch (\Exception $e) {
-                    //Do nothing
-                }
-            }
+            $filterHash = $this->getFilterHashForProcedure($procedureId, $request);
         }
 
         $this->availableRouteParameters = [
@@ -117,8 +108,25 @@ class MenuBuilder
             'procedureId'       => $procedureId,
             'orgaId'            => $orgaId,
             'organisationId'    => $orgaId,
-            'filterHash'        => $filterHash
+            'filterHash'        => $filterHash,
         ];
+    }
+
+    /**
+     * Get the filterHash for a given procedureId if the procedure can be found in the Hash list.
+     * Otherwise returns null as a default value.
+     */
+    private function getFilterHashForProcedure(string $procedureId, Request $request): ?string
+    {
+        $filterHash = null;
+        if ($request->hasSession() && $request->getSession()->has('hashList')) {
+            $hashList = $request->getSession()->get('hashList');
+            if (array_key_exists($procedureId, $hashList)) {
+                $filterHash = $hashList[$procedureId]['assessment']['hash'];
+            }
+        }
+
+        return $filterHash;
     }
 
     /**
