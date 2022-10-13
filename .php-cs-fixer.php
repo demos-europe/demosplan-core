fa80f6891834ae6545d6e260b4726387022f7571
Demos-CI <devops-system+github@demos-deutschland.de>
2022-10-13 07:28:26 +0000

    style: Apply php-cs-fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS E-Partizipation GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
 
 use demosplan\DemosPlanCoreBundle\Utilities\DemosPlanPath;
 
