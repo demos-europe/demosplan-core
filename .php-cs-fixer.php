37710938df26a7aab45b2dd628f7e621c7a7e481
Borys Shumyachkin <shumyachkin@demos-deutschland.de>
2022-10-13 09:27:28 +0200

    test if php-cs-fixer action is failing if there is stuff to commit

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,12 +1,5 @@
 <?php
 
-/**
- * This file is part of the package demosplan.
- *
- * (c) 2010-present DEMOS E-Partizipation GmbH, for more information see the license file.
- *
- * All rights reserved
- */
 
 use demosplan\DemosPlanCoreBundle\Utilities\DemosPlanPath;
 
