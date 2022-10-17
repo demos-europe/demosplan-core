1037f2416f87e9b347b17097c0bc337000b9cf99
Hans Spieß <spiess@demos-deutschland.de>
2022-10-17 09:18:04 +0200

    fix(refs T28857): improve loader naming

--- client/ui/.storybook/main.js
+++ client/ui/.storybook/main.js
@@ -20,12 +20,13 @@ module.exports = {
   ],
   webpackFinal: async config => {
     /**
-     * This rule is executed first. It ensures that the <license> blocks at the top
-     * of Vue components do break vue-docgen-loader. The loader returns an empty string.
+     * This rule is executed first. It ensures that the <license> blocks
+     * at the top of Vue components do not break vue-docgen-loader.
+     * The loader returns an empty string for all <license> blocks.
      */
     config.module.rules.push({
       resourceQuery: /blockType=license/,
-      loader: require.resolve('./eraseLicenseLoader.js')
+      loader: require.resolve('./removeSFCBlockLoader.js')
     })
     return config
   }
