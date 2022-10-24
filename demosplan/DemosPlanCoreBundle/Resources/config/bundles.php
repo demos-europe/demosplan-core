1660361be9da58346fcc36cab03b83b947c714e5
Hamza Bakri <bakri@demos-deutschland.de>
2022-10-24 19:41:47 +0200

    Feat (ref: T29168): add license header text

--- demosplan/DemosPlanCoreBundle/Resources/config/bundles.php
+++ demosplan/DemosPlanCoreBundle/Resources/config/bundles.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS E-Partizipation GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
