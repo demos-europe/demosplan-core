6515c05dc964bda7d473b0bdb0873b433d274639
Florian Salis <salis@demos-deutschland.de>
2022-10-24 22:38:54 +0200

    fix(refs T29632): Fix pasting Text in editor
    The editor logic changed. So we had to change sowmthing too
    (editor.view.lastClicked -> editor.view.input.lastClicked)
    
    Restore removed logic - also the other Part
    I have overseen something.... :-/
    
    replace substring with slice since its depricated

--- client/js/components/core/DpEditor/DpEditor.vue
+++ client/js/components/core/DpEditor/DpEditor.vue
@@ -863,10 +863,20 @@ export default {
     handleInsertText (text) {
       text = text.replace(/\n/g, '<br>')
 
+      // If user hasn't clicked into tiptap editor yet
+      if (this.editor.view.input.lastClick.x === 0 && this.editor.view.input.lastClick.y === 0) {
+        this.appendText(text)
+      } else { // If user has clicked into tiptap editor at some point, but editor may currently not have focus
+        this.insertTextAtCursorPos(text)
+      }
+    },
+
+    insertTextAtCursorPos (text) {
       // Remove p tags so text is inserted without adding new paragraph
       if (this.startsWithTag(text, 'p')) {
-        text = text.substr(3)
-        text = text.slice(0, -4)
+        text = text
+          .slice(3)
+          .slice(0, -4)
       }
 
       this.editor.commands.insertHTML(text)
