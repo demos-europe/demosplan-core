10450ef576d9ae67886cd36b5dbad94a1d03dd6e
breierDemos <breier@demos-deutschland.de>
2022-10-27 09:37:27 +0200

    feat(refs T29399): Remove error message bag

--- demosplan/DemosPlanCoreBundle/ResourceTypes/InstitutionTagResourceType.php
+++ demosplan/DemosPlanCoreBundle/ResourceTypes/InstitutionTagResourceType.php
@@ -302,9 +302,4 @@ class InstitutionTagResourceType extends DplanResourceType implements UpdatableD
 
         return $violations;
     }
-
-    public function addCreationErrorMessage(array $parameters): void
-    {
-        $this->messageBag->add('error', 'error.unique.tag.label');
-    }
 }
