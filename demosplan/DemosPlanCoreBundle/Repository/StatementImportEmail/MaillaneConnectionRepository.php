a75aea4c5ad533d53db93ad9c65a5ed157047ff2
Christian Dressler <107701274+dresslerdemos@users.noreply.github.com>
2022-10-31 14:44:22 +0100

    feature (refs T29399): allow orga admins to manage tags (#153)
    Co-authored-by: Lamia Gülsah Elmas <elmas@demos-deutschland.de>
    Co-authored-by: Demos-CI <devops-system+github@demos-deutschland.de>
    Co-authored-by: breierDemos <breier@demos-deutschland.de>
    Co-authored-by: Florian Salis <salis@demos-deutschland.de>

--- demosplan/DemosPlanCoreBundle/Repository/StatementImportEmail/MaillaneConnectionRepository.php
+++ demosplan/DemosPlanCoreBundle/Repository/StatementImportEmail/MaillaneConnectionRepository.php
@@ -16,13 +16,7 @@ use demosplan\DemosPlanCoreBundle\Entity\Procedure\MaillaneConnection;
 use demosplan\DemosPlanCoreBundle\Entity\Procedure\Procedure;
 use demosplan\DemosPlanCoreBundle\Exception\ViolationsException;
 use demosplan\DemosPlanCoreBundle\Repository\FluentRepository;
-use Doctrine\ORM\NonUniqueResultException;
 use Doctrine\ORM\NoResultException;
-use Doctrine\Persistence\ManagerRegistry;
-use EDT\DqlQuerying\ConditionFactories\DqlConditionFactory;
-use EDT\DqlQuerying\SortMethodFactories\SortMethodFactory;
-use InvalidArgumentException;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
  * @template-extends FluentRepository<MaillaneConnection>
@@ -30,23 +24,7 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 class MaillaneConnectionRepository extends FluentRepository
 {
     /**
-     * @var ValidatorInterface
-     */
-    private $validator;
-
-    public function __construct(
-        DqlConditionFactory $dqlConditionFactory,
-        ManagerRegistry $registry,
-        SortMethodFactory $sortMethodFactory,
-        string $entityClass,
-        ValidatorInterface $validator)
-    {
-        parent::__construct($dqlConditionFactory, $registry, $sortMethodFactory, $entityClass);
-        $this->validator = $validator;
-    }
-
-    /**
-     * Fetch procedure by Maillane account ID
+     * Fetch procedure by Maillane account ID.
      *
      * @throws NoResultException
      */
@@ -72,7 +50,7 @@ class MaillaneConnectionRepository extends FluentRepository
 
     /**
      * Create a MaillaneConnection with necessary properties
-     * and validate it
+     * and validate it.
      *
      * @throws ViolationsException
      */
@@ -82,11 +60,7 @@ class MaillaneConnectionRepository extends FluentRepository
         $maillaneConnection->setMaillaneAccountId($maillaneAccountId);
         $maillaneConnection->setRecipientEmailAddress($recipientMailAddress);
 
-        // validation
-        $violations = $this->validator->validate($maillaneConnection);
-        if (0 < count($violations)) {
-            throw ViolationsException::fromConstraintViolationList($violations);
-        }
+        $this->validate($maillaneConnection);
 
         return $maillaneConnection;
     }
