5ce9457fef3ef32007ee716ca2ac611616d95a74
Christian Dressler <dressler@demos-deutschland.de>
2022-11-08 09:20:28 +0100

    fix: update dplan to EDT 0.14.0 and fix event problems

--- demosplan/DemosPlanCoreBundle/EventSubscriber/FileResourceTypeSubscriber.php
+++ demosplan/DemosPlanCoreBundle/EventSubscriber/FileResourceTypeSubscriber.php
@@ -12,16 +12,21 @@ declare(strict_types=1);
 
 namespace demosplan\DemosPlanCoreBundle\EventSubscriber;
 
-use demosplan\DemosPlanCoreBundle\Event\GetFilePropertiesEvent;
+use Carbon\Carbon;
+use demosplan\DemosPlanCoreBundle\Entity\File;
 use demosplan\DemosPlanCoreBundle\Event\IsFileAvailableEvent;
 use demosplan\DemosPlanCoreBundle\Event\IsFileDirectlyAccessibleEvent;
+use demosplan\DemosPlanCoreBundle\Logic\ApiRequest\GetPropertiesEvent;
 use demosplan\DemosPlanCoreBundle\ResourceTypes\FileResourceType;
 use demosplan\DemosPlanUserBundle\Exception\UserNotFoundException;
 use demosplan\DemosPlanUserBundle\Logic\CurrentUserInterface;
+use EDT\JsonApi\ResourceTypes\PropertyBuilder;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class FileResourceTypeSubscriber implements EventSubscriberInterface
 {
+    use ResourcePropertyEventSubscriberTrait;
+
     /**
      * @var CurrentUserInterface
      */
@@ -35,9 +40,9 @@ class FileResourceTypeSubscriber implements EventSubscriberInterface
     public static function getSubscribedEvents(): array
     {
         return [
-            IsFileAvailableEvent::class             => 'isFileAvailable',
-            IsFileDirectlyAccessibleEvent::class    => 'isFileDirectlyAccessible',
-            GetFilePropertiesEvent::class           => 'getFileProperties',
+            IsFileAvailableEvent::class          => 'isFileAvailable',
+            IsFileDirectlyAccessibleEvent::class => 'isFileDirectlyAccessible',
+            GetPropertiesEvent::class            => 'getFileProperties',
         ];
     }
 
@@ -64,17 +69,56 @@ class FileResourceTypeSubscriber implements EventSubscriberInterface
     /**
      * @throws UserNotFoundException
      */
-    public function getFileProperties(GetFilePropertiesEvent $event): void
+    public function getFileProperties(GetPropertiesEvent $event): void
     {
-        if (!$event->getType() instanceof FileResourceType) {
+        $resourceType = $event->getType();
+        if (!$resourceType instanceof FileResourceType) {
             return;
         }
-        $properties = $event->getProperties();
-        if ($this->currentUser->hasPermission('feature_import_statement_pdf')) {
-            $properties['id']->filterable()->sortable();
-            $properties['hash']->readable(true)->filterable()->sortable();
-            $properties['filename']->readable(true, [self::class, 'getFileName']);
-            $properties['created']->readable(true, [$this, 'getCreated']);
+
+        if (!$this->currentUser->hasPermission('feature_import_statement_pdf')) {
+            return;
         }
+
+        $properties = $event->getProperties();
+        $idPropertyBuilder = $this->getPropertyBuilder($properties, $resourceType->id->getAsNamesInDotNotation());
+        if (null === $idPropertyBuilder) {
+            $idPropertyBuilder = new PropertyBuilder($resourceType->id, $resourceType->getEntityClass());
+            $properties[] = $idPropertyBuilder;
+        }
+        $idPropertyBuilder->filterable()->sortable();
+
+        $hashPropertyBuilder = $this->getPropertyBuilder($properties, $resourceType->hash->getAsNamesInDotNotation());
+        if (null === $hashPropertyBuilder) {
+            $hashPropertyBuilder = new PropertyBuilder($resourceType->hash, $resourceType->getEntityClass());
+            $properties[] = $hashPropertyBuilder;
+        }
+        $hashPropertyBuilder->readable(true)->filterable()->sortable();
+
+        $filenamePropertyBuilder = $this->getPropertyBuilder($properties, $resourceType->filename->getAsNamesInDotNotation());
+        if (null === $filenamePropertyBuilder) {
+            $filenamePropertyBuilder = new PropertyBuilder($resourceType->filename, $resourceType->getEntityClass());
+            $properties[] = $filenamePropertyBuilder;
+        }
+        $filenamePropertyBuilder->readable(true, [self::class, 'getFileName']);
+
+        $createdPropertyBuilder = $this->getPropertyBuilder($properties, $resourceType->created->getAsNamesInDotNotation());
+        if (null === $createdPropertyBuilder) {
+            $createdPropertyBuilder = new PropertyBuilder($resourceType->created, $resourceType->getEntityClass());
+            $properties[] = $createdPropertyBuilder;
+        }
+        $createdPropertyBuilder->readable(true, [$this, 'getCreated']);
+
+        $event->setProperties($properties);
+    }
+
+    public static function getFileName(File $file): string
+    {
+        return $file->getFilename();
+    }
+
+    public function getCreated(File $file): string
+    {
+        return Carbon::instance($file->getCreated())->toIso8601String();
     }
 }
