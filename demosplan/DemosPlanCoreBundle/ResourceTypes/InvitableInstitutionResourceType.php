80a47ce921b6856c4c6f08fe158dc0a2f20af422
breierDemos <breier@demos-deutschland.de>
2022-11-09 14:17:36 +0100

    feat (ref T29964): Initial creation of InvitableInstitutionResourceType

---
+++ demosplan/DemosPlanCoreBundle/ResourceTypes/InvitableInstitutionResourceType.php
@@ -0,0 +1,72 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS E-Partizipation GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
+namespace demosplan\DemosPlanCoreBundle\ResourceTypes;
+
+
+use demosplan\DemosPlanCoreBundle\Entity\User\Orga;
+use demosplan\DemosPlanCoreBundle\Logic\ApiRequest\ResourceType\DplanResourceType;
+use EDT\PathBuilding\End;
+use EDT\Querying\Contracts\PathsBasedInterface;
+
+/**
+ * @template-extends DplanResourceType<Orga>
+ *
+ * @property-read End                              $name
+ * @property-read End                              $createdDate
+ * @property-read InstitutionTagResourceType       $assignedTags
+ * @property-read End                              $deleted
+ * @property-read End                              $showlist
+ */
+class InvitableInstitutionResourceType extends DplanResourceType
+{
+
+    public static function getName(): string
+    {
+        return 'InvitableInstitution';
+    }
+
+    public function getEntityClass(): string
+    {
+        return Orga::class;
+    }
+
+    public function isAvailable(): bool
+    {
+        return true;
+    }
+
+    public function isReferencable(): bool
+    {
+        return true;
+    }
+
+    public function isDirectlyAccessible(): bool
+    {
+        return true;
+    }
+
+    public function getAccessCondition(): PathsBasedInterface
+    {
+        return $this->conditionFactory->allConditionsApply(
+            $this->conditionFactory->propertyHasValue(false, ...$this->deleted),
+            $this->conditionFactory->propertyHasValue(true, ...$this->showlist),
+        );
+    }
+
+    protected function getProperties(): array
+    {
+        return [
+            $this->createAttribute($this->name)->readable(true),
+            $this->createAttribute($this->createdDate)->readable(true)->sortable(),
+        ];
+    }
+}
