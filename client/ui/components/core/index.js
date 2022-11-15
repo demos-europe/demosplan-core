8053ef9c8602278e4d9e19125731477f441bd7d3
Demos-CI <devops-system+github@demos-deutschland.de>
2022-11-15 16:51:46 +0000

    style: Update license header

--- client/ui/components/core/index.js
+++ client/ui/components/core/index.js
@@ -1,3 +1,12 @@
+/**
+ * (c) 2010-present DEMOS E-Partizipation GmbH.
+ *
+ * This file is part of the package demosplan,
+ * for more information see the license file.
+ *
+ * All rights reserved
+ */
+
 import dataTableSearch from './DpDataTable/DataTableSearch'
 import DpAccordion from './DpAccordion'
 import DpAnonymizeText from './DpAnonymizeText'
