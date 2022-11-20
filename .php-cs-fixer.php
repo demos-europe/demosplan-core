980705b18642dab6c07f91ce7e558b2992fdee47
Borys Shumyachkin <shumyachkin@demos-deutschland.de>
2022-11-20 13:34:48 +0100

    test if config file is the problem

---
+++ .php-cs-fixer.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS E-Partizipation GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__)
+    ->exclude([
+        'lib',
+        'node_modules',
+        'services',
+        'vendor',
+    ]);
+
+$config = new PhpCsFixer\Config();
+
+$config
+    ->setRiskyAllowed(false)
+    ->setRules(
+        [
+            'array_syntax'           => ['syntax' => 'short'],
+            '@PSR2'                  => true,
+            '@Symfony'               => true,
+            'binary_operator_spaces' => [
+                'operators' => [
+                    '=>' => 'align',
+                ],
+            ],
+            'phpdoc_no_alias_tag'    => [
+                'replacements' => ['type' => 'var', 'link' => 'see'],
+            ],
+        ]
+    )
+    ->setCacheFile('.php-cs-fixer.cache')
+    ->setFinder($finder);
+
+return $config;
