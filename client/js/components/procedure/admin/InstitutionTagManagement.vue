2bd7e2236f7c5910b93390989638609a3fc2a4a8
Natalija Sakuta <sakuta@demos-deutschland.de>
2022-11-21 14:13:23 +0100

    fix(T29401): remove duplicate licenses

--- client/js/components/procedure/admin/InstitutionTagManagement.vue
+++ client/js/components/procedure/admin/InstitutionTagManagement.vue
@@ -7,15 +7,6 @@
   All rights reserved
 </license>
 
-<license>
-(c) 2010-present DEMOS E-Partizipation GmbH.
-
-This file is part of the package demosplan,
-for more information see the license file.
-
-All rights reserved
-</license>
-
 <template>
   <dp-tabs
     tab-size="medium"
