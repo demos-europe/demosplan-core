6ab93c8961c0963e85fbb9cefab742572b9d8c82
Moritz Mandler <mandler@demos-deutschland.de>
2022-11-23 14:03:12 +0100

    fix (refs T29401): deleted a not used property declaration as well as an unused method; Declared another property type-restrictive; Moved the id out of permissionChecks to avoid faulty responses when accessing this resourceType indirectly; Resolved a sonarClout line-split issue.

--- demosplan/DemosPlanCoreBundle/ResourceTypes/InvitableInstitutionResourceType.php
+++ demosplan/DemosPlanCoreBundle/ResourceTypes/InvitableInstitutionResourceType.php
@@ -37,16 +37,12 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
  * @property-read End                              $createdDate
  * @property-read InstitutionTagResourceType       $assignedTags
  * @property-read End                              $deleted
- * @property-read End                              $showlist
  * @property-read UserResourceType                 $users
  * @property-read OrgaStatusInCustomerResourceType $statusInCustomers
  */
 class InvitableInstitutionResourceType extends DplanResourceType implements UpdatableDqlResourceTypeInterface
 {
-    /**
-     * @var ValidatorInterface
-     */
-    private $validator;
+    private ValidatorInterface $validator;
 
     public function __construct(ValidatorInterface $validator)
     {
@@ -107,11 +103,11 @@ class InvitableInstitutionResourceType extends DplanResourceType implements Upda
     protected function getProperties(): array
     {
         $allowedProperties = [];
+        $allowedProperties[] = $this->createAttribute($this->id)->readable(true);
 
         if ($this->currentUser->hasPermission('feature_institution_tag_assign')
             || $this->currentUser->hasPermission('feature_institution_tag_read')
         ) {
-            $allowedProperties[] = $this->createAttribute($this->id)->readable(true);
             $allowedProperties[] = $this->createAttribute($this->name)->readable(true);
             $allowedProperties[] = $this->createAttribute($this->createdDate)->readable(true)->sortable();
             $allowedProperties[] = $this->createToManyRelationship($this->assignedTags)->readable(true)->filterable();
@@ -138,47 +134,39 @@ class InvitableInstitutionResourceType extends DplanResourceType implements Upda
     {
         $violations = new ConstraintViolationList([]);
         $updater = new PropertiesUpdater($properties);
-        $updater->ifPresent($this->assignedTags, function (Collection $newAssignedTags) use ($institution, $violations): void {
-            $currentlyAssignedTags = $institution->getAssignedTags();
+        $updater->ifPresent(
+            $this->assignedTags,
+            function (Collection $newAssignedTags) use ($institution, $violations): void {
+                $currentlyAssignedTags = $institution->getAssignedTags();
 
-            // removed tags
-            $removedTags = $currentlyAssignedTags->filter(
-                static fn (InstitutionTag $currentTag): bool => !$newAssignedTags->contains($currentTag)
-            );
+                // removed tags
+                $removedTags = $currentlyAssignedTags->filter(
+                    static fn (InstitutionTag $currentTag): bool => !$newAssignedTags->contains($currentTag)
+                );
 
-            // new tags
-            $newTags = $newAssignedTags->filter(
-                static fn (InstitutionTag $newTag): bool => !$currentlyAssignedTags->contains($newTag)
-            );
+                // new tags
+                $newTags = $newAssignedTags->filter(
+                    static fn (InstitutionTag $newTag): bool => !$currentlyAssignedTags->contains($newTag)
+                );
 
-            foreach ($removedTags as $removedTag) {
-                $institution->removeAssignedTag($removedTag);
-                $violations->addAll($this->validator->validate($removedTag));
+                foreach ($removedTags as $removedTag) {
+                    $institution->removeAssignedTag($removedTag);
+                    $violations->addAll($this->validator->validate($removedTag));
+                }
+
+                foreach ($newTags as $newTag) {
+                    $institution->addAssignedTag($newTag);
+                    $violations->addAll($this->validator->validate($newTag));
+                }
+
+                $violations->addAll($this->validator->validate($institution));
             }
-
-            foreach ($newTags as $newTag) {
-                $institution->addAssignedTag($newTag);
-                $violations->addAll($this->validator->validate($newTag));
-            }
-
-            $violations->addAll($this->validator->validate($institution));
-        });
+        );
 
         if (0 !== $violations->count()) {
             throw ViolationsException::fromConstraintViolationList($violations);
         }
 
         return new ResourceChange($institution, $this, $properties);
-    }
-
-    private function isMemberOfPlanningOrga(): bool
-    {
-        $orga = $this->currentUser->getUser()->getOrga();
-        $subdomain = $this->globalConfig->getSubdomain();
-        $isMunicipality = $orga->hasType(OrgaType::MUNICIPALITY, $subdomain);
-        $isPlanningAgency = $orga->hasType(OrgaType::PLANNING_AGENCY, $subdomain);
-        $isHearingAuthorityAgency = $orga->hasType(OrgaType::HEARING_AUTHORITY_AGENCY, $subdomain);
-
-        return $isMunicipality || $isPlanningAgency || $isHearingAuthorityAgency;
     }
 }
