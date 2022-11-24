8a3ea7f79593bc287eaa8ad2027e5f848bfc508b
Borys Shumyachkin <shumyachkin@demos-deutschland.de>
2022-11-24 17:09:00 +0100

    trigger commit

--- client/js/bundles/admin/statistics.js
+++ client/js/bundles/admin/statistics.js
@@ -1,11 +1,4 @@
-/**
- * (c) 2010-present DEMOS E-Partizipation GmbH.
- *
- * This file is part of the package demosplan,
- * for more information see the license file.
- *
- * All rights reserved
- */
+
 
 /**
  * This is the entrypoint for statistics.html.twig
