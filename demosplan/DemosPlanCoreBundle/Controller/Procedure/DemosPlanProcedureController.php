4e5d9602bad37d555394b10171fbf734a6d53932
Hamza Bakri <bakri@demos-deutschland.de>
2022-11-25 07:49:17 +0100

    Fix (ref: T30079): remove unneeded annotation

--- demosplan/DemosPlanCoreBundle/Controller/Procedure/DemosPlanProcedureController.php
+++ demosplan/DemosPlanCoreBundle/Controller/Procedure/DemosPlanProcedureController.php
@@ -1961,7 +1961,6 @@ class DemosPlanProcedureController extends BaseController
                 $currentStatementId = $templateVars['publicStatements']['statements'][$key]['id'];
                 $currentStatement = $statementService->getStatement($currentStatementId);
                 if (null !== $currentStatement) {
-                    /* @var AdditionalDataEvent $event * */
                     $this->eventDispatcher->dispatch(new AdditionalDataEvent($currentStatement, $templateVars['publicStatements']['statements'][$key]));
                 }
             }
