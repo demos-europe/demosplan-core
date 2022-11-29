db45a72e6e167a84493ad056ce49f1b137635483
Sami Mussbach <mussbach@demos-deutschland.de>
2022-11-29 18:02:25 +0100

    feat (refs Ado #2286): provide a container init command
    Command is called in an initContainer to build an environment
    the main application container needs.
    Finally this command will do a lot more like migrations,
    elasticsearch populate and the like. For now, it only creates
    a database and populates it with the prod fixtures if needed

---
+++ demosplan/DemosPlanCoreBundle/Command/ContainerInitCommand.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS E-Partizipation GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
+namespace demosplan\DemosPlanCoreBundle\Command;
+
+use Doctrine\DBAL\Exception\ConnectionException;
+use Doctrine\ORM\EntityManagerInterface;
+use EFrane\ConsoleAdditions\Batch\Batch;
+use Exception;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+class ContainerInitCommand extends CoreCommand
+{
+    protected static $defaultName = 'dplan:container:init';
+    protected static $defaultDescription = 'Perform startup tasks as an init container in kubernetes setup';
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager, ParameterBagInterface $parameterBag, string $name = null)
+    {
+        parent::__construct($parameterBag, $name);
+        $this->entityManager = $entityManager;
+    }
+
+    public function configure(): void
+    {
+        $this
+           ->setHelp(
+                <<<EOT
+Perform startup tasks as an init container in kubernetes setup. Usage:
+    php bin/<project> dplan:container:init
+EOT
+            );
+    }
+
+    /**
+     * Update demosplan.
+     *
+     * @throws Exception
+     */
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+
+        $connection = $this->entityManager->getConnection();
+        try {
+            $connection->getDatabase();
+        } catch (ConnectionException $throwable) {
+            // create database, if it does not exist yet
+            Batch::create($this->getApplication(), $output)
+                ->add('dplan:db:init --with-fixtures=ProdData --create-database')
+                ->run();
+
+        }
+
+        return 0;
+    }
+
+}
