598096722a6b77f2243352fb5792c3c215bbc5da
Borys Shumyachkin <shumyachkin@demos-deutschland.de>
2022-11-30 13:34:12 +0100

    trigger action

--- demosplan/DemosPlanAdminBundle/DemosPlanAdminBundle.php
+++ demosplan/DemosPlanAdminBundle/DemosPlanAdminBundle.php
@@ -1,12 +1,6 @@
 <?php
 
-/**
- * This file is part of the package demosplan.
- *
- * (c) 2010-present DEMOS E-Partizipation GmbH, for more information see the license file.
- *
- * All rights reserved
- */
+
 
 namespace demosplan\DemosPlanAdminBundle;
 
