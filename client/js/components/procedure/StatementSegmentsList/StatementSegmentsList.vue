a21760490cf9c94920fb650a7308a654ef4b9064
Ahmad Alahrashi <alahrashi@demos-deutschland.de>
2022-11-30 11:44:42 +0100

    chore (refs T29990): adjust styling for statement attachments link list.

--- client/js/components/procedure/StatementSegmentsList/StatementSegmentsList.vue
+++ client/js/components/procedure/StatementSegmentsList/StatementSegmentsList.vue
@@ -93,17 +93,25 @@
                 </span>
               </template>
               <template v-if="statement">
-                <span class="weight--bold">{{ Translator.trans('original.pdf') }}</span>
-                <statement-meta-attachments-link
-                  v-if="originalAttachment.hash"
-                  :attachment="originalAttachment" />
-                <span
-                  v-if="additionalAttachments.length > 0"
-                  class="weight--bold">{{ Translator.trans('more.attachments') }}</span>
-                <statement-meta-attachments-link
-                  v-for="attachment in additionalAttachments"
-                  :key="attachment.hash"
-                  :attachment="attachment" />
+                <div class="overflow-x-scroll overflow-word-break max-height-500 width-600">
+                  <span class="weight--bold">{{ Translator.trans('original.pdf') }}</span>
+                  <div class="u-pr-0_75">
+                    <statement-meta-attachments-link
+                      class="display--block whitespace--normal"
+                      v-if="originalAttachment.hash"
+                      :attachment="originalAttachment" />
+                  </div>
+                  <span
+                    v-if="additionalAttachments.length > 0"
+                    class="weight--bold">{{ Translator.trans('more.attachments') }}</span>
+                  <div class="u-pr-0_75">
+                    <statement-meta-attachments-link
+                      class="display--block whitespace--normal"
+                      v-for="attachment in additionalAttachments"
+                      :key="attachment.hash"
+                      :attachment="attachment" />
+                  </div>
+                </div>
               </template>
             </dp-flyout>
           </li>
