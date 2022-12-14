04fc883e713ee3d72f2c4a6e78c1a533f4e9dddf
Christian Dressler <dressler@demos-deutschland.de>
2022-12-14 10:15:27 +0100

    feature (refs T29879): add `NOT SIZE` condition operator support
    DiPlanung ADO Ticketnummer: 1740

--- demosplan/DemosPlanCoreBundle/Permissions/PermissionDrupalConditionFactory.php
+++ demosplan/DemosPlanCoreBundle/Permissions/PermissionDrupalConditionFactory.php
@@ -17,12 +17,16 @@ use EDT\Querying\ConditionParsers\Drupal\PredefinedDrupalConditionFactory;
 use EDT\Querying\Contracts\PathsBasedInterface;
 
 /**
- * Adds the `FALSE` operator to the usual Drupal filter operators. This operator is currently only needed
- * when evaluating permission filters and should not be exposed in the Web-API.
+ * Adds support for additional operators to the usual Drupal filter parsing. These operators are
+ * needed when evaluating permission filters. This class should be used for permission parsing only;
+ * if the added operators are needed for other places, e.g. via the Web-API too, it is valid to copy
+ * their definitions into a separate class instead of trying to reuse this one.
  */
 class PermissionDrupalConditionFactory extends PredefinedDrupalConditionFactory
 {
     public const FALSE = 'FALSE';
+
+    public const NOT_SIZE = 'NOT SIZE';
 
     protected PathsBasedConditionFactoryInterface $conditionFactory;
 
@@ -35,7 +39,12 @@ class PermissionDrupalConditionFactory extends PredefinedDrupalConditionFactory
     protected function getOperatorFunctions(): array
     {
         $functions = parent::getOperatorFunctions();
-        $functions[self::FALSE] = fn (array $path, $conditionValue): PathsBasedInterface => $this->conditionFactory->false();
+        $functions[self::FALSE] = fn (
+            array $path, $conditionValue
+        ): PathsBasedInterface => $this->conditionFactory->false();
+        $functions[self::NOT_SIZE] = fn (
+            array $path, $conditionValue
+        ): PathsBasedInterface => $this->conditionFactory->propertyHasNotSize($conditionValue, ...$path);
 
         return $functions;
     }
