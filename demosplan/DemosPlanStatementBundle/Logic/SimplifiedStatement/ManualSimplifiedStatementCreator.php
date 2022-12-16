33f94e9e6841d906f30b2532e31b8889d17cbcb6
Hamza Bakri <bakri@demos-deutschland.de>
2022-12-16 09:36:19 +0100

    Feat (ref: T30254): readd SimplifiedStatementCreator in core
    (ADO 1740)

--- demosplan/DemosPlanStatementBundle/Logic/SimplifiedStatement/ManualSimplifiedStatementCreator.php
+++ demosplan/DemosPlanStatementBundle/Logic/SimplifiedStatement/ManualSimplifiedStatementCreator.php
@@ -12,7 +12,6 @@ namespace demosplan\DemosPlanStatementBundle\Logic\SimplifiedStatement;
 
 use DemosEurope\DemosplanAddon\Contracts\CurrentUserInterface;
 use DemosEurope\DemosplanAddon\Contracts\MessageBagInterface;
-use DemosEurope\DemosplanAddon\Logic\SimplifiedStatementCreator;
 use Exception;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -20,6 +19,7 @@ use Symfony\Component\Routing\RouterInterface;
 use demosplan\DemosPlanCoreBundle\Entity\Statement\Statement;
 use demosplan\DemosPlanCoreBundle\Exception\MessageBagException;
 use demosplan\DemosPlanCoreBundle\Logic\FileUploadService;
+use demosplan\DemosPlanStatementBundle\Logic\SimplifiedStatement\SimplifiedStatementCreator;
 use demosplan\DemosPlanStatementBundle\Logic\StatementHandler;
 
 /**
