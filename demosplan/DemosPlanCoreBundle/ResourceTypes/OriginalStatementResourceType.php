a4fdfc0124d4436331e0a6c2935dfbbe0f4a8969
Demos-CI <devops-system+github@demos-deutschland.de>
2022-12-27 12:14:05 +0000

    style: Apply php-cs-fixer

--- demosplan/DemosPlanCoreBundle/ResourceTypes/OriginalStatementResourceType.php
+++ demosplan/DemosPlanCoreBundle/ResourceTypes/OriginalStatementResourceType.php
@@ -12,10 +12,8 @@ declare(strict_types=1);
 
 namespace demosplan\DemosPlanCoreBundle\ResourceTypes;
 
-use DemosEurope\DemosplanAddon\Contracts\Events\GetPropertiesEventInterface;
 use demosplan\DemosPlanCoreBundle\Entity\Statement\Statement;
 use demosplan\DemosPlanCoreBundle\Event\IsOriginalStatementAvailableEvent;
-use demosplan\DemosPlanCoreBundle\Logic\ApiRequest\GetPropertiesEvent;
 use demosplan\DemosPlanCoreBundle\Logic\ApiRequest\ResourceType\DplanResourceType;
 use EDT\PathBuilding\End;
 use EDT\Querying\Contracts\PathsBasedInterface;
