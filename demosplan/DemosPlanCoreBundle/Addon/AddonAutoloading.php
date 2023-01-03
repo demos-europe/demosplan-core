e0b3f33a32f1d6995da95f17fd5b7e2cdd7dcef9
AlexMenschoid <meyer-schlichtmann@demos-deutschland.de>
2023-01-03 10:27:23 +0100

    feat: Create AddonAutoloading class
    and move registering the autoloading from the bundle registration to the bootstrap step to avoid potential problems with unavailable classes during the request processing until bundle registration.

---
+++ demosplan/DemosPlanCoreBundle/Addon/AddonAutoloading.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS E-Partizipation GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
+namespace demosplan\DemosPlanCoreBundle\Addon;
+
+use demosplan\DemosPlanCoreBundle\Utilities\DemosPlanPath;
+
+/**
+ * Handles the addon autoloading
+ */
+class AddonAutoloading
+{
+    /**
+     * Prevent the autoloader from being added multiple times
+     */
+    private static bool $autoloadingConfigured = false;
+
+    /**
+     * Registers an autoload for classes from the addons autoload_classmap.
+     */
+    public static function register(): void
+    {
+        if (self::$autoloadingConfigured) {
+            return;
+        }
+
+        $classMapPath = DemosPlanPath::getRootPath('addons/vendor/composer/autoload_classmap.php');
+        if (!file_exists($classMapPath)) {
+            return;
+        }
+
+        $classmap = include_once $classMapPath;
+
+        spl_autoload_register(static function (string $class) use ($classmap): void {
+            if (array_key_exists($class, $classmap)) {
+                include_once $classmap[$class];
+            }
+        });
+
+        self::$autoloadingConfigured = true;
+    }
+}
