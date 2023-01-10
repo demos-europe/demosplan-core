ccd13f58b0db44cd47c54f9214de1f4d795a6088
Sami Mussbach <mussbach@demos-deutschland.de>
2023-01-10 16:44:14 +0100

    feat: use env var for rabbitmq user password in java service

--- docker/javaservice/start.sh
+++ docker/javaservice/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -jar -Xmx$JAVASERVICE_MEMORY -Dfile.encoding=UTF-8 -Dlogpath=/var/logs/demosplan /opt/demosplan/demosplan.jar --spring.config.location=classpath:application.properties,/opt/demosplan/override.properties
+java -jar -Xmx$JAVASERVICE_MEMORY -Dfile.encoding=UTF-8 -Dlogpath=/var/logs/demosplan -Drabbitmq.password="$RABBITMQ_USER_PASSWORD" /opt/demosplan/demosplan.jar --spring.config.location=classpath:application.properties,/opt/demosplan/override.properties
