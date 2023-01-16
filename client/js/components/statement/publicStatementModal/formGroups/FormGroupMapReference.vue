58dc62befff7949c9a9754836897438756b7cd7d
Stefanie Grünberger <gruenberger@demos-deutschland.de>
2023-01-16 17:43:42 +0100

    bug(refs T30859): Move permission request to container

--- client/js/components/statement/publicStatementModal/formGroups/FormGroupMapReference.vue
+++ client/js/components/statement/publicStatementModal/formGroups/FormGroupMapReference.vue
@@ -9,6 +9,7 @@
 
 <template>
   <fieldset
+    v-if="hasPermission('field_statement_location')"
     :required="required"
     role="radiogroup"
     aria-labelledby="statementMapReference"
@@ -62,13 +63,12 @@
     </div>
 
     <div
-      v-if="hasPermission('field_statement_location')"
+      v-if="hasPermission('field_statement_county')"
       :class="[
         statement.r_location === 'county' ? 'bg-color--grey-light-2' : '',
         'c-statement__formblock layout__item height-90 height-auto-lap-down u-3-of-10 u-1-of-1-palm'
       ]">
       <dp-radio
-        v-if="hasPermission('field_statement_county')"
         id="locationcounty"
         :label="{
           text: Translator.trans('statement.map.reference.choose_county')
