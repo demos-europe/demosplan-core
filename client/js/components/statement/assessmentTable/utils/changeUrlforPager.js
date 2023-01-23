fe53415012baa638b666eed862a062e0a53e685f
Demos-CI <devops-system+github@demos-deutschland.de>
2023-01-23 12:02:42 +0000

    style: Update license header

--- client/js/components/statement/assessmentTable/utils/changeUrlforPager.js
+++ client/js/components/statement/assessmentTable/utils/changeUrlforPager.js
@@ -1,4 +1,13 @@
 /**
+ * (c) 2010-present DEMOS E-Partizipation GmbH.
+ *
+ * This file is part of the package demosplan,
+ * for more information see the license file.
+ *
+ * All rights reserved
+ */
+
+/**
  * Update the Url matching the pager
  * @param pager {Object<count, current_page>}
  *
