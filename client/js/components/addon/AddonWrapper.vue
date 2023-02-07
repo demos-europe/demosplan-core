b549b653dbeae08a97e5e3ebe385b097fe374b12
Velimir Müller <velimir@demos-deutschland.de>
2023-02-07 11:47:49 +0100

    feat addons : simplify binding of addon props ADO#1740
    Since we can simply pass the `addonProps` prop instead of wrapping the output in a function, this change has been applied now.

--- client/js/components/addon/AddonWrapper.vue
+++ client/js/components/addon/AddonWrapper.vue
@@ -11,7 +11,7 @@
   <div>
     <component
       :is="component"
-      v-bind="getCurrentProperties()"
+      v-bind="addonProps"
     />
   </div>
 </template>
@@ -48,10 +48,6 @@ export default {
   },
 
   methods: {
-    getCurrentProperties() {
-      return this.addonProps
-    },
-
     loadComponents(hookName) {
       const params = {
         hookName: hookName
