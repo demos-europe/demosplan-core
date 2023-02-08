50f6a68d508aa52b25b2e38021c5358f70ee00fb
Demos-CI <devops-system+github@demos-deutschland.de>
2023-02-08 15:20:14 +0000

    style: Apply php-cs-fixer

--- demosplan/DemosPlanCoreBundle/Logic/ApiRequest/DplanDrupalConditionFactory.php
+++ demosplan/DemosPlanCoreBundle/Logic/ApiRequest/DplanDrupalConditionFactory.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS E-Partizipation GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
 namespace demosplan\DemosPlanCoreBundle\Logic\ApiRequest;
 
 use EDT\ConditionFactory\PathsBasedConditionFactoryInterface;
@@ -15,11 +23,10 @@ class DplanDrupalConditionFactory extends PredefinedDrupalConditionFactory
 
     public function getOperatorFunctions(): array
     {
-        $operators =  parent::getOperatorFunctions();
+        $operators = parent::getOperatorFunctions();
         $operators['IS NULL'] = fn ($conditionValue, array $path): PathsBasedInterface => $this->conditionFactory->propertyIsNull($path);
         $operators['IS NOT NULL'] = fn ($conditionValue, array $path): PathsBasedInterface => $this->conditionFactory->propertyIsNotNull($path);
 
         return $operators;
     }
-
 }
