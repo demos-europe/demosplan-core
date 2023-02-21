035b0b0d4a07fb7f3015f37ef8f8d5fd1e9226b8
ahmad-demos <59571234+ahmad-demos@users.noreply.github.com>
2023-02-21 11:28:47 +0100

    Update client/js/components/procedure/StatementSegmentsList/StatementMeta/StatementMetaAttachments.vue
    Co-authored-by: salisdemos <40487461+salisdemos@users.noreply.github.com>

--- client/js/components/procedure/StatementSegmentsList/StatementMeta/StatementMetaAttachments.vue
+++ client/js/components/procedure/StatementSegmentsList/StatementMeta/StatementMetaAttachments.vue
@@ -38,9 +38,7 @@
         v-else />
       <dp-upload-files
         :class="editable ? '' : 'pointer-events-none opacity-7'"
-        :get-file-by-hash="(hash) => {
-          return Routing.generate('core_file', { hash: hash })
-        }"
+        :get-file-by-hash="hash => Routing.generate('core_file', { hash: hash })"
         ref="uploadStatementAttachment"
         id="uploadStatementAttachment"
         name="uploadStatementAttachment"
