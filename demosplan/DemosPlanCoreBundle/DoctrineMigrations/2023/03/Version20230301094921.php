75421d68816485a0d86515f98472633652815fc7
Alexander Marten <marten@demos-deutschland.de>
2023-03-02 17:29:48 +0100

    fix (refs T31604): Fix occurrences of "Web-Atlas" in gis-urls of blueprints by replacing them with basemap-urls.
    This handles color-layers as well as gray-layers.

--- demosplan/DemosPlanCoreBundle/DoctrineMigrations/2023/03/Version20230301094921.php
+++ demosplan/DemosPlanCoreBundle/DoctrineMigrations/2023/03/Version20230301094921.php
@@ -19,10 +19,12 @@ use Doctrine\Migrations\AbstractMigration;
 class Version20230301094921 extends AbstractMigration
 {
     private const NEW_GIS_NAME = 'basemap';
-    private const NEW_GIS_LAYER = 'de_basemapde_web_raster_farbe';
+    private const NEW_GIS_LAYER_COLOR = 'de_basemapde_web_raster_farbe';
+    private const NEW_GIS_LAYER_GRAY = 'de_basemapde_web_raster_grau';
     private const NEW_GIS_URL = 'https://sgx.geodatenzentrum.de/wms_basemapde';
 
-    private const OLD_GIS_URL = '%webatlas%';
+    private const OLD_GIS_URL = 'https://sg.geodatenzentrum.de/wms_webatlasde%';
+    private const OLD_GIS_URL_GRAY = 'https://sg.geodatenzentrum.de/wms_webatlasde_grau%';
 
     public function getDescription(): string
     {
@@ -36,7 +38,7 @@ class Version20230301094921 extends AbstractMigration
     {
         $this->abortIfNotMysql();
 
-        // Replace all Web-Atlas Entries by basemap (only for procedure templates).
+        //color
         $this->addSql(
             'UPDATE _gis AS g
             INNER JOIN _procedure AS p ON g._p_id = p._p_id
@@ -44,12 +46,33 @@ class Version20230301094921 extends AbstractMigration
                 g._g_name = :gisName,
                 g._g_layers = :gisLayer,
                 g._g_url = :newGisUrl
-            WHERE g._g_url LIKE :oldGisUrl AND p._p_master = 1 AND p._p_deleted = 0',
+            WHERE g._g_url LIKE :oldGisUrl
+            AND g._g_url NOT LIKE :oldGisUrlGray
+            AND p._p_master = 1 AND p._p_deleted = 0',
             [
                 'gisName'     => self::NEW_GIS_NAME,
-                'gisLayer'    => self::NEW_GIS_LAYER,
+                'gisLayer'    => self::NEW_GIS_LAYER_COLOR,
                 'newGisUrl'   => self::NEW_GIS_URL,
                 'oldGisUrl'   => self::OLD_GIS_URL,
+                'oldGisUrlGray'   => self::OLD_GIS_URL_GRAY,
+            ]
+        );
+
+        //grey
+        $this->addSql(
+            'UPDATE _gis AS g
+            INNER JOIN _procedure AS p ON g._p_id = p._p_id
+            SET
+                g._g_name = :gisName,
+                g._g_layers = :gisLayer,
+                g._g_url = :newGisUrl
+            WHERE g._g_url LIKE :oldGisUrlGray
+            AND p._p_master = 1 AND p._p_deleted = 0',
+            [
+                'gisName'     => self::NEW_GIS_NAME,
+                'gisLayer'    => self::NEW_GIS_LAYER_GRAY,
+                'newGisUrl'   => self::NEW_GIS_URL,
+                'oldGisUrlGray'   => self::OLD_GIS_URL_GRAY,
             ]
         );
     }
