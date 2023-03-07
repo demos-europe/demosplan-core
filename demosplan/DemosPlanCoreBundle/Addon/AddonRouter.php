4d040f9622d82b56e1d34cd786dc8352a7a8dc19
Hamza Bakri <bakri@demos-deutschland.de>
2023-03-07 14:21:29 +0100

    F (ref:T31336): in process

--- demosplan/DemosPlanCoreBundle/Addon/AddonRouter.php
+++ demosplan/DemosPlanCoreBundle/Addon/AddonRouter.php
@@ -2,6 +2,8 @@
 
 namespace demosplan\DemosPlanCoreBundle\Addon;
 
+use DemosEurope\DemosplanAddon\DemosPipes\Controller\AnnotatedStatementPdfPercentageDistributionApiController;
+use DemosEurope\DemosplanAddon\Utilities\AddonPath;
 use demosplan\DemosPlanCoreBundle\Utilities\DemosPlanPath;
 use Symfony\Bundle\FrameworkBundle\Routing\RouteLoaderInterface;
 use Symfony\Component\Routing\Loader\AnnotationClassLoader;
@@ -10,27 +12,31 @@ use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Route;
 use function GuzzleHttp\Psr7\_parse_request_uri;
 
-class AddonRouter extends AnnotationDirectoryLoader implements RouteLoaderInterface
+class AddonRouter extends AnnotationClassLoader implements RouteLoaderInterface
 {
     public function loadRoutes(): RouteCollection
     {
-        $path = DemosPlanPath::getRootPath('addons/cache/demosplam-addon-dedmospipes-main/src/Controller');
-        return $this->load($path);
+        //$path = AddonPath::getRootPath('addons/cache/demosplam-addon-dedmospipes-main/src/Controller/');
+        //$path = "/srv/www/demosplan/addons/cache/demosplan-addon-demospipes-main/src/Controller/AnnotatedStatementPdfPercentageDistributionApiController";
+        //return $this->load($path);
         // get Controllers from addons
         // pass controller class strings to $this->load()
         // merge results into one RouteCollection
-        //if(class_exists(AnnotatedStatementPdfPercentageDistributionApiController::class)) {
-        //   var_dump('addonRouter class exists');
-        //   $this->load(AnnotatedStatementPdfPercentageDistributionApiController::class);
-        //}
-
+        if(class_exists(AnnotatedStatementPdfPercentageDistributionApiController::class)) {
+           //dd('addonRouter class exists');
+           $collection = $this->load(AnnotatedStatementPdfPercentageDistributionApiController::class);
+           //dd($collection);
+        }
+        //dd('gibts nicht');
         //$x = $this->load(AnnotatedStatementPdfPercentageDistributionApiController::class);
         //$route = new RouteCollection();
         //$route->add($x);
         //var_dump('addonRouter class does not exists');
         //return new RouteCollection();
         //return $route;
-
+        //$collection = new RouteCollection();
+        //dd($collection);
+        return $collection;
     }
 
     protected function configureRoute(Route $route, \ReflectionClass $class, \ReflectionMethod $method, object $annot)
