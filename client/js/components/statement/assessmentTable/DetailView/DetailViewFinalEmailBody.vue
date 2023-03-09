3651e8aa666d3fc8d3565dd3ddffcd9637188d30
Steffi <101879352+gruenbergerdemos@users.noreply.github.com>
2023-03-09 12:13:18 +0100

    bug(refs T31795): add slots for boilerplate (#969)
    * bug(refs T31795): Add slot for boilerplate in twig
    
    DpEditor was refactored and now there is a slot for the boilerplateModal needed.
    Also renamed the event for inserting boilerplates, because camelCase does not work in html.
    
    * bug(refs T31795): Rename event for inserting boilerplates
    
    * bug(refs T31795): Remove link
    
    (cherry picked from commit 80b0251673cabdd1b1b73333a28070bd41e269ce)

--- client/js/components/statement/assessmentTable/DetailView/DetailViewFinalEmailBody.vue
+++ client/js/components/statement/assessmentTable/DetailView/DetailViewFinalEmailBody.vue
@@ -10,21 +10,43 @@
 <template>
   <dp-editor
     hidden-input="r_send_body"
-    :procedure-id="procedureId"
     :toolbar-items="toolbarItems"
-    v-model="text" />
+    v-model="text">
+    <template v-slot:modal="modalProps">
+      <dp-boiler-plate-modal
+        ref="boilerPlateModal"
+        boiler-plate-type="email"
+        :procedure-id="procedureId"
+        @insert="text => modalProps.handleInsertText(text)" />
+    </template>
+    <template v-slot:button>
+      <button
+        :class="prefixClass('menubar__button')"
+        type="button"
+        v-tooltip="Translator.trans('boilerplate.insert')"
+        @click.stop="openBoilerPlate">
+        <i :class="prefixClass('fa fa-puzzle-piece')" />
+      </button>
+    </template>
+  </dp-editor>
 </template>
 
 <script>
+import DpBoilerPlateModal from '@DpJs/components/statement/DpBoilerPlateModal'
+import { prefixClassMixin } from '@demos-europe/demosplan-ui'
+
 export default {
   name: 'AssessmentStatementDetailFinalEmail',
 
   components: {
+    DpBoilerPlateModal,
     DpEditor: async () => {
       const { DpEditor } = await import('@demos-europe/demosplan-ui')
       return DpEditor
     }
   },
+
+  mixins: [prefixClassMixin],
 
   props: {
     initText: {
@@ -42,10 +64,15 @@ export default {
     return {
       text: this.initText,
       toolbarItems: {
-        boilerPlate: 'email',
         headings: [1, 2, 3],
         linkButton: true
       }
+    }
+  },
+
+  methods: {
+    openBoilerPlate () {
+      this.$refs.boilerPlateModal.toggleModal()
     }
   }
 }
