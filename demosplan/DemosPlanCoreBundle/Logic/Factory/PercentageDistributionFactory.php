7037ba6b253efca93864bda2daa608f14cdbdc1d
breierDemos <breier@demos-deutschland.de>
2023-03-13 16:22:45 +0100

    feature (refs T30254): fix spelling and add missing use statements :/

--- demosplan/DemosPlanCoreBundle/Logic/Factory/PercentageDistributionFactory.php
+++ demosplan/DemosPlanCoreBundle/Logic/Factory/PercentageDistributionFactory.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace demosplan\DemosPlanCoreBundle\Logic\Factory;
 
 
+use DemosEurope\DemosplanAddon\Contracts\Factory\PercentageDistributionFactoryInterface;
 use DemosEurope\DemosplanAddon\Contracts\ValueObject\PercentageDistributionInterface;
 use demosplan\DemosPlanCoreBundle\ValueObject\PercentageDistribution;
 
