3fa65a02922197bd820d723212eec72f047df901
Florian Salis <salis@demos-deutschland.de>
2023-03-28 09:34:17 +0200

    test: Remove commented out tests

--- tests/frontend/unit/DpBulkEdit.spec.js
+++ tests/frontend/unit/DpBulkEdit.spec.js
@@ -6,13 +6,7 @@
  *
  * All rights reserved
  */
-
-import $ from 'jquery'
 import DpBulkEditStatement from '@DpJs/components/statement/assessmentTable/DpBulkEditStatement'
-import shallowMountWithGlobalMocks from '@DpJs/VueConfigLocal'
-import Vuex from 'vuex'
-
-global.$ = $
 
 describe('DpBulkEditStatement', () => {
   it('should be an object', () => {
@@ -22,61 +16,4 @@ describe('DpBulkEditStatement', () => {
   it('should be named DpBulkEdit', () => {
     expect(DpBulkEditStatement.name).toBe('DpBulkEditStatement')
   })
-
-  let store
-  let actions
-  let getters
-
-  const stubs = {
-    DpTiptap: '<h3>DpLinkModal</h3>'
-  }
-
-  /*
-   *BeforeEach(() => {
-   *  actions = {
-   *    setProcedureIdAction: jest.fn(),
-   *    setSelectedElementsAction: jest.fn(),
-   *    addToSelectionAction: jest.fn()
-   *  }
-   *
-   *  getters = {
-   *    selectedElementsLength: jest.fn()
-   *  }
-   *
-   *  store = new Vuex.Store({
-   *    modules: {
-   *      statement: {
-   *        namespaced: true,
-   *        state: {
-   *          procedureId: '',
-   *          selectedElements: {},
-   *          statements: {}
-   *        },
-   *        actions,
-   *        getters
-   *      }
-   *    }
-   *  })
-   *})
-   */
-
-  /*
-   * @TODO: find out how to fix 'Routing is not defined' error
-   * it('renders correct markup with isEditMode = true', () => {
-   *
-   *     let wrapper = shallowMountWithGlobalMocks(
-   *         DpBulkEdit,
-   *         {
-   *             propsData: {
-   *                 procedureId: '2a1cc2f5-bd9e-11e8-b87f-4f2df2384097',
-   *                 isEditMode: true,
-   *                 token: ''
-   *             },
-   *             store,
-   *             actions
-   *          },
-   *     );
-   *     expect(wrapper.html()).toMatchSnapshot()
-   * });
-   */
 })
