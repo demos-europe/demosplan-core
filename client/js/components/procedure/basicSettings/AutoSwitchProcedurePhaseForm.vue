8d4f7057b2d72b819d002325678b75280c29d22b
Florian Salis <salis@demos-deutschland.de>
2023-04-05 11:04:08 +0200

    chore(refs T32012): adust if/else logic
    to ensure that max one element can be rendered

--- client/js/components/procedure/basicSettings/AutoSwitchProcedurePhaseForm.vue
+++ client/js/components/procedure/basicSettings/AutoSwitchProcedurePhaseForm.vue
@@ -80,7 +80,7 @@
       </div>
 
       <dp-inline-notification
-        v-if="hasPermission('feature_auto_switch_to_procedure_end_phase') && isParticipationPhaseSelected"
+        v-else-if="hasPermission('feature_auto_switch_to_procedure_end_phase') && isParticipationPhaseSelected"
         class="u-mb-0"
         :message="Translator.trans('period.autoswitch.hint', { phase: Translator.trans(isInternal ? 'procedure.phases.internal.analysis' : 'procedure.phases.external.evaluating')})"
         type="warning" />
