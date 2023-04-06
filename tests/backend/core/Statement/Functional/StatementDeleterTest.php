37a88f85f9cc2e4f670ec9a3cca284781ac0f46f
Alexander Marten <marten@demos-deutschland.de>
2023-04-06 10:45:19 +0200

    fix (refs T22439): Empty internId of original statement in case of last chlid (normal statement) will be deleted.
    To "free" the previous given internId and allow to use them again.
    Extract deleteStatementObject() into own servcie to avoid making the StatementService even bigger.
    Add new permission to decide if the internId of the related originalSTN should be nulled.
    This is also the first step do acutally automatically delete the related original statement in case of deleting the last child statement.

---
+++ tests/backend/core/Statement/Functional/StatementDeleterTest.php
@@ -0,0 +1,55 @@
+<?php declare(strict_types=1);
+
+
+namespace Tests\Core\Statement\Functional;
+
+
+use demosplan\DemosPlanCoreBundle\Entity\Statement\StatementMeta;
+use demosplan\DemosPlanCoreBundle\Entity\Statement\Tag;
+use Tests\Base\FunctionalTestCase;
+
+class StatementDeleterTest extends FunctionalTestCase
+{
+    public function testEmtpyInternIdOfOriginalInCaseOfDeleteLastChild()
+    {
+        //get statement with OSTN (with set internID) with only one child
+        //delete statement
+        //check if related original stn has null as internid
+
+
+        $testTag1 = $this->getTagReference('testFixtureTag_1');
+        $testStatement2 = $this->getStatementReference('testStatement2');
+        static::assertInstanceOf(StatementMeta::class, $testStatement2->getMeta());
+
+        $amountOfMetasBefore = $this->countEntries(StatementMeta::class);
+
+        $amountOfTagsBefore = count($testStatement2->getTags());
+        $entireAmountOfTagsBefore = count($this->getEntries(Tag::class));
+
+        $initialAmountOfStatementsOfTag1 = count($testTag1->getStatements());
+        $this->sut->addTagToStatement($testTag1, $testStatement2);
+
+        // total amount of tags in DB has not changed
+        static::assertCount($entireAmountOfTagsBefore, $this->getEntries(Tag::class));
+        static::assertCount($initialAmountOfStatementsOfTag1 + 1, $testTag1->getStatements());
+        static::assertContains($testStatement2, $testTag1->getStatements());
+        static::assertContains($testTag1, $testStatement2->getTags());
+        $tags = $testStatement2->getTags();
+        static::assertCount($amountOfTagsBefore + 1, $tags);
+
+        // the actually deletion:
+        $result = $this->sut->deleteStatement($testStatement2->getId());
+
+        static::assertTrue($result);
+        static::assertCount($initialAmountOfStatementsOfTag1, $testTag1->getStatements());
+        static::assertNotContains($testStatement2, $testTag1->getStatements());
+        // total amount of tags in DB has still not changed
+        static::assertCount($entireAmountOfTagsBefore, $this->getEntries(Tag::class));
+
+        // total amount of StatementMeta in DB is decremeted
+        static::assertSame(
+            $amountOfMetasBefore - 1,
+            $this->countEntries(StatementMeta::class)
+        );
+    }
+}
