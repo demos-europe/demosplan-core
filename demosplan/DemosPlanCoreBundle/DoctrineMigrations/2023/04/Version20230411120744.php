e7990b9b934e402e3b086b44b5dd613aa4ee624b
Alexander Marten <marten@demos-deutschland.de>
2023-04-11 15:29:45 +0200

    fix (refs T28005): Prepare creation of unique constraint by deleting "duplicate" extern Ids of child-statements

---
+++ demosplan/DemosPlanCoreBundle/DoctrineMigrations/2023/04/Version20230411120744.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types = 1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+class Version20230411120744 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'refs T28005: Delete externIds of all child-statements in order to get these from related original statements.';
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function up(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+        $this->addSql('UPDATE `_statement` SET `_st_extern_id` = NULL WHERE `_st_o_id` IS NOT NULL');
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function down(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+        //impossible
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function abortIfNotMysql(): void
+    {
+        $this->abortIf(
+            'mysql' !== $this->connection->getDatabasePlatform()->getName(),
+            "Migration can only be executed safely on 'mysql'."
+        );
+    }
+}
