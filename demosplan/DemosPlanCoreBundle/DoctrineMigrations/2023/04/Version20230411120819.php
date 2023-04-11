65096fa5a5306fae10509a0f08354e4770295c8b
Alexander Marten <marten@demos-deutschland.de>
2023-04-11 16:01:35 +0200

    fix (refs T28005): Create unique constraint for externId per procedure

---
+++ demosplan/DemosPlanCoreBundle/DoctrineMigrations/2023/04/Version20230411120819.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types = 1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+class Version20230411120819 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'refs T28005: Create unique constraint on database to avoid duplicate externIds caused by race conditions.';
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function up(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+        $this->addSql('CREATE UNIQUE INDEX externId_procedure ON _statement (_st_extern_id, _p_id)');
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function down(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+        $this->addSql('DROP INDEX externId_procedure ON _statement');
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function abortIfNotMysql(): void
+    {
+        $this->abortIf(
+            'mysql' !== $this->connection->getDatabasePlatform()->getName(),
+            "Migration can only be executed safely on 'mysql'."
+        );
+    }
+}
