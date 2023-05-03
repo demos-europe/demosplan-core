84267f46fa14873b026a0da90c81678c10ffa484
Velimir Müller <velimir@demos-deutschland.de>
2023-05-03 12:28:12 +0200

    fix(refs T32450): make radio buttons reactive when initial data is set
    - We are receiving a wrong key from data
    - To make the radio buttons toggle the rendering of the institution form fields it was necessary to check for the key `toeb`
    - Data send us the key `toeb` but this is outdated. The new term is `institution` hence the key must be checked and adjusted in case it is `toeb`
    - This is needed to make the usually read-only props reactive to determine if the statement belongs to an institution or not

--- client/js/components/procedure/DpSimplifiedNewStatementForm.vue
+++ client/js/components/procedure/DpSimplifiedNewStatementForm.vue
@@ -547,7 +547,7 @@ export default {
       if (typeof this.values.submitter === 'undefined' || Object.keys(this.values.submitter).length === 0) {
         Vue.set(this.values, 'submitter', {})
         for (const [key, value] of Object.entries(submitterProperties)) {
-          Vue.set(this.values.submitter, key, value)
+          Vue.set(this.values.submitter, key === 'toeb' ? 'institution' : key, value)
         }
       }
     },
