82820c44061bd19dccac59fc8ede387e7eec4542
Demos-CI <devops-system+github@demos-deutschland.de>
2023-05-05 14:59:11 +0000

    style: Apply php-cs-fixer

--- config/linters/rector.php
+++ config/linters/rector.php
@@ -20,10 +20,8 @@ use Rector\PHPUnit\Set\PHPUnitLevelSetList;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 use Rector\Symfony\Set\SensiolabsSetList;
-use Rector\Symfony\Set\SwiftmailerSetList;
 use Rector\Symfony\Set\SymfonyLevelSetList;
 use Rector\Symfony\Set\TwigLevelSetList;
-use Rector\Symfony\Set\TwigSetList;
 use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromAssignsRector;
 
 return static function (RectorConfig $rectorConfig): void {
@@ -31,29 +29,29 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([
 //        SetList::CODE_QUALITY,
 //        SymfonyLevelSetList::UP_TO_SYMFONY_44,
-        //SymfonyLevelSetList::UP_TO_SYMFONY_54,
-        //LevelSetList::UP_TO_PHP_53,
+        // SymfonyLevelSetList::UP_TO_SYMFONY_54,
+        // LevelSetList::UP_TO_PHP_53,
 //        LevelSetList::UP_TO_PHP_74,
         LevelSetList::UP_TO_PHP_81,
-        //TwigLevelSetList::UP_TO_TWIG_240,
-        //SensiolabsSetList::ANNOTATIONS_TO_ATTRIBUTES,
-        //PHPUnitLevelSetList::UP_TO_PHPUNIT_90,
-        //DoctrineSetList::DOCTRINE_DBAL_40,
+        // TwigLevelSetList::UP_TO_TWIG_240,
+        // SensiolabsSetList::ANNOTATIONS_TO_ATTRIBUTES,
+        // PHPUnitLevelSetList::UP_TO_PHPUNIT_90,
+        // DoctrineSetList::DOCTRINE_DBAL_40,
     ]);
     $rectorConfig->skip([
         TypedPropertyFromAssignsRector::class,
         UnionTypesRector::class,
         __DIR__.'/../../config/bundles.php',
     ]);
-    //$rectorConfig->paths([__DIR__ . '/../../demosplan']);
+    // $rectorConfig->paths([__DIR__ . '/../../demosplan']);
 
     $rectorConfig->phpVersion(PhpVersion::PHP_81);
     $rectorConfig->symfonyContainerXml(
         '/tmp/diplanbau/cache/dev/demosplan_DemosPlanCoreBundle_Application_DemosPlanKernelDevDebugContainer.xml'
     );
     $rectorConfig->autoloadPaths([__DIR__.'/../../vendor/autoload.php']);
-    //$rectorConfig->importNames();
-    //$rectorConfig->disableParallel();
+    // $rectorConfig->importNames();
+    // $rectorConfig->disableParallel();
     $rectorConfig->parallel(seconds: 180, jobSize: 10);
     // Path to phpstan with extensions, that PHPSTan in Rector uses to determine types
     $rectorConfig->phpstanConfig(__DIR__.'/../../vendor/phpstan/phpstan-symfony/extension.neon');
