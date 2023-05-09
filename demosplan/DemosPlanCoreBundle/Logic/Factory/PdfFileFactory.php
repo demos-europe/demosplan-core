2b8cad36dea1017fbece4026f0932f141a0cbd2b
Hamza Bakri <bakri@demos-deutschland.de>
2023-05-09 09:08:47 +0200

    F T32243 add PdfFileFactory
    Signed-off-by: Hamza Bakri <bakri@demos-deutschland.de>

---
+++ demosplan/DemosPlanCoreBundle/Logic/Factory/PdfFileFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS E-Partizipation GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
+namespace demosplan\DemosPlanCoreBundle\Logic\Factory;
+
+use demosplan\DemosPlanCoreBundle\ValueObject\Statement\PdfFile;
+use DemosEurope\DemosplanAddon\Contracts\Factory\PdfFileFactoryInterface;
+use DemosEurope\DemosplanAddon\Contracts\Entities\PdfFileInterface;
+
+class PdfFileFactory implements PdfFileFactoryInterface
+{
+    public function createPdfFile(): PdfFileInterface
+    {
+        return new PdfFile();
+    }
+}
