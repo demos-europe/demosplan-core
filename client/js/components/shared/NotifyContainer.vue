968526d3dd90985805740b1d0e4213d72489e8ae
Demos-CI <devops-system+github@demos-deutschland.de>
2023-05-10 14:51:06 +0000

    style: Update license header

--- client/js/components/shared/NotifyContainer.vue
+++ client/js/components/shared/NotifyContainer.vue
@@ -1,4 +1,13 @@
 <license>
+  (c) 2010-present DEMOS E-Partizipation GmbH.
+
+  This file is part of the package demosplan,
+  for more information see the license file.
+
+  All rights reserved
+</license>
+
+<license>
   (c) 2010-present DEMOS plan GmbH.
 
   This file is part of the package demosplan,
