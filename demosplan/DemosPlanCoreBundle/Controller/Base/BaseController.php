44b9e521c3cce6502502461222ecb92d9ebbe399
Hamza Bakri <bakri@demos-deutschland.de>
2023-05-10 11:40:07 +0200

    F T32243 move the method generateDownloadFilename to PdfNameService
    - this method is needed in the addon, it is better to put it in a service which kann be available via an interface to the aadons
    
    Signed-off-by: Hamza Bakri <bakri@demos-deutschland.de>

--- demosplan/DemosPlanCoreBundle/Controller/Base/BaseController.php
+++ demosplan/DemosPlanCoreBundle/Controller/Base/BaseController.php
@@ -236,35 +236,6 @@ abstract class BaseController extends AbstractController
     }
 
     /**
-     * Generiere den Downloadfilename aus dem übergebenen Dateinamen
-     * Der IE braucht eine Extrabehandlung.
-     *
-     * @param string $filename
-     *
-     * @return string
-     */
-    protected function generateDownloadFilename($filename)
-    {
-        // der IE benötigt mal wieder eine Extrabehandlung.
-        $filenameURLEncoded = urlencode($filename);
-        // Leerzeichen sollen nicht als + dargestellt werden
-        $filenameURLEncoded = str_replace('+', '_', $filenameURLEncoded);
-
-        // " müssen maskiert werden, damit sie nicht im Filename unten den String beenden (je nach Browser unterschiedlich
-        // interpretiert)
-        $filename = str_replace('"', '\"', $filename);
-
-        // filename*=UTF-8'' ist legacy für den IE (http://greenbytes.de/tech/webdav/rfc5987.html)
-        // http://blogs.msdn.com/b/ieinternals/archive/2010/06/07/content-disposition-attachment-and-international-unicode-characters.aspx
-        if (false !== stripos(getenv('HTTP_USER_AGENT'), 'MSIE')
-            || false !== stripos(getenv('HTTP_USER_AGENT'), 'Internet Explorer')) {
-            return sprintf('attachment;filename="%s";', $filenameURLEncoded);
-        } else {
-            return sprintf('attachment;filename="%s"; filename*=UTF-8\'\'%s', $filename, $filenameURLEncoded);
-        }
-    }
-
-    /**
      * Suche rekursiv in arrays.
      *
      * @param string $needle
