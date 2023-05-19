bb6763b8ce4a9765fe8fb9225d799796819abe40
Saeed Jalalifar <jalalifar@demos-deutschland.de>
2023-05-19 10:28:09 +0200

    feature (refs T32212): add new necessary interfaces to StatementFieldDefinition entity class

--- demosplan/DemosPlanCoreBundle/Entity/Procedure/StatementFieldDefinition.php
+++ demosplan/DemosPlanCoreBundle/Entity/Procedure/StatementFieldDefinition.php
@@ -11,6 +11,8 @@
 namespace demosplan\DemosPlanCoreBundle\Entity\Procedure;
 
 use DateTime;
+use DemosEurope\DemosplanAddon\Contracts\Entities\StatementFieldDefinitionInterface;
+use DemosEurope\DemosplanAddon\Contracts\Entities\StatementFormDefinitionInterface;
 use DemosEurope\DemosplanAddon\Contracts\Entities\UuidEntityInterface;
 use demosplan\DemosPlanCoreBundle\Entity\CoreEntity;
 use Doctrine\ORM\Mapping as ORM;
@@ -28,7 +30,7 @@ use Gedmo\Mapping\Annotation as Gedmo;
  * })
  * @ORM\Entity(repositoryClass="demosplan\DemosPlanProcedureBundle\Repository\StatementFieldDefinitionRepository")
  */
-class StatementFieldDefinition extends CoreEntity implements UuidEntityInterface
+class StatementFieldDefinition extends CoreEntity implements UuidEntityInterface, StatementFieldDefinitionInterface
 {
     /**
      * @var string|null
@@ -78,7 +80,7 @@ class StatementFieldDefinition extends CoreEntity implements UuidEntityInterface
     private $required = true;
 
     /**
-     * @var StatementFormDefinition
+     * @var StatementFormDefinitionInterface
      *
      * @ORM\ManyToOne(targetEntity="StatementFormDefinition", inversedBy="fieldDefinitions")
      * @JoinColumn(referencedColumnName="id", nullable=false)
@@ -94,7 +96,7 @@ class StatementFieldDefinition extends CoreEntity implements UuidEntityInterface
 
     public function __construct(
         string $fieldName,
-        StatementFormDefinition $statementFormDefinition,
+        StatementFormDefinitionInterface $statementFormDefinition,
         int $orderNumber,
         bool $enabled,
         bool $required
@@ -126,7 +128,7 @@ class StatementFieldDefinition extends CoreEntity implements UuidEntityInterface
         return $this->id;
     }
 
-    public function getStatementFormDefinition(): StatementFormDefinition
+    public function getStatementFormDefinition(): StatementFormDefinitionInterface
     {
         return $this->statementFormDefinition;
     }
