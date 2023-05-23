d3f3478ac25dd1080bdec51fea520194a9197e3b
Demos-CI <devops-system+github@demos-deutschland.de>
2023-05-23 07:45:39 +0000

    style: Update license header

--- client/js/components/button/NavButton.vue
+++ client/js/components/button/NavButton.vue
@@ -1,3 +1,12 @@
+<license>
+  (c) 2010-present DEMOS plan GmbH.
+
+  This file is part of the package demosplan,
+  for more information see the license file.
+
+  All rights reserved
+</license>
+
 <template>
   <!--The attribute data-responsive-menu-helper is used as a hook to append an EventListener in GlobalEventlisteners.js-->
   <button
--- client/js/store/statement/Fragment.js
+++ client/js/store/statement/Fragment.js
@@ -1,4 +1,13 @@
 /**
+ * (c) 2010-present DEMOS plan GmbH.
+ *
+ * This file is part of the package demosplan,
+ * for more information see the license file.
+ *
+ * All rights reserved
+ */
+
+/**
  * (c) 2010-present DEMOS E-Partizipation GmbH.
  *
  * This file is part of the package demosplan,
