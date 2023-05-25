7da4b64399e568254be1495bd01888ab774e5387
hwiem <40487668+hwiem@users.noreply.github.com>
2023-05-25 15:18:50 +0200

    refactor(refs T30969): move DpRegisterFlyout (#1403)
    - include RegisterFlyout in demosplan, so it can be removed
    from demosplan-ui (it's too specific to be included there)

--- client/js/bundles/user/setPassword.js
+++ client/js/bundles/user/setPassword.js
@@ -11,12 +11,13 @@
  * This is the entrypoint for user_set_password.html.twig
  */
 
-import { DpRegisterFlyout, dpValidate } from '@demos-europe/demosplan-ui'
+import { dpValidate } from '@demos-europe/demosplan-ui'
 import { initialize } from '@DpJs/InitVue'
+import RegisterFlyout from '@DpJs/components/user/RegisterFlyout'
 import SetPassword from '@DpJs/components/user/portalUser/SetPassword'
 
 const components = {
-  DpRegisterFlyout,
+  RegisterFlyout,
   SetPassword
 }
 
