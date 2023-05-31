ee26dfd42babf27d72c2feec2b6a745789b3445c
Hamza Bakri <bakri@demos-deutschland.de>
2023-05-31 06:49:00 +0200

    f (ref T32243) add GetDatasheetVersionEvent to retrieve the datasheet version from addon
    - this event will be handled in the Datasheet addon
    
    Signed-off-by: Hamza Bakri <bakri@demos-deutschland.de>

---
+++ demosplan/DemosPlanCoreBundle/Event/Procedure/GetDatasheetVersionEvent.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
+namespace demosplan\DemosPlanCoreBundle\Event\Procedure;
+
+use DemosEurope\DemosplanAddon\Contracts\Events\GetDatasheetVersionEventInterface;
+
+class GetDatasheetVersionEvent implements GetDatasheetVersionEventInterface
+{
+    private string $procedureId;
+
+    public function __construct(string $procedureId)
+    {
+        $this->procedureId = $procedureId;
+    }
+
+    public function getProcedureId(): string
+    {
+        return $this->procedure;
+    }
+
+    public function setDatasheetVersion(int $datasheetVersion)
+    {
+        $this->datasheetVersion = $datasheetVersion;
+    }
+
+    public function getDatasheetVersion(): int
+    {
+        return $this->datasheetVersion;
+    }
+}
