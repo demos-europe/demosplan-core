94df44e9c2eea778dbffc1dcbed55aaecda2985d
Demos-CI <devops-system+github@demos-deutschland.de>
2023-06-05 15:51:22 +0000

    style: Apply php-cs-fixer

--- demosplan/DemosPlanCoreBundle/Logic/Statement/ElasticsearchFilterArrayTransformer.php
+++ demosplan/DemosPlanCoreBundle/Logic/Statement/ElasticsearchFilterArrayTransformer.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /**
  * This file is part of the package demosplan.
  *
- * (c) 2010-present DEMOS E-Partizipation GmbH, for more information see the license file.
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
  *
  * All rights reserved
  */
@@ -14,6 +14,7 @@ namespace demosplan\DemosPlanCoreBundle\Logic\Statement;
 
 use demosplan\DemosPlanStatementBundle\Logic\ElasticSearchService;
 use Psr\Log\LoggerInterface;
+
 use function is_array;
 
 class ElasticsearchFilterArrayTransformer
