676287a8f30307e2f516103971d124ca0c238061
Moritz Mandler <mandler@demos-deutschland.de>
2023-06-08 16:10:58 +0200

    feature (refs T31885): added use Statement for UnexpectedValueException within the DemosPlanMiscContentController.

--- demosplan/DemosPlanCoreBundle/Controller/MiscContent/DemosPlanMiscContentController.php
+++ demosplan/DemosPlanCoreBundle/Controller/MiscContent/DemosPlanMiscContentController.php
@@ -35,6 +35,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use UnexpectedValueException;
 
 /**
  * Ausgabe Newseiten und andere Einzelseiten.
