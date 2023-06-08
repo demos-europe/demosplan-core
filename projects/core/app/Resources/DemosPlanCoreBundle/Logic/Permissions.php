11464a7a37cd2d2f57df422b2dfe78e69a9c3dd3
Sami Mussbach <mussbach@demos-deutschland.de>
2023-06-08 18:01:35 +0200

    chore: add github workflow to automatically update vendor lists

---
+++ projects/core/app/Resources/DemosPlanCoreBundle/Logic/Permissions.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
+namespace Projects\core\app\Resources\DemosPlanCoreBundle\Logic;
+
+use demosplan\DemosPlanCoreBundle\Permissions\ProjectPermissions;
+
+class Permissions extends ProjectPermissions
+{
+
+    public function projectGlobalPermissions(): void
+    {
+
+    }
+
+    public function projectProcedurePermissions(): void
+    {
+
+    }
+}
