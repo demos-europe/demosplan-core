c5cb44b916849ae6ebb3308127615750b69a9b31
Moritz Mandler <mandler@demos-deutschland.de>
2023-06-08 16:22:00 +0200

    feature (refs T31885): properly initialize collections before usage.

--- demosplan/DemosPlanCoreBundle/Controller/MiscContent/DemosPlanMiscContentController.php
+++ demosplan/DemosPlanCoreBundle/Controller/MiscContent/DemosPlanMiscContentController.php
@@ -35,6 +35,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Tightenco\Collect\Support\Collection;
 use UnexpectedValueException;
 
 /**
@@ -473,6 +474,8 @@ class DemosPlanMiscContentController extends BaseController
      */
     public function informationAction(CurrentUserInterface $userProvider, FaqHandler $faqHandler): Response
     {
+        $platformCategories = new Collection();
+        $customFaqCategories = new Collection();
         try {
             $platformCategories = $faqHandler->getPlatformFaqCategories();
             $customFaqCategories = $faqHandler->getCustomFaqCategoriesByNamesOrCustom(FaqCategory::FAQ_CATEGORY_TYPES_MANDATORY);
