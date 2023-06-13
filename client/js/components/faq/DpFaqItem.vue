4563f2cb849f1eb56ba1cf437297bf91c9617d21
Stefanie Grünberger <gruenberger@demos-deutschland.de>
2023-06-13 08:58:32 +0200

    feat(refs T33153): Revert custom label for multiselect
    Vue multiselect uses here option and not props.

--- client/js/components/faq/DpFaqItem.vue
+++ client/js/components/faq/DpFaqItem.vue
@@ -16,6 +16,7 @@
       <dp-multiselect
         v-if="availableGroupOptions.length > 1"
         :allow-empty="false"
+        :custom-label="option =>`${option.title}`"
         data-cy="selectedGroups"
         multiple
         :options="availableGroupOptions"
