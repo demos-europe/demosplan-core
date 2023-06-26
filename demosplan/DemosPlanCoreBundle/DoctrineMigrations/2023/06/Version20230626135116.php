13aba4c04a5cbc519d6e10c9893f0fc887845b64
Demos-CI <devops-system+github@demos-deutschland.de>
2023-06-26 14:10:58 +0000

    style: Apply php-cs-fixer

--- demosplan/DemosPlanCoreBundle/DoctrineMigrations/2023/06/Version20230626135116.php
+++ demosplan/DemosPlanCoreBundle/DoctrineMigrations/2023/06/Version20230626135116.php
@@ -1,4 +1,14 @@
-<?php declare(strict_types = 1);
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
 
 namespace Application\Migrations;
 
@@ -22,7 +32,6 @@ class Version20230626135116 extends AbstractMigration
 
         $this->addSql('CREATE TABLE procedure_message (id CHAR(36) NOT NULL, procedure_id CHAR(36) NOT NULL, message LONGTEXT NOT NULL, created_date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, modification_date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, error TINYINT(1) DEFAULT false NOT NULL, deleted TINYINT(1) DEFAULT false NOT NULL, request_count INT DEFAULT 1 NOT NULL, UNIQUE INDEX UNIQ_E7F5DA961624BCD2 (procedure_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE procedure_message ADD CONSTRAINT FK_E7F5DA961624BCD2 FOREIGN KEY (procedure_id) REFERENCES _procedure (_p_id)');
-
     }
 
     /**
@@ -34,7 +43,6 @@ class Version20230626135116 extends AbstractMigration
 
         $this->addSql('ALTER TABLE procedure_message DROP FOREIGN KEY FK_E7F5DA961624BCD2');
         $this->addSql('DROP TABLE procedure_message');
-
     }
 
     /**
