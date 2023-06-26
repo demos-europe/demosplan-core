05f1648cb481f70abe5d19928a66c415a94042bc
Hamza Bakri <bakri@demos-deutschland.de>
2023-06-26 15:56:00 +0200

    fix add test for the new class 'NameGenerator'
    Signed-off-by: Hamza Bakri <bakri@demos-deutschland.de>

---
+++ tests/backend/core/Procedure/Functional/NameGeneratorTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
+namespace Tests\Core\Procedure\Functional;
+
+use demosplan\DemosPlanCoreBundle\Logic\Procedure\NameGenerator;
+use Tests\Base\FunctionalTestCase;
+
+class NameGeneratorTest extends FunctionalTestCase
+{
+    /**
+     * @var GenerateName
+     */
+    protected $sut;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->sut = new NameGenerator();
+    }
+
+    public function testGenerateDownloadFilename()
+    {
+        $fileName = 'TestFi leN-am"eString.pdf';
+        $result = $this->sut->generateDownloadFilename($fileName);
+        $expectedFileName = 'attachment;filename="TestFi leN-am\"eString.pdf"; filename*=UTF-8\'\'TestFi_leN-am%22eString.pdf';
+        self::assertSame($expectedFileName, $this->sut->generateDownloadFilename($fileName));
+    }
+}
