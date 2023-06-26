eb9ad5d8e4367b0e6e25b90338c45e75b810739f
Demos-CI <devops-system+github@demos-deutschland.de>
2023-06-26 09:58:26 +0000

    style: Apply php-cs-fixer

--- demosplan/DemosPlanCoreBundle/Tools/DocxImporterRabbitmq.php
+++ demosplan/DemosPlanCoreBundle/Tools/DocxImporterRabbitmq.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /**
  * This file is part of the package demosplan.
  *
- * (c) 2010-present DEMOS E-Partizipation GmbH, for more information see the license file.
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
  *
  * All rights reserved
  */
@@ -33,7 +33,7 @@ class DocxImporterRabbitmq implements DocxImporterInterface
     }
 
     /**
-     * Import docx via RabbitMQ
+     * Import docx via RabbitMQ.
      *
      * @throws Exception
      */
