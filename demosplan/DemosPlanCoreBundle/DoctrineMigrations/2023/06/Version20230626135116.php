379cbf9477c55e532a7e6bda2400b845b6b20942
Saeed Jalalifar <jalalifar@demos-deutschland.de>
2023-07-03 10:31:49 +0200

    feature (refs T32796): default 0 for requestCount and change description

--- demosplan/DemosPlanCoreBundle/DoctrineMigrations/2023/06/Version20230626135116.php
+++ demosplan/DemosPlanCoreBundle/DoctrineMigrations/2023/06/Version20230626135116.php
@@ -20,7 +20,7 @@ class Version20230626135116 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'refs T:32796 create ProcedureMessage in relation with XBeteiligung ';
+        return 'refs 32796 create ProcedureMessage in relation with XBeteiligung ';
     }
 
     /**
@@ -30,7 +30,7 @@ class Version20230626135116 extends AbstractMigration
     {
         $this->abortIfNotMysql();
 
-        $this->addSql('CREATE TABLE procedure_message (id CHAR(36) NOT NULL, procedure_id CHAR(36) NOT NULL, message LONGTEXT NOT NULL, created_date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, modification_date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, error TINYINT(1) DEFAULT false NOT NULL, deleted TINYINT(1) DEFAULT false NOT NULL, request_count INT DEFAULT 1 NOT NULL, UNIQUE INDEX UNIQ_E7F5DA961624BCD2 (procedure_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE procedure_message (id CHAR(36) NOT NULL, procedure_id CHAR(36) NOT NULL, message LONGTEXT NOT NULL, created_date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, modification_date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, error TINYINT(1) DEFAULT false NOT NULL, deleted TINYINT(1) DEFAULT false NOT NULL, request_count INT DEFAULT 0 NOT NULL, UNIQUE INDEX UNIQ_E7F5DA961624BCD2 (procedure_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE procedure_message ADD CONSTRAINT FK_E7F5DA961624BCD2 FOREIGN KEY (procedure_id) REFERENCES _procedure (_p_id)');
     }
 
