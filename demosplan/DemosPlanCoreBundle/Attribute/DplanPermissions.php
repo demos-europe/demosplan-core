1d10fbf811e6847e05a44ce7f6bfb71e6da6e33e
AlexMenschoid <meyer-schlichtmann@demos-deutschland.de>
2023-07-04 15:22:17 +0200

    feat: adds attribute for plan permissions
    This adds a new php attribute which acts exactly like the old dplanPermissions annotation. With the advent of attributes, we should replace the old annotation with the new, now better fitting, attribute.

---
+++ demosplan/DemosPlanCoreBundle/Attribute/DplanPermissions.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
+declare(strict_types=1);
+
+namespace demosplan\DemosPlanCoreBundle\Attribute;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_METHOD)]
+class DplanPermissions
+{
+    private array $permissions;
+    public function __construct(string|array $permissions)
+    {
+        if (is_string($permissions)) {
+            $permissions = [$permissions];
+        }
+
+        $this->permissions = $permissions;
+    }
+
+    public function getPermissions(): array
+    {
+        return $this->permissions;
+    }
+}
