ed4b9184e321bef8013a2825a3c85f5b0b6d73d1
Hamza Bakri <bakri@demos-deutschland.de>
2023-07-05 12:41:27 +0200

    fix adjust implemented classes
    Signed-off-by: Hamza Bakri <bakri@demos-deutschland.de>

--- demosplan/DemosPlanCoreBundle/Entity/User/User.php
+++ demosplan/DemosPlanCoreBundle/Entity/User/User.php
@@ -32,6 +32,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
+use Hslavich\OneloginSamlBundle\Security\User\SamlUserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 use UnexpectedValueException;
 
@@ -48,7 +49,7 @@ use function in_array;
  *
  * @UserWithMatchingDepartmentInOrgaConstraint()
  */
-class User implements AddonUserInterface
+class User implements SamlUserInterface, AddonUserInterface
 {
     /**
      * @var string|null
