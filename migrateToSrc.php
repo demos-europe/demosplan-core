af60e0c396187a1d7572183f56d0bab4da0016f7
AlexMenschoid <meyer-schlichtmann@demos-deutschland.de>
2023-07-07 09:56:01 +0200

    feat (refs T30603): adds migration script stump
    including a rough outline of the plan in form of TODOs, so you don't always have to check back with the ticket.

---
+++ migrateToSrc.php
@@ -0,0 +1,19 @@
+<?php
+
+require('vendor/autoload.php');
+
+/**
+ * This is the migration script to transform demosplan/DemosPlanCoreBundle into src/
+ */
+
+// TODO: Execute Doctrine Migrations
+    // TODO: First create the migrations
+
+// TODO: Execute Rector for changing all namespaces
+    // TODO: First create Rector rule
+
+// TODO: Execute fulltext search+replace for usage of namespaces in all remaining places
+
+// TODO: Change composer.json. New name: 'demos-europe/demosplan-core', new psr-4 namespace: 'DemosEurope\\Demosplan'
+
+// TODO: Fix all namespaces in the addon contracts as well as all Addons.
