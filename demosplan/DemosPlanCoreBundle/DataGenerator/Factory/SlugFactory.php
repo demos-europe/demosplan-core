3e261fbefa0c4d719c7333fd78fc07008e4f9616
Alexander Marten <marten@demos-deutschland.de>
2023-07-10 16:02:44 +0200

    fix (refs T33175): cleanup SlugFactory to improve readability

--- demosplan/DemosPlanCoreBundle/DataGenerator/Factory/SlugFactory.php
+++ demosplan/DemosPlanCoreBundle/DataGenerator/Factory/SlugFactory.php
@@ -37,21 +37,11 @@ use Zenstruck\Foundry\RepositoryProxy;
  */
 final class SlugFactory extends ModelFactory
 {
-    /**
-     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
-     *
-     * @todo inject services if required
-     */
     public function __construct()
     {
         parent::__construct();
     }
 
-    /**
-     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
-     *
-     * @todo add your default values here
-     */
     protected function getDefaults(): array
     {
         return [
@@ -59,14 +49,9 @@ final class SlugFactory extends ModelFactory
         ];
     }
 
-    /**
-     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
-     */
     protected function initialize(): self
     {
-        return $this
-            // ->afterInstantiate(function(Slug $slug): void {})
-        ;
+        return $this;
     }
 
     protected static function getClass(): string
