87b408fc691219c822869ac87fcc94dca120c379
Demos-CI <devops-system+github@demos-deutschland.de>
2023-07-13 13:36:25 +0000

    style: Apply php-cs-fixer

--- demosplan/DemosPlanCoreBundle/Controller/Procedure/DemosPlanProcedureAPIController.php
+++ demosplan/DemosPlanCoreBundle/Controller/Procedure/DemosPlanProcedureAPIController.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of the package demosplan.
  *
- * (c) 2010-present DEMOS E-Partizipation GmbH, for more information see the license file.
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
  *
  * All rights reserved
  */
@@ -83,6 +83,7 @@ class DemosPlanProcedureAPIController extends APIController
      *        methods={"GET"},
      *        name="dplan_api_procedure_"
      * )
+     *
      * @DplanPermissions("area_public_participation")
      */
     public function listAction(Request $request): APIResponse
@@ -102,6 +103,7 @@ class DemosPlanProcedureAPIController extends APIController
      *     name="dp_api_procedure_mark_participated",
      *     options={"expose": true}
      * )
+     *
      * @DplanPermissions("feature_procedures_mark_participated")
      *
      * @param string $procedureId
@@ -129,6 +131,7 @@ class DemosPlanProcedureAPIController extends APIController
      *     name="dp_api_procedure_unmark_participated",
      *     options={"expose": true}
      * )
+     *
      * @DplanPermissions("feature_procedures_mark_participated")
      *
      * @param string $procedureId
@@ -158,6 +161,7 @@ class DemosPlanProcedureAPIController extends APIController
      *     name="dp_api_procedure_get_statement_empty_filters",
      *     options={"expose": true}
      * )
+     *
      * @DplanPermissions("area_admin_assessmenttable")
      *
      * @return \demosplan\DemosPlanCoreBundle\Response\APIResponse|JsonResponse
@@ -175,6 +179,7 @@ class DemosPlanProcedureAPIController extends APIController
      *     name="dp_api_procedure_get_original_statement_empty_filters",
      *     options={"expose": true}
      * )
+     *
      * @DplanPermissions("area_admin_assessmenttable")
      *
      * @return \demosplan\DemosPlanCoreBundle\Response\APIResponse|JsonResponse
@@ -190,6 +195,7 @@ class DemosPlanProcedureAPIController extends APIController
      *     name="dp_api_procedure_get_original_filters",
      *     options={"expose": true}
      * )
+     *
      * @DplanPermissions("area_admin_assessmenttable")
      *
      * @param string $procedureId
@@ -226,6 +232,7 @@ class DemosPlanProcedureAPIController extends APIController
      *     name="dp_api_procedure_get_statement_filters",
      *     options={"expose": true}
      * )
+     *
      * @DplanPermissions("area_admin_assessmenttable")
      *
      * @param string $procedureId
@@ -262,6 +269,7 @@ class DemosPlanProcedureAPIController extends APIController
      *     name="dplan_api_procedure_update_filter_hash",
      *     options={"expose": true}
      * )
+     *
      * @DplanPermissions("area_admin_assessmenttable")
      *
      * @param string $procedureId
@@ -280,6 +288,7 @@ class DemosPlanProcedureAPIController extends APIController
      *     name="dplan_api_procedure_update_original_filter_hash",
      *     options={"expose": true}
      * )
+     *
      * @DplanPermissions("area_admin_assessmenttable")
      *
      * @param string $procedureId
@@ -437,6 +446,7 @@ class DemosPlanProcedureAPIController extends APIController
      *     name="dplan_api_procedure_delete_statement_filter",
      *     options={"expose": true}
      * )
+     *
      * @DplanPermissions("area_admin_assessmenttable")
      *
      * @param string $filterSetId
@@ -512,6 +522,7 @@ class DemosPlanProcedureAPIController extends APIController
      *     name="dplan_api_procedure_add_invited_public_affairs_bodies",
      *     options={"expose": true}
      * )
+     *
      * @DplanPermissions("area_admin_invitable_institution")
      */
     public function addInvitedPublicAffairsAgentsAction(
@@ -542,6 +553,7 @@ class DemosPlanProcedureAPIController extends APIController
      *     path="/verfahren/suche/ajax",
      *     options={"expose": true},
      * )
+     *
      * @DplanPermissions("area_public_participation")
      *
      * @return JsonResponse
