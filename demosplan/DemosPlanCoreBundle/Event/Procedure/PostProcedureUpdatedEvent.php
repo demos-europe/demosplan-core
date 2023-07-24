e5064550a2c1bccf09e283dde50ff0042c1190b6
Saeed Jalalifar <jalalifar@demos-deutschland.de>
2023-07-24 09:47:32 +0200

    feature: add missing getProcedure method

--- demosplan/DemosPlanCoreBundle/Event/Procedure/PostProcedureUpdatedEvent.php
+++ demosplan/DemosPlanCoreBundle/Event/Procedure/PostProcedureUpdatedEvent.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 namespace demosplan\DemosPlanCoreBundle\Event\Procedure;
 
 use DemosEurope\DemosplanAddon\Contracts\Entities\EntityInterface;
+use DemosEurope\DemosplanAddon\Contracts\Entities\ProcedureInterface;
 use DemosEurope\DemosplanAddon\Contracts\Events\PostProcedureUpdatedEventInterface;
 use demosplan\DemosPlanCoreBundle\Entity\Procedure\Procedure;
 use demosplan\DemosPlanCoreBundle\Event\DPlanEvent;
@@ -76,5 +77,9 @@ class PostProcedureUpdatedEvent extends DPlanEvent implements PostProcedureUpdat
         }
 
         return $modifiedValues;
+    }
+    public function getProcedure(): ProcedureInterface
+    {
+        return $this->getProcedure();
     }
 }
