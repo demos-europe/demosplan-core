438fa860a1a9ca081a22950dabbe2abfe9d5962c
Demos-CI <devops-system+github@demos-deutschland.de>
2023-08-04 07:41:45 +0000

    style: Apply php-cs-fixer

--- demosplan/DemosPlanCoreBundle/Logic/ApiRequest/JsonApiResourceTypeService.php
+++ demosplan/DemosPlanCoreBundle/Logic/ApiRequest/JsonApiResourceTypeService.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
 namespace demosplan\DemosPlanCoreBundle\Logic\ApiRequest;
 
 use Carbon\Carbon;
@@ -36,6 +44,7 @@ use EDT\Wrapping\Properties\UpdatableRelationship;
 use EDT\Wrapping\Utilities\SchemaPathProcessor;
 use Pagerfanta\Pagerfanta;
 use Psr\EventDispatcher\EventDispatcherInterface;
+
 use function in_array;
 use function is_array;
 
