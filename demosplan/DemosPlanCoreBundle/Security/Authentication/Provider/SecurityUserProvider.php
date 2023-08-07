3dfd1b2ae625b5cf931f13875e95532c164ead75
Sami Mussbach <mussbach@demos-deutschland.de>
2023-08-07 19:12:46 +0200

    chore (refs T34194): avoid database call for anonymous user

--- demosplan/DemosPlanCoreBundle/Security/Authentication/Provider/SecurityUserProvider.php
+++ demosplan/DemosPlanCoreBundle/Security/Authentication/Provider/SecurityUserProvider.php
@@ -12,6 +12,8 @@ declare(strict_types=1);
 
 namespace demosplan\DemosPlanCoreBundle\Security\Authentication\Provider;
 
+use DemosEurope\DemosplanAddon\Contracts\Entities\UserInterface as AddonContractUserInterface;
+use demosplan\DemosPlanCoreBundle\Entity\User\AnonymousUser;
 use demosplan\DemosPlanCoreBundle\Entity\User\SecurityUser;
 use demosplan\DemosPlanCoreBundle\Entity\User\User;
 use demosplan\DemosPlanCoreBundle\Repository\UserRepository;
@@ -70,6 +72,12 @@ class SecurityUserProvider implements UserProviderInterface, PasswordUpgraderInt
 
     private function loadUserByLogin(string $login): User
     {
+        // avoid database call for anonymous user
+        if (AddonContractUserInterface::ANONYMOUS_USER_LOGIN === $login) {
+
+           return new AnonymousUser();
+        }
+
         $userEntity = $this->userRepository->findOneBy(['login' => $login]);
         if (!$userEntity) {
             throw new UserNotFoundException(sprintf('No user found for "%s"', $login));
