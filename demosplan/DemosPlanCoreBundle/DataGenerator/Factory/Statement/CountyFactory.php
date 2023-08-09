011be934a5f45fecdd681e456343c7853da4c111
Alexander Marten <marten@demos-deutschland.de>
2023-08-09 19:27:10 +0200

    fix (refs T31520 + T32997): cleanup CountyFactory.php

--- demosplan/DemosPlanCoreBundle/DataGenerator/Factory/Statement/CountyFactory.php
+++ demosplan/DemosPlanCoreBundle/DataGenerator/Factory/Statement/CountyFactory.php
@@ -26,55 +26,24 @@ use Zenstruck\Foundry\RepositoryProxy;
  * @method static County[]|Proxy[]                 findBy(array $attributes)
  * @method static County[]|Proxy[]                 randomRange(int $min, int $max, array $attributes = [])
  * @method static County[]|Proxy[]                 randomSet(int $number, array $attributes = [])
- *
- * @phpstan-method        Proxy<County> create(array|callable $attributes = [])
- * @phpstan-method static Proxy<County> createOne(array $attributes = [])
- * @phpstan-method static Proxy<County> find(object|array|mixed $criteria)
- * @phpstan-method static Proxy<County> findOrCreate(array $attributes)
- * @phpstan-method static Proxy<County> first(string $sortedField = 'id')
- * @phpstan-method static Proxy<County> last(string $sortedField = 'id')
- * @phpstan-method static Proxy<County> random(array $attributes = [])
- * @phpstan-method static Proxy<County> randomOrCreate(array $attributes = [])
- * @phpstan-method static RepositoryProxy<County> repository()
- * @phpstan-method static list<Proxy<County>> all()
- * @phpstan-method static list<Proxy<County>> createMany(int $number, array|callable $attributes = [])
- * @phpstan-method static list<Proxy<County>> createSequence(iterable|callable $sequence)
- * @phpstan-method static list<Proxy<County>> findBy(array $attributes)
- * @phpstan-method static list<Proxy<County>> randomRange(int $min, int $max, array $attributes = [])
- * @phpstan-method static list<Proxy<County>> randomSet(int $number, array $attributes = [])
  */
 final class CountyFactory extends ModelFactory
 {
-    /**
-     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
-     *
-     * @todo inject services if required
-     */
     public function __construct()
     {
         parent::__construct();
     }
 
-    /**
-     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
-     *
-     * @todo add your default values here
-     */
     protected function getDefaults(): array
     {
         return [
-            'name' => self::faker()->text(36),
+            'name' => self::faker()->country(),
         ];
     }
 
-    /**
-     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
-     */
     protected function initialize(): self
     {
-        return $this
-            // ->afterInstantiate(function(County $county): void {})
-        ;
+        return $this;
     }
 
     protected static function getClass(): string
