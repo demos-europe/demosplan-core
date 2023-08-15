9753107a71440f406bda67d8f7e2c50f82b8197a
Florian Salis <salis@demos-deutschland.de>
2023-08-15 22:01:44 +0200

    feat(refs T34313): Add Logic to change Tabs on hashchange for listFinalGroup

--- client/js/bundles/statement/listFinalGroup.js
+++ client/js/bundles/statement/listFinalGroup.js
@@ -14,6 +14,7 @@
 import DpMapModal from '@DpJs/components/statement/assessmentTable/DpMapModal'
 import DpPublicStatementList from '@DpJs/components/statement/publicStatementLists/DpPublicStatementList'
 import { initialize } from '@DpJs/InitVue'
+import { prefixClass } from '@demos-europe/demosplan-ui'
 import publicStatement from '@DpJs/store/statement/PublicStatement'
 import Tabs from '@DpJs/lib/statement/Tabs'
 
@@ -26,6 +27,27 @@ const stores = {
   publicStatement
 }
 
+const setTabFromHash = () => {
+  const titles = document.querySelectorAll('.c-tabs__titles > li')
+
+  if (location.hash === '#votedStatementsList') {
+    document.querySelector('#myStatementsList').classList.remove(prefixClass('is-active-tab'))
+    document.querySelector('#votedStatementsList').classList.add(prefixClass('is-active-tab'))
+    titles[0].classList.remove(prefixClass('is-active-tab'))
+    titles[1].classList.add(prefixClass('is-active-tab'))
+  } else {
+    document.querySelector('#myStatementsList').classList.add(prefixClass('is-active-tab'))
+    document.querySelector('#votedStatementsList').classList.remove(prefixClass('is-active-tab'))
+    titles[0].classList.add(prefixClass('is-active-tab'))
+    titles[1].classList.remove(prefixClass('is-active-tab'))
+  }
+}
 initialize(components, stores).then(() => {
   Tabs()
+
+  setTabFromHash()
+
+  addEventListener('hashchange', event => {
+    setTabFromHash()
+  })
 })
