c0adb93fa301eede75041decbb683a76a3b00f75
Florian Salis <salis@demos-deutschland.de>
2023-08-28 10:16:49 +0200

    chore: remove unused Files

--- client/fe/webpack/resolveAliases.js
+++ client/fe/webpack/resolveAliases.js
@@ -14,13 +14,7 @@ const config = require('../config/config').config
 function resolveAliases () {
   const aliases = {
     '@DpJs': config.absoluteRoot + 'client/js',
-    vue: 'vue/dist/vue.esm.js',
-    'prosemirror-model': config.absoluteRoot + 'node_modules/prosemirror-model',
-    'prosemirror-state': config.absoluteRoot + 'node_modules/prosemirror-state',
-    'prosemirror-view': config.absoluteRoot + 'node_modules/prosemirror-view',
-    'prosemirror-history': config.absoluteRoot + 'node_modules/prosemirror-history',
-    'prosemirror-tables': config.absoluteRoot + 'node_modules/prosemirror-tables',
-    'prosemirror-utils': config.absoluteRoot + 'node_modules/prosemirror-utils'
+    vue: 'vue/dist/vue.esm.js'
   }
 
   glob.sync(config.oldBundlesPath + 'Demos*Bundle').forEach(dir => {
