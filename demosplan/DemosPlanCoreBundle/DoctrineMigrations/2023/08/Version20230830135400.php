2e58513f0461aadbbee449d9aa3dbe9458cd11bf
Sami Mussbach <mussbach@demos-deutschland.de>
2023-08-30 16:23:30 +0200

    chore: External orga id from identity provider can be longer than 36 chars

---
+++ demosplan/DemosPlanCoreBundle/DoctrineMigrations/2023/08/Version20230830135400.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types = 1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Platforms\MySQLPlatform;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+class Version20230830135400 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'External orga id from identity provider can be longer than 36 chars.';
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function up(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+
+        $this->addSql('ALTER TABLE _orga CHANGE _o_gw_id _o_gw_id VARCHAR(256) DEFAULT NULL');
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function down(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+
+        $this->addSql('ALTER TABLE _orga CHANGE _o_gw_id _o_gw_id VARCHAR(36) DEFAULT NULL');
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function abortIfNotMysql(): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof MySqlPlatform,
+            "Migration can only be executed safely on 'mysql'."
+        );
+    }
+}
