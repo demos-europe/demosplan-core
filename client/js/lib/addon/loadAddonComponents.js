467c192120f6c5f6dbaac1508b324cd4aeb31b88
Velimir Müller <velimir@demos-deutschland.de>
2023-08-31 10:44:22 +0200

    feat(refs T29585 ): load addons logic moved to dpjs libs
    This function must only return the addon. The evaluation of the string must happen in the corresponding parent component that loads the addon.

---
+++ client/js/lib/addon/loadAddonComponents.js
@@ -0,0 +1,25 @@
+import { checkResponse, dpRpc } from '@demos-europe/demosplan-ui'
+
+export default function loadComponents (hookName) {
+  const params = {
+    hookName: hookName
+  }
+
+  const addons = []
+
+  return dpRpc('addons.assets.load', params)
+    .then(response => checkResponse(response))
+    .then(response => {
+      const result = response[0].result
+
+      for (const key of Object.keys(result)) {
+        const addon = result[key]
+        const contentKey = addon.entry + '.umd.js'
+        const content = addon.content[contentKey]
+
+        addons.push(content)
+      }
+
+      return addons
+    })
+}
