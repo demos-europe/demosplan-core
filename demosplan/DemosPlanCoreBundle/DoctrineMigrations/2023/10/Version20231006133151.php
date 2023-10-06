4b31a3e08a73596c551ab492ec908787b3642fc0
Moritz Mandler <mandler@demos-deutschland.de>
2023-10-06 15:43:17 +0200

    feature (refs T34504): Setup SupportContact entity and added Migration and empty repoyitory.

---
+++ demosplan/DemosPlanCoreBundle/DoctrineMigrations/2023/10/Version20231006133151.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types = 1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Platforms\MySQLPlatform;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+class Version20231006133151 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'refs T34504: Create SupportContact Table and setup eMailAddress and customer relations';
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function up(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+
+        $this->addSql('CREATE TABLE _support_contact (id CHAR(36) NOT NULL, email_id CHAR(36) DEFAULT NULL, customer CHAR(36) DEFAULT NULL, title VARCHAR(255) DEFAULT NULL, phone_number VARCHAR(255) DEFAULT NULL, text LONGTEXT DEFAULT NULL, visible TINYINT(1) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, INDEX IDX_8C8C0928A832C1C9 (email_id), INDEX IDX_8C8C092881398E09 (customer), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE _support_contact ADD CONSTRAINT FK_8C8C0928A832C1C9 FOREIGN KEY (email_id) REFERENCES email_address (id)');
+        $this->addSql('ALTER TABLE _support_contact ADD CONSTRAINT FK_8C8C092881398E09 FOREIGN KEY (customer) REFERENCES customer (_c_id)');
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function down(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+
+        $this->addSql('ALTER TABLE _support_contact DROP FOREIGN KEY FK_8C8C0928A832C1C9');
+        $this->addSql('ALTER TABLE _support_contact DROP FOREIGN KEY FK_8C8C092881398E09');
+        $this->addSql('DROP TABLE _support_contact');
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function abortIfNotMysql(): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof MySqlPlatform,
+            "Migration can only be executed safely on 'mysql'."
+        );
+    }
+}
---
+++ demosplan/DemosPlanCoreBundle/Entity/User/SupportContact.php
@@ -0,0 +1,152 @@
+<?php
+declare(strict_types=1);
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+namespace demosplan\DemosPlanCoreBundle\Entity\User;
+
+use DemosEurope\DemosplanAddon\Contracts\Entities\UuidEntityInterface;
+use demosplan\DemosPlanCoreBundle\Entity\CoreEntity;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+use demosplan\DemosPlanCoreBundle\Entity\EmailAddress;
+
+/**
+ * @ORM\Table(name="_support_contact")
+ *
+ * @ORM\Entity(repositoryClass="demosplan\DemosPlanCoreBundle\Repository\SupportContactRepository")
+ */
+class SupportContact extends CoreEntity implements UuidEntityInterface
+{
+    use TimestampableEntity;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="id", type="string", length=36, options={"fixed":true})
+     *
+     * @ORM\Id
+     *
+     * @ORM\GeneratedValue(strategy="CUSTOM")
+     *
+     * @ORM\CustomIdGenerator(class="\demosplan\DemosPlanCoreBundle\Doctrine\Generator\UuidV4Generator")
+     */
+    private ?string $id;
+
+    /**
+     * @ORM\Column(name="title", type="string", length=255, nullable=true)
+     */
+    private ?string $title;
+
+    /**
+     * @ORM\Column(name="phone_number", type="string", length=255, nullable=true)
+     */
+    private ?string $phoneNumber;
+
+    /**
+     *
+     * @ORM\ManyToOne(
+     *     targetEntity="demosplan\DemosPlanCoreBundle\Entity\EmailAddress",
+     *     cascade={"persist"})
+     *
+     * @ORM\JoinColumn(name="email_id",
+     *     referencedColumnName="id",
+     *     nullable = true)
+     */
+    #[Assert\Valid]
+    private ?EmailAddress $eMailAddress;
+
+    /**
+     * @ORM\Column(name="text", type="text", nullable=true)
+     */
+    private ?string $text;
+
+    /**
+     * @ORM\Column(name="visible", type="boolean")
+     */
+    private bool $visible = false;
+
+    /**
+     *
+     * @ORM\ManyToOne(targetEntity="demosplan\DemosPlanCoreBundle\Entity\User\Customer", inversedBy="contacts")
+     *
+     * @ORM\JoinColumn(name="customer", referencedColumnName="_c_id", nullable=true)
+     */
+    private ?Customer $customer;
+
+    public function __construct(
+        ?string $title,
+        ?string $phoneNumber,
+        ?EmailAddress $emailAddress,
+        ?string $text,
+        ?Customer $customer,
+        bool $visible
+    ) {
+        $this->title = $title;
+        $this->phoneNumber = $phoneNumber;
+        $this->eMailAddress = $emailAddress;
+        $this->text = $text;
+        $this->customer = $customer;
+        $this->visible = $visible;
+    }
+
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getPhoneNumber(): string
+    {
+        return $this->phoneNumber;
+    }
+
+    public function setPhoneNumber(string $phoneNumber): void
+    {
+        $this->phoneNumber = $phoneNumber;
+    }
+
+    public function getEMailAddress(): string
+    {
+        return $this->eMailAddress;
+    }
+
+    public function setEMailAddress(string $eMailAddress): void
+    {
+        $this->eMailAddress = $eMailAddress;
+    }
+
+    public function getText(): string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): void
+    {
+        $this->text = $text;
+    }
+
+    public function isVisible(): bool
+    {
+        return $this->visible;
+    }
+
+    public function setVisible(bool $visible): void
+    {
+        $this->visible = $visible;
+    }
+}
