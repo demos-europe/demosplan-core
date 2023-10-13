16a488287c7f9b420effe3ef2dc704c9e62b0511
Lamia Gülsah Elmas <elmas@demos-deutschland.de>
2023-10-13 18:00:53 +0200

    Feat(refs:34504): change the css and reduce code, code beautify

--- client/js/components/faq/DpFaqSupport.vue
+++ client/js/components/faq/DpFaqSupport.vue
@@ -10,38 +10,38 @@ All rights reserved
 <template>
   <div
     id="scFaq"
-    class="c-support-wrapper space-inset-m">
-    <h2 class="font-normal">
+    class="c-support space-inset-m color--black bg-color--blue-light-3">
+    <h2 class="font-normal color--black">
       {{ Translator.trans('support.heading') }}
     </h2>
-    <p class="mb-5">
+    <p class="u-mt-0_75">
       {{ Translator.trans('support.introduction') }}
     </p>
     <p>
       {{ Translator.trans('support.contact.advice') }}
     </p>
-    <h3 class="mt-5">
+    <h3 class="u-mt-0_75">
       {{ Translator.trans('support.content') }}
     </h3>
     <ul
-      class="mb-5"
+      class="u-mb-0_75"
       :class="contactLength === 1 ? '' : 'grid lg:grid-cols-3 gap-3'">
       <li
         v-for="contact in contacts"
         :v-key="contact.id"
-        class="space-inset-m h-48 c-support-card"
+        class="space-inset-m c-support__card color--black bg-color--white"
         :class="contactLength === 1 ? 'lg:w-8/12' : ''">
         <dp-faq-support-card
-            :title="contact.attributes.title"
-            :email="contact.attributes.eMailAddress"
-            :phone-number="contact.attributes.phoneNumber"
-            :reachability="contact.attributes.text" />
+          :title="contact.attributes.title"
+          :email="contact.attributes.eMailAddress"
+          :phone-number="contact.attributes.phoneNumber"
+          :reachability="contact.attributes.text" />
       </li>
     </ul>
     <h3>
       {{ Translator.trans('support.technical') }}
     </h3>
-    <div class="lg:w-8/12 space-inset-m pt-0 h-48 c-support-card">
+    <div class="lg:w-8/12 space-inset-m u-pv-0 c-support__card color--black bg-color--white">
       <dp-faq-support-card />
     </div>
   </div>
