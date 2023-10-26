0145e191123d68e4b87838ed95248cd724f41c7c
Moritz Mandler <mandler@demos-deutschland.de>
2023-10-26 12:23:03 +0200

    feature (refs T34604): started work on migration for diplanrog faqs

--- demosplan/DemosPlanCoreBundle/DoctrineMigrations/2023/10/Version20231023103423.php
+++ demosplan/DemosPlanCoreBundle/DoctrineMigrations/2023/10/Version20231023103423.php
@@ -9,6 +9,9 @@ use Doctrine\Migrations\AbstractMigration;
 
 class Version20231023103423 extends AbstractMigration
 {
+    private const CHANGE_WAS_IST_DIPLANBETEILIGUNG_OLD = '<p>DiPlanBeteiligung ist ein Service, der Ihnen die digitale Beteiligung an Planungen, insbesondere im Bauwesen (aktuell Bauleitplanung, später Landesplanung und Planfeststellung), einfach und effizient ermöglicht. Sie nehmen entweder als Bürger*in, Unternehmen oder als Mitarbeiter*in einer Behörde bzw. eines Träger öffentlicher Belange (TöB) an diesem Verfahren teil. Abhängig von Ihrer Rolle stehen Ihnen unterschiedliche Möglichkeiten zur Verfügung, um Ihre Stellungnahmen einzubringen.</p>';
+    private const CHANGE_WAS_IST_DIPLANBETEILIGUNG_NEW = '<p>DiPlanBeteiligung ist ein Service, der Ihnen die digitale Beteiligung an Planungen, insbesondere im Bauwesen (Bauleitplanung, Raumordnung und Planfeststellung), einfach und effizient ermöglicht. Sie nehmen entweder als Bürger*in, Unternehmen oder als Mitarbeiter*in einer Behörde bzw. eines Trägers öffentlicher Belange (TöB) an diesem Verfahren teil. Abhängig von Ihrer Rolle stehen Ihnen unterschiedliche Möglichkeiten zur Verfügung, um Ihre Stellungnahmen einzubringen.</p>';
+
     public function getDescription(): string
     {
         return 'refs T34604: Adjust platform faqs for diplanrog';
@@ -19,7 +22,28 @@ class Version20231023103423 extends AbstractMigration
      */
     public function up(Schema $schema): void
     {
+        $roleIds = [
+            'FP' => [
+                'RMOPSD' => $this->connection->fetchOne('SELECT _r_id FROM _role WHERE _r_code = "RMOPSD";'),
+                'RMOPSA' => $this->connection->fetchOne('SELECT _r_id FROM _role WHERE _r_code = "RMOPSA";'),
+                'RMOPFB' => $this->connection->fetchOne('SELECT _r_id FROM _role WHERE _r_code = "RMOPFB";'),
+                'RMOPPO' => $this->connection->fetchOne('SELECT _r_id FROM _role WHERE _r_code = "RMOPPO";'),
+            ],
+            'Institutions' => [
+                'RPSOCO' => $this->connection->fetchOne('SELECT _r_id FROM _role WHERE _r_code = "RPSOCO";'),
+                'RPSODE' => $this->connection->fetchOne('SELECT _r_id FROM _role WHERE _r_code = "RPSODE";'),
+            ],
+            'public' => [
+                'RGUEST' => $this->connection->fetchOne('SELECT _r_id FROM _role WHERE _r_code = "RGUEST";'),
+                'RCITIZ' => $this->connection->fetchOne('SELECT _r_id FROM _role WHERE _r_code = "RCITIZ";'),
+            ],
+        ];
+
         $this->abortIfNotMysql();
+        $this->addSql(
+            'UPDATE platform_faq SET text = :newText WHERE title = :title',
+            ['title' => 'Was ist DiPlanBeteiligung?', 'newText' => self::CHANGE_WAS_IST_DIPLANBETEILIGUNG_NEW]
+        );
 
 
 
@@ -31,7 +55,10 @@ class Version20231023103423 extends AbstractMigration
     public function down(Schema $schema): void
     {
         $this->abortIfNotMysql();
-
+        $this->addSql(
+            'UPDATE platform_faq SET text = :newText WHERE title = :title',
+            ['title' => 'Was ist DiPlanBeteiligung?', 'newText' => self::CHANGE_WAS_IST_DIPLANBETEILIGUNG_OLD]
+        );
 
     }
 
