577f3bb32a830305d26f7d9dc81c78a55780447d
Demos-CI <devops-system+github@demos-deutschland.de>
2023-11-15 16:02:52 +0000

    style: Apply php-cs-fixer

--- demosplan/DemosPlanCoreBundle/Logic/Document/ElementsService.php
+++ demosplan/DemosPlanCoreBundle/Logic/Document/ElementsService.php
@@ -397,8 +397,6 @@ class ElementsService extends CoreService implements ElementsServiceInterface
     /**
      * Fügt ein Element hinzu.
      *
-     * @return array
-     *
      * @throws Exception
      */
     public function addElement(array $data): ?array
@@ -645,8 +643,6 @@ class ElementsService extends CoreService implements ElementsServiceInterface
 
     /**
      * Convert datetime element array.
-     *
-     * @return mixed
      */
     protected function convertDateTime(array $element)
     {
@@ -789,10 +785,6 @@ class ElementsService extends CoreService implements ElementsServiceInterface
 
     /**
      * Kopiert alle Elements (Planunterlagenkategorien) von einem Verfahren in ein anderes.
-     *
-     * @param string $destinationProcedureId
-     *
-     * @return array
      *
      * @throws Exception
      */
