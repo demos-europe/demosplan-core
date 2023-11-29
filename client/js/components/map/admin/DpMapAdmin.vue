cfd489c9c7e44f1232969c5ba400a4ca140f5fbb
Hans Spieß <spiess@demos-deutschland.de>
2023-11-29 17:54:23 +0100

    fix(refs T35630): WIP add slider component
    the behavior which triggers a bug may best be caught by guiding
    users to not produce weird scale selections, which is best accomplished
    with a range slider component. this way, users can only limit
    the range edges.
    
    vue-slider-component has a solid user base, gh stars, is actively maintained
    and has a roadmap for vue3.

--- client/js/components/map/admin/DpMapAdmin.vue
+++ client/js/components/map/admin/DpMapAdmin.vue
@@ -11,6 +11,7 @@
 import { DpMultiselect, DpTooltipIcon } from '@demos-europe/demosplan-ui'
 import DpMapView from '@DpJs/components/map/map/DpMapView'
 import DpOlMap from '@DpJs/components/map/map/DpOlMap'
+import VueSlider from 'vue-slider-component'
 
 export default {
   name: 'DpMapAdmin',
@@ -19,10 +20,17 @@ export default {
     DpOlMap,
     DpMapView,
     DpMultiselect,
-    DpTooltipIcon
+    DpTooltipIcon,
+    VueSlider
   },
 
   props: {
+    availableScales: {
+      type: Array,
+      required: false,
+      default: () => []
+    },
+
     selectedScales: {
       type: Array,
       required: false,
@@ -32,13 +40,37 @@ export default {
 
   data () {
     return {
-      scales: this.selectedScales
+      scales: this.selectedScales,
+      scaleValue: ['250', '35000']
+    }
+  },
+
+  computed: {
+    maxScale () {
+      return 10
     }
   },
 
   methods: {
     sortSelected (type) {
       this[type].sort((a, b) => (parseInt(a.value) > parseInt(b.value)) ? 1 : ((parseInt(b.value) > parseInt(a.value)) ? -1 : 0))
+    },
+    show (e) {
+      console.log(e)
+    },
+    marks (val) {
+      return {
+        label: this.availableScales.find(scale => scale.value === val).label,
+        labelStyle: {
+          transform: 'translateX(-4px) translateY(-30px) rotate(315deg)',
+          position: 'relative',
+          marginTop: '0'
+        }
+      }
+    },
+    tooltipFormatter (v, b) {
+      console.log(v, b)
+      return v
     }
   },
 
