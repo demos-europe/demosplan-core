de987ea1f75a192cf0d06de51fab393fd695589d
Hamza Bakri <bakri@demos-deutschland.de>
2023-11-29 13:58:41 +0100

    f T35528 add migration to adjust the email address property
    Signed-off-by: Hamza Bakri <bakri@demos-deutschland.de>

---
+++ demosplan/DemosPlanCoreBundle/DoctrineMigrations/2023/11/Version20231129120204.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types = 1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Platforms\MySQLPlatform;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+class Version20231129120204 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'refs T: the email address property have to be of type a string, not any more a relation to EmailAddress and with no unique constraint.';
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function up(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+        $this->addSql('TRUNCATE support_contact');
+        $this->addSql('ALTER TABLE support_contact DROP FOREIGN KEY FK_8C8C0928B08E074E');
+        $this->addSql('DROP INDEX IDX_8C8C0928B08E074E ON support_contact');
+        $this->addSql('ALTER TABLE support_contact CHANGE email_address email_address VARCHAR(255)');
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function down(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+        $this->addSql('ALTER TABLE support_contact CHANGE email_address email_address CHAR(36) DEFAULT NULL');
+        $this->addSql('ALTER TABLE support_contact ADD CONSTRAINT FK_8C8C0928B08E074E FOREIGN KEY (email_address) REFERENCES email_address (id)');
+        $this->addSql('CREATE INDEX IDX_8C8C0928B08E074E ON support_contact (email_address)');
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function abortIfNotMysql(): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof MySqlPlatform,
+            "Migration can only be executed safely on 'mysql'."
+        );
+    }
+}
