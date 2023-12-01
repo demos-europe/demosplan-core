a38a5e473e589c33ebcebe812113dd2b677231f5
breierDemos <breier@demos-deutschland.de>
2023-12-01 09:21:32 +0100

    fix (T33578): Use the right allowed-file-types "zip" for participation import.

--- client/js/components/procedure/AdministrationImport/ParticipationImport/ParticipationImport.vue
+++ client/js/components/procedure/AdministrationImport/ParticipationImport/ParticipationImport.vue
@@ -17,7 +17,7 @@ All rights reserved
       method="post"
       enctype="multipart/form-data">
       <dp-upload-files
-        allowed-file-types="xls"
+        allowed-file-types="zip"
         :basic-auth="dplan.settings.basicAuth"
         :get-file-by-hash="hash => Routing.generate('core_file_procedure', { hash: hash, procedureId: procedureId })"
         :max-file-size="100 * 1024 * 1024/* 100 MiB */"
