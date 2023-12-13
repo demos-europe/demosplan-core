4796016cd8821196f67dd6b0442d9f6d002e2c43
Hamza Bakri <bakri@demos-deutschland.de>
2023-12-13 14:20:51 +0100

    f T35519 add method description
    Signed-off-by: Hamza Bakri <bakri@demos-deutschland.de>

--- demosplan/DemosPlanCoreBundle/Event/GetEmailIdsEvent.php
+++ demosplan/DemosPlanCoreBundle/Event/GetEmailIdsEvent.php
@@ -15,6 +15,9 @@ namespace demosplan\DemosPlanCoreBundle\Event;
 use DemosEurope\DemosplanAddon\Contracts\Events\GetEmailIdsEventInterface;
 use Symfony\Contracts\EventDispatcher\Event;
 
+/**
+ * This event will be catched and handled in the addons. It allows to retrieve emails ids used by the addons.
+ */
 class GetEmailIdsEvent extends Event implements GetEmailIdsEventInterface
 {
     private array $emailIds;
