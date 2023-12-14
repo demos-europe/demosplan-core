244ced648db5a1371496fc953f71ec4e2ac7c1d6
Hamza Bakri <bakri@demos-deutschland.de>
2023-12-14 09:20:15 +0100

    f T35519 adjust method description
    Signed-off-by: Hamza Bakri <bakri@demos-deutschland.de>

--- demosplan/DemosPlanCoreBundle/Repository/EmailAddressRepository.php
+++ demosplan/DemosPlanCoreBundle/Repository/EmailAddressRepository.php
@@ -55,6 +55,9 @@ class EmailAddressRepository extends FluentRepository implements EmailAddressRep
     /**
      * Checks if any EmailAddress entities are not referenced anymore and if so deletes them.
      *
+     * The parameter $emailIds contain emails ids which used in one or many addons and which have to be
+     * not removed while deleting orphan emails.
+     *
      * @return int the number of deletions
      */
     public function deleteOrphanEmailAddresses(array $emailIds): int
