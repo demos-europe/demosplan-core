e386a5a0cb90af3bd2186e546159d1aabd1a0910
Christian Dressler <dressler@demos-deutschland.de>
2024-01-04 14:32:40 +0100

    refactor: move method docblock into parent interface

--- demosplan/DemosPlanCoreBundle/Repository/EmailAddressRepository.php
+++ demosplan/DemosPlanCoreBundle/Repository/EmailAddressRepository.php
@@ -55,14 +55,6 @@ class EmailAddressRepository extends CoreRepository implements EmailAddressRepos
         return $foundEmailAddressEntity;
     }
 
-    /**
-     * Checks if any EmailAddress entities are not referenced anymore and if so deletes them.
-     *
-     * The parameter $emailIds contain emails ids which used in one or many addons and which have to be
-     * not removed while deleting orphan emails.
-     *
-     * @return int the number of deletions
-     */
     public function deleteOrphanEmailAddresses(array $emailIds): int
     {
         $connection = $this->getEntityManager()->getConnection();
