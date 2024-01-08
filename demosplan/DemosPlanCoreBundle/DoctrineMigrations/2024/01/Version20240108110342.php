8f2385bdb55e721186becc815e3cec0c0d7d6b80
Demos-CI <devops-system+github@demos-deutschland.de>
2024-01-08 19:04:18 +0000

    style: Apply php-cs-fixer

--- demosplan/DemosPlanCoreBundle/DoctrineMigrations/2024/01/Version20240108110342.php
+++ demosplan/DemosPlanCoreBundle/DoctrineMigrations/2024/01/Version20240108110342.php
@@ -1,4 +1,14 @@
-<?php declare(strict_types = 1);
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
 
 namespace Application\Migrations;
 
@@ -52,10 +62,7 @@ class Version20240108110342 extends AbstractMigration
         $rejectedCustomerKommunes = $this->selectCustomerKommunes('rejected');
         $this->setCustomerRelations($rejectedCustomerKommunes);
 
-
-
-//        echo var_dump($this->customerKommunes);
-
+        //        echo var_dump($this->customerKommunes);
     }
 
     /**
@@ -105,6 +112,7 @@ class Version20240108110342 extends AbstractMigration
             $customer['kommunes'] = $kommunes;
             $customerKommunes[] = $customer;
         }
+
         return $customerKommunes;
     }
 
