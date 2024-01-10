63af7df580916eacd6f5e5a2af104e09d0db3e37
Stefanie Grünberger <gruenberger@demos-deutschland.de>
2024-01-10 10:26:25 +0100

    bug(refs T35838): Check for overlapping segments
    If we get overlapping segments from data, delete all segments to prevent breaking the site.

--- client/js/store/procedure/SplitStatementStore.js
+++ client/js/store/procedure/SplitStatementStore.js
@@ -218,11 +218,28 @@ const SplitStatementStore = {
             return []
           }
           const initialData = data.data.attributes.segmentDraftList.data
-          const segments = initialData.attributes.segments
+          let segments = initialData.attributes.segments
             // Filter out segments with less than 10 characters as those may lead the frontend to crash
             // (because often that are closing or opening tags)
             // and should probably not be needed in a real world scenario.
             .filter(segment => (segment.charEnd - segment.charStart) > 10)
+
+          // Check for overlapping semgents, because these cause errors.
+          // If we get faulty data, delete all segments
+          for (let i = 0; i < segments.length; i++) {
+            for (let j = i + 1; j < segments.length; j++) {
+              // Check for overlap
+              if (
+                (segments[i].charStart >= segments[j].charStart && segments[i].charStart <= segments[j].charEnd) ||
+                (segments[j].charStart >= segments[i].charStart && segments[j].charStart <= segments[i].charEnd)
+              ) {
+                // Overlapping segments found
+                segments = []
+                dplan.notify.notify('error', Translator.trans('error.split_statement.segments'))
+              }
+            }
+          }
+
 
           commit('setProperty', { prop: 'initialData', val: initialData })
           commit('setProperty', { prop: 'initialSegments', val: segments })
