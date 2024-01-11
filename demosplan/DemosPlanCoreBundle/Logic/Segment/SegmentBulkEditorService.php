1c5feff5e113105b4a00e6fdd7772e85b6c57b9c
Catalina Sierra Zamudio <sierra-zamudio@demos-deutschland.de>
2024-01-11 17:24:35 +0100

    feat (refs T31120): Move business logic to Service

--- demosplan/DemosPlanCoreBundle/Logic/Segment/SegmentBulkEditorService.php
+++ demosplan/DemosPlanCoreBundle/Logic/Segment/SegmentBulkEditorService.php
@@ -6,13 +6,16 @@ declare(strict_types=1);
 namespace demosplan\DemosPlanCoreBundle\Logic\Segment;
 use demosplan\DemosPlanCoreBundle\Entity\Statement\Segment;
 use demosplan\DemosPlanCoreBundle\Entity\User\User;
+use demosplan\DemosPlanCoreBundle\EntityValidator\SegmentValidator;
+use demosplan\DemosPlanCoreBundle\Exception\InvalidArgumentException;
 use demosplan\DemosPlanCoreBundle\Exception\UserNotFoundException;
+use demosplan\DemosPlanCoreBundle\Logic\Segment\Handler\SegmentHandler;
 use demosplan\DemosPlanCoreBundle\Logic\User\UserHandler;
 
 
 class SegmentBulkEditorService
 {
-    public function __construct(protected UserHandler $userHandler
+    public function __construct(protected UserHandler $userHandler, protected SegmentHandler $segmentHandler, protected SegmentValidator $segmentValidator
 
     ) {
 
@@ -57,6 +60,26 @@ class SegmentBulkEditorService
 
         return $user;
 
+    }
+
+    /**
+     * Given an array of segment ids and a procedureId returns the corresponding list of
+     * segment entities, validating that every id finds a match in a Segment and that they all
+     * belong to the procedure.
+     *
+     * @param array<int, string> $segmentIds
+     * @param string             $procedureId
+     *
+     * @return array<int, Segment>
+     *
+     * @throws InvalidArgumentException
+     */
+    public function getValidSegments(array $segmentIds, $procedureId): array
+    {
+        $segments = $this->segmentHandler->findByIds($segmentIds);
+        $this->segmentValidator->validateSegments($segmentIds, $segments, $procedureId);
+
+        return $segments;
     }
 
 
