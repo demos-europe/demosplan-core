c207d4660dadd45964fdf37d7d08fb9938f73aba
Demos-CI <devops-system+github@demos-deutschland.de>
2024-01-29 16:45:02 +0000

    style: Apply php-cs-fixer

--- demosplan/DemosPlanCoreBundle/Logic/Customer/CustomerDeleter.php
+++ demosplan/DemosPlanCoreBundle/Logic/Customer/CustomerDeleter.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
 namespace demosplan\DemosPlanCoreBundle\Logic\Customer;
 
 use demosplan\DemosPlanCoreBundle\Logic\CoreService;
@@ -18,7 +26,5 @@ class CustomerDeleter extends CoreService
 
     public function deleteCustomer(string $customerId): void
     {
-
     }
-
 }
