50f6dd9057f23b1b119497a6dd7036e29260fcf7
Natalija Sakuta <sakuta@demos-deutschland.de>
2024-02-15 12:54:40 +0100

    ref(T34504): .length property works on arrays, not on objects.  so length returns undefined. Convert object into array first with Object.keys(), to have the possibility to get length

--- client/js/components/support/DpSupport.vue
+++ client/js/components/support/DpSupport.vue
@@ -19,18 +19,18 @@ All rights reserved
       {{ Translator.trans('support.contact.advice') }}
     </p>
     <h3
-      v-if="contacts.length > 0"
+      v-if="Object.keys(contacts).length > 0"
       class="u-mt-0_75">
       {{ Translator.trans('support') }}
     </h3>
     <ul
       class="u-mb-0_75"
-      :class="{ 'grid lg:grid-cols-3 gap-3': contacts.length !== 1 }">
+      :class="{ 'grid lg:grid-cols-3 gap-3': Object.keys(contacts).length !== 1 }">
       <li
         v-for="contact in contacts"
         :key="contact.id"
         class="bg-color--white"
-        :class="{ 'lg:w-8/12': contacts.length === 1 }">
+        :class="{ 'lg:w-8/12': Object.keys(contacts).length === 1 }">
         <dp-support-card
           :title="contact.attributes.title"
           :email="contact.attributes.eMailAddress"
@@ -115,6 +115,8 @@ export default {
 
   mounted () {
     this.fetchCustomerContactsData()
+    console.log('contacts:', this.contacts)
+    setTimeout(() => console.log('contacts:', this.contacts), 1000)
   }
 }
 </script>
