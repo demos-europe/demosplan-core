a3f8036a5f6e2d4d0e05caa0140b1ffcd8182a86
Catalina Sierra Zamudio <sierra-zamudio@demos-deutschland.de>
2024-02-21 12:26:16 +0100

    bug (ref T17533) Set print to 1 by default for basemap gislayer. Update existing blueprints (created by user as well as master ones) to have print as 1 on their corresponding gislayers

--- demosplan/DemosPlanCoreBundle/DataFixtures/ORM/ProdData/LoadProcedureData.php
+++ demosplan/DemosPlanCoreBundle/DataFixtures/ORM/ProdData/LoadProcedureData.php
@@ -93,6 +93,7 @@ class LoadProcedureData extends ProdFixture implements DependentFixtureInterface
         $gisLayer->setUrl('https://sgx.geodatenzentrum.de/wms_basemapde');
         $gisLayer->setLayers('de_basemapde_web_raster_farbe');
         $gisLayer->setType('base');
+        $gisLayer->setPrint(1);
         $gisLayer->setEnabled(true);
         $gisLayer->setProcedureId($procedureMaster->getId());
         $gisLayer->setCategory($gisLayerCategoryMaster);
---
+++ demosplan/DemosPlanCoreBundle/DoctrineMigrations/2024/02/Version20240220142305.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+
+final class Version20240220142305 extends AbstractMigration
+{
+    private const BASEMAP_GIS_URL = 'https://sgx.geodatenzentrum.de/wms_basemapde%';
+    private const BASEMAP_GIS_NAME = 'basemap';
+
+    public function getDescription(): string
+    {
+        return 'refs T17533: Update _g_print setting for gislayer (which use basemap) of blueprints.
+        - Gislayer of blueprints having this criteria are updated:
+
+        a) default masterblueprint
+        b) blueprints created by users
+        c) masterblueprints set by the user (which replace the default masterblueprint)';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+        // Update gis layers where its procedure is a master template
+        $this->addSql(
+            'UPDATE _gis as g
+        INNER JOIN _procedure AS p ON g._p_id = p._p_id
+        SET g._g_print = 1
+        WHERE g._g_url like :basemapGisUrl
+        AND g._g_name = :basemapGisName
+        AND (p._p_id IN (SELECT _procedure FROM customer) OR p.is_master = 1 OR p._p_master = 1)',
+            [
+                'basemapGisUrl' => self::BASEMAP_GIS_URL,
+                'basemapGisName' => self::BASEMAP_GIS_NAME,
+            ]
+        );
+
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function down(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+
+        // This migration is not reversible because:
+        // we can't be sure the _g_print flag was already set to 1
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function abortIfNotMysql(): void
+    {
+        $this->abortIf(
+            'mysql' !== $this->connection->getDatabasePlatform()->getName(),
+            "Migration can only be executed safely on 'mysql'."
+        );
+    }
+}
