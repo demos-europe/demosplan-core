39c9072480a9a9a4f17d67b62614beef302402dd
Demos-CI <devops-system+github@demos-deutschland.de>
2024-02-27 09:18:48 +0000

    style: Apply php-cs-fixer

--- demosplan/DemosPlanCoreBundle/Controller/Statement/DemosPlanAssessmentController.php
+++ demosplan/DemosPlanCoreBundle/Controller/Statement/DemosPlanAssessmentController.php
@@ -454,8 +454,6 @@ class DemosPlanAssessmentController extends BaseController
 
     /**
      * Save Filter params from Request to keep Filters between page loads e.g pager.
-     *
-     * @return mixed
      */
     protected function rememberFilters(Request $request)
     {
