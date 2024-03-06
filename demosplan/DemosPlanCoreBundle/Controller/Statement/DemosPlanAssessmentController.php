1dd5722f5a90714495ecd76eeaf50d5ef363349f
Saeed Jalalifar <jalalifar@demos-deutschland.de>
2024-03-06 09:10:36 +0100

    feature (ref 36690): Adjustment Link

--- demosplan/DemosPlanCoreBundle/Controller/Statement/DemosPlanAssessmentController.php
+++ demosplan/DemosPlanCoreBundle/Controller/Statement/DemosPlanAssessmentController.php
@@ -194,11 +194,10 @@ class DemosPlanAssessmentController extends BaseController
                 }
                 $filterSet = $assessmentHandler->handleFilterHash($request, $procedureId);
                 $routeName = $currentUser->getUser()->hasRole(Role::PROCEDURE_DATA_INPUT) ?
-                    'DemosPlan_statement_single_view' : 'dplan_assessmenttable_view_table';
-                $routeParameters = 'DemosPlan_statement_single_view' === $routeName ?
+                    'DemosPlan_statement_orga_list' : 'dplan_assessmenttable_view_table';
+                $routeParameters = $routeName === 'DemosPlan_statement_single_view' ?
                     [
-                        'procedureId' => $procedureId,
-                        'statementId' => $newStatement->getId(),
+                        'procedureId' => $procedureId
                     ] :
                     [
                         'procedureId' => $procedureId,
