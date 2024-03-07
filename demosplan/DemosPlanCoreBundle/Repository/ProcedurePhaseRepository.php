71497feadf97c821dca871ff9a0a1df3526a031f
Alexander Marten <marten@demos-deutschland.de>
2024-03-07 16:07:53 +0100

    feat (refs T36340): WIP: add missing repo for newly created ProcedurePhase entity

---
+++ demosplan/DemosPlanCoreBundle/Repository/ProcedurePhaseRepository.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+
+namespace demosplan\DemosPlanCoreBundle\Repository;
+
+
+use DemosEurope\DemosplanAddon\Logic\ApiRequest\FluentRepository;
+
+class ProcedurePhaseRepository  extends FluentRepository
+{
+
+}
