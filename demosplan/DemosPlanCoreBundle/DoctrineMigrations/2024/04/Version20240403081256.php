70c63a8a29c725882eebbca8126bf25f3c0a2dd0
Alexander Marten <marten@demos-deutschland.de>
2024-04-03 10:47:54 +0200

    feat (refs T36340): Allow storing phase-iteration to the statement entity

---
+++ demosplan/DemosPlanCoreBundle/DoctrineMigrations/2024/04/Version20240403081256.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types = 1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Platforms\MySQLPlatform;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+class Version20240403081256 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'refs T36340: Allow iteration of each phase by adding a counter.
+         Step6: Add phase-counter to statement entity.';
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function up(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+        $this->addSql(' ALTER TABLE _statement ADD phase_iteration SMALLINT UNSIGNED DEFAULT 1 NOT NULL');
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function down(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+        $this->addSql('ALTER TABLE _statement DROP phase_iteration');
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function abortIfNotMysql(): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof MySqlPlatform,
+            "Migration can only be executed safely on 'mysql'."
+        );
+    }
+}
