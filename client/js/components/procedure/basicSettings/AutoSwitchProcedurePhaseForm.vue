5ebad1b1f207b36238d65b20da712d866856a9e2
Florian Salis <salis@demos-deutschland.de>
2024-04-10 12:43:35 +0200

    fix(refs DPLAN-11403, DPLAN-11402): Rollback unwanted changes

--- client/js/components/procedure/basicSettings/AutoSwitchProcedurePhaseForm.vue
+++ client/js/components/procedure/basicSettings/AutoSwitchProcedurePhaseForm.vue
@@ -64,7 +64,10 @@
             enforce-plausible-dates
             :min-date="startDate"
             required
-^
+            :data-cy="{
+                endDate: dataCyEndDate,
+                startDate: dataCyStartDate
+            }"
             start-disabled
             :start-id="startDateId"
             :start-name="startDateId"
