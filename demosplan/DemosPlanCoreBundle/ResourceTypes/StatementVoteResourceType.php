341c03c080e92b44bd0074ba93426b9c22d90b8f
Catalina Sierra Zamudio <sierra-zamudio@demos-deutschland.de>
2024-04-10 10:02:42 +0200

    feat (ref T11359) Add StatementVoteResourceType

---
+++ demosplan/DemosPlanCoreBundle/ResourceTypes/StatementVoteResourceType.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
+namespace demosplan\DemosPlanCoreBundle\ResourceTypes;
+
+use DemosEurope\DemosplanAddon\EntityPath\Paths;
+use demosplan\DemosPlanCoreBundle\Entity\Statement\Statement;
+use demosplan\DemosPlanCoreBundle\Entity\Statement\StatementVote;
+use demosplan\DemosPlanCoreBundle\Entity\Statement\Tag;
+use demosplan\DemosPlanCoreBundle\Logic\ApiRequest\ResourceType\DplanResourceType;
+use EDT\JsonApi\ResourceConfig\Builder\ResourceConfigBuilderInterface;
+use EDT\PathBuilding\End;
+
+
+/**
+ * @template-extends DplanResourceType<Tag>
+ *
+ * @property-read StatementResourceType $statement
+ * @property-read UserResourceType $user
+ * @property-read End $firstname
+ * @property-read End $lastname
+ * @property-read End $active
+ * @property-read End $deleted
+ *
+ *
+ */
+final class StatementVoteResourceType extends DplanResourceType
+{
+    protected function getProperties(): array
+    {
+        return [
+            $this->createIdentifier()->readable()->sortable()->filterable(),
+            $this->createAttribute($this->firstname)->readable(),
+            $this->createToOneRelationship($this->user)->readable(),
+        ];
+    }
+
+    protected function getAccessConditions(): array
+    {
+        $currentProcedure = $this->currentProcedureService->getProcedure();
+        if (null === $currentProcedure) {
+            return [$this->conditionFactory->false()];
+        }
+
+        $procedureId = $currentProcedure->getId();
+        $procedureCondition = $this->conditionFactory->propertyHasValue($procedureId, Paths::statementVote()->statement->procedure->id);
+
+
+
+
+        return [$procedureCondition];
+
+    }
+
+    public static function getName(): string
+    {
+        return 'StatementVote';
+    }
+
+    public function getEntityClass(): string
+    {
+        return StatementVote::class;
+    }
+
+    public function isAvailable(): bool
+    {
+        //return false;
+        return $this->currentUser->hasPermission('field_statement_votes') && null !== $this->currentProcedureService->getProcedure();
+    }
+}
