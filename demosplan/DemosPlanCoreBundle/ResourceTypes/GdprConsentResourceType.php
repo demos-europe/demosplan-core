efb0d544a721deed88abc5ec5f6b2d121a21a7f8
Catalina Sierra Zamudio <sierra-zamudio@demos-deutschland.de>
2024-04-17 16:05:11 +0200

    feat (ref T11359) Disable GET and LIST request for GdprConsentResourceType (it is only reachable through include)

--- demosplan/DemosPlanCoreBundle/ResourceTypes/GdprConsentResourceType.php
+++ demosplan/DemosPlanCoreBundle/ResourceTypes/GdprConsentResourceType.php
@@ -60,4 +60,15 @@ final class GdprConsentResourceType extends DplanResourceType
     {
         return null !== $this->currentProcedureService->getProcedure();
     }
+
+    public function isGetAllowed(): bool
+    {
+        return false;
+    }
+
+    public function isListAllowed(): bool
+    {
+        return false;
+    }
+
 }
