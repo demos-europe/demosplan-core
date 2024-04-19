91cb2657bd00b41cfdf42c7eadc0a740721ff008
Sami Mussbach <mussbach@demos-deutschland.de>
2024-04-19 16:21:17 +0200

    chore: set project migrations as migrated on container setup
    project migrations needs to be set as migrated when a project is
    newly build from fixtures. The database is set up from scratch
    and migrations will not work and will do more harm than help

--- demosplan/DemosPlanCoreBundle/Command/ContainerInitCommand.php
+++ demosplan/DemosPlanCoreBundle/Command/ContainerInitCommand.php
@@ -38,7 +38,7 @@ class ContainerInitCommand extends CoreCommand
     private const OPTION_CUSTOMER_CONFIG = 'customerConfig';
 
     protected static $defaultName = 'dplan:container:init';
-    protected static $defaultDescription = 'Perform startup tasks as an init container in kubernetes setup';
+    protected static $defaultDescription = 'Perform startup tasks that may be used e.g. as an init container in kubernetes setup';
 
     public function __construct(
         private readonly EntityManagerInterface $entityManager,
@@ -57,7 +57,7 @@ class ContainerInitCommand extends CoreCommand
         $this
            ->setHelp(
                <<<EOT
-Perform startup tasks as an init container in kubernetes setup. Usage:
+Perform startup tasks that may be used e.g. as an init container in kubernetes setup. Usage:
     php bin/<project> dplan:container:init
 EOT
            );
