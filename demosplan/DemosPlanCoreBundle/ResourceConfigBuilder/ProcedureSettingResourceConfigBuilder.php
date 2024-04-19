f91501099f7d9a96ec23c29a483dc0c61a58af5d
Catalina Sierra Zamudio <sierra-zamudio@demos-deutschland.de>
2024-04-19 14:09:17 +0200

    feat (ref DPLAN-11443) Expose ProcedureResourceType fields

---
+++ demosplan/DemosPlanCoreBundle/ResourceConfigBuilder/ProcedureSettingResourceConfigBuilder.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
+namespace demosplan\DemosPlanCoreBundle\ResourceConfigBuilder;
+
+use DemosEurope\DemosplanAddon\Contracts\Entities\ProcedureSettingsInterface;
+use EDT\DqlQuerying\Contracts\ClauseFunctionInterface;
+use EDT\JsonApi\PropertyConfig\Builder\AttributeConfigBuilderInterface;
+use EDT\JsonApi\ResourceConfig\Builder\MagicResourceConfigBuilder;
+
+/**
+ * @property-read AttributeConfigBuilderInterface<ClauseFunctionInterface<bool>, ProcedureSettingsInterface> $boundingBox
+ * @property-read AttributeConfigBuilderInterface<ClauseFunctionInterface<bool>, ProcedureSettingsInterface> $mapExtent
+ * @property-read AttributeConfigBuilderInterface<ClauseFunctionInterface<bool>, ProcedureSettingsInterface> $scales
+ * @property-read AttributeConfigBuilderInterface<ClauseFunctionInterface<bool>, ProcedureSettingsInterface> $informationUrl
+ * @property-read AttributeConfigBuilderInterface<ClauseFunctionInterface<bool>, ProcedureSettingsInterface> $copyright
+ */
+class ProcedureSettingResourceConfigBuilder extends MagicResourceConfigBuilder
+{
+}
--- demosplan/DemosPlanCoreBundle/ResourceTypes/ProcedureSettingsResourceType.php
+++ demosplan/DemosPlanCoreBundle/ResourceTypes/ProcedureSettingsResourceType.php
@@ -12,8 +12,11 @@ declare(strict_types=1);
 
 namespace demosplan\DemosPlanCoreBundle\ResourceTypes;
 
+use DemosEurope\DemosplanAddon\EntityPath\Paths;
 use demosplan\DemosPlanCoreBundle\Entity\Procedure\ProcedureSettings;
 use demosplan\DemosPlanCoreBundle\Logic\ApiRequest\ResourceType\DplanResourceType;
+use demosplan\DemosPlanCoreBundle\ResourceConfigBuilder\ProcedureSettingResourceConfigBuilder;
+use EDT\JsonApi\ResourceConfig\Builder\ResourceConfigBuilderInterface;
 use EDT\PathBuilding\End;
 
 /**
@@ -28,9 +31,24 @@ class ProcedureSettingsResourceType extends DplanResourceType
         return 'ProcedureSettings';
     }
 
-    protected function getProperties(): array
+    protected function getProperties(): ResourceConfigBuilderInterface
     {
-        return [];
+        $configBuilder = $this->getConfig(ProcedureSettingResourceConfigBuilder::class);
+        $configBuilder->id
+            ->readable();
+        $configBuilder->boundingBox
+            ->readable();
+        $configBuilder->mapExtent
+            ->readable();
+        $configBuilder->scales
+            ->readable();
+        $configBuilder->informationUrl
+            ->readable();
+        $configBuilder->copyright
+            ->readable();
+
+        return $configBuilder;
+
     }
 
     public function getEntityClass(): string
@@ -40,11 +58,31 @@ class ProcedureSettingsResourceType extends DplanResourceType
 
     public function isAvailable(): bool
     {
+        return null !== $this->currentProcedureService->getProcedure();
+    }
+
+    public function isGetAllowed(): bool
+    {
+        return false;
+    }
+
+    public function isListAllowed(): bool
+    {
         return false;
     }
 
     protected function getAccessConditions(): array
     {
-        return [];
+        $currentProcedure = $this->currentProcedureService->getProcedure();
+        if (null === $currentProcedure) {
+            return [$this->conditionFactory->false()];
+        }
+
+        $procedureId = $currentProcedure->getId();
+
+        return [
+            $this->conditionFactory->propertyHasValue($procedureId, Paths::procedureSettings()->procedure->id),
+            $this->conditionFactory->propertyHasValue(false, Paths::procedureSettings()->procedure->deleted),
+        ];
     }
 }
