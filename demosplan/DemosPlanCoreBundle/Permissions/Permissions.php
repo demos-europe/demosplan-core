4d6c726b8e0685e88888406b7aa84c675304f46b
Sami Mussbach <mussbach@demos-deutschland.de>
2024-04-22 16:36:30 +0200

    chore: set procedure permissions only once

--- demosplan/DemosPlanCoreBundle/Permissions/Permissions.php
+++ demosplan/DemosPlanCoreBundle/Permissions/Permissions.php
@@ -984,8 +984,6 @@ class Permissions implements PermissionsInterface, PermissionEvaluatorInterface
     {
         // Prüfe, ob der User ins Verfahren darf
         if (null !== $this->procedure) {
-            $this->setProcedurePermissions();
-
             $readPermission = $this->hasPermissionsetRead();
             $owns = $this->ownsProcedure();
             $apiUserMayAccess = $this->hasPermission('feature_procedure_api_access');
