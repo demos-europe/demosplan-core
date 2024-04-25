30ceafe9d2a63385c50a776199fc2d0d42fb7e00
sakutademos <110987766+sakutademos@users.noreply.github.com>
2024-04-25 11:07:09 +0200

    Feat(DPLAN-11460): adjust fullscreen mode for stn and segments list (#2979)
    * feat(DPLAN-3309):  adjust the fullscreen mode for the Abschnittsliste and the STN-Liste
    
    * feat(DPLAN-11460): move handleFullscreenMode() method into the mixin

--- client/js/components/statement/listStatements/ListStatements.vue
+++ client/js/components/statement/listStatements/ListStatements.vue
@@ -8,23 +8,34 @@
 </license>
 
 <template>
-  <div>
+  <div :class="{ 'top-0 left-0 w-full h-full fixed z-fixed bg-white': isFullscreen }">
     <!-- Header -->
     <dp-sticky-element
       border
-      class="py-2">
-      <div class="flex mb-2">
-        <search-modal
-          :search-in-fields="searchFields"
-          @search="(term, selectedFields) => applySearch(term, selectedFields)"
-          ref="searchModal" />
+      class="py-2"
+      :class="{ 'fixed top-0 left-0 w-full h-1/6 px-2': isFullscreen }">
+      <div class="flex items-center justify-between mb-2">
+        <div class="flex">
+          <search-modal
+            :search-in-fields="searchFields"
+            @search="(term, selectedFields) => applySearch(term, selectedFields)"
+            ref="searchModal" />
+          <dp-button
+            class="ml-2"
+            variant="outline"
+            data-cy="listStatements:searchReset"
+            :href="Routing.generate('dplan_procedure_statement_list', { procedureId: procedureId })"
+            :disabled="searchValue === ''"
+            :text="Translator.trans('search.reset')" />
+        </div>
         <dp-button
-          class="ml-auto"
+          data-cy="editorFullscreen"
+          :icon="isFullscreen ? 'compress' : 'expand'"
+          icon-size="medium"
+          hide-text
           variant="outline"
-          data-cy="listStatements:searchReset"
-          :href="Routing.generate('dplan_procedure_statement_list', { procedureId: procedureId })"
-          :disabled="searchValue === ''"
-          :text="Translator.trans('search.reset')" />
+          :text="isFullscreen ? Translator.trans('editor.fullscreen.close') : Translator.trans('editor.fullscreen')"
+          @click="handleFullscreenMode()" />
       </div>
       <dp-bulk-edit-header
         class="layout__item u-12-of-12 u-mt-0_5"
@@ -96,6 +107,7 @@
     <template v-if="!isLoading && items.length > 0">
       <dp-data-table
         data-cy="listStatements"
+        :class="{ 'px-2 overflow-y-scroll h-5/6': isFullscreen }"
         has-flyout
         :is-selectable="isSourceAndCoupledProcedure"
         :header-fields="headerFields"
@@ -370,6 +382,7 @@ export default {
         limits: [10, 25, 50, 100],
         perPage: 10
       },
+      isFullscreen: false,
       headerFields: [
         { field: 'externId', label: Translator.trans('id') },
         { field: 'internId', label: Translator.trans('internId.shortened'), colClass: 'w-8' },
@@ -860,6 +873,15 @@ export default {
           .catch(e => {
             console.error(e)
           })
+      }
+    },
+
+    handleFullscreenMode () {
+      this.isFullscreen = !this.isFullscreen
+      if (this.isFullscreen) {
+        document.querySelector('html').setAttribute('style', 'overflow: hidden')
+      } else {
+        document.querySelector('html').removeAttribute('style')
       }
     },
 
