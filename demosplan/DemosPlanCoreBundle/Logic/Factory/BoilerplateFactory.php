f89402479156821cb56280d9b832e0863ba5e091
Demos-CI <devops-system+github@demos-deutschland.de>
2024-05-28 14:31:37 +0000

    style: Apply php-cs-fixer

--- demosplan/DemosPlanCoreBundle/Logic/Factory/BoilerplateFactory.php
+++ demosplan/DemosPlanCoreBundle/Logic/Factory/BoilerplateFactory.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
 namespace demosplan\DemosPlanCoreBundle\Logic\Factory;
 
 use DemosEurope\DemosplanAddon\Contracts\Entities\BoilerplateInterface;
@@ -8,7 +16,7 @@ use demosplan\DemosPlanCoreBundle\Entity\Procedure\Boilerplate;
 
 class BoilerplateFactory implements BoilerplateFactoryInterface
 {
-    public function createBoilerplate() : BoilerplateInterface
+    public function createBoilerplate(): BoilerplateInterface
     {
         return new Boilerplate();
     }
