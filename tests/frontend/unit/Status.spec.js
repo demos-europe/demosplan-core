f2017d6c5410c830fe35475eeacbbfbd228199d1
Stefanie Grünberger <gruenberger@demos-deutschland.de>
2024-06-05 15:25:32 +0200

    test: Update jest test for Status

--- tests/frontend/unit/Status.spec.js
+++ tests/frontend/unit/Status.spec.js
@@ -7,8 +7,48 @@
  * All rights reserved
  */
 
+import shallowMountWithGlobalMocks from '@DpJs/VueConfigLocal'
 import Status from '@DpJs/components/statement/fragment/Status'
 
 describe('Status', () => {
+  let wrapper
 
+  beforeEach(() => {
+    wrapper = shallowMountWithGlobalMocks(Status, {
+      propsData: {
+        status: '',
+        fragmentId: 'testId',
+        archivedOrgaName: 'OrgaName',
+        archivedDepartmentName: 'DepartmentName',
+        voteAdvicePending: '',
+        badge: false,
+        tooltip: true,
+        transNone: 'fragment.voteAdvice.status.none',
+        transDone: 'fragment.voteAdvice.status.done'
+      }
+    })
+  })
+
+  it('should display question icon when status is empty', () => {
+    expect(wrapper.find('.fa-question').exists()).toBe(true)
+  })
+
+  it('should display check icon when status is not empty', async () => {
+    await wrapper.setProps({ status: 'done' })
+    expect(wrapper.find('.fa-check').exists()).toBe(true)
+  })
+
+  it('should display hourglass icon when voteAdvicePending is not empty', async () => {
+    await wrapper.setProps({ voteAdvicePending: 'pending' })
+    expect(wrapper.find('.fa-hourglass-half').exists()).toBe(true)
+  })
+
+  it('should display tooltip when tooltip prop is true', () => {
+    expect(wrapper.find('v-popover-stub').exists()).toBe(true)
+  })
+
+  it('should not display tooltip when tooltip prop is false', async () => {
+    await wrapper.setProps({ tooltip: false })
+    expect(wrapper.find('v-popover-stub').exists()).toBe(false)
+  })
 })
