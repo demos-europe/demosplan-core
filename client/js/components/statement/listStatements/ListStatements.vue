042c331415c0969d0f2b991ffdf2325d0c74edd2
Natalija Sakuta <sakuta@demos-deutschland.de>
2024-06-24 13:42:07 +0200

    feat(DPLAN-3268): rename the parameter

--- client/js/components/statement/listStatements/ListStatements.vue
+++ client/js/components/statement/listStatements/ListStatements.vue
@@ -445,7 +445,7 @@ export default {
         }
 
         if (docxHeaders) {
-          parameters['table-headers'] = { // ToDo: how should it be passed, discuss it with the BE
+          parameters.tableHeaders = {
             c_left: docxHeaders.left,
             c_middle: docxHeaders.middle,
             c_right: docxHeaders.right
