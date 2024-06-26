13a379a7e957f2baacfd0762879eca62fd5e43df
Natalija Sakuta <sakuta@demos-deutschland.de>
2024-06-26 15:15:28 +0200

    feat(DPLAN-3268): improve tests

--- tests/frontend/unit/StatementExportModal.spec.js
+++ tests/frontend/unit/StatementExportModal.spec.js
@@ -1,37 +1,35 @@
-import StatementExportModal from '@DpJs/components/statement/StatementExportModal'
-import { describe, beforeEach, afterEach, it, expect } from '@jest/globals'
+import { describe, beforeEach, afterEach, beforeAll, it, expect } from '@jest/globals'
+import { DpModal } from '@demos-europe/demosplan-ui'
+import { sessionStorageMock } from './__mocks__/sessionStorage.mock'
 import shallowMountWithGlobalMocks from '@DpJs/VueConfigLocal'
-import { shallowMount, mount } from '@vue/test-utils'
-import { DpModal, DpInput } from '@demos-europe/demosplan-ui'
-describe('StatementExportModal.vue', () => {
+import StatementExportModal from '@DpJs/components/statement/StatementExportModal'
+
+describe('StatementExportModal', () => {
+  let sessionStorageValue
   let wrapper
-  let mocks
-  let stubs
+
+  beforeAll(() => {
+    Object.defineProperty(window, 'sessionStorage', {
+      value: sessionStorageMock
+    })
+
+    sessionStorageValue = 'Stored Column Title'
+    window.sessionStorage.setItem('exportModal:docxCol:col1', JSON.stringify(sessionStorageValue))
+  })
 
   beforeEach(() => {
-    mocks = {
-      Translator: {
-        trans: jest.fn(key => key)
-      }
-    }
-
-    stubs = {
-      DpModal,
-      DpInput
-    }
-
-    wrapper = shallowMount(StatementExportModal, {
+    wrapper = shallowMountWithGlobalMocks(StatementExportModal, {
       propsData: {
         isSingleStatementExport: false
       },
-      stubs,
-      mocks,
-      data() {
-        return {
-          active: 'docx'
-        }
+      stubs: {
+        DpModal
       }
     })
+
+    const button = wrapper.find('[data-cy="exportModal:open"]')
+    button.trigger('click')
+    wrapper.vm.setInitialValues()
   })
 
   afterEach(() => {
@@ -39,82 +37,97 @@ describe('StatementExportModal.vue', () => {
   })
 
   it('opens the modal when the button is clicked', async () => {
-    const button = wrapper.find('[data-cy="exportModal:open"]')
-    await button.trigger('click')
     const modal = wrapper.findComponent({ name: 'DpModal' })
     expect(modal.isVisible()).toBe(true)
   })
 
+  it('sets the initial values correctly', () => {
+    expect(wrapper.vm.$data.active).toBe('docx')
+    expect(wrapper.vm.docxColumns.col1.title).toBe(sessionStorageValue)
+    expect(wrapper.vm.docxColumns.col2.title).toBe(null)
+    expect(wrapper.vm.docxColumns.col3.title).toBe(null)
+  })
 
   it('renders input fields when export type is docx or zip', async () => {
-    const button = wrapper.find('[data-cy="exportModal:open"]')
-    await button.trigger('click')
-    await wrapper.vm.$nextTick()
+    const exportTypes = ['docx', 'zip']
 
-    const inputs = wrapper.findAllComponents({ name: 'DpInput' })
-    expect(inputs.length).toBe(Object.keys(wrapper.vm.docxColumns).length)
+    exportTypes.map(async exportType => {
+      await wrapper.setData({ active: exportType })
+      const inputs = wrapper.findAllComponents({ name: 'DpInput' })
+
+      expect(inputs.length).toBe(Object.keys(wrapper.vm.docxColumns).length)
+    })
   })
 
   it('does not render input fields when export type is not docx or zip', async () => {
-    wrapper.setData({ active: 'xlsx' })
-    await wrapper.vm.$nextTick()
+    await wrapper.setData({ active: 'xlsx' })
+    const inputs = wrapper.findAllComponents({ name: 'DpInput' })
 
-    const button = wrapper.find('[data-cy="exportModal:open"]')
-    await button.trigger('click')
-
-    await wrapper.vm.$nextTick()
-
-    const inputs = wrapper.findAllComponents({ name: 'dp-input' })
     expect(inputs.length).toBe(0)
   })
 
-  it('handles export correctly 1', async () => {
-    wrapper.setData({
-      docxColumns: {
-        col1: { title: 'Title1' },
-        col2: { title: 'Title2' },
-        col3: { title: 'Title3' }
+  it('emits export event with initial column titles when no changes are made', () => {
+    const emitSpy = jest.spyOn(wrapper.vm, '$emit')
+    wrapper.vm.handleExport()
+
+    expect(emitSpy).toHaveBeenCalledWith('export', {
+      route: 'dplan_statement_segments_export',
+      docxHeaders: {
+        col1: sessionStorageValue,
+        col2: null,
+        col3: null
       }
     })
+  })
+
+  it('emits export event with updated col2 title', () => {
     const spy = jest.spyOn(wrapper.vm, '$emit')
-    await wrapper.vm.handleExport()
+    wrapper.setData({
+      ...wrapper.vm.docxColumns,
+      docxColumns: {
+        col2: { title: 'Test Column Title' }
+      }
+    })
+    wrapper.vm.handleExport()
 
     expect(spy).toHaveBeenCalledWith('export', {
       route: 'dplan_statement_segments_export',
       docxHeaders: {
-        col1: 'Title1',
-        col2: 'Title2',
-        col3: 'Title3'
+        col1: sessionStorageValue,
+        col2: 'Test Column Title',
+        col3: null
       }
     })
-
   })
 
-  it('closes window after the handleExport function', async () =>  {
-    const toggleSpy = jest.spyOn(wrapper.vm.$refs.exportModalInner, 'toggle')
+  it('emits export event with null docxHeaders for xlsx export type',  () => {
+    const emitSpy = jest.spyOn(wrapper.vm, '$emit')
+    wrapper.setData({ active: 'xlsx' })
+    wrapper.vm.handleExport()
 
-    // Call handleExport
-    await wrapper.vm.handleExport()
-
-    // Expect toggle method to have been called
-    expect(toggleSpy).toHaveBeenCalled()
-  })
-
-  it('handles export correctly 2', async () => {
-    wrapper.setData({
-      active: 'xlsx',
-      docxColumns: {
-        col1: { title: 'Title1' },
-        col2: { title: 'Title2' },
-        col3: { title: 'Title3' }
-      }
-    })
-    const spy = jest.spyOn(wrapper.vm, '$emit')
-    await wrapper.vm.handleExport()
-
-    expect(spy).toHaveBeenCalledWith('export', {
+    expect(emitSpy).toHaveBeenCalledWith('export', {
       route: 'dplan_statement_xls_export',
       docxHeaders: null
     })
+  })
+
+  it('closes the DpModal after executing the handleExport function', () =>  {
+    const toggleSpy = jest.spyOn(wrapper.vm.$refs.exportModalInner, 'toggle')
+    wrapper.vm.handleExport()
+
+    expect(toggleSpy).toHaveBeenCalled()
+  })
+
+  it('renders radio buttons when isSingleStatementExport is false', () => {
+    const radioButtons = wrapper.findAllComponents({ name: 'DpRadio' })
+
+    expect(radioButtons.length).toBe(Object.keys(wrapper.vm.exportTypes).length)
+  })
+
+  it('does not render radio buttons when isSingleStatementExport is true', async () => {
+    await wrapper.setProps({ isSingleStatementExport: true })
+    const radioButtons = wrapper.findAllComponents({ name: 'DpRadio' })
+
+    expect(radioButtons.length).toBe(0)
   })
 })
