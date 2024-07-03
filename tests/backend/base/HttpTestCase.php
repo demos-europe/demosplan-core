e5cfdf891c42da50f431ae4029f3bcef5fdbede8
Catalina Sierra Zamudio <sierra-zamudio@demos-deutschland.de>
2024-07-03 17:06:43 +0200

    f_test undo changes

--- tests/backend/base/HttpTestCase.php
+++ tests/backend/base/HttpTestCase.php
@@ -10,21 +10,12 @@
 
 namespace Tests\Base;
 
-use demosplan\DemosPlanCoreBundle\Entity\Procedure\Procedure;
-use demosplan\DemosPlanCoreBundle\Entity\User\User;
 use demosplan\DemosPlanCoreBundle\EventListener\SetHttpTestPermissionsListener;
-use Lexik\Bundle\JWTAuthenticationBundle\Security\Authentication\Token\JWTUserToken;
-use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTManager;
-use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 
 class HttpTestCase extends FunctionalTestCase
 {
-    public const JWT_AUTHORIZATION_HEADER = 'HTTP_X-JWT-Authorization';
-    public const DEMOSPLAN_PROCEDURE_ID_HEADER = 'HTTP_X_DEMOSPLAN_PROCEDURE_ID';
-
     protected ?KernelBrowser $client;
-    protected ?JWTManager $tokenManager;
 
     protected function setUp(): void
     {
@@ -32,7 +23,6 @@ class HttpTestCase extends FunctionalTestCase
         // the createClient() method cannot be used when kernel is booted
         static::ensureKernelShutdown();
         $this->client = static::createClient();
-        $this->tokenManager = $this->getContainer()->get(JWTTokenManagerInterface::class);
     }
 
     /**
@@ -42,36 +32,5 @@ class HttpTestCase extends FunctionalTestCase
     protected function enablePermissions(array $permissionsToEnable): void
     {
         $this->client->setServerParameter(SetHttpTestPermissionsListener::X_DPLAN_TEST_PERMISSIONS, implode(',', $permissionsToEnable));
-    }
-
-    /**
-     * This method is essential for simulating an authenticated user in tests.
-     * Initializes a user for authentication by creating a JWT token,
-     * setting it in a JWTUserToken, and then setting this token in the token storage.
-     */
-    protected function initializeUser(User $user): string
-    {
-        $token = $this->tokenManager->create($user);
-        $userToken = new JWTUserToken($user->getDplanRolesArray(), $user, $token);
-        $this->tokenStorage->setToken($userToken);
-
-        return $token;
-    }
-
-    /**
-     * Generates additional headers for HTTP requests, including JWT authorization and optional procedure ID.
-     *
-     * This method prepares the HTTP headers required for authenticated requests. It includes the JWT token for
-     * authorization and, if provided, the procedure ID to specify the context of the request.
-     */
-    protected function getAdditionalHeaders(string $jwtToken, ?Procedure $procedure): array
-    {
-        $headers[self::JWT_AUTHORIZATION_HEADER] = "Bearer $jwtToken";
-
-        if (null !== $procedure) {
-            $headers[self::DEMOSPLAN_PROCEDURE_ID_HEADER] = $procedure->getId();
-        }
-
-        return $headers;
     }
 }
