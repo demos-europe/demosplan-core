4fe1fa7d5903c512d17f7c1ae499c7cb045923c6
Demos-CI <devops-system+github@demos-deutschland.de>
2024-07-03 09:25:25 +0000

    style: Apply php-cs-fixer

--- tests/backend/integration/ProcedureMapSettingApiTest.php
+++ tests/backend/integration/ProcedureMapSettingApiTest.php
@@ -77,6 +77,5 @@ class ProcedureMapSettingApiTest extends HttpTestCase
         self::assertEquals('ProcedureMapSetting', $decodedContent['included'][0]['type']);
         self::assertEquals($this->procedureMapSetting->getId(), $decodedContent['included'][0]['id']);
         self::assertFalse($decodedContent['included'][0]['attributes']['useGlobalInformationUrl']);
-
     }
 }
