0f0ae4240bb9817e433bd32b5256855d06d0bc14
Saeed Jalalifar <jalalifar@demos-deutschland.de>
2024-07-16 11:40:55 +0200

    feature (ref DPLAN163): -  add missed return

--- demosplan/DemosPlanCoreBundle/ResourceTypes/StatementResourceType.php
+++ demosplan/DemosPlanCoreBundle/ResourceTypes/StatementResourceType.php
@@ -278,7 +278,7 @@ final class StatementResourceType extends AbstractStatementResourceType implemen
                     return '' === $draftsListJson ? null : Json::decodeToArray($draftsListJson);
                 });
             $configBuilder->status->readable(true, function (Statement $statement) {
-                $this->statementService->getProcessintStatus($statement);
+                return $this->statementService->getProcessintStatus($statement);
             })->filterable();
         }
 
