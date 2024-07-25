f4786a23805e3e7cf174859a6ef0a59376748a67
breierDemos <106672028+breierDemos@users.noreply.github.com>
2024-07-25 12:30:22 +0200

    feat (DPLAN-11982): images in segments export (3483)
    * feat (DPLAN-11982): add new class ImageManager. Extract the handling of images in segment export in new class.
    
    * feat (DPLAN-11982): add new class HtmlHelper. Add new test HtmlHelperTest. Move SegmentsExporter::getHtmlValidText to HtmlHelper. Fix HtmlHelper::getHtmlValidText to strip all a tags without href.
    
    * feat (DPLAN-11982): Add new method HtmlHelper::extractUrlsByClass() for extracting urls from specific a tags in a html text. Also add new test case for new method.
    
    * feat (DPLAN-11982): Adjust doc block of HtmlHelper::extractUrlByClass().
    
    * feat (DPLAN-11982): Adjust comments in HtmlHelper::extractUrlByClass().
    
    * feat (DPLAN-11982): Add new ValueObject ImageReference for holding image data. Use new ValueObject.
    
    * feat (DPLAN-11982): Add new Method HtmlHelper::updateLinkTextWithClass(). Add new test case for new method.
    
    * feat (DPLAN-11982): Not use static here
    
    * feat (DPLAN-11982): Adjust Html::extractUrlsByClass to return an array containing value objects of type ImageReference. Rename Method to HtmlHelper::extractImageDataByClass.
    
    * feat (DPLAN-11982): Add new ValueObject ConvertedSegment.
    
    * feat (DPLAN-11982): Adjust ImageLinkConverter to handle not only segment recommendation text. Instead, it converts the segment text and the recommendation text. Adjust the export logic to add images in segment text and recommendation text.
    
    * style: Apply php-cs-fixer
    
    * feat (DPLAN-11982): Adjust ImageLinkConverterTest to match new implementation of ImageLinkConverter.
    
    * style: Apply php-cs-fixer
    
    * feat (DPLAN-11982): Adjust HtmlHelper::updateLinkTextWithClass() function to add styles and a new href value. Adjust test to match new implementation.
    
    * style: Apply php-cs-fixer
    
    * feat (DPLAN-11982): Adjust HtmlHelper::updateLinkTextWithClass(). Use the link text to build the new href value.
    
    * style: Apply php-cs-fixer
    
    * feat (DPLAN-11982): Add HtmlHelper::removeLinkTagsByClass(). Add test case for it.
    
    * feat (DPLAN-11982): Adjust HtmlHelper::removeLinkTagsByClass(). Add a prefix to the link text.
    
    * feat (DPLAN-11982): Adjust HtmlHelper::extractImageDataByClass(). Adjust test. Added a prefix for the image reference.
    
    * style: Apply php-cs-fixer
    
    * feat (DPLAN-11982): Extend the test cases in ImageLinkConverterTest to test the conversion of image links in segment texts. Adjust the ImageLinkConverter to work how intended.
    
    * style: Apply php-cs-fixer
    
    * feat (DPLAN-11982): Adjust preamble.
    
    * feat (DPLAN-11982): Adjust link class for links from the pipeline.
    
    ---------
    
    Co-authored-by: Demos-CI <devops-system+github@demos-deutschland.de>

---
+++ demosplan/DemosPlanCoreBundle/Logic/Segment/Export/Utils/HtmlHelper.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
+namespace demosplan\DemosPlanCoreBundle\Logic\Segment\Export\Utils;
+
+use demosplan\DemosPlanCoreBundle\Services\HTMLSanitizer;
+use demosplan\DemosPlanCoreBundle\ValueObject\SegmentExport\ImageReference;
+
+class HtmlHelper
+{
+    public const LINK_CLASS_FOR_DARSTELLUNG_STELL = 'pdf_importer_image';
+
+    public function __construct(private readonly HTMLSanitizer $htmlSanitizer)
+    {
+    }
+
+    public function getHtmlValidText(string $text): string
+    {
+        /** @var string $text $text */
+        $text = str_replace('<br>', '<br/>', $text);
+
+        // strip all a tags without href
+        $pattern = '/<a(?![^>]*\bhref=)([^>]*)>(.*?)<\/a>/i';
+        $text = preg_replace($pattern, '$2', $text);
+
+        // avoid problems in phpword parser
+        return $this->htmlSanitizer->purify($text);
+    }
+
+    /**
+     * Extracts URLs from the given HTML text that are contained within <a> tags with the specified class, and
+     * creates ImageReference objects from those URLs. The link text of the <a> tags is used as the image reference.
+     * The image reference is prefixed with the specified prefix.
+     *
+     * @return array<int, ImageReference> an array containing the extracted ImageReference objects
+     */
+    public function extractImageDataByClass(string $htmlText, string $class, string $prefix): array
+    {
+        $imageReferences = [];
+
+        // The regex pattern to match <a> tags with the specified class and extract their href attributes and link text
+        $pattern =
+            '/<a\b[^>]*class="[^"]*\b'.preg_quote($class, '/').'\b[^"]*"[^>]*href="([^"]*)"[^>]*>(.*?)<\/a>/i';
+
+        // Perform the regex match
+        if (preg_match_all($pattern, $htmlText, $matches)) {
+            foreach ($matches[1] as $index => $url) {
+                $linkText = $matches[2][$index];
+                // Create an ImageReference object with linkText as imageReference and url as imagePath
+                $imageReference = new ImageReference($prefix.$linkText, $url);
+                $imageReferences[] = $imageReference;
+            }
+        }
+
+        return $imageReferences;
+    }
+
+    /**
+     * Updates the link text of all links with the specified class by appending a prefix, changing the href value based
+     * on the link text, changing the text color to blue and underlining the text.
+     */
+    public function updateLinkTextWithClass(string $htmlText, string $className, string $prefix): string
+    {
+        $pattern = '/(<a\b[^>]*class="[^"]*\b'
+            .preg_quote($className, '/').'\b[^"]*")[^>]*(href="[^"]*")[^>]*(>)(.*?)(<\/a>)/i';
+        $replacement = '$1 href="#'.$prefix.'$4" style="color: blue; text-decoration: underline;"$3'.$prefix.'$4$5';
+
+        return preg_replace($pattern, $replacement, $htmlText);
+    }
+
+    /**
+     * Removes all <a> tags with the specified class from the given HTML text.
+     * The inner text of the removed tags is replaced with the specified prefix.
+     */
+    public function removeLinkTagsByClass(string $htmlText, string $className, string $prefix): string
+    {
+        // Regex pattern to match <a> tags with the specified class and capture their inner text
+        $pattern = '/<a\b[^>]*class="[^"]*\b'.preg_quote($className, '/').'\b[^"]*"[^>]*>(.*?)<\/a>/i';
+        // Replacement string to keep only the inner text
+        $replacement = $prefix.'$1';
+
+        return preg_replace($pattern, $replacement, $htmlText);
+    }
+}
