019873da8a41c5d562eda6030867fd57fbad93d5
Ahmad Alahrashi <email-vom-gpg-key>
2024-08-15 09:18:56 +0200

    chore (refs DPLAN-12286, DPLAN-12253): Adjust SideBar Styling.
    - Fix search-select component (add new Tag), when no Tags available.
    - Adjust Style for tagTopics when there are few Tags || Topics.

--- client/js/components/statement/splitStatement/SideBar.vue
+++ client/js/components/statement/splitStatement/SideBar.vue
@@ -38,7 +38,7 @@
     <!-- Tags Section -->
     <div
       aria-labelledby="floatingContextButton_tags"
-      class="flex-1 flex overflow-y-hidden pl-2 pr-5 -mr-4"
+      :class="['flex-1', 'flex', 'pl-2', 'pr-5', '-mr-4', { 'overflow-y-hidden': availableTags.length }]"
       @mouseover="showFloatingContextButton.tags = true"
       @mouseleave="showFloatingContextButton.tags = false">
 
@@ -52,7 +52,7 @@
 
       <div
         v-else
-        class="flex flex-col">
+        class="flex flex-col w-12">
         <div>
           <!-- search available tags -->
           <search-select
@@ -70,7 +70,7 @@
 
         <div
           v-if="tagTopics.length"
-          class="flex-1 overflow-y-scroll mt-2 pr-1">
+          class="flex-1 overflow-y-scroll min-h-11 mt-2 pr-1">
           <!-- categorized tags -->
           <tag-select
             v-for="(topic, idx) in tagTopics"
