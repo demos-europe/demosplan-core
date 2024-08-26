68cbdec256e66942176a3ca146af64f003ca7977
Demos-CI <devops-system+github@demos-deutschland.de>
2024-08-26 12:52:05 +0000

    style: Apply php-cs-fixer

--- demosplan/DemosPlanCoreBundle/Entity/User/Orga.php
+++ demosplan/DemosPlanCoreBundle/Entity/User/Orga.php
@@ -34,9 +34,9 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
+use Illuminate\Support\Collection as IlluminateCollection;
 use Stringable;
 use Symfony\Component\Validator\Constraints as Assert;
-use Illuminate\Support\Collection as IlluminateCollection;
 
 /**
  * @ORM\Table(
