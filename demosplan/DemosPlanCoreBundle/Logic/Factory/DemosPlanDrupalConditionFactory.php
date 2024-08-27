d68bba4dd6302a113522e58288604f46b1e27ec4
Hamza Bakri <bakri@demos-deutschland.de>
2024-08-27 09:07:50 +0200

    f DPLAN 12254 add DemosPlanDrupalConditionFactory: in some cases however conditions do have values, we do need the OPERATORS 'IS NULL' and 'IS NOT NULL' and since they are only available in the condition without values, some filters are not working anymore. The DemosPlanDrupalConditionFactory extends the PredefinedDrupalConditionFactory and adds two operators (IsNull and IsNotNull) to the operators with value
    Signed-off-by: Hamza Bakri <bakri@demos-deutschland.de>

---
+++ demosplan/DemosPlanCoreBundle/Logic/Factory/DemosPlanDrupalConditionFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
+namespace demosplan\DemosPlanCoreBundle\Logic\Factory;
+
+use EDT\Querying\ConditionParsers\Drupal\DrupalConditionFactoryInterface;
+use EDT\Querying\ConditionParsers\Drupal\PredefinedDrupalConditionFactory;
+use EDT\Querying\Contracts\PathsBasedInterface;
+use EDT\Querying\Drupal\StandardOperator;
+
+/**
+ * @template TCondition of PathsBasedInterface
+ *
+ * @template-implements DrupalConditionFactoryInterface<TCondition>
+ *
+ * @phpstan-import-type DrupalValue from DrupalConditionFactoryInterface
+ */
+class DemosPlanDrupalConditionFactory extends PredefinedDrupalConditionFactory
+{
+    /**
+     * @return array<non-empty-string, callable(DrupalValue, non-empty-list<non-empty-string>|null): TCondition>
+     */
+    protected function getOperatorFunctionsWithValue(): array
+    {
+        $operators = parent::getOperatorFunctionsWithValue();
+
+        $operators[StandardOperator::IS_NULL] = fn ($value, ?array $path): PathsBasedInterface => $this->conditionFactory->propertyIsNull($path);
+        $operators[StandardOperator::IS_NOT_NULL] = fn ($value, ?array $path): PathsBasedInterface => $this->conditionFactory->propertyIsNotNull($path);
+
+        return $operators;
+    }
+}
