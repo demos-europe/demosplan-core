88625d0c83c24b040a51c89988d0c19fd407312e
Catalina Sierra Zamudio <sierra-zamudio@demos-deutschland.de>
2024-09-12 15:05:41 +0200

    feat (DPLAN-11379) Add customer_id column to _platform_content table

---
+++ demosplan/DemosPlanCoreBundle/DoctrineMigrations/2024/09/Version20240912125339.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types = 1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Platforms\MySQLPlatform;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+class Version20240912125339 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'refs DPLAN-11379: Add customer_id to _platform_content table';
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function up(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+        $this->addSql('ALTER TABLE _platform_content ADD customer_id VARCHAR(100) DEFAULT NOT NULL');
+        $this->addSql('CREATE INDEX _platform_content_customer_FK ON _platform_content (customer_id)');
+
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function down(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+        $this->addSql('DROP INDEX _platform_content_customer_FK ON _platform_content');
+        $this->addSql('ALTER TABLE _platform_content DROP customer_id');
+
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function abortIfNotMysql(): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof MySqlPlatform,
+            "Migration can only be executed safely on 'mysql'."
+        );
+    }
+}
