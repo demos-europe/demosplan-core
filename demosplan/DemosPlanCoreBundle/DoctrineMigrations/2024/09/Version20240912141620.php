6879b9b65ce7d6ca5c5ca478a30e9460a38bcfee
Demos-CI <devops-system+github@demos-deutschland.de>
2024-09-13 07:59:40 +0000

    style: Apply php-cs-fixer

--- demosplan/DemosPlanCoreBundle/DoctrineMigrations/2024/09/Version20240912141620.php
+++ demosplan/DemosPlanCoreBundle/DoctrineMigrations/2024/09/Version20240912141620.php
@@ -33,16 +33,14 @@ class Version20240912141620 extends AbstractMigration
         $this->addSql('SET foreign_key_checks = 0;');
         $this->addSql('ALTER TABLE _platform_content ADD customer_id CHAR(36) NOT NULL');
 
-
         // Retrieve the first customer ID
         $firstCustomer = $this->connection->fetchAssociative('SELECT _c_id FROM customer LIMIT 1');
         $firstCustomerId = $firstCustomer['_c_id'];
 
         // Update existing _platform_content entries without customer_id
         $this->addSql('UPDATE _platform_content SET customer_id = :customer_id', [
-            'customer_id' => $firstCustomerId
+            'customer_id' => $firstCustomerId,
         ]);
-
 
         $this->write('Migrating data from _platform_content to _platform_content with customer_id');
         // Retrieve all entries from _platform_content
@@ -50,13 +48,13 @@ class Version20240912141620 extends AbstractMigration
 
         // Retrieve all customer IDs
         $customerIds = $this->connection->fetchAllAssociative('SELECT _c_id FROM customer WHERE _c_id != :first_customer_id', [
-            'first_customer_id' => $firstCustomerId
+            'first_customer_id' => $firstCustomerId,
         ]);
 
         foreach ($platformContentEntries as $entry) {
-            $this->write('Migrating entry with title: ' . $entry['_pc_title']);
+            $this->write('Migrating entry with title: '.$entry['_pc_title']);
             foreach ($customerIds as $customer) {
-                $this->write('Migrating entry for customer: ' . $customer['_c_id']);
+                $this->write('Migrating entry for customer: '.$customer['_c_id']);
                 $this->addSql('INSERT INTO _platform_content SET
     _pc_id = UUID(),
     _pc_create_date = NOW(),
@@ -72,26 +70,23 @@ class Version20240912141620 extends AbstractMigration
     _pc_pdf_title = :_pc_pdf_title,
     _pc_enabled = :_pc_enabled,
     customer_id = :customer_id;', [
-                    '_pc_type' => $entry['_pc_type'],
-                    '_pc_title' => $entry['_pc_title'],
-                    '_pc_description' => $entry['_pc_description'],
-                    '_pc_text' => $entry['_pc_text'],
-                    '_pc_picture' => $entry['_pc_picture'],
+                    '_pc_type'          => $entry['_pc_type'],
+                    '_pc_title'         => $entry['_pc_title'],
+                    '_pc_description'   => $entry['_pc_description'],
+                    '_pc_text'          => $entry['_pc_text'],
+                    '_pc_picture'       => $entry['_pc_picture'],
                     '_pc_picture_title' => $entry['_pc_picture_title'],
-                    '_pc_pdf' => $entry['_pc_pdf'],
-                    '_pc_pdf_title' => $entry['_pc_pdf_title'],
-                    '_pc_enabled' => $entry['_pc_enabled'],
-                    'customer_id' => $customer['_c_id']
+                    '_pc_pdf'           => $entry['_pc_pdf'],
+                    '_pc_pdf_title'     => $entry['_pc_pdf_title'],
+                    '_pc_enabled'       => $entry['_pc_enabled'],
+                    'customer_id'       => $customer['_c_id'],
                 ]);
             }
-
-
         }
         $this->addSql('SET foreign_key_checks = 1;');
 
         $this->addSql('ALTER TABLE _platform_content ADD CONSTRAINT FK_42348F4F9395C3F3 FOREIGN KEY (customer_id) REFERENCES customer (_c_id) ON DELETE CASCADE');
         $this->addSql('CREATE INDEX IDX_42348F4F9395C3F3 ON _platform_content (customer_id)');
-
     }
 
     /**
