24d70465b337e19503c46492fd46dec32200b0ec
Catalina Sierra Zamudio <sierra-zamudio@demos-deutschland.de>
2024-09-13 09:01:42 +0200

    feat (DPLAN-11379) Adjust migration to copy platform_content entries per customer

--- demosplan/DemosPlanCoreBundle/DoctrineMigrations/2024/09/Version20240912141620.php
+++ demosplan/DemosPlanCoreBundle/DoctrineMigrations/2024/09/Version20240912141620.php
@@ -30,10 +30,57 @@ class Version20240912141620 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->abortIfNotMysql();
-
+        $this->addSql('SET foreign_key_checks = 0;');
         $this->addSql('ALTER TABLE _platform_content ADD customer_id CHAR(36) NOT NULL');
+
+
+
+        $this->write('Migrating data from _platform_content to _platform_content with customer_id');
+        // Retrieve all entries from _platform_content
+        $platformContentEntries = $this->connection->fetchAllAssociative('SELECT * FROM _platform_content');
+
+        // Retrieve all customer IDs
+        $customerIds = $this->connection->fetchAllAssociative('SELECT _c_id FROM customer');
+
+        foreach ($platformContentEntries as $entry) {
+            $this->write('Migrating entry with title: ' . $entry['_pc_title']);
+            foreach ($customerIds as $customer) {
+                $this->write('Migrating entry for customer: ' . $customer['_c_id']);
+                $this->addSql('INSERT INTO _platform_content SET
+    _pc_id = UUID(),
+    _pc_create_date = NOW(),
+    _pc_modify_date = NOW(),
+    _pc_delete_date = NOW(),
+    _pc_type = :_pc_type,
+    _pc_title = :_pc_title,
+    _pc_description = :_pc_description,
+    _pc_text = :_pc_text,
+    _pc_picture = :_pc_picture,
+    _pc_picture_title = :_pc_picture_title,
+    _pc_pdf = :_pc_pdf,
+    _pc_pdf_title = :_pc_pdf_title,
+    _pc_enabled = :_pc_enabled,
+    customer_id = :customer_id;', [
+                    '_pc_type' => $entry['_pc_type'],
+                    '_pc_title' => $entry['_pc_title'],
+                    '_pc_description' => $entry['_pc_description'],
+                    '_pc_text' => $entry['_pc_text'],
+                    '_pc_picture' => $entry['_pc_picture'],
+                    '_pc_picture_title' => $entry['_pc_picture_title'],
+                    '_pc_pdf' => $entry['_pc_pdf'],
+                    '_pc_pdf_title' => $entry['_pc_pdf_title'],
+                    '_pc_enabled' => $entry['_pc_enabled'],
+                    'customer_id' => $customer['_c_id']
+                ]);
+            }
+
+
+        }
+        $this->addSql('SET foreign_key_checks = 1;');
+
         $this->addSql('ALTER TABLE _platform_content ADD CONSTRAINT FK_42348F4F9395C3F3 FOREIGN KEY (customer_id) REFERENCES customer (_c_id) ON DELETE CASCADE');
         $this->addSql('CREATE INDEX IDX_42348F4F9395C3F3 ON _platform_content (customer_id)');
+
     }
 
     /**
