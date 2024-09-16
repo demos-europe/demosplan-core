ac52c6f66a51cc2b724d19247effc3c81d24e5b3
Moritz Mandler <mandler@demos-deutschland.de>
2024-09-16 14:35:10 +0200

    fix(refs DPLAN-1917): Add map-params to newly created customers via GenerateCustomerCommand

--- demosplan/DemosPlanCoreBundle/Command/Data/GenerateCustomerCommand.php
+++ demosplan/DemosPlanCoreBundle/Command/Data/GenerateCustomerCommand.php
@@ -14,6 +14,7 @@ namespace demosplan\DemosPlanCoreBundle\Command\Data;
 
 use DemosEurope\DemosplanAddon\Contracts\Entities\RoleInterface;
 use DemosEurope\DemosplanAddon\Contracts\Entities\UserInterface;
+use demosplan\DemosPlanCoreBundle\Application\DemosPlanKernel;
 use demosplan\DemosPlanCoreBundle\Command\CoreCommand;
 use demosplan\DemosPlanCoreBundle\Entity\User\AiApiUser;
 use demosplan\DemosPlanCoreBundle\Entity\User\Customer;
@@ -33,6 +34,11 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
+use Symfony\Component\Validator\Constraints\Url;
+use Symfony\Component\Validator\Constraints\UrlValidator;
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Webmozart\Assert\Assert;
 use function in_array;
 use function is_string;
 
@@ -40,6 +46,13 @@ class GenerateCustomerCommand extends CoreCommand
 {
     private const OPTION_NAME = 'name';
     private const OPTION_SUBDOMAIN = 'subdomain';
+
+    private const MAP_PARAMETERS = 'map-parameters'; // use value 'default' to automatically insert default values
+
+    private const DEFAULT_BASE_LAYER_URL = 'https://sgx.geodatenzentrum.de/wms_basemapde';
+    private const DEFAULT_BASE_LAYER_LAYERS = 'de_basemapde_web_raster_farbe';
+    private const DEFAULT_MAP_ATTRIBUTION = '© basemap.de / BKG ({currentYear})';
+
 
     protected static $defaultName = 'dplan:data:generate-customer';
     protected static $defaultDescription = 'Creates a new customer';
@@ -62,6 +75,7 @@ class GenerateCustomerCommand extends CoreCommand
         ParameterBagInterface $parameterBag,
         private readonly RoleRepository $roleRepository,
         private readonly UserService $userService,
+        private readonly ValidatorInterface $validator,
         ?string $name = null
     ) {
         parent::__construct($parameterBag, $name);
@@ -86,6 +100,13 @@ class GenerateCustomerCommand extends CoreCommand
             InputOption::VALUE_REQUIRED,
             'The subdomain of the customer to be created. If omitted it will be asked interactively.'
         );
+        $this->addOption(
+            self::MAP_PARAMETERS,
+            'm',
+            InputOption::VALUE_REQUIRED,
+            'The Map parameters of the customer to be created.
+             If omitted or value !== "default" it will be asked interactively.'
+        );
     }
 
     /**
@@ -103,9 +124,20 @@ class GenerateCustomerCommand extends CoreCommand
             $subdomain = $this->askSubdomain($input, $output);
         }
 
+        $mapOption = $input->getOption(self::MAP_PARAMETERS);
+        $mapParams = $this->getMapParams($mapOption, $input, $output);
+
         try {
             // create customer
             $customer = $this->customerService->createCustomer($name, $subdomain);
+            $customer->setBaseLayerUrl($mapParams[0]);
+            $customer->setBaseLayerLayers($mapParams[1]);
+            $customer->setMapAttribution($mapParams[2]);
+            $kernel = $this->getApplication()->getKernel();
+            /** @var DemosPlanKernel $kernel */
+            $activeProject = $kernel->getActiveProject();
+
+
             $this->registerDefaultUsers($customer);
             $this->entityManager->flush();
 
@@ -186,5 +218,63 @@ class GenerateCustomerCommand extends CoreCommand
             );
             $this->userService->updateUserObject($user);
         }
+    }
+
+    private function getMapParams(?string $mapParams, InputInterface $input, OutputInterface $output): array
+    {
+        if ('default' === $mapParams) {
+            return [
+                self::DEFAULT_BASE_LAYER_URL,
+                self::DEFAULT_BASE_LAYER_LAYERS,
+                self::DEFAULT_MAP_ATTRIBUTION,
+            ];
+        }
+        $questionMapUrl = new Question(
+            "Please enter the Base Layer URL of the customer\nDefaults to: ".self::DEFAULT_BASE_LAYER_URL.":\n",
+            self::DEFAULT_BASE_LAYER_URL
+        );
+        $questionMapUrl->setValidator($this->assertMapUrl(...));
+        $mapUrl = $this->helper->ask($input, $output, $questionMapUrl);
+
+        $questionMapLayers = new Question(
+            "Please enter the Base Layers of the customer\n Defaults to: ".self::DEFAULT_BASE_LAYER_LAYERS.":\n",
+            self::DEFAULT_BASE_LAYER_LAYERS
+        );
+        $questionMapLayers->setValidator($this->assertIsNonEmptyString(...));
+        $mapLayers = $this->helper->ask($input, $output, $questionMapLayers);
+
+        $questionMapAttribution = new Question(
+            "Please enter the map attribution of the customer\nDefaults to: ".self::DEFAULT_MAP_ATTRIBUTION.":\n",
+            self::DEFAULT_MAP_ATTRIBUTION
+        );
+        $questionMapAttribution->setValidator($this->assertIsNonEmptyString(...));
+        $mapAttribution = $this->helper->ask($input, $output, $questionMapAttribution);
+
+        return [$mapUrl, $mapLayers, $mapAttribution];
+    }
+
+    private function assertMapUrl(mixed $mapUrl): string
+    {
+        if (!is_string($mapUrl)) {
+            throw new InvalidArgumentException('Customer Base Layer URL must be a string.');
+        }
+        $constraint = new Url();
+        $violations = $this->validator->validate($mapUrl, $constraint);
+        if (0 < count($violations)) {
+            $violationMessage = '';
+            foreach ($violations as $violation) {
+                $violationMessage .= $violation->getMessage()."\n";
+            }
+            throw new InvalidArgumentException($violationMessage);
+        }
+
+        return $mapUrl;
+    }
+
+    private function assertIsNonEmptyString(mixed $value): string
+    {
+        Assert::stringNotEmpty('the value should be a non-empty string');
+
+        return $value;
     }
 }
