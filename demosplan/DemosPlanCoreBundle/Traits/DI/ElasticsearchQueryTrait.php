79fee3247facdb066462dbfe2256fd6f5d1e0923
MoritzMandler <89914798+MoritzMandler@users.noreply.github.com>
2024-09-17 16:47:41 +0200

    fix(refs DPLAN-12428): cast MaxPerPage Param from string to int (3679)
    * fix(refs DPLAN-12428): The params retrieved from the
    form are parsed as strings. We need an integer to call the method $paginator->setMaxPerPage().
    
    As the JsonApiPaginationParser::getRequestProfilePositiveInt method already does - the StatementService::getFormValues now also casts this value to an integer.
    
    * style: Apply php-cs-fixer
    
    * fix(refs DPLAN-12428): use type cast for setMaxPerPage method calls
    
    * style: Apply php-cs-fixer
    
    ---------
    
    Co-authored-by: Demos-CI <devops-system+github@demos-deutschland.de>

--- demosplan/DemosPlanCoreBundle/Traits/DI/ElasticsearchQueryTrait.php
+++ demosplan/DemosPlanCoreBundle/Traits/DI/ElasticsearchQueryTrait.php
@@ -89,7 +89,7 @@ trait ElasticsearchQueryTrait
         $esQuery,
         $limit = -1,
         $page = 1,
-        Index $index = null): array
+        ?Index $index = null): array
     {
         $result = [];
 
@@ -149,7 +149,7 @@ trait ElasticsearchQueryTrait
                 $limit = 1;
             }
 
-            $paginator->setMaxPerPage($limit);
+            $paginator->setMaxPerPage((int) $limit);
 
             // try to paginate Result, check for validity
             try {
