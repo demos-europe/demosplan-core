1f9b762641988a12be0299dfd4a83dd04fef8225
Demos-CI <devops-system+github@demos-deutschland.de>
2024-09-24 12:33:10 +0000

    style: Apply php-cs-fixer

--- demosplan/DemosPlanCoreBundle/ResourceTypes/GlobalNewsResourceType.php
+++ demosplan/DemosPlanCoreBundle/ResourceTypes/GlobalNewsResourceType.php
@@ -149,11 +149,11 @@ final class GlobalNewsResourceType extends AbstractNewsResourceType
                     }
 
                     // Adjust files
-                    //$pictureFileRef = $this->pictureFile->getAsNamesInDotNotation();
+                    // $pictureFileRef = $this->pictureFile->getAsNamesInDotNotation();
 
-                    //Assert::notNull($pictureFileRef);
-                    /** @var File $file */
-                    //$file = $this->resourceTypeStore->getFileResourceType()->getEntity($fileRef[ContentField::ID]);
+                    // Assert::notNull($pictureFileRef);
+                    /* @var File $file */
+                    // $file = $this->resourceTypeStore->getFileResourceType()->getEntity($fileRef[ContentField::ID]);
 
                     return [];
                 }
