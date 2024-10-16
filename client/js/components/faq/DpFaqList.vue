827cce5842ef14d26a73a832d4705f27483f91b4
hwiem <40487668+hwiem@users.noreply.github.com>
2024-10-16 10:34:15 +0200

    refactor(refs DPLAN-24): allow dragging an faq item into another category [vue 3] (3111)
    - add `item` attribute to each layer/category to keep track inside the store
    of the index an item has in the interface
    - index is needed to update the treeOrder/mapOrder attribute, which is used
    to persist the order of the items in the BE
    - uiIndexList is removed from the store again, as it is replaced by the
    previously described approach
    
    * refactor(refs T19759, DPLAN-274): reduce code duplication
    * refactor(refs T19759, DPLAN-274): use new method for base layer list, too
    * refactor(refs T19759, DPLAN-274): handle orderType = 'mapOrder'
    - if orderType is 'mapOrder', layers don't need to be moved between
    categories, so we don't need to call removeItem and addItem
    - instead, we just update the mapOrder attribute
    
    * refactor(refs T19759, DPLAN-274): allow dragging to another category

--- client/js/components/faq/DpFaqList.vue
+++ client/js/components/faq/DpFaqList.vue
@@ -159,48 +159,81 @@ export default {
      * @param {String} parentId
      */
     updateCategorySort (event, item, parentId) {
-      const catCpy = JSON.parse(JSON.stringify(this.faqCategories[parentId]))
-      const children = catCpy.relationships.faq.data
-      const itemToBeMoved = children.splice(event.oldIndex, 1)[0]
-      children.splice(event.newIndex, 0, itemToBeMoved)
+      let categoryIds = []
+      const sourceId = event.from.id
+      let targetId = event.to.id
 
-      const newSort = children.map(item => {
-        return {
-          id: item.id,
-          type: item.type
-        }
-      })
+      if (targetId === 'noIdGiven') {
+        const targetList = event.to.querySelector('ul')
 
-      catCpy.relationships.faq.data = newSort
-
-      this.updateCategory({ ...catCpy, id: catCpy.id })
-
-      const manualSortParam = 'manualsort=' + newSort.reduce((acc, item, idx) => {
-        return idx !== newSort.length - 1 ? acc + item.id + ',' : acc + item.id
-      }, '')
-
-      const categoryParam = 'category=custom_category'
-      const categoryIdParam = 'categoryId=' + parentId
-
-      const postParams = manualSortParam + '&' + categoryParam + '&' + categoryIdParam
-
-      const xhr = new XMLHttpRequest()
-      const url = Routing.generate('DemosPlan_faq_administration_faq')
-      xhr.open('POST', url, true)
-
-      xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-      xhr.setRequestHeader('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
-      xhr.setRequestHeader('Upgrade-Insecure-Requests', '1')
-
-      // Display notifications on success or failure of the request
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status >= 200 && xhr.status < 300) {
-          dplan.notify.notify('confirm', Translator.trans('confirm.sort.saved'))
-        } else if (xhr.readyState === 4) {
-          dplan.notify.error(Translator.trans('error.update.manual.order'))
+        if (targetList) {
+          targetId = targetList.id
         }
       }
-      xhr.send(postParams)
+
+      if (sourceId !== targetId) {
+        categoryIds = [event.from.id, targetId]
+      } else {
+        categoryIds = [parentId]
+      }
+
+      // remove item from old category, add to new category
+      categoryIds.forEach(id => {
+        const category = JSON.parse(JSON.stringify(this.faqCategories[id]))
+        const children = category.relationships.faq.data
+        const itemIsChild = children.findIndex(child => child.id === item.id) !== -1
+
+        // move to another category
+        if (sourceId !== targetId) {
+          if (itemIsChild) {
+            // remove item from old category
+            children.splice(event.oldIndex, 1)
+            this.updateCategory({ ...category, id: category.id })
+          } else {
+            // add item to new category
+            children.splice(event.newIndex, 0, { id: item.id, type: item.type })
+            this.updateCategory({ ...category, id: category.id })
+          }
+        }
+
+        // move inside the same category
+        if (sourceId === targetId) {
+          if (itemIsChild) {
+            // remove item from old position
+            const item = children.splice(event.oldIndex, 1)[0]
+            // add item at new position
+            children.splice(event.newIndex, 0, item)
+            this.updateCategory({ ...category, id: category.id })
+          }
+        }
+
+        const manualSortParam = 'manualsort=' + children.reduce((acc, child, idx) => {
+          return idx !== children.length - 1 ? acc + child.id + ',' : acc + child.id
+        }, '')
+
+        const categoryParam = 'category=custom_category'
+        const categoryIdParam = 'categoryId=' + id
+
+        const postParams = manualSortParam + '&' + categoryParam + '&' + categoryIdParam
+
+        const xhr = new XMLHttpRequest()
+        const url = Routing.generate('DemosPlan_faq_administration_faq')
+        xhr.open('POST', url, true)
+
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+        xhr.setRequestHeader('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
+        xhr.setRequestHeader('Upgrade-Insecure-Requests', '1')
+
+        // Display notifications on success or failure of the request
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState === 4 && xhr.status >= 200 && xhr.status < 300) {
+            dplan.notify.notify('confirm', Translator.trans('confirm.sort.saved'))
+          } else if (xhr.readyState === 4) {
+            dplan.notify.error(Translator.trans('error.update.manual.order'))
+          }
+        }
+        xhr.send(postParams)
+      })
     }
   },
 
