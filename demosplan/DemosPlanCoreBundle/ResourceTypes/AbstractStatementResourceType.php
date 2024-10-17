53474203bf432628cd96a20f5e83b0be652abb77
Demos-CI <devops-system+github@demos-deutschland.de>
2024-10-17 13:53:42 +0000

    style: Apply php-cs-fixer

--- demosplan/DemosPlanCoreBundle/ResourceTypes/AbstractStatementResourceType.php
+++ demosplan/DemosPlanCoreBundle/ResourceTypes/AbstractStatementResourceType.php
@@ -188,8 +188,7 @@ abstract class AbstractStatementResourceType extends DplanResourceType
             ->readable(true)->aliasedPath(Paths::statement()->parent->id);
         $configBuilder->phase
             ->readable(true,
-                fn (Statement $statement): string
-                => $this->statementService->getInternalOrExternalPhaseNameFromObject($statement)
+                fn (Statement $statement): string => $this->statementService->getInternalOrExternalPhaseNameFromObject($statement)
             );
         $configBuilder->polygon->readable(true);
         $configBuilder->priority->readable(true);
