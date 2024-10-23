dc52cfb4e89d863bf4853976c0617c5de93be44a
Catalina Sierra Zamudio <sierra-zamudio@demos-deutschland.de>
2024-10-23 16:34:48 +0200

    fix (DPLAN-12665) Define variable

--- demosplan/DemosPlanCoreBundle/Logic/Statement/StatementPhaseService.php
+++ demosplan/DemosPlanCoreBundle/Logic/Statement/StatementPhaseService.php
@@ -52,10 +52,16 @@ class StatementPhaseService
         throw new UndefinedPhaseException($phaseKey);
     }
 
+    /**
+     * // If the publicstatement is internal, then return only the internal phases
+     * // if the publicstatement is external, then return only the external phases
+     * @param string $publicStatement
+     * @return array
+     */
     public function getAvailablePhases(string $publicStatement): array
     {
-        // If the publicstatement is internal, then return only the internal phases
-        // if the publicstatement is external, then return only the external phases
+
+        $phases = [];
 
         if (StatementInterface::EXTERNAL === $publicStatement) {
             foreach ($this->globalConfig->getExternalPhasesAssoc() as $internalPhase) {
