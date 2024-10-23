d19e69469cb05f8130a722c9180cfafdaaca9b20
Velimir Müller <mueller@demos-deutschland.de>
2024-10-23 15:11:44 +0200

    feat(refs DPLAN-12697): properly re-structure converterData

--- client/js/lib/prosemirror/converterData.js
+++ client/js/lib/prosemirror/converterData.js
@@ -1,1 +1,111 @@
-export const converterData = JSON.parse(JSON.stringify({"data": {"type": "SegmentedStatement", "id": "4b36eff3-c6b5-4a1b-bced-ffaf7d57c3e9", "relationships": {"draftSegments": {"data": [{"id": "3e6ed81f-4fee-4eb9-89d4-c6f100277b01", "type": "DraftSegment"}, {"id": "ee1083d0-c625-43b4-bf5b-43093ea70af6", "type": "DraftSegment"}, {"id": "7456641f-d62d-4171-9d44-9607b417dafb", "type": "DraftSegment"}]}, "statement": {"data": {"type": "Statement", "id": "cb4b7a63-0d61-4ae8-b31f-ceb26b5fc535"}}}}, "included": [{"type": "DraftSegment", "id": "3e6ed81f-4fee-4eb9-89d4-c6f100277b01", "attributes": {"position": {"start": 0, "stop": 809}, "segment_text": "<p><strong>Planfeststellung f\u00fcr das Bauvorhaben 'S 84 Neubau Niederwartha - Mei\u00dfen, BA 2.2 und BA 3' hier: Anh\u00f6rungsverfahren</strong><br><br>Sehr geehrte Damen und Herren, Sehr geehrte Frau Storch,<br><br>im Rahmen des Anh\u00f6rungsverfahrens m\u00f6chten wir uns wie folgt zum o.g. Planfeststellungsentwi positionieren:<br><br>Im Planungsbereich befinden sich mehrere Anlagen (Trinkwasserleitungen incl. dazugeh\u00f6riger Steuerkabel und Armaturen) der Wasserversorgung Brockwitz-R\u00f6dern GmbH. Detaillierte Bestandsunterlagen hierzu wurden bereits im Zuge der bisherigen Planungen geliefert. Bestandsdaten im dwg-, dxf-, und pdf-Format k\u00f6nnen bei Bedarf gern bei uns angefordert werden</p><p></p><p>Auch musste ich leider in der Vergangenheit immer wieder feststellen, dass fehlerhafte Vermessung vorgenommen worden sind."}, "relationships": {"tags": {"data": [{"id": "fb8523f8-4985-42a8-b5de-c22e5184a403", "score": 0.89574, "type": "Tag"}, {"id": "4d459668-d24c-40ff-ab7d-ff6a5105fa01", "score": 0.37006, "type": "Tag"}]}}}, {"type": "DraftSegment", "id": "ee1083d0-c625-43b4-bf5b-43093ea70af6", "attributes": {"position": {"start": 810, "stop": 918}, "segment_text": " Die zur Verf\u00fcgung gestellten Unterlagen lassen keine R\u00fcckschl\u00fcsse auf eine ordnungsgem\u00e4\u00dfe Vermessung nehmen."}, "relationships": {"tags": {"data": [{"id": "7d4b3d57-6853-4b04-9610-a9fcecfb822b", "score": 1.0, "type": "Tag"}]}}}, {"type": "DraftSegment", "id": "7456641f-d62d-4171-9d44-9607b417dafb", "attributes": {"position": {"start": 918, "stop": 2592}, "segment_text": "</p><ol><li><p>Zudem sind Naturschutzbelange nicht -insbesondere nicht angemessen- ber\u00fccksichtigt. Das FFH-Gebiet Bosel und Elbh\u00e4nge n\u00f6rdlich Mei\u00dfen ist ein Habitat unter anderem der Mopsfledermaus, des gro\u00dfen Mausohrs, des Fetthennen-Bl\u00e4ulings, des Eremiten und Schmetterlings 'Russischer B\u00e4r'. Pflanzen von landesweiter Bedeutung sind unter anderem das blau gr\u00fcne Labkraut, das Sch\u00f6nastmoos, die Wiesen-Kuhschelle, der blaue Lattig und der aufrechte Ziest.</p></li><li><p>nach Ma\u00dfgabe der Planung sollen Pflanzbest\u00e4nde und Lebensr\u00e4ume f\u00fcr Tiere zerst\u00f6rt werden, weitere Beeintr\u00e4chtigungen im Zuge der Bauma\u00dfnahme sind zu bef\u00fcrchten, ebenso Substanzverluste durch die im Zuge des Stra\u00dfenverkehrs entstehenden Immissionsbelastungen.</p></li></ol><p><strong>Hinweise:</strong><br><br>Auf Seite 98 des Erl\u00e4uterungsberichtes wird auf eine Verf\u00fcllung s\u00fcdlich der geplanten Retentionsfl\u00e4che (Flurst\u00fcck 590; hier: Gemarkung Brockwitz) hingewiesen. Diese Verf\u00fcllung war bisher nicht bekannt. Es kann unsererseits nicht ausgeschlossen werden, dass es sich hier um eine bisher unbekannte Altablagerung handelt, deren Inhalt und Gr\u00f6\u00dfe uns demzufolge ebenfalls nicht bekannt sind.<br><br>Auf Seite 83 des Erl\u00e4uterungsberichtes wird auf eine geplante offene Bauwasserhaltung im Ma\u00dfnahmebereich DL 1 - Durchlass \u201eLanger Graben\u201c hingewiesen. An dieser Stelle kann nicht ausgeschlossen werden, dass das zu haltende Grundwasser durch anstromig vorhandene Altlasten mit LHKW kontaminiert ist.<br><br>Auf Grund der im Planungsgebiet weiterhin vorhandenen Schadstofffahnen sind auch bei ggf. weiteren erforderlichen Bauwasserhaltungen altlastenspezifische Schadstoffe nicht auszuschlie\u00dfen.</p>"}, "relationships": {"tags": {"data": [{"id": "02a9d69a-8022-449c-af19-aff03fe41ea8", "score": 0.56869, "type": "Tag"}, {"id": "417f3e7a-58ae-4b6c-ab6d-d284f226b171", "score": 0.51364, "type": "Tag"}]}}}, {"type": "Tag", "id": "fb8523f8-4985-42a8-b5de-c22e5184a403", "attributes": {"title": "01.3 Kenntnisnahme (ohne Relevanz)"}}, {"type": "Tag", "id": "4d459668-d24c-40ff-ab7d-ff6a5105fa01", "attributes": {"title": "06.8 Stra\u00dfenplanung Sonstiges"}}, {"type": "Tag", "id": "7d4b3d57-6853-4b04-9610-a9fcecfb822b", "attributes": {"title": "06.8 Stra\u00dfenplanung Sonstiges"}}, {"type": "Tag", "id": "02a9d69a-8022-449c-af19-aff03fe41ea8", "attributes": {"title": "05 Schutzgebiete"}}, {"type": "Tag", "id": "417f3e7a-58ae-4b6c-ab6d-d284f226b171", "attributes": {"title": "05.6 Altlasten"}}]}))
+export const converterData = JSON.parse(JSON.stringify(
+  {
+    "data": {
+      "type": "SegmentedStatement",
+      "id": "4b36eff3-c6b5-4a1b-bced-ffaf7d57c3e9",
+      "relationships": {
+        "draftSegments": {
+          "data": [
+            {"id": "3e6ed81f-4fee-4eb9-89d4-c6f100277b01", "type": "DraftSegment"},
+            {"id": "ee1083d0-c625-43b4-bf5b-43093ea70af6", "type": "DraftSegment"},
+            {"id": "7456641f-d62d-4171-9d44-9607b417dafb", "type": "DraftSegment"}
+          ]
+        },
+        "statement": {
+          "data": {
+            "type": "Statement",
+            "id": "cb4b7a63-0d61-4ae8-b31f-ceb26b5fc535"}
+        }
+      }
+    },
+    "included": [
+      {
+        "type": "DraftSegment",
+        "id": "3e6ed81f-4fee-4eb9-89d4-c6f100277b01",
+        "attributes": {
+          "position": {
+            "start": 0, "stop": 809
+          },
+          "segment_text": "<p><strong>Planfeststellung f\u00fcr das Bauvorhaben 'S 84 Neubau Niederwartha - Mei\u00dfen, BA 2.2 und BA 3' hier: Anh\u00f6rungsverfahren</strong><br><br>Sehr geehrte Damen und Herren, Sehr geehrte Frau Storch,<br><br>im Rahmen des Anh\u00f6rungsverfahrens m\u00f6chten wir uns wie folgt zum o.g. Planfeststellungsentwi positionieren:<br><br>Im Planungsbereich befinden sich mehrere Anlagen (Trinkwasserleitungen incl. dazugeh\u00f6riger Steuerkabel und Armaturen) der Wasserversorgung Brockwitz-R\u00f6dern GmbH. Detaillierte Bestandsunterlagen hierzu wurden bereits im Zuge der bisherigen Planungen geliefert. Bestandsdaten im dwg-, dxf-, und pdf-Format k\u00f6nnen bei Bedarf gern bei uns angefordert werden</p><p></p><p>Auch musste ich leider in der Vergangenheit immer wieder feststellen, dass fehlerhafte Vermessung vorgenommen worden sind."
+        },
+        "relationships": {
+          "tags": {
+            "data": [
+              { "id": "fb8523f8-4985-42a8-b5de-c22e5184a403", "score": 0.89574, "type": "Tag"},
+              { "id": "4d459668-d24c-40ff-ab7d-ff6a5105fa01", "score": 0.37006, "type": "Tag"}
+            ]
+          }
+        }
+      },
+      {
+        "type":
+        "DraftSegment",
+        "id": "ee1083d0-c625-43b4-bf5b-43093ea70af6",
+        "attributes": {
+          "position": {
+            "start": 810, "stop": 918
+          },
+          "segment_text": " Die zur Verf\u00fcgung gestellten Unterlagen lassen keine R\u00fcckschl\u00fcsse auf eine ordnungsgem\u00e4\u00dfe Vermessung nehmen."
+        },
+        "relationships": {
+          "tags": {
+            "data": [
+              {
+                "id": "7d4b3d57-6853-4b04-9610-a9fcecfb822b", "score": 1.0, "type": "Tag"
+              }
+            ]
+          }
+      }
+      },
+      {
+      "type": "DraftSegment",
+        "id": "7456641f-d62d-4171-9d44-9607b417dafb",
+        "attributes": {
+        "position": {
+          "start": 918, "stop": 2592
+        },
+        "segment_text": "</p><ol><li><p>Zudem sind Naturschutzbelange nicht -insbesondere nicht angemessen- ber\u00fccksichtigt. Das FFH-Gebiet Bosel und Elbh\u00e4nge n\u00f6rdlich Mei\u00dfen ist ein Habitat unter anderem der Mopsfledermaus, des gro\u00dfen Mausohrs, des Fetthennen-Bl\u00e4ulings, des Eremiten und Schmetterlings 'Russischer B\u00e4r'. Pflanzen von landesweiter Bedeutung sind unter anderem das blau gr\u00fcne Labkraut, das Sch\u00f6nastmoos, die Wiesen-Kuhschelle, der blaue Lattig und der aufrechte Ziest.</p></li><li><p>nach Ma\u00dfgabe der Planung sollen Pflanzbest\u00e4nde und Lebensr\u00e4ume f\u00fcr Tiere zerst\u00f6rt werden, weitere Beeintr\u00e4chtigungen im Zuge der Bauma\u00dfnahme sind zu bef\u00fcrchten, ebenso Substanzverluste durch die im Zuge des Stra\u00dfenverkehrs entstehenden Immissionsbelastungen.</p></li></ol><p><strong>Hinweise:</strong><br><br>Auf Seite 98 des Erl\u00e4uterungsberichtes wird auf eine Verf\u00fcllung s\u00fcdlich der geplanten Retentionsfl\u00e4che (Flurst\u00fcck 590; hier: Gemarkung Brockwitz) hingewiesen. Diese Verf\u00fcllung war bisher nicht bekannt. Es kann unsererseits nicht ausgeschlossen werden, dass es sich hier um eine bisher unbekannte Altablagerung handelt, deren Inhalt und Gr\u00f6\u00dfe uns demzufolge ebenfalls nicht bekannt sind.<br><br>Auf Seite 83 des Erl\u00e4uterungsberichtes wird auf eine geplante offene Bauwasserhaltung im Ma\u00dfnahmebereich DL 1 - Durchlass \u201eLanger Graben\u201c hingewiesen. An dieser Stelle kann nicht ausgeschlossen werden, dass das zu haltende Grundwasser durch anstromig vorhandene Altlasten mit LHKW kontaminiert ist.<br><br>Auf Grund der im Planungsgebiet weiterhin vorhandenen Schadstofffahnen sind auch bei ggf. weiteren erforderlichen Bauwasserhaltungen altlastenspezifische Schadstoffe nicht auszuschlie\u00dfen.</p>"
+        },
+        "relationships": {
+          "tags": {
+            "data": [
+              {"id": "02a9d69a-8022-449c-af19-aff03fe41ea8", "score": 0.56869, "type": "Tag"},
+              {"id": "417f3e7a-58ae-4b6c-ab6d-d284f226b171", "score": 0.51364, "type": "Tag"}
+            ]
+          }
+        }
+      },
+      {
+        "type": "Tag",
+        "id": "fb8523f8-4985-42a8-b5de-c22e5184a403",
+        "attributes": {
+          "title": "01.3 Kenntnisnahme (ohne Relevanz)"
+        }
+      }, {
+        "type": "Tag",
+        "id": "4d459668-d24c-40ff-ab7d-ff6a5105fa01",
+        "attributes": {"title": "06.8 Stra\u00dfenplanung Sonstiges"}
+      },
+      {
+        "type": "Tag",
+        "id": "7d4b3d57-6853-4b04-9610-a9fcecfb822b",
+        "attributes": {
+          "title": "06.8 Stra\u00dfenplanung Sonstiges"
+        }
+      },
+      {
+        "type": "Tag",
+        "id": "02a9d69a-8022-449c-af19-aff03fe41ea8",
+        "attributes": {
+          "title": "05 Schutzgebiete"
+        }
+      },
+      {
+        "type": "Tag",
+        "id": "417f3e7a-58ae-4b6c-ab6d-d284f226b171",
+        "attributes": {
+          "title": "05.6 Altlasten"
+        }
+      }
+    ]
+  }))
