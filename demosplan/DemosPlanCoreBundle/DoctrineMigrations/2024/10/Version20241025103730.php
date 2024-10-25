22152cb6033bfbb7ab169967beab87b07c4d5824
Moritz Mandler <mandler@demos-deutschland.de>
2024-10-25 13:01:36 +0200

    fix(refs f_BEAA2-10_working-on-addon-integration) add migration and properties with getters/setters for new global ProcedureSettings columns.

---
+++ demosplan/DemosPlanCoreBundle/DoctrineMigrations/2024/10/Version20241025103730.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types = 1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Platforms\MySQLPlatform;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+class Version20241025103730 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'refs T:BEAA2-10 add new map related properties to the procedureSettings';
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function up(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+
+        $this->addSql('ALTER TABLE _procedure_settings ADD _ps_pictogram_copyright VARCHAR(512) DEFAULT \'\' NOT NULL, ADD _ps_pictogram_alt_text VARCHAR(512) DEFAULT \'\' NOT NULL');
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function down(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+
+        $this->addSql('ALTER TABLE _procedure_settings DROP _ps_pictogram_copyright, DROP _ps_pictogram_alt_text');
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function abortIfNotMysql(): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof MySqlPlatform,
+            "Migration can only be executed safely on 'mysql'."
+        );
+    }
+}
