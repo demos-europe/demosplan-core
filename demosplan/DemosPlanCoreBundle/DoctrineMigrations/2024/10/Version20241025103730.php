4956f49ed01d2b28270fcaf4f649177dc649eaeb
Demos-CI <devops-system+github@demos-deutschland.de>
2024-10-25 11:02:06 +0000

    style: Apply php-cs-fixer

--- demosplan/DemosPlanCoreBundle/DoctrineMigrations/2024/10/Version20241025103730.php
+++ demosplan/DemosPlanCoreBundle/DoctrineMigrations/2024/10/Version20241025103730.php
@@ -1,4 +1,14 @@
-<?php declare(strict_types = 1);
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
 
 namespace Application\Migrations;
 
@@ -40,7 +50,7 @@ class Version20241025103730 extends AbstractMigration
     private function abortIfNotMysql(): void
     {
         $this->abortIf(
-            !$this->connection->getDatabasePlatform() instanceof MySqlPlatform,
+            !$this->connection->getDatabasePlatform() instanceof MySQLPlatform,
             "Migration can only be executed safely on 'mysql'."
         );
     }
