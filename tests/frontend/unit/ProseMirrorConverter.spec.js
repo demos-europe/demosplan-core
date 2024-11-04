904c76c9dff59c56fdd933b59a171e727035833c
Velimir Müller <mueller@demos-deutschland.de>
2024-11-04 10:55:07 +0100

    feat(refs DPLAN-12697): remove test from html to prosemirror conversion
    - these are not needed because we only need the html format and never transform html back to prosemirror

--- tests/frontend/unit/ProseMirrorConverter.spec.js
+++ tests/frontend/unit/ProseMirrorConverter.spec.js
@@ -14,21 +14,6 @@ describe('ProseMirrorConverter', () => {
     expect(converterWithProseMirror).toBeInstanceOf(ProseMirrorConverter)
   })
 
-  it('Should set the HTML data accordingly', () => {
-    const htmlString = `
-      <custom-html-tag>
-        ${converterData.data.relationships.draftSegments.data.map(segment => `
-        <custom-content
-          type="${converterData.data.type}"
-          id="${segment.id}"
-          draft-segments="[${converterData.data.relationships.draftSegments.data.map(s => s.id).join(', ')}]"
-          statement-id="${converterData.data.relationships.statement.data.id}">
-        </custom-content>`).join('')}
-      </custom-html-tag>`.trim()
-    const converterWithHtml = converter.fromHtml(htmlString)
-    expect(converterWithHtml).toBeInstanceOf(ProseMirrorConverter)
-  })
-
   it('Creates a valid HTML string from prosemirror data', () => {
     const proseMirrorData = converterData
     const validHTML = `
@@ -45,24 +30,5 @@ describe('ProseMirrorConverter', () => {
       </custom-html-tag>`.trim()
     const convertedProseMirrorData = converter.fromProseMirror(proseMirrorData).toHtml().getHtml()
     expect(normalizeHtmlString(convertedProseMirrorData)).toBe(normalizeHtmlString(validHTML))
-  })
-
-  it('Creates valid prosemirror data from HTML string', () => {
-    const proseMirrorData = converterData
-    const htmlString = `
-      <custom-html-tag>
-        ${proseMirrorData.data.relationships.draftSegments.data.map(segment => `
-        <custom-content
-          type="${proseMirrorData.data.type}"
-          id="${segment.id}"
-          draft-segments="[${proseMirrorData.data.relationships.draftSegments.data.map(draft => {
-            draft.id, draft.segment_text ?? 'no text'
-          }).join(', ')}]"
-          statement-id="${proseMirrorData.data.relationships.statement.data.id}"
-        </custom-content>`).join('')}
-      </custom-html-tag>`.trim()
-    const convertedHTML = converter.fromHtml(htmlString).toProseMirror()
-    expect(convertedHTML).toHaveProperty('data')
-    expect(Object.keys(convertedHTML.data)).toStrictEqual(['type', 'id', 'relationships'])
   })
 })
