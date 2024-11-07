e0895a11218760572723e76e69d0e47f902b5e00
Sami Mussbach <mussbach@demos-deutschland.de>
2024-11-07 18:18:02 +0100

    chore: fix pagerfanta pagination after major update

--- demosplan/DemosPlanCoreBundle/Utilities/Pagination/DemosPlanArrayAdapter.php
+++ demosplan/DemosPlanCoreBundle/Utilities/Pagination/DemosPlanArrayAdapter.php
@@ -17,6 +17,17 @@ class DemosPlanArrayAdapter extends ArrayAdapter
     /** @var int */
     protected $nbResults = 0;
 
+    /**
+     * The constructor is needed as parent constructor defines array variable as private
+     * and we need to access it here
+     */
+    public function __construct(
+        private readonly array $array,
+    ) {
+        parent::__construct($array);
+
+    }
+
     public function setNbResults(int $resultCount)
     {
         $this->nbResults = $resultCount;
@@ -25,6 +36,11 @@ class DemosPlanArrayAdapter extends ArrayAdapter
     public function getNbResults(): int
     {
         return $this->nbResults;
+    }
+
+    public function getSlice(int $offset, int $length): iterable
+    {
+        return $this->array;
     }
 
 }
