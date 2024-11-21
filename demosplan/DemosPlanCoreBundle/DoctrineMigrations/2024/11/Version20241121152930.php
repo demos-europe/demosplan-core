36baace615694c628b628b92ceab377634bd9f55
Catalina Sierra Zamudio <sierra-zamudio@demos-deutschland.de>
2024-11-21 16:36:06 +0100

    feat (DPLAN-12914) Add  InstitutionTagCategory entity, migration and repository

---
+++ demosplan/DemosPlanCoreBundle/DoctrineMigrations/2024/11/Version20241121152930.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types = 1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Platforms\MySQLPlatform;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+class Version20241121152930 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'refs T: DPLAN-12914 Add institution tag category';
+
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function up(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+
+        $this->addSql('CREATE TABLE institution_tag_category (id CHAR(36) NOT NULL, customer_id CHAR(36) NOT NULL, name VARCHAR(255) NOT NULL, creation_date DATETIME NOT NULL, modification_date DATETIME NOT NULL, INDEX IDX_867DAF689395C3F3 (customer_id), UNIQUE INDEX unique_category_name_for_customer (customer_id, name), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE institution_tag_category ADD CONSTRAINT FK_867DAF689395C3F3 FOREIGN KEY (customer_id) REFERENCES customer (_c_id)');
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function down(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+
+        $this->addSql('ALTER TABLE institution_tag_category DROP FOREIGN KEY FK_867DAF689395C3F3');
+        $this->addSql('DROP TABLE institution_tag_category');
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function abortIfNotMysql(): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof MySqlPlatform,
+            "Migration can only be executed safely on 'mysql'."
+        );
+    }
+}
---
+++ demosplan/DemosPlanCoreBundle/Repository/InstitutionTagCategoryRepository.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
+namespace demosplan\DemosPlanCoreBundle\Repository;
+
+use demosplan\DemosPlanCoreBundle\Entity\User\InstitutionTag;
+use demosplan\DemosPlanCoreBundle\Entity\User\InstitutionTagCategory;
+use demosplan\DemosPlanCoreBundle\Exception\ResourceNotFoundException;
+use demosplan\DemosPlanCoreBundle\Repository\IRepository\ObjectInterface;
+use Exception;
+
+/**
+ * @template-extends CoreRepository<InstitutionTagCategory>
+ */
+class InstitutionTagCategoryRepository extends CoreRepository implements ObjectInterface
+{
+    /**
+     * @return InstitutionTagCategory|null
+     */
+    public function get($entityId)
+    {
+        return $this->find($entityId);
+    }
+
+    /**
+     * @param InstitutionTagCategory $institutionTagCategory
+     *
+     * @return InstitutionTagCategory
+     */
+    public function addObject($institutionTagCategory)
+    {
+        $manager = $this->getEntityManager();
+        $manager->persist($institutionTagCategory);
+        $manager->flush();
+
+        return $institutionTagCategory;
+    }
+
+    /**
+     * @param InstitutionTagCategory $institutionTagCategory
+     *
+     * @return InstitutionTagCategory
+     */
+    public function updateObject($institutionTagCategory)
+    {
+        $em = $this->getEntityManager();
+        $em->persist($institutionTagCategory);
+        $em->flush();
+
+        return $institutionTagCategory;
+    }
+
+    /**
+     * @param string $institutionTagCategoryId
+     *
+     * @return bool
+     *
+     * @throws ResourceNotFoundException
+     */
+    public function delete($institutionTagCategoryId)
+    {
+        $tag = $this->get($institutionTagCategoryId);
+        if (!$tag instanceof InstitutionTagCategory) {
+            throw new ResourceNotFoundException("InstitutionTag with ID {$institutionTagCategoryId} was not found.");
+        }
+
+        return $this->deleteObject($tag);
+    }
+
+    /**
+     * @param InstitutionTagCategory $institutionTagCategory
+     *
+     * @return bool
+     */
+    public function deleteObject($institutionTagCategory)
+    {
+        try {
+            $this->getEntityManager()->remove($institutionTagCategory);
+            $this->getEntityManager()->flush();
+
+            return true;
+        } catch (Exception $e) {
+            $this->logger->error('Delete statementVote failed: ', [$e]);
+        }
+
+        return false;
+    }
+}
