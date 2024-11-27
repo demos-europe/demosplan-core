807af722741fd0ca740d0ea0f0ef37e8fcb1f855
breierDemos <breier@demos-deutschland.de>
2024-11-27 16:57:54 +0100

    feat (refs DPLAN-12914): Make name of InstitutionTagCategory updatable.

--- demosplan/DemosPlanCoreBundle/ResourceTypes/InstitutionTagCategoryResourceType.php
+++ demosplan/DemosPlanCoreBundle/ResourceTypes/InstitutionTagCategoryResourceType.php
@@ -43,7 +43,8 @@ class InstitutionTagCategoryResourceType extends DplanResourceType
             ->setReadableByPath();
         $institutionTagCategoryConfig->name
             ->setReadableByPath()
-            ->addPathCreationBehavior();
+            ->addPathCreationBehavior()
+            ->addPathUpdateBehavior();
 
         $institutionTagCategoryConfig->customer
             ->setRelationshipType($this->resourceTypeStore->getCustomerResourceType());
