defe8448314027500564bf5a9562d9b7c8b26798
bakridemos <102021913+bakridemos@users.noreply.github.com>
2024-11-29 11:47:07 +0100

    fix-DPLAN-12816-retain-layers-preselection-after-saving-or-updating-layers (4064)
    * f DPLAN-12816 adjust condition :
    - 'VisibilityGroupId' can be null (default value) or can be an empty string. Layers in the same group can be updated only when 'VisibilityGroupId' is not null and has to be not empty too otherwise the layer does not have any group. That's why the condition has to be adjusted to have the expected behavior.
    - the 'getVisibilityGroupId' method is adjusted too : we must retrieve only layers that's belong to the current procedure.
     Using only the 'visibilityGroupId' to get procedure relevant layers leads to have wrong result and updating layers that's does not belong to the current procedure
    
    Signed-off-by: Hamza Bakri <bakri@demos-deutschland.de>
    
    * style: Apply php-cs-fixer
    
    ---------
    
    Signed-off-by: Hamza Bakri <bakri@demos-deutschland.de>
    Co-authored-by: Demos-CI <devops-system+github@demos-deutschland.de>

--- demosplan/DemosPlanCoreBundle/Logic/Map/MapService.php
+++ demosplan/DemosPlanCoreBundle/Logic/Map/MapService.php
@@ -84,7 +84,7 @@ class MapService extends CoreService
         private readonly MapRepository $mapRepository,
         MapScreenshotter $mapScreenshotter,
         private readonly MasterTemplateService $masterTemplateService,
-        private readonly StatementService $statementService
+        private readonly StatementService $statementService,
     ) {
         $this->fileService = $fileService;
         $this->httpCall = $httpCall;
@@ -717,9 +717,9 @@ class MapService extends CoreService
      *
      * @throws Exception
      */
-    public function getVisibilityGroup($visibilityGroupId)
+    public function getVisibilityGroup($visibilityGroupId, $procedureId)
     {
-        return $this->mapRepository->getByVisibilityGroupId($visibilityGroupId);
+        return $this->mapRepository->getByVisibilityGroupId($visibilityGroupId, $procedureId);
     }
 
     /**
@@ -727,7 +727,7 @@ class MapService extends CoreService
      *
      * @throws Exception
      */
-    public function getMapOptions(string $procedureId = null): MapOptions
+    public function getMapOptions(?string $procedureId = null): MapOptions
     {
         $procedureId ??= $this->procedureService->calculateCopyMasterId(null);
 
