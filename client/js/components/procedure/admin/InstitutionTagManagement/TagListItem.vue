9f8bb75279f6f52b25ecb2bcc4993d14251f4e2e
Heidrun Wiemold <wiemold@demos-deutschland.de>
2024-12-06 10:04:34 +0100

    fix(refs DPLAN-12981, #AB17874): remove redundant requests
    when deleting a category that has tags, the deletion of the tags is
    handled in the BE, so we don't need to send separate delete requests for
    each tag

--- client/js/components/procedure/admin/InstitutionTagManagement/TagListItem.vue
+++ client/js/components/procedure/admin/InstitutionTagManagement/TagListItem.vue
@@ -176,13 +176,10 @@ export default {
     },
 
     deleteCategoryAndTags () {
-      const { id, children, name } = this.item
-      const promises = [
-        this.deleteCategory(id),
-        ...children.map(tag => this.deleteTag(tag))
-      ]
+      const { id, name } = this.item
 
-      Promise.allSettled(promises)
+      // Tags are deleted in BE along with category
+      this.deleteInstitutionTagCategory(id)
         .then(() => {
           dplan.notify.confirm(Translator.trans('confim.category_and_tags.deleted', { category: name }))
           this.$emit('item:deleted', id)
