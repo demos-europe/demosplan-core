c5deb8aaf1ff3e926161f03b3f2b6fab403a82ef
Sami Mussbach <mussbach@demos-deutschland.de>
2024-12-11 15:43:24 +0100

    chore: ensure integer as page limit

--- demosplan/DemosPlanCoreBundle/Logic/Statement/StatementService.php
+++ demosplan/DemosPlanCoreBundle/Logic/Statement/StatementService.php
@@ -3046,7 +3046,7 @@ class StatementService extends CoreService implements StatementServiceInterface
                 $limit = $defaultLimits[0];
             }
 
-            $paginator->setMaxPerPage($limit);
+            $paginator->setMaxPerPage((int)$limit);
             // try to paginate Result, check for validity
             try {
                 $paginator->setCurrentPage($page);
