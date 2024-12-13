140e005b7b6b3a7f29fca2debf16fbea42519534
Velimir Müller <mueller@demos-deutschland.de>
2024-12-13 11:19:38 +0100

    feat(refs DPLAN-12013): add topic and tag row components

---
+++ client/js/components/TagListTagRow.vue
@@ -0,0 +1,75 @@
+<template>
+  <tr class="border-b">
+    <td class="checkbox u-ph-0 u-pb-0_25 u-pt-0_5 text-left">
+      <input type="hidden" :name="`${tag.id}:r_itemtype`" value="tag">
+
+      <label class="u-m-0">
+        <input
+          type="checkbox"
+          :name="`${tag.id}:r_selected`"
+          data-cy="listTags:selectTopic"
+          data-checkable-item>
+      </label>
+    </td>
+    <td><a class="u-1-of-1 block o-hellip">{{ tag.title }}</a></td>
+    <td class="text-center relative">
+      <div class="relative">
+        <dp-contextual-help
+          v-if="tag.boilerplate"
+          class="color--grey block"
+          icon="file"
+          :text="tag.boilerplate">
+        </dp-contextual-help>
+      </div>
+    </td>
+
+    <td class="text-right">
+      <tags-list-controls
+        :tag="tag"
+        tag-type="tag"
+        @toggleInputs="evt => handleControlEvent(evt)" />
+    </td>
+  </tr>
+</template>
+
+<script>
+import TagsListControls from "./TagsListControls.vue";
+import { DpContextualHelp } from '@demos-europe/demosplan-ui/';
+
+export default {
+  name: 'TagListTagRow',
+  components: {
+    DpContextualHelp,
+    TagsListControls
+  },
+
+  props: {
+    tag: {
+      type: Object,
+      required: true
+    }
+  },
+
+  data () {
+    return {
+      isVisible: {
+        move: false,
+        rename: false
+      }
+    }
+  },
+
+  methods: {
+    handleControlEvent (event) {
+      console.log(event)
+      if (event.type === 'tag' && event.action === 'move') {
+        this.isVisible.move = true
+        this.isVisible.rename = false
+      } else if (event.action === 'rename') {
+        this.isVisible.move = false
+        this.isVisible.rename = true
+      }
+    }
+  },
+}
+</script>
---
+++ client/js/components/TagListTopicRow.vue
@@ -0,0 +1,118 @@
+<template>
+  <tr class="border-b">
+    <td class="checkbox u-ph-0 u-pb-0_25 u-pt-0_5 text-left">
+      <input type="hidden" :name="`${topic.id}:r_itemtype`" value="topic">
+
+      <label class="u-m-0">
+        <input
+          type="checkbox"
+          :name="`${topic.id}:r_selected`"
+          data-cy="listTags:selectTopic"
+          data-checkable-item>
+      </label>
+    </td>
+
+    <td class="weight--bold">
+      {{topic.name }}
+
+      <div class="overflow-hidden">
+        <div v-if="isInsertVisible" class="o-toggle__target u-mv-0_25 flex space-inline-s" :data-toggle-id="`insert-${topic.id}`">
+          <input
+            :data-form-actions-submit-target="`#topicInsertBtn-${topic.id}`"
+            class="o-form__control-input u-2-of-5"
+            type="text"
+            data-cy="nameNewTag"
+            :name="`${topic.id}:r_newtags`"
+            :placeholder="Translator.trans('tag.name')"
+            :aria-label="Translator.trans('tag.name')">
+
+          <button
+            class="btn btn--primary"
+            name="r_createtags"
+            data-cy="createNewTag"
+            :value="topic.id"
+            :id="`topicInsertBtn-${topic.id}`">
+            {{ Translator.trans('topic.insertTag') }}
+          </button>
+        </div>
+      </div>
+
+      <div class="overflow-hidden">
+        <div v-if="isRenameVisible" class="o-toggle__target u-mv-0_25 flex space-inline-s" :data-toggle-id="`rename-${topic.id}`">
+          <input
+            data-cy="renameTopicField"
+            v-if="isVisible.rename"
+            :data-form-actions-submit-target="`#topicRenameBtn-${topic.id}`"
+            class="o-form__control-input u-2-of-5"
+            type="text"
+            :value="topic.title"
+            :name="`${topic.id}:r_rename`">
+
+          <button
+            class="btn btn--primary"
+            name="r_renametopic"
+            data-cy="renameTopicSave"
+            :value="topic.id"
+            :id="`topicRenameBtn-${topic.id}`">
+            {{ Translator.trans('topic.rename') }}
+          </button>
+        </div>
+      </div>
+    </td>
+    <td />
+    <td class="text-right">
+      <tags-list-controls
+        :tag="topic"
+        tag-type="topic"
+        @toggleInputs="evt => handleControlEvent(evt)" />
+    </td>
+  </tr>
+</template>
+<script>
+import TagsListControls from './TagsListControls'
+
+export default {
+  name: 'TagListTopicRow',
+  components: {
+    TagsListControls
+  },
+
+  props: {
+    topic: {
+      type: Object,
+      required: true
+    }
+  },
+
+  data () {
+    return {
+      isVisible: {
+        insert: false,
+        rename: false
+      }
+    }
+  },
+
+  methods: {
+    handleControlEvent (event) {
+      console.log(event)
+      if (event.type === 'topic' && event.action === 'insert') {
+        this.isVisible.insert = true
+        this.isVisible.rename = false
+      } else if (event.action === 'rename') {
+        this.isVisible.insert = false
+        this.isVisible.rename = true
+      }
+    }
+  },
+
+  computed: {
+    isInsertVisible () {
+      return this.isVisible?.insert ?? false
+    },
+    isRenameVisible () {
+      return this.isVisible?.rename ?? false
+    }
+  },
+}
+</script>
