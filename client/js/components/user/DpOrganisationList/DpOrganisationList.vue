7667433f22bceefa6e407740faefbab3b128a1ce
Natalija Sakuta <sakuta@demos-deutschland.de>
2025-01-22 16:47:26 +0100

    fix(DPLAN-13082): fetch organisations only after all ids are deleted from the store

--- client/js/components/user/DpOrganisationList/DpOrganisationList.vue
+++ client/js/components/user/DpOrganisationList/DpOrganisationList.vue
@@ -360,15 +360,19 @@ export default {
         return
       }
 
-      ids.forEach(id => {
+      const deleteOrganisations = ids.map(id =>
         this.deleteOrganisation(id)
           .then(() => {
             // Remove deleted item from itemSelections
             delete this.itemSelections[id]
             // Confirm notification for organisations is done in BE
-            this.getItemsByPage()
           })
-      })
+      )
+
+      Promise.all(deleteOrganisations)
+        .then(() => {
+          this.getItemsByPage()
+        })
     },
 
     fetchFilteredOrganisations (selected, page) {
