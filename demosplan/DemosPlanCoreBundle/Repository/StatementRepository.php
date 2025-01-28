225d2b451381ea4f4966c7d1718c0a03751f74f6
Demos-CI <devops-system+github@demos-deutschland.de>
2025-01-28 16:28:11 +0000

    style: Apply php-cs-fixer

--- demosplan/DemosPlanCoreBundle/Repository/StatementRepository.php
+++ demosplan/DemosPlanCoreBundle/Repository/StatementRepository.php
@@ -46,7 +46,6 @@ use demosplan\DemosPlanCoreBundle\Exception\StatementAlreadyConnectedToGdprConse
 use demosplan\DemosPlanCoreBundle\Logic\ApiRequest\FluentStatementQuery;
 use demosplan\DemosPlanCoreBundle\Logic\FileService;
 use demosplan\DemosPlanCoreBundle\Logic\User\CustomerService;
-use demosplan\DemosPlanCoreBundle\Repository\ConsultationTokenRepository;
 use demosplan\DemosPlanCoreBundle\Repository\IRepository\ArrayInterface;
 use demosplan\DemosPlanCoreBundle\Repository\IRepository\ObjectInterface;
 use Doctrine\Common\Collections\ArrayCollection;
@@ -78,13 +77,13 @@ use function array_combine;
 class StatementRepository extends CoreRepository implements ArrayInterface, ObjectInterface
 {
     public function __construct(
-        DqlConditionFactory                       $dqlConditionFactory,
-        Reindexer                                 $reindexer,
+        DqlConditionFactory $dqlConditionFactory,
+        Reindexer $reindexer,
         private readonly EventDispatcherInterface $eventDispatcher,
-        ManagerRegistry                           $registry,
-        SortMethodFactory                         $sortMethodFactory,
-        string                                    $entityClass,
-        private readonly CustomerService          $customerService, private readonly ProcedureCoupleTokenRepository $coupleTokenRepository,
+        ManagerRegistry $registry,
+        SortMethodFactory $sortMethodFactory,
+        string $entityClass,
+        private readonly CustomerService $customerService, private readonly ProcedureCoupleTokenRepository $coupleTokenRepository,
     ) {
         parent::__construct($dqlConditionFactory, $registry, $reindexer, $sortMethodFactory, $entityClass);
     }
@@ -1276,7 +1275,6 @@ class StatementRepository extends CoreRepository implements ArrayInterface, Obje
      */
     public function getNextValidExternalIdForProcedure(string $procedureId): int
     {
-
         $token = $this->coupleTokenRepository->findOneBy(['sourceProcedure' => $procedureId]);
         if (null !== $token) {
             $coupledProcedure = $token->getTargetProcedure();
@@ -1284,7 +1282,6 @@ class StatementRepository extends CoreRepository implements ArrayInterface, Obje
             $token = $this->coupleTokenRepository->findOneBy(['targetProcedure' => $procedureId]);
             $coupledProcedure = $token->getSourceProcedure();
         }
-
 
         if (null !== $coupledProcedure) {
             $coupledProcedureId = $coupledProcedure->getId();
@@ -1309,8 +1306,6 @@ class StatementRepository extends CoreRepository implements ArrayInterface, Obje
                 ->getQuery();
             $statementArrays = $query1->getResult();
         }
-
-
 
         $query2 = $this->getEntityManager()->createQueryBuilder()
             ->select('draftStatement.number')
