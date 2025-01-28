56231853fbcc2097d7dce605b8dfe150a02adc26
hwiem <40487668+hwiem@users.noreply.github.com>
2025-01-28 16:47:53 +0100

    cherry-pick from e226455cb25a136db34172c31d0249605401ae43

--- client/js/store/core/initStore.js
+++ client/js/store/core/initStore.js
@@ -72,31 +72,45 @@ function initStore (storeModules, apiStoreModules, presetStoreModules) {
             },
             successCallbacks: [
               async (success) => {
-                const response = await success.json()
+                // If the response body is empty, contentType will be null
+                const contentType = success.headers.get('Content-Type')
 
-                const meta = response.data?.meta
-                  ? response.data.meta
-                  : response.meta || null
-                if (meta?.messages) {
-                  handleResponseMessages(meta)
+                if (contentType && contentType.includes('application/json')) {
+                  const response = await success.json()
+
+                  const meta = response.data?.meta
+                    ? response.data.meta
+                    : response.meta || null
+                  if (meta?.messages) {
+                    handleResponseMessages(meta)
+                  }
+
+                  return Promise.resolve(response)
                 }
 
-                return Promise.resolve(response)
+                return Promise.resolve(success)
               }
             ],
             errorCallbacks: [
               async (error) => {
-                const response = await error.json()
+                // If the response body is empty, contentType will be null
+                const contentType = error.headers.get('Content-Type')
 
-                const meta = response.data?.meta
-                  ? response.data.meta
-                  : response.meta || null
+                if (contentType && contentType.includes('application/json')) {
+                  const response = await error.json()
 
-                if (meta?.messages) {
-                  handleResponseMessages(meta)
+                  const meta = response.data?.meta
+                    ? response.data.meta
+                    : response.meta || null
+
+                  if (meta?.messages) {
+                    handleResponseMessages(meta)
+                  }
+
+                  return Promise.reject(response)
                 }
 
-                return Promise.reject(response)
+                return Promise.reject(error)
               }
             ]
           }),
