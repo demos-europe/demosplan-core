433012573f9d306877db3bfd2dd94562f5d664e1
Steffi <101879352+gruenbergerdemos@users.noreply.github.com>
2025-02-10 13:29:57 +0100

    bug(refs DPLAN-12402): Remove redundant error message (4319)
    We don't need the error notification in the else block, because data-dp-validate in the form already throws an error if it is not validated.

--- client/js/components/assessmenttable/DpNewStatement.vue
+++ client/js/components/assessmenttable/DpNewStatement.vue
@@ -250,8 +250,6 @@ export default {
     submit () {
       if (this.dpValidate.newStatementForm) {
         document.querySelector('[data-dp-validate="newStatementForm"]').submit()
-      } else {
-        dplan.notify.notify('error', Translator.trans('error.mandatoryfields.no_asterisk'))
       }
     },
 
