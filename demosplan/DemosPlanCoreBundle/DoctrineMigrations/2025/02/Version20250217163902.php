ef053723dc842d401b5e04baf9d728bfc22d5a31
Sami Mussbach <mussbach@demos-deutschland.de>
2025-02-17 17:45:18 +0100

    chore (refs DPLAN-15001): add index to files table

---
+++ demosplan/DemosPlanCoreBundle/DoctrineMigrations/2025/02/Version20250217163902.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types = 1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Platforms\MySQLPlatform;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+class Version20250217163902 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'refs DPLAN-15001: Add index on _files._f_hash';
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function up(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+
+        $this->addSql('CREATE INDEX IF NOT EXISTS IDX_8C0DACC5DCE34FD9 ON _files (_f_hash)');
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function down(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+
+        $this->addSql('DROP INDEX IF EXISTS IDX_8C0DACC5DCE34FD9 ON _files');
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function abortIfNotMysql(): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof MySQLPlatform,
+            "Migration can only be executed safely on 'mysql'."
+        );
+    }
+}
