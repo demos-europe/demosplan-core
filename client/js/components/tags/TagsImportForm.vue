96b3c952cfdae51b83e7d85e23241172e9be4bbb
sakutademos <110987766+sakutademos@users.noreply.github.com>
2025-02-21 15:42:03 +0100

    fix(DPLAN-15045): add hint and adjust position of the tooltip (#4362)
    fix(DPLAN-15045): add hint and adjust position of the tooltip in the additional PR in the ui

--- client/js/components/tags/TagsImportForm.vue
+++ client/js/components/tags/TagsImportForm.vue
@@ -12,13 +12,10 @@
         :value="dplan.csrfToken">
 
       <fieldset class="flow-root pb-1">
-        <dp-contextual-help
-          class="float-right"
-          :text="Translator.trans('tags.import.help')"
-        ></dp-contextual-help>
         <dp-label
           :text="Translator.trans('tags.import')"
           for="uploadTags"
+          :hint="Translator.trans('tags.import.hint')"
           :tooltip="Translator.trans('tags.import.help')" />
         <a download :href="availableEntity.exampleFile" target="_blank">{{ Translator.trans('example.file') }}</a>
         <dp-upload
