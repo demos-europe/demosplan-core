28c233dfbf4b378a46fe00f9b23fe98a61d171b7
Florian Salis <salis@demos-deutschland.de>
2025-03-04 15:07:41 +0100

    fix(refs dplan-14909): Always send assignee and place when setting one.
    For some reason, sometimes, when both assignee and place changed, only place where send. Because it happens soemtimes, I think it is some sort of race condition between the request and an update of the initial segment. Because I monitored that the initial segment gets updated aswell even though, It should not at that very moment.
    
    To prevent all that, I decided to bypass the vuex-json-api and update the resourcetype "manually".

--- client/js/components/procedure/StatementSegmentsList/StatementSegment.vue
+++ client/js/components/procedure/StatementSegmentsList/StatementSegment.vue
@@ -623,7 +623,7 @@ export default {
         }
       }
 
-      return dpApi.patch(Routing.generate('api_resource_update', { resourceType: 'StatementSegment', resourceId: this.segment.id }), {}, payload)
+      dpApi.patch(Routing.generate('api_resource_update', { resourceType: 'StatementSegment', resourceId: this.segment.id }), {}, payload)
         .then(checkResponse)
         .then(() => {
           this.claimLoading = false
@@ -678,9 +678,18 @@ export default {
 
     save () {
       const comments = this.segment.relationships.comments ? { ...this.segment.relationships.comments } : null
+      const rel = this.updateRelationships()
 
-      this.updateRelationships()
-      return this.saveSegmentAction(this.segment.id)
+      const payload = {
+        data: {
+          id: this.segment.id,
+          type: 'StatementSegment',
+          relationships: { assignee: rel.assignee, place: rel.place }
+        }
+      }
+
+      dpApi.patch(Routing.generate('api_resource_update', { resourceType: 'StatementSegment', resourceId: this.segment.id }), {}, payload)
+        .then(checkResponse)
         .then(() => {
           /*
            * @improve - once the vuex-json-api resolves with a response,
@@ -863,9 +872,12 @@ export default {
       }
 
       this.setSegment({ ...updated, id: this.segment.id })
+
+      return relations
     },
 
     updateSegment (key, val) {
+      console.log('updateSegment', key, val)
       const updated = { ...this.segment, ...{ attributes: { ...this.segment.attributes, ...{ [key]: val } } } }
       this.setSegment({ ...updated, id: this.segment.id })
     }
