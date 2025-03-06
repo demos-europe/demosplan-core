8970e493afcb0f8e3ff5ed3df2f90a8a140695f0
Steffi <101879352+gruenbergerdemos@users.noreply.github.com>
2025-03-06 13:49:16 +0100

    Update client/js/components/procedure/StatementSegmentsList/StatementMeta/StatementMetaFinalEmail.vue
    Co-authored-by: hwiem <40487668+hwiem@users.noreply.github.com>

--- client/js/components/procedure/StatementSegmentsList/StatementMeta/StatementMetaFinalEmail.vue
+++ client/js/components/procedure/StatementSegmentsList/StatementMeta/StatementMetaFinalEmail.vue
@@ -180,6 +180,7 @@ export default {
     explanationNoSendingEmail () {
       const { authorFeedback, feedback, publicStatement } = this.statement.attributes
       let sendFinalEmail = false
+      
       if ((feedback === 'snailmail' && this.statement.relationships.votes.data) ||
           feedback === 'email') {
         sendFinalEmail = true
