595786dc547cc19914aca092f928da6c2dc3ef0b
Alexander Marten <marten@demos-deutschland.de>
2025-03-09 14:40:07 +0100

    feat (refs DPLAN-14783): Cover update/deletion/creation of elements, singleDocument or Paragraph via resourceTypes by using EDTEvents

--- demosplan/DemosPlanCoreBundle/EventSubscriber/ReportSubscriber.php
+++ demosplan/DemosPlanCoreBundle/EventSubscriber/ReportSubscriber.php
@@ -10,11 +10,23 @@
 
 namespace demosplan\DemosPlanCoreBundle\EventSubscriber;
 
+use demosplan\DemosPlanCoreBundle\Entity\Document\Elements;
+use demosplan\DemosPlanCoreBundle\Entity\Document\Paragraph;
+use demosplan\DemosPlanCoreBundle\Entity\Document\SingleDocument;
+use demosplan\DemosPlanCoreBundle\Entity\Report\ReportEntry;
 use demosplan\DemosPlanCoreBundle\Entity\User\User;
 use demosplan\DemosPlanCoreBundle\Event\Procedure\ProcedureEditedEvent;
 use demosplan\DemosPlanCoreBundle\Event\StatementAnonymizeRpcEvent;
+use demosplan\DemosPlanCoreBundle\Logic\Report\ElementReportEntryFactory;
+use demosplan\DemosPlanCoreBundle\Logic\Report\ParagraphReportEntryFactory;
 use demosplan\DemosPlanCoreBundle\Logic\Report\ProcedureReportEntryFactory;
 use demosplan\DemosPlanCoreBundle\Logic\Report\ReportService;
+use demosplan\DemosPlanCoreBundle\Logic\Report\SingleDocumentReportEntryFactory;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use EDT\JsonApi\Event\AfterCreationEvent;
+use EDT\JsonApi\Event\AfterDeletionEvent;
+use EDT\JsonApi\Event\AfterUpdateEvent;
 use Exception;
 
 class ReportSubscriber extends BaseEventSubscriber
@@ -26,6 +38,9 @@ class ReportSubscriber extends BaseEventSubscriber
 
     public function __construct(
         private readonly ProcedureReportEntryFactory $procedureReportEntryFactory,
+        private readonly ElementReportEntryFactory $elementReportEntryFactory,
+        private readonly SingleDocumentReportEntryFactory $singleDocumentReportEntryFactory,
+        private readonly ParagraphReportEntryFactory $paragraphReportEntryFactory,
         ReportService $reportService
     ) {
         $this->reportService = $reportService;
@@ -37,8 +52,11 @@ class ReportSubscriber extends BaseEventSubscriber
     public static function getSubscribedEvents(): array
     {
         return [
-            ProcedureEditedEvent::class       => ['onEditProcedure'],
-            StatementAnonymizeRpcEvent::class => ['onStatementAnonymizeRpc'],
+            AfterUpdateEvent::class             => ['afterUpdate'],
+            AfterDeletionEvent::class           => ['afterDeletion'],
+            AfterCreationEvent::class           => ['afterCreation'],
+            ProcedureEditedEvent::class         => ['onEditProcedure'],
+            StatementAnonymizeRpcEvent::class   => ['onStatementAnonymizeRpc'],
         ];
     }
 
@@ -76,4 +94,119 @@ class ReportSubscriber extends BaseEventSubscriber
     {
         $this->reportService->addReportsOnStatementAnonymization($event);
     }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException|\Doctrine\ORM\ORMException
+     */
+    public function afterUpdate(AfterUpdateEvent $event): void
+    {
+        switch ($event->getType()->getName()) {
+            case 'Elements':
+                /** @var Elements $element */
+                $element = $event->getEntity();
+                $report = $this->elementReportEntryFactory->createElementEntry(
+                    $element,
+                    ReportEntry::CATEGORY_UPDATE,
+                    $element->getModifyDate()->getTimestamp()
+                );
+                break;
+            case 'SingleDocument':
+                /** @var SingleDocument $singleDocument */
+                $singleDocument = $event->getEntity();
+                $report = $this->singleDocumentReportEntryFactory->createSingleDocumentEntry(
+                    $singleDocument,
+                    ReportEntry::CATEGORY_UPDATE,
+                    $singleDocument->getModifyDate()->getTimestamp()
+                );
+                break;
+            case 'Paragraph':
+                /** @var Paragraph $paragraph */
+                $paragraph = $event->getEntity();
+                $report = $this->paragraphReportEntryFactory->createParagraphEntry(
+                    $paragraph,
+                    ReportEntry::CATEGORY_UPDATE,
+                    $paragraph->getModifyDate()->getTimestamp()
+                );
+                break;
+        }
+
+        $this->reportService->persistAndFlushReportEntries($report);
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function afterCreation(AfterCreationEvent $event): void
+    {
+        switch ($event->getType()->getName()) {
+            case 'Elements':
+                /** @var Elements $element */
+                $element = $event->getEntity();
+                $report = $this->elementReportEntryFactory->createElementEntry(
+                    $element,
+                    ReportEntry::CATEGORY_ADD,
+                    $element->getCreateDate()->getTimestamp()
+                );
+                break;
+            case 'SingleDocument':
+                /** @var SingleDocument $singleDocument */
+                $singleDocument = $event->getEntity();
+                $report = $this->singleDocumentReportEntryFactory->createSingleDocumentEntry(
+                    $singleDocument,
+                    ReportEntry::CATEGORY_ADD,
+                    $singleDocument->getCreateDate()->getTimestamp()
+                );
+                break;
+            case 'Paragraph':
+                /** @var Paragraph $paragraph */
+                $paragraph = $event->getEntity();
+                $report = $this->paragraphReportEntryFactory->createParagraphEntry(
+                    $paragraph,
+                    ReportEntry::CATEGORY_ADD,
+                    $paragraph->getCreateDate()->getTimestamp()
+                );
+                break;
+        }
+
+        $this->reportService->persistAndFlushReportEntries($report);
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function afterDeletion(AfterCreationEvent $event): void
+    {
+        switch ($event->getType()->getName()) {
+            case 'Elements':
+                /** @var Elements $element */
+                $element = $event->getEntity();
+                $report = $this->elementReportEntryFactory->createElementEntry(
+                    $element,
+                    ReportEntry::CATEGORY_DELETE,
+                );
+                break;
+            case 'SingleDocument':
+                /** @var SingleDocument $singleDocument */
+                $singleDocument = $event->getEntity();
+                $report = $this->singleDocumentReportEntryFactory->createSingleDocumentEntry(
+                    $singleDocument,
+                    ReportEntry::CATEGORY_DELETE,
+                );
+                break;
+            case 'Paragraph':
+                /** @var Paragraph $paragraph */
+                $paragraph = $event->getEntity();
+                $report = $this->paragraphReportEntryFactory->createParagraphEntry(
+                    $paragraph,
+                    ReportEntry::CATEGORY_DELETE,
+                );
+                break;
+        }
+
+        $this->reportService->persistAndFlushReportEntries($report);
+    }
+
 }
