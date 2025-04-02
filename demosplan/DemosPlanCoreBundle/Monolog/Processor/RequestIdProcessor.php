ee54a835243b9d92568fc3c17611ef693d9372ed
Demos-CI <devops-system+github@demos-deutschland.de>
2025-04-02 18:00:00 +0000

    style: Apply php-cs-fixer

--- demosplan/DemosPlanCoreBundle/Monolog/Processor/RequestIdProcessor.php
+++ demosplan/DemosPlanCoreBundle/Monolog/Processor/RequestIdProcessor.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
 namespace demosplan\DemosPlanCoreBundle\Monolog\Processor;
 
 use Monolog\Processor\ProcessorInterface;
@@ -21,16 +29,18 @@ class RequestIdProcessor implements ProcessorInterface
     public function __invoke(array $record): array
     {
         // If we already generated an ID for this request instance, reuse it
-        if ($this->requestId !== null) {
+        if (null !== $this->requestId) {
             $record['extra']['rid'] = $this->requestId;
+
             return $record;
         }
 
         $request = $this->requestStack->getCurrentRequest();
         if (!$request) {
             // If not in a request context (e.g., console), generate a shorter one-time ID
-            $this->requestId = 'c' . base_convert(random_int(0, 1679615), 10, 36) . base_convert(time(), 10, 36);
+            $this->requestId = 'c'.base_convert(random_int(0, 1679615), 10, 36).base_convert(time(), 10, 36);
             $record['extra']['rid'] = $this->requestId;
+
             return $record;
         }
 
@@ -39,12 +49,13 @@ class RequestIdProcessor implements ProcessorInterface
             $this->requestId = $request->headers->get('X-Request-ID');
         } else {
             // Generate a shorter, more concise ID (base36 encoding of timestamp+random)
-            $this->requestId = base_convert(random_int(0, 1679615), 10, 36) . base_convert(time(), 10, 36);
+            $this->requestId = base_convert(random_int(0, 1679615), 10, 36).base_convert(time(), 10, 36);
             // Store on request for middleware/controllers to access
             $request->attributes->set('request_id', $this->requestId);
         }
 
         $record['extra']['rid'] = $this->requestId;
+
         return $record;
     }
 }
--- tests/backend/core/Monolog/Unit/RequestIdProcessorTest.php
+++ tests/backend/core/Monolog/Unit/RequestIdProcessorTest.php
@@ -32,19 +32,19 @@ class RequestIdProcessorTest extends TestCase
         // Arrange
         $requestStack = $this->createMock(RequestStack::class);
         $requestStack->method('getCurrentRequest')->willReturn(null);
-        
+
         $processor = new RequestIdProcessor($requestStack);
         $record = ['message' => 'Test message', 'extra' => []];
-        
+
         // Act
         $result = $processor($record);
-        
+
         // Assert
         $this->assertArrayHasKey('rid', $result['extra']);
         // Request ID for CLI should start with 'c'
         $this->assertStringStartsWith('c', $result['extra']['rid']);
     }
-    
+
     /**
      * Test that the processor reuses the same request ID for multiple log records.
      */
@@ -53,19 +53,19 @@ class RequestIdProcessorTest extends TestCase
         // Arrange
         $requestStack = $this->createMock(RequestStack::class);
         $requestStack->method('getCurrentRequest')->willReturn(null);
-        
+
         $processor = new RequestIdProcessor($requestStack);
         $record1 = ['message' => 'Test message 1', 'extra' => []];
         $record2 = ['message' => 'Test message 2', 'extra' => []];
-        
+
         // Act
         $result1 = $processor($record1);
         $result2 = $processor($record2);
-        
+
         // Assert
         $this->assertSame($result1['extra']['rid'], $result2['extra']['rid']);
     }
-    
+
     /**
      * Test that the processor uses the X-Request-ID header if it exists.
      */
@@ -75,23 +75,23 @@ class RequestIdProcessorTest extends TestCase
         $request = $this->createMock(Request::class);
         $headers = $this->createMock(HeaderBag::class);
         $request->headers = $headers;
-        
+
         $headers->method('has')->with('X-Request-ID')->willReturn(true);
         $headers->method('get')->with('X-Request-ID')->willReturn('test-request-id');
-        
+
         $requestStack = $this->createMock(RequestStack::class);
         $requestStack->method('getCurrentRequest')->willReturn($request);
-        
+
         $processor = new RequestIdProcessor($requestStack);
         $record = ['message' => 'Test message', 'extra' => []];
-        
+
         // Act
         $result = $processor($record);
-        
+
         // Assert
         $this->assertEquals('test-request-id', $result['extra']['rid']);
     }
-    
+
     /**
      * Test that the processor generates a new ID and stores it on the request.
      */
@@ -103,28 +103,28 @@ class RequestIdProcessorTest extends TestCase
         $attributes = $this->createMock(ParameterBag::class);
         $request->headers = $headers;
         $request->attributes = $attributes;
-        
+
         $headers->method('has')->with('X-Request-ID')->willReturn(false);
-        
+
         // We expect the processor to set the request_id attribute
         $attributes->expects($this->once())
             ->method('set')
             ->with($this->equalTo('request_id'), $this->anything());
-        
+
         $requestStack = $this->createMock(RequestStack::class);
         $requestStack->method('getCurrentRequest')->willReturn($request);
-        
+
         $processor = new RequestIdProcessor($requestStack);
         $record = ['message' => 'Test message', 'extra' => []];
-        
+
         // Act
         $result = $processor($record);
-        
+
         // Assert
         $this->assertArrayHasKey('rid', $result['extra']);
         $this->assertNotEmpty($result['extra']['rid']);
     }
-    
+
     /**
      * Test that the generated request ID format is correct (base36 encoded).
      */
@@ -136,18 +136,18 @@ class RequestIdProcessorTest extends TestCase
         $attributes = $this->createMock(ParameterBag::class);
         $request->headers = $headers;
         $request->attributes = $attributes;
-        
+
         $headers->method('has')->with('X-Request-ID')->willReturn(false);
-        
+
         $requestStack = $this->createMock(RequestStack::class);
         $requestStack->method('getCurrentRequest')->willReturn($request);
-        
+
         $processor = new RequestIdProcessor($requestStack);
         $record = ['message' => 'Test message', 'extra' => []];
-        
+
         // Act
         $result = $processor($record);
-        
+
         // Assert
         $this->assertArrayHasKey('rid', $result['extra']);
         // Request ID should only contain alphanumeric characters (base36)
