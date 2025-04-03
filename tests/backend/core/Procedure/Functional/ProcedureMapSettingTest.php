609b9b9a55548e411693fea1e9dc1778d00ceb50
Demos-CI <devops-system+github@demos-deutschland.de>
2025-04-03 10:48:36 +0000

    style: Apply php-cs-fixer

--- tests/backend/core/Procedure/Functional/ProcedureMapSettingTest.php
+++ tests/backend/core/Procedure/Functional/ProcedureMapSettingTest.php
@@ -10,43 +10,8 @@
 
 namespace Tests\Core\Procedure\Functional;
 
-use Carbon\Carbon;
-use DateTime;
 use DemosEurope\DemosplanAddon\Contracts\Config\GlobalConfigInterface;
-use demosplan\DemosPlanCoreBundle\DataFixtures\ORM\TestData\LoadCustomerData;
-use demosplan\DemosPlanCoreBundle\DataFixtures\ORM\TestData\LoadProcedureData;
-use demosplan\DemosPlanCoreBundle\DataFixtures\ORM\TestData\LoadProcedureTypeData;
-use demosplan\DemosPlanCoreBundle\DataFixtures\ORM\TestData\LoadUserData;
-use demosplan\DemosPlanCoreBundle\Entity\Document\Elements;
-use demosplan\DemosPlanCoreBundle\Entity\Document\Paragraph;
-use demosplan\DemosPlanCoreBundle\Entity\Document\ParagraphVersion;
-use demosplan\DemosPlanCoreBundle\Entity\Document\SingleDocument;
-use demosplan\DemosPlanCoreBundle\Entity\Document\SingleDocumentVersion;
-use demosplan\DemosPlanCoreBundle\Entity\File;
-use demosplan\DemosPlanCoreBundle\Entity\ManualListSort;
-use demosplan\DemosPlanCoreBundle\Entity\Map\GisLayer;
-use demosplan\DemosPlanCoreBundle\Entity\Map\GisLayerCategory;
-use demosplan\DemosPlanCoreBundle\Entity\News\News;
-use demosplan\DemosPlanCoreBundle\Entity\Procedure\Boilerplate;
-use demosplan\DemosPlanCoreBundle\Entity\Procedure\BoilerplateCategory;
-use demosplan\DemosPlanCoreBundle\Entity\Procedure\BoilerplateGroup;
-use demosplan\DemosPlanCoreBundle\Entity\Procedure\HashedQuery;
-use demosplan\DemosPlanCoreBundle\Entity\Procedure\NotificationReceiver;
 use demosplan\DemosPlanCoreBundle\Entity\Procedure\Procedure;
-use demosplan\DemosPlanCoreBundle\Entity\Procedure\ProcedureSubscription;
-use demosplan\DemosPlanCoreBundle\Entity\Procedure\ProcedureType;
-use demosplan\DemosPlanCoreBundle\Entity\Procedure\UserFilterSet;
-use demosplan\DemosPlanCoreBundle\Entity\Report\ReportEntry;
-use demosplan\DemosPlanCoreBundle\Entity\Setting;
-use demosplan\DemosPlanCoreBundle\Entity\Statement\DraftStatement;
-use demosplan\DemosPlanCoreBundle\Entity\Statement\DraftStatementVersion;
-use demosplan\DemosPlanCoreBundle\Entity\Statement\Statement;
-use demosplan\DemosPlanCoreBundle\Entity\Statement\StatementFragment;
-use demosplan\DemosPlanCoreBundle\Entity\Statement\Tag;
-use demosplan\DemosPlanCoreBundle\Entity\Statement\TagTopic;
-use demosplan\DemosPlanCoreBundle\Entity\User\Orga;
-use demosplan\DemosPlanCoreBundle\Entity\User\User;
-use demosplan\DemosPlanCoreBundle\Exception\ProcedureNotFoundException;
 use demosplan\DemosPlanCoreBundle\Logic\ContentService;
 use demosplan\DemosPlanCoreBundle\Logic\EntityHelper;
 use demosplan\DemosPlanCoreBundle\Logic\FileService;
@@ -57,10 +22,6 @@ use demosplan\DemosPlanCoreBundle\Logic\Procedure\ProcedureService;
 use demosplan\DemosPlanCoreBundle\Logic\Report\ReportService;
 use demosplan\DemosPlanCoreBundle\Resources\config\GlobalConfig;
 use demosplan\DemosPlanCoreBundle\ResourceTypes\ProcedureMapSettingResourceType;
-use Doctrine\ORM\ORMInvalidArgumentException;
-use Exception;
-use InvalidArgumentException;
-use Psr\Log\NullLogger;
 use ReflectionMethod;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Tests\Base\FunctionalTestCase;
@@ -100,12 +61,10 @@ class ProcedureMapSettingTest extends FunctionalTestCase
         $contentService = $this->getContainer()->get(ContentService::class);
         $masterTemplateService = $this->getContainer()->get(MasterTemplateService::class);
         $coordinateJsonConverter = $this->getContainer()->get(CoordinateJsonConverter::class);
-
     }
 
     public function testGetMasterTemplateBoundingBox()
     {
-
         $contentService = $this->getContainer()->get(ContentService::class);
         $masterTemplateService = $this->getContainer()->get(MasterTemplateService::class);
         $coordinateJsonConverter = $this->getContainer()->get(CoordinateJsonConverter::class);
@@ -123,21 +82,19 @@ class ProcedureMapSettingTest extends FunctionalTestCase
         // Assert the result
         $expectedResult = [
             'start' => [
-                'latitude' => 555555.41,
+                'latitude'  => 555555.41,
                 'longitude' => 9999999.13,
             ],
             'end' => [
-                'latitude' => 611330.65,
+                'latitude'  => 611330.65,
                 'longitude' => 6089742.54,
             ],
         ];
         $this->assertEquals($expectedResult, $result);
-
     }
 
     public function testGetConfigBoundingBox()
     {
-
         $contentService = $this->getContainer()->get(ContentService::class);
         $masterTemplateService = $this->getContainer()->get(MasterTemplateService::class);
         $coordinateJsonConverter = $this->getContainer()->get(CoordinateJsonConverter::class);
@@ -151,22 +108,15 @@ class ProcedureMapSettingTest extends FunctionalTestCase
 
         $globalConfigBoundingBoxExpectedValue = $convertFlatListToCoordinatesMethod->invoke($procedureMapSettingResourceType, $globalConfig->getMapMaxBoundingbox(), true);
 
-
-
-
         // Use reflection to access the protected method
         $getMapSettingMethod = new ReflectionMethod(ProcedureMapSettingResourceType::class, 'getMapSetting');
         $getMapSettingMethod->setAccessible(true);
-
-
 
         // Call the protected method
         $masterTemplateService->getMasterTemplate()->getSettings()->setBoundingBox('');
 
         $result = $getMapSettingMethod->invoke($procedureMapSettingResourceType, 'getBoundingBox', 'getMapMaxBoundingbox');
 
-
         $this->assertEquals($globalConfigBoundingBoxExpectedValue, $result);
-
     }
 }
