9f875d475ea6b7beb683d465e818bff594e95f8f
Demos-CI <devops-system+github@demos-deutschland.de>
2025-04-10 13:32:24 +0000

    style: Apply php-cs-fixer

--- demosplan/DemosPlanCoreBundle/Utils/CustomField/CustomFieldValidator.php
+++ demosplan/DemosPlanCoreBundle/Utils/CustomField/CustomFieldValidator.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
 namespace demosplan\DemosPlanCoreBundle\Utils\CustomField;
 
 use demosplan\DemosPlanCoreBundle\CustomField\CustomFieldList;
@@ -17,13 +25,14 @@ class CustomFieldValidator
     private EntityManagerInterface $entityManager;
 
     public function __construct(EntityManagerInterface $entityManager, array $sourceToTargetMap = [
-        'PROCEDURE' => 'SEGMENT',
+        'PROCEDURE'          => 'SEGMENT',
         'PROCEDURE_TEMPLATE' => 'SEGMENT',
     ], array $classNameToClassPathtMap = [
-        'PROCEDURE' => Procedure::class,
-        'PROCEDURE_TEMPLATE' =>  Procedure::class,
-        'SEGMENT' =>  Segment::class,
-    ]) {
+        'PROCEDURE'          => Procedure::class,
+        'PROCEDURE_TEMPLATE' => Procedure::class,
+        'SEGMENT'            => Segment::class,
+    ])
+    {
         $this->sourceToTargetMap = $sourceToTargetMap;
         $this->classNameToClassPathtMap = $classNameToClassPathtMap;
         $this->entityManager = $entityManager;
@@ -52,12 +61,7 @@ class CustomFieldValidator
     private function validateSourceToTargetMapping(?string $sourceEntity, ?string $targetEntity): void
     {
         if ($this->sourceToTargetMap[$sourceEntity] !== $targetEntity) {
-            throw new InvalidArgumentException(sprintf(
-                'The target entity "%s" does not match the expected target entity "%s" for source entity "%s".',
-                $targetEntity,
-                $this->sourceToTargetMap[$sourceEntity],
-                $sourceEntity
-            ));
+            throw new InvalidArgumentException(sprintf('The target entity "%s" does not match the expected target entity "%s" for source entity "%s".', $targetEntity, $this->sourceToTargetMap[$sourceEntity], $sourceEntity));
         }
     }
 
@@ -65,17 +69,12 @@ class CustomFieldValidator
     {
         $sourceEntityClass = $this->classNameToClassPathtMap[$sourceEntity];
 
-
         // Query the repository for the entity
         $repository = $this->entityManager->getRepository($sourceEntityClass);
         $entity = $repository->find($sourceEntityId);
 
         if (null === $entity) {
-            throw new InvalidArgumentException(sprintf(
-                'The sourceEntityId "%s" was not found in the sourceEntity "%s".',
-                $sourceEntityId,
-                $sourceEntity
-            ));
+            throw new InvalidArgumentException(sprintf('The sourceEntityId "%s" was not found in the sourceEntity "%s".', $sourceEntityId, $sourceEntity));
         }
     }
 }
