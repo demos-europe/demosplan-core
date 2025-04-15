44e26a312b9e246c30ae2688ef9da57b61ee7b66
Catalina Sierra Zamudio <sierra-zamudio@demos-deutschland.de>
2025-04-15 15:04:10 +0200

    feat DPLAN-15340: Add migration for customFields column

---
+++ demosplan/DemosPlanCoreBundle/DoctrineMigrations/2025/04/Version20250415123411.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types = 1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Platforms\MySQLPlatform;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+class Version20250415123411 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'refs DPLAN-15340: Add custom fields colum to store values of custom fields for Segment Entity ';
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function up(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+        $this->addSql('ALTER TABLE _statement ADD custom_fields JSON DEFAULT NULL COMMENT \'(DC2Type:json)\'');
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function down(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+        $this->addSql('ALTER TABLE _statement DROP custom_fields');
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function abortIfNotMysql(): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof MySQLPlatform,
+            "Migration can only be executed safely on 'mysql'."
+        );
+    }
+}
