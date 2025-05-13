4081207eb5ba8df4939ebed8651724e50de57853
Alexander Marten <marten@demos-deutschland.de>
2025-05-13 10:56:33 +0200

    Fix (refs DPLAN-15423): remove testcase which was outdated and partly wrong and already covered with other tests

--- tests/backend/core/Core/Functional/EntityContentChangeServiceTest.php
+++ tests/backend/core/Core/Functional/EntityContentChangeServiceTest.php
@@ -18,7 +18,6 @@ use demosplan\DemosPlanCoreBundle\Entity\User\User;
 use demosplan\DemosPlanCoreBundle\Logic\EntityContentChangeService;
 use demosplan\DemosPlanCoreBundle\Logic\EntityHelper;
 use demosplan\DemosPlanCoreBundle\Logic\Statement\StatementService;
-use Monolog\Logger;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Tests\Base\FunctionalTestCase;
 
@@ -186,62 +185,4 @@ class EntityContentChangeServiceTest extends FunctionalTestCase
         static::assertSame($this->testUser->getId(), $changeOfStatement->getUserId());
     }
 
-    /**
-     * Testing if content changes are detected and working.
-     * Cases:
-     *  - object update with content change, using field memo → expect no change detected
-     *  - array update with content change, using field memo  → expect change detected.
-     *
-     * Also mock logger to check if logging is working.
-     * Always expect Statement instance as return value if it successfully updated the entry.
-     * Else it must be false.
-     */
-    public function testUpdateStatementWithContentChanges(): void
-    {
-        self::markSkippedForCIIntervention();
-        // Need to implement this. But first unit tests have to work again.
-
-        /** @var Statement $statement */
-        $statement = $this->getReference('testStatement');
-        $statementArray = $this->entityHelper->toArray($statement);
-
-        $this->assertArrayHasKey('ident', $statementArray);
-        $this->assertArrayHasKey('memo', $statementArray);
-
-        $this->assertEquals($statement->getMemo(), $statementArray['memo']);
-
-        $mockBuilder = $this->getMockBuilder(Logger::class);
-        $mockBuilder->disableOriginalConstructor();
-
-        // Somehow need a way to check if this message has been logged
-        $logger = $mockBuilder->getMock();
-        $logger->method('info')->with('Could not determine content changes for statement because of object structure.');
-
-        // ->will(function($message) {
-//        $testCase::assertEquals('Could not determine content changes for statement because of object structure.', $message);
-//        });
-
-        $updatedStatementObject = null;
-
-        // update array. expect content change to be detected. checking by requesting entity contentChange.
-        $statementArray['memo'] = 'somethingElseThanBeforeInArrayStructure';
-        $updatedStatementObject = $this->statementService->updateStatement($statementArray);
-        $this->assertInstanceOf(Statement::class, $updatedStatementObject);
-        $this->assertEquals($updatedStatementObject->getMemo(), 'somethingElseThanBeforeInArrayStructure');
-        /** @var EntityContentChange[] $contentChange */
-        $contentChange = $this->sut->getChangesByEntityId($updatedStatementObject->getId());
-        $lastChange = $contentChange[count($contentChange) - 1];
-        $this->assertEquals($lastChange->getEntityField(), 'memo');
-
-        // Reset $updatedStatementObject
-        $updatedStatementObject = null;
-
-        // update object. set logger to expect no content change detected
-        $statement->setMemo('somethingElseThanBefore');
-        // set logger to check if we get log entry → see mock
-        $this->statementService->setLogger($logger);
-        $updatedStatementObject = $this->statementService->updateStatement($statement);
-        $this->assertInstanceOf(Statement::class, $updatedStatementObject);
-        $this->assertEquals($updatedStatementObject->getMemo(), 'somethingElseThanBefore');
-    }
 }
