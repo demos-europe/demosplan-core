400a03ce289e549525980e99308d7e810bc38cb2
Moritz Mandler <mandler@demos-deutschland.de>
2025-05-15 16:19:58 +0200

    ADO-17872: manually go over the resourceType and fix final things.

--- demosplan/DemosPlanCoreBundle/ResourceTypes/InvitablePublicAgencyResourceType.php
+++ demosplan/DemosPlanCoreBundle/ResourceTypes/InvitablePublicAgencyResourceType.php
@@ -12,33 +12,21 @@ declare(strict_types=1);
 
 namespace demosplan\DemosPlanCoreBundle\ResourceTypes;
 
+use DemosEurope\DemosplanAddon\Contracts\Entities\OrgaStatusInCustomerInterface;
+use DemosEurope\DemosplanAddon\Contracts\Entities\OrgaTypeInterface;
+use DemosEurope\DemosplanAddon\Contracts\Entities\RoleInterface;
 use DemosEurope\DemosplanAddon\EntityPath\Paths;
 use demosplan\DemosPlanCoreBundle\Entity\User\Orga;
-use demosplan\DemosPlanCoreBundle\Entity\User\OrgaStatusInCustomer;
-use demosplan\DemosPlanCoreBundle\Entity\User\OrgaType;
-use demosplan\DemosPlanCoreBundle\Entity\User\Role;
 use demosplan\DemosPlanCoreBundle\Exception\CustomerNotFoundException;
 use demosplan\DemosPlanCoreBundle\Logic\ApiRequest\ResourceType\DplanResourceType;
 use demosplan\DemosPlanCoreBundle\ResourceConfigBuilder\InvitablePublicAgencyResourceConfigBuilder;
+use EDT\JsonApi\ApiDocumentation\DefaultField;
+use EDT\JsonApi\ApiDocumentation\DefaultInclude;
 use EDT\JsonApi\ResourceConfig\Builder\ResourceConfigBuilderInterface;
-use EDT\PathBuilding\End;
 use EDT\Querying\Contracts\PathException;
 
 /**
  * @template-extends DplanResourceType<Orga>
- *
- * @property-read End                              $legalName
- * @property-read End                              $name
- * @property-read End                              $competenceDescription
- * @property-read End                              $deleted
- * @property-read End                              $showlist
- * @property-read UserResourceType                 $users
- * @property-read OrgaStatusInCustomerResourceType $statusInCustomers
- * @property-read End                              $participationFeedbackEmailAddress
- * @property-read End                              $ccEmailAddresses
- * @property-read InstitutionLocationContactResourceType $locationContacts
- * @property-read InstitutionTagResourceType $assignedTags
- * @property-read End $contactPerson
  */
 class InvitablePublicAgencyResourceType extends DplanResourceType
 {
@@ -79,11 +67,11 @@ class InvitablePublicAgencyResourceType extends DplanResourceType
             $this->conditionFactory->propertyHasValue(false, Paths::orga()->deleted),
             $this->conditionFactory->propertyHasValue(true, Paths::orga()->showlist),
             $this->conditionFactory->propertyHasValue(
-                Role::GPSORG,
+                RoleInterface::GPSORG,
                 Paths::orga()->users->roleInCustomers->role->groupCode
             ),
             $this->conditionFactory->propertyHasValue(
-                OrgaType::PUBLIC_AGENCY,
+                OrgaTypeInterface::PUBLIC_AGENCY,
                 Paths::orga()->statusInCustomers->orgaType->name
             ),
             $this->conditionFactory->propertyHasValue(
@@ -91,7 +79,7 @@ class InvitablePublicAgencyResourceType extends DplanResourceType
                 Paths::orga()->statusInCustomers->customer->id
             ),
             $this->conditionFactory->propertyHasValue(
-                OrgaStatusInCustomer::STATUS_ACCEPTED,
+                OrgaStatusInCustomerInterface::STATUS_ACCEPTED,
                 Paths::orga()->statusInCustomers->status
             ),
         ];
@@ -115,20 +103,20 @@ class InvitablePublicAgencyResourceType extends DplanResourceType
         /** @var InvitablePublicAgencyResourceConfigBuilder $configBuilder */
         $configBuilder = $this->getConfig(InvitablePublicAgencyResourceConfigBuilder::class);
 
-        $configBuilder->id->readable();
+        // Add identifier property
+        $configBuilder->id->setReadableByPath();
 
         // Base properties that are always readable
-        $configBuilder->legalName->readable(true)->aliasedPath(Paths::orga()->name);
-        $configBuilder->participationFeedbackEmailAddress->readable()->aliasedPath(Paths::orga()->email2);
+        $configBuilder->legalName->setReadableByPath(DefaultField::YES)->setAliasedPath(Paths::orga()->name);
+        $configBuilder->participationFeedbackEmailAddress->setReadableByPath()->setAliasedPath(Paths::orga()->email2);
         $configBuilder->locationContacts
             ->setRelationshipType($this->resourceTypeStore->getInstitutionLocationContactResourceType())
-            ->readable()
-            ->aliasedPath(Paths::orga()->addresses);
+            ->setReadableByPath()
+            ->setAliasedPath(Paths::orga()->addresses);
 
         // Conditional properties based on permissions
         if ($this->currentUser->hasPermission('field_organisation_competence')) {
-            $configBuilder->competenceDescription->readable(
-                true,
+            $configBuilder->competenceDescription->setReadableByCallable(
                 static function (Orga $orga): ?string {
                     $competenceDescription = $orga->getCompetence();
                     if ('-' === $competenceDescription || '' === $competenceDescription) {
@@ -136,23 +124,24 @@ class InvitablePublicAgencyResourceType extends DplanResourceType
                     }
 
                     return $competenceDescription;
-                }
+                },
+                DefaultField::YES
             );
         }
 
         if ($this->currentUser->hasPermission('field_organisation_email2_cc')) {
-            $configBuilder->ccEmailAddresses->readable()->aliasedPath(Paths::orga()->ccEmail2);
+            $configBuilder->ccEmailAddresses->setReadableByPath()->setAliasedPath(Paths::orga()->ccEmail2);
         }
 
         if ($this->currentUser->hasPermission('field_organisation_contact_person')) {
-            $configBuilder->contactPerson->readable();
+            $configBuilder->contactPerson->setReadableByPath();
         }
 
         if ($this->currentUser->hasPermission('feature_institution_tag_read')) {
             $configBuilder->assignedTags
                 ->setRelationshipType($this->resourceTypeStore->getInstitutionTagResourceType())
-                ->readable(true, null, true)
-                ->filterable();
+                ->setReadableByPath(DefaultField::YES, DefaultInclude::YES)
+                ->setFilterable();
         }
 
         return $configBuilder;
