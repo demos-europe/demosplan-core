edf135b15af1a52b8e2924f9d5ddfff280f84a9a
Demos-CI <devops-system+github@demos-deutschland.de>
2025-05-15 15:17:32 +0000

    style: Apply php-cs-fixer

--- demosplan/DemosPlanCoreBundle/ResourceTypes/InvitableInstitutionResourceType.php
+++ demosplan/DemosPlanCoreBundle/ResourceTypes/InvitableInstitutionResourceType.php
@@ -89,7 +89,6 @@ final class InvitableInstitutionResourceType extends DplanResourceType
         ];
     }
 
-
     protected function getProperties(): ResourceConfigBuilderInterface
     {
         /** @var BaseOrgaResourceConfigBuilder $configBuilder */
