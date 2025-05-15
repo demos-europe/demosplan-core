158a460a9cd2bf1be419f755af9108fd9fcd71fd
Alexander Marten <marten@demos-deutschland.de>
2025-05-15 17:53:55 +0200

    Fix (refs DPLAN-15423): WIP: add test to cover new entitycontentchange on update a custom field of a segment

--- tests/backend/core/Core/Functional/EntityContentChangeServiceTest.php
+++ tests/backend/core/Core/Functional/EntityContentChangeServiceTest.php
@@ -11,12 +11,18 @@
 namespace Tests\Core\Core\Functional;
 
 use DateTime;
+use DemosEurope\DemosplanAddon\Utilities\Json;
 use demosplan\DemosPlanCoreBundle\DataFixtures\ORM\TestData\LoadUserData;
+use demosplan\DemosPlanCoreBundle\DataGenerator\Factory\CustomFields\CustomFieldConfigurationFactory;
+use demosplan\DemosPlanCoreBundle\DataGenerator\Factory\Procedure\ProcedureFactory;
+use demosplan\DemosPlanCoreBundle\DataGenerator\Factory\Statement\SegmentFactory;
 use demosplan\DemosPlanCoreBundle\Entity\EntityContentChange;
+use demosplan\DemosPlanCoreBundle\Entity\Statement\Segment;
 use demosplan\DemosPlanCoreBundle\Entity\Statement\Statement;
 use demosplan\DemosPlanCoreBundle\Entity\User\User;
 use demosplan\DemosPlanCoreBundle\Logic\EntityContentChangeService;
 use demosplan\DemosPlanCoreBundle\Logic\EntityHelper;
+use demosplan\DemosPlanCoreBundle\Logic\Segment\SegmentBulkEditorService;
 use demosplan\DemosPlanCoreBundle\Logic\Statement\StatementService;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Tests\Base\FunctionalTestCase;
@@ -28,6 +34,9 @@ class EntityContentChangeServiceTest extends FunctionalTestCase
 
     /** @var StatementService */
     protected $statementService;
+
+    /** @var SegmentBulkEditorService */
+    protected $segmentBulkEditService;
 
     /**
      * @var Session
@@ -48,6 +57,7 @@ class EntityContentChangeServiceTest extends FunctionalTestCase
         parent::setUp();
 
         $this->sut = $this->getContainer()->get(EntityContentChangeService::class);
+        $this->segmentBulkEditService = $this->getContainer()->get(SegmentBulkEditorService::class);
         $this->statementService = $this->getContainer()->get(StatementService::class);
         $this->entityHelper = $this->getContainer()->get(EntityHelper::class);
 
@@ -183,5 +193,53 @@ class EntityContentChangeServiceTest extends FunctionalTestCase
         static::assertSame($updatedStatement->getId(), $changeOfStatement->getEntityId());
         static::assertSame('memo', $changeOfStatement->getEntityField());
         static::assertSame($this->testUser->getId(), $changeOfStatement->getUserId());
+    }
+
+    /**
+     * Testing the history of the custom field value change
+     */
+    public function testEntityContentChangeEntryOnUpdateCustomFieldValue(): void
+    {
+        $procedure = ProcedureFactory::createOne();
+        $segments = SegmentFactory::createMany(2, ['procedure' => $procedure]);
+        $customField = CustomFieldConfigurationFactory::new()
+            ->withRelatedProcedure($procedure->_real())
+            ->asRadioButton('Color')->create();
+
+        $this->segmentBulkEditService->updateSegments(
+            $segments,
+            [],
+            [],
+            $this->testUser,
+            null,
+            [
+                ['id' => $customField->getId(), 'value' => 'orange'],
+                ['id' => $customField->getId(), 'value' => 'orange'],
+            ]
+        );
+
+        $historyOfSegment1 = $this->getEntries(
+            EntityContentChange::class,
+            ['entityType' => Segment::class, 'entityId' => $segments[0]->getId(), 'entityField' => 'Color']
+        );
+        self::assertArrayHasKey(0, $historyOfSegment1);
+        self::assertInstanceOf(EntityContentChange::class, $historyOfSegment1[0]);
+
+
+        $historyOfSegment2 = $this->getEntries(
+            EntityContentChange::class,
+            ['entityType' => Segment::class, 'entityId' => $segments[1], 'entityField' => 'Color']
+        );
+        self::assertArrayHasKey(0, $historyOfSegment2);
+        self::assertInstanceOf(EntityContentChange::class, $historyOfSegment2[0]);
+
+//        $change2 = Json::decodeToArray($historyOfSegment2[0]->getContentChange());
+//        self::assertIsArray($change2);
+//        self::assertEquals('Orange', $segment1->getCustomFields()->getCustomFieldsValues()[0]->getValue());
+//
+//        self::assertEquals('Orange', $segment1->getCustomFields()->getCustomFieldsValues()[0]->getValue());
+//        self::assertEquals('Orange', $segment1->getCustomFields()->getCustomFieldsValues()[0]->getValue());
+//        self::assertEquals('Bread', $segment2->getCustomFields()->getCustomFieldsValues()[1]->getValue());
+//
     }
 }
