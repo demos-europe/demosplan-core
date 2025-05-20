dc2d5b45a1cedd83c60b85186f03b119841a323e
Saeed Jalalifar <jalalifar@demos-deutschland.de>
2025-05-20 11:41:06 +0200

    b (ref DPLAN-15432): create an event to cover update tag in topicalTagAddon

---
+++ demosplan/DemosPlanCoreBundle/Event/Tag/UpdateTagEvent.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace demosplan\DemosPlanCoreBundle\Event\Tag;
+
+use DemosEurope\DemosplanAddon\Contracts\Events\UpdateTagEventInterface;
+use demosplan\DemosPlanCoreBundle\Event\DPlanEvent;
+
+class UpdateTagEvent extends DPlanEvent implements UpdateTagEventInterface
+{
+    public function __construct(private string $tagId)
+    {
+    }
+
+    public function getTagId(): string
+    {
+        return $this->tagId;
+    }
+
+}
