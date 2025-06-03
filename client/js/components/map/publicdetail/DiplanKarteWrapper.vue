307cb779272da3905247f133812cb943647e6d40
hwiem <40487668+hwiem@users.noreply.github.com>
2025-06-03 10:19:03 +0200

    feat(refs DPLAN-15588, #AB28108): use diplan-karten in public detail (4683)
    Get something running.
    
    to make yarn install work with diplan-karten, we need
    to set ADO_NPM_AUTH_IDENT wich has to be generated as PAT in ADO

---
+++ client/js/components/map/publicdetail/DiplanKarteWrapper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="h-full w-full">
+    <dp-button
+      id="statementModalButton"
+      :class="prefixClass('left-[365px] top-[24px] pt-[11px] pb-[11px] pl-[20px] pr-[20px] absolute z-above-zero')"
+      data-cy="statementModal"
+      rounded
+      :text="Translator.trans('statement.participate')"
+      @click="openStatementModalOrLoginPage" />
+
+    <diplan-karte />
+  </div>
+</template>
+
+
+<script setup>
+import { DpButton, prefixClassMixin } from '@demos-europe/demosplan-ui'
+import { MapPlugin } from '@init/diplan-karten'
+import { getCurrentInstance } from 'vue'
+
+const instance = getCurrentInstance()
+
+instance.appContext.app.mixin(prefixClassMixin)
+instance.appContext.app.use(MapPlugin, {
+  template: {
+    compilerOptions: {
+      isCustomElement: (tag) => tag === "diplan-karte",
+    }
+  }
+})
+
+const openStatementModalOrLoginPage= (event) => {
+  if (!hasPermission('feature_new_statement')) {
+    window.location.href = Routing.generate('DemosPlan_user_login_alternative')
+
+    return
+  }
+
+  event.preventDefault()
+  event.stopPropagation()
+  toggleStatementModal({})
+}
+
+const toggleStatementModal = (updateStatementPayload) => {
+  instance.parent.refs.statementModal.toggleModal(true, updateStatementPayload)
+}
+</script>
