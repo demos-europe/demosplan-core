e3f0d599fdaac0bd0fbfe2c2b94fd3817cfb74fc
Moritz Mandler <mandler@demos-deutschland.de>
2025-06-16 14:48:35 +0200

    DPLAN-15602: use the SegmentsByStatementsExporter instead of the now abstract class. Remove abstract class from services.yml

--- demosplan/DemosPlanCoreBundle/Controller/Segment/SegmentsExportController.php
+++ demosplan/DemosPlanCoreBundle/Controller/Segment/SegmentsExportController.php
@@ -22,7 +22,6 @@ use demosplan\DemosPlanCoreBundle\Logic\Procedure\NameGenerator;
 use demosplan\DemosPlanCoreBundle\Logic\Procedure\ProcedureHandler;
 use demosplan\DemosPlanCoreBundle\Logic\Segment\Export\SegmentExporterFileNameGenerator;
 use demosplan\DemosPlanCoreBundle\Logic\Segment\SegmentsByStatementsExporter;
-use demosplan\DemosPlanCoreBundle\Logic\Segment\SegmentsExporter;
 use demosplan\DemosPlanCoreBundle\Logic\Statement\StatementHandler;
 use demosplan\DemosPlanCoreBundle\Logic\ZipExportService;
 use demosplan\DemosPlanCoreBundle\ResourceTypes\StatementResourceType;
@@ -62,7 +61,7 @@ class SegmentsExportController extends BaseController
         methods: 'GET'
     )]
     public function exportAction(
-        SegmentsExporter $exporter,
+        SegmentsByStatementsExporter $segmentsExporter,
         StatementHandler $statementHandler,
         SegmentExporterFileNameGenerator $fileNameGenerator,
         string $procedureId,
@@ -78,8 +77,8 @@ class SegmentsExportController extends BaseController
         $censorInstitutionData = $this->getBooleanQueryParameter(self::INSTITUTION_CENSOR_PARAMETER);
 
         $response = new StreamedResponse(
-            static function () use ($procedure, $statement, $exporter, $tableHeaders, $censorCitizenData, $censorInstitutionData, $isObscure) {
-                $exportedDoc = $exporter->export(
+            static function () use ($procedure, $statement, $segmentsExporter, $tableHeaders, $censorCitizenData, $censorInstitutionData, $isObscure) {
+                $exportedDoc = $segmentsExporter->export(
                     $procedure,
                     $statement,
                     $tableHeaders,
