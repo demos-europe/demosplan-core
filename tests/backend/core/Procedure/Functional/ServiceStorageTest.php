b532e9b3e102d386ef7bc9e907a2a5648a704085
Catalina Sierra Zamudio <sierra-zamudio@demos-deutschland.de>
2025-06-26 10:07:15 +0200

    feat DPLAN-15078: Add test to create new procedure to test default value to be true

--- tests/backend/core/Procedure/Functional/ServiceStorageTest.php
+++ tests/backend/core/Procedure/Functional/ServiceStorageTest.php
@@ -304,4 +304,29 @@ class ServiceStorageTest extends FunctionalTestCase
         // Should maintain original value since permission is not present
         static::assertEquals($originalValue, $updatedProcedure->getSettings()->getAllowAnonymousStatements());
     }
+
+    public function testAllowAnonymousStatementsDefaultValue(): void
+    {
+        // Create new procedure to test default value
+        $procedureData = [
+            'r_copymaster' => $this->masterBlueprint->getId(),
+            'agencyMainEmailAddress' => 'test@example.com',
+            'action' => 'new',
+            'r_startdate' => '01.02.2055',
+            'r_enddate' => '01.02.2056',
+            'r_externalName' => 'testAnonymousDefault',
+            'r_name' => 'testAnonymousDefault',
+            'r_master' => false,
+            'orgaId' => $this->testUser->getOrganisationId(),
+            'orgaName' => $this->testUser->getOrgaName(),
+            'publicParticipationPhase' => 'configuration',
+            'r_procedure_type' => $this->procedureType->getId(),
+            'r_desc' => 'Test default anonymous statements value',
+        ];
+
+        $procedure = $this->sut->administrationNewHandler($procedureData, $this->testUser->getId());
+
+        // Default value should be true
+        static::assertTrue($procedure->getSettings()->getAllowAnonymousStatements());
+    }
 }
