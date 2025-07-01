f69e4a33bb342258cc425cf7a45c2ef137827cd8
Natalija Sakuta <sakuta@demos-deutschland.de>
2025-07-01 16:34:05 +0200

    fix(DPLAN-15841): Add a permission check in the template for the institution-tagging feature and the ability to filter institutions by tags, as this feature isn’t available in all projects

--- client/js/components/procedure/admin/InstitutionTagManagement/OrganisationTable.vue
+++ client/js/components/procedure/admin/InstitutionTagManagement/OrganisationTable.vue
@@ -24,68 +24,70 @@ All rights reserved
           @reset="handleReset"
           @search="handleSearch" />
 
-        <div class="sm:relative flex flex-col sm:flex-row flex-wrap space-x-1 space-x-reverse space-y-1 col-span-1 sm:col-span-7 ml-0 pl-0 sm:ml-2 sm:pl-[38px]">
-          <div class="sm:absolute sm:top-0 sm:left-0 mt-1">
-            <dp-flyout
-              align="left"
-              :aria-label="Translator.trans('filters.more')"
-              class="bg-surface-medium rounded pb-1 pt-[4px]"
-              data-cy="dpAddOrganisationList:filterCategories">
-              <template v-slot:trigger>
-                <span :title="Translator.trans('filters.more')">
-                  <dp-icon
-                    aria-hidden="true"
-                    class="inline"
-                    icon="faders" />
-                </span>
-              </template>
-              <!-- 'More filters' flyout -->
-              <div>
-                <button
-                  class="btn--blank o-link--default ml-auto"
-                  data-cy="dpAddOrganisationList:toggleAllFilterCategories"
-                  v-text="Translator.trans('toggle_all')"
-                  @click="filterManager.toggleAllCategories" />
-                <div v-if="!isLoading">
-                  <dp-checkbox
-                    v-for="category in allFilterCategories"
-                    :key="category.id"
-                    :id="`filterCategorySelect:${category.label}`"
-                    :checked="selectedFilterCategories.includes(category.label)"
-                    :data-cy="`dpAddOrganisationList:filterCategoriesSelect:${category.label}`"
-                    :disabled="filterCategoryHelpers.checkIfDisabled(appliedFilterQuery, category.id)"
-                    :label="{
-                      text: `${category.label} (${filterCategoryHelpers.getSelectedOptionsCount(appliedFilterQuery, category.id)})`
-                    }"
-                    @change="filterManager.handleChange(category.label, !selectedFilterCategories.includes(category.label))" />
+        <template v-if="hasPermission('feature_institution_tag_read')">
+          <div class="sm:relative flex flex-col sm:flex-row flex-wrap space-x-1 space-x-reverse space-y-1 col-span-1 sm:col-span-7 ml-0 pl-0 sm:ml-2 sm:pl-[38px]">
+            <div class="sm:absolute sm:top-0 sm:left-0 mt-1">
+              <dp-flyout
+                align="left"
+                :aria-label="Translator.trans('filters.more')"
+                class="bg-surface-medium rounded pb-1 pt-[4px]"
+                data-cy="dpAddOrganisationList:filterCategories">
+                <template v-slot:trigger>
+                  <span :title="Translator.trans('filters.more')">
+                    <dp-icon
+                      aria-hidden="true"
+                      class="inline"
+                      icon="faders" />
+                  </span>
+                </template>
+                <!-- 'More filters' flyout -->
+                <div>
+                  <button
+                    class="btn--blank o-link--default ml-auto"
+                    data-cy="dpAddOrganisationList:toggleAllFilterCategories"
+                    v-text="Translator.trans('toggle_all')"
+                    @click="filterManager.toggleAllCategories" />
+                  <div v-if="!isLoading">
+                    <dp-checkbox
+                      v-for="category in allFilterCategories"
+                      :key="category.id"
+                      :id="`filterCategorySelect:${category.label}`"
+                      :checked="selectedFilterCategories.includes(category.label)"
+                      :data-cy="`dpAddOrganisationList:filterCategoriesSelect:${category.label}`"
+                      :disabled="filterCategoryHelpers.checkIfDisabled(appliedFilterQuery, category.id)"
+                      :label="{
+                        text: `${category.label} (${filterCategoryHelpers.getSelectedOptionsCount(appliedFilterQuery, category.id)})`
+                      }"
+                      @change="filterManager.handleChange(category.label, !selectedFilterCategories.includes(category.label))" />
+                  </div>
                 </div>
-              </div>
-            </dp-flyout>
+              </dp-flyout>
+            </div>
+
+            <filter-flyout
+              v-for="category in filterCategoriesToBeDisplayed"
+              :key="`filter_${category.label}`"
+              ref="filterFlyout"
+              :category="{ id: category.id, label: category.label }"
+              class="inline-block"
+              :data-cy="`dpAddOrganisationList:${category.label}`"
+              :initial-query-ids="queryIds"
+              :member-of="category.memberOf"
+              :operator="category.comparisonOperator"
+              :path="category.rootPath"
+              @filterApply="(filtersToBeApplied) => filterManager.applyFilter(filtersToBeApplied, category.id)"
+              @filterOptions:request="(params) => filterManager.createFilterOptions({ ...params, categoryId: category.id})" />
           </div>
 
-          <filter-flyout
-            v-for="category in filterCategoriesToBeDisplayed"
-            :key="`filter_${category.label}`"
-            ref="filterFlyout"
-            :category="{ id: category.id, label: category.label }"
-            class="inline-block"
-            :data-cy="`dpAddOrganisationList:${category.label}`"
-            :initial-query-ids="queryIds"
-            :member-of="category.memberOf"
-            :operator="category.comparisonOperator"
-            :path="category.rootPath"
-            @filterApply="(filtersToBeApplied) => filterManager.applyFilter(filtersToBeApplied, category.id)"
-            @filterOptions:request="(params) => filterManager.createFilterOptions({ ...params, categoryId: category.id})" />
-        </div>
-
-        <dp-button
-          class="h-fit col-span-1 sm:col-span-2 mt-1 justify-center"
-          data-cy="dpAddOrganisationList:resetFilter"
-          :disabled="!isQueryApplied"
-          :text="Translator.trans('reset')"
-          variant="outline"
-          v-tooltip="Translator.trans('search.filter.reset')"
-          @click="filterManager.reset" />
+          <dp-button
+            class="h-fit col-span-1 sm:col-span-2 mt-1 justify-center"
+            data-cy="dpAddOrganisationList:resetFilter"
+            :disabled="!isQueryApplied"
+            :text="Translator.trans('reset')"
+            variant="outline"
+            v-tooltip="Translator.trans('search.filter.reset')"
+            @click="filterManager.reset" />
+        </template>
         <!-- Slot for bulk actions -->
       </div>
       <slot name="bulkActions" />
@@ -470,6 +472,10 @@ export default {
     },
 
     getInstitutionTagCategories (isInitial = false) {
+      if (!hasPermission('feature_institution_tag_read')) {
+        return
+      }
+
       return this.fetchInstitutionTagCategories({
         fields: {
           InstitutionTagCategory: [
