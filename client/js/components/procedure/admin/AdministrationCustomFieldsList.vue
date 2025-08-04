ac293ff6ae71b5d2dda301f66c08898593c058a3
Lars Hüllner <lars@demos-international.com>
2025-08-04 12:50:10 +0200

    feat(refs: DPLAN-16008): make customFields editable and add warning msg

--- client/js/components/procedure/admin/AdministrationCustomFieldsList.vue
+++ client/js/components/procedure/admin/AdministrationCustomFieldsList.vue
@@ -66,8 +66,21 @@
       data-cy="customFields:table"
       has-flyout
       :header-fields="headerFields"
-      :items="customFieldsReduced"
+      :items="customFieldItems"
       track-by="id">
+      <template v-slot:name="rowData">
+        <div v-if="rowData.edit">
+          <dp-input
+            v-model="newRowData.name"
+            id="cfName"
+            required
+          />
+        </div>
+        <div v-else>
+          {{ rowData.name }}
+        </div>
+      </template>
+
       <template v-slot:options="rowData">
         <ul>
           <li
@@ -75,12 +88,92 @@
             :key="index"
             class="mb-1"
             :data-cy="`customFields:option${option}`">
-            {{ option }}
+            <div v-if="rowData.edit">
+              <div class="flex">
+                <dp-input
+                  v-model="newRowData.options[index]"
+                  :id="'option' + index"
+                  :key="'option' + index"
+                  required
+                />
+
+                <dp-button
+                  v-if="index >= rowData.options.length"
+                  class="w-[20px] inline-block ml-1"
+                  :data-cy="`customFields:removeOptionInput:${option}`"
+                  hide-text
+                  icon="x"
+                  :text="Translator.trans('remove')"
+                  variant="subtle"
+                  @click="deleteOptionOnEdit(index)"
+                />
+              </div>
+            </div>
+
+            <div v-else>
+              {{ option }}
+            </div>
+          </li>
+          <li v-if="rowData.edit">
+            <dp-button
+              data-cy="customFields:addOptionOnEdit"
+              icon="plus"
+              variant="subtle"
+              :text="Translator.trans('option.add')"
+              @click="addOptionInputOnEdit(rowData)" />
           </li>
         </ul>
       </template>
+
+      <template v-slot:description="rowData">
+        <div v-if="rowData.edit">
+          <dp-input v-model="newRowData.description" id="cfDescription" />
+        </div>
+        <div v-else>
+          {{ rowData.description }}
+        </div>
+      </template>
+
       <template v-slot:flyout="rowData">
-        <div class="float-right">
+        <div class="flex float-right">
+          <button
+            v-if="!rowData.edit"
+            class="btn--blank o-link--default"
+            data-cy="customFields:editField"
+            :aria-label="Translator.trans('item.edit')"
+            @click="editCustomField(rowData)">
+            <i
+              class="fa fa-pencil"
+              aria-hidden="true" />
+          </button>
+
+          <template v-else>
+            <button
+              :aria-label="Translator.trans('save')"
+              class="btn--blank o-link--default u-mr-0_25 inline-block"
+              data-cy="customFields:saveEdit"
+              @click="saveEditedFields()">
+              <dp-icon
+                icon="check"
+                aria-hidden="true" />
+            </button>
+
+            <button
+              class="btn--blank o-link--default inline-block"
+              data-cy="customFields:abortEdit"
+              @click="abortFieldEdit(rowData)"
+              :aria-label="Translator.trans('abort')">
+              <dp-icon
+                icon="xmark"
+                aria-hidden="true" />
+            </button>
+          </template>
+
+          <dp-confirm-dialog
+            ref="confirmDialog"
+            data-cy="customFields:saveEditConfirm"
+            :message="Translator.trans('custom.field.edit.message.warning')" />
+
           <button
             v-if="!rowData.open"
             :aria-label="Translator.trans('item.edit')"
@@ -92,10 +185,12 @@
               aria-hidden="true"
               icon="caret-down" />
           </button>
+
           <template v-else>
             <button
+              v-if="!rowData.edit"
               :aria-label="Translator.trans('save')"
-              class="btn--blank o-link--default u-mr-0_25 inline-block"
+              class="btn--blank o-link--default"
               data-cy="customFields:hideOptions"
               @click="hideOptions(rowData)">
               <dp-icon
@@ -114,6 +209,7 @@
 <script>
 import {
   DpButton,
+  DpConfirmDialog,
   DpDataTable,
   DpIcon,
   DpInlineNotification,
@@ -121,7 +217,7 @@ import {
   DpLabel,
   DpLoading
 } from '@demos-europe/demosplan-ui'
-import { mapActions, mapState } from 'vuex'
+import { mapActions, mapMutations, mapState } from 'vuex'
 import CreateCustomFieldForm from '@DpJs/components/procedure/admin/CreateCustomFieldForm'
 
 export default {
@@ -130,6 +226,7 @@ export default {
   components: {
     CreateCustomFieldForm,
     DpButton,
+    DpConfirmDialog,
     DpDataTable,
     DpIcon,
     DpInlineNotification,
@@ -152,11 +249,13 @@ export default {
 
   data () {
     return {
+      customFieldItems: [],
       headerFields: [
         { field: 'name', label: Translator.trans('name'), colClass: 'u-3-of-12' },
         { field: 'options', label: Translator.trans('options'), colClass: 'u-4-of-12' },
         { field: 'description', label: Translator.trans('description'), colClass: 'u-5-of-12' }
       ],
+      initialRowData: {},
       isLoading: false,
       isNewFieldFormOpen: false,
       isSuccess: false,
@@ -164,6 +263,8 @@ export default {
         '',
         ''
       ],
+      newRowData: {},
+      // ToDo: Find out if expandedFields are still needed: it seems as they can be deleted, if the list items are not expanded through dpDataTable expand mechanism
       expandedFields: {},
     }
   },
@@ -185,31 +286,11 @@ export default {
       return this.newFieldOptions.filter((option, index) => index > 1)
     },
 
-    /**
-     * CustomFields reduced to the format we need in the FE
-     * @return {({id: *, name: *, description: *, options: *, open: boolean}|undefined)[]}
-     */
-    customFieldsReduced () {
-      return Object.values(this.customFields)
-        .map(field => {
-          if (field) {
-            const { id, attributes } = field
-            const { description, name, options } = attributes
-
-            return {
-              id,
-              name,
-              description,
-              options,
-              open: this.expandedFields[id] || false
-            }
-          }
-        })
-        .filter(field => field !== undefined)
-    },
-
     displayedOptions () {
       return (rowData) => {
+        if (rowData.edit && this.newRowData.options) {
+          return this.newRowData.options
+        }
         return rowData.open ? rowData.options : rowData.options.slice(0, 2)
       }
     },
@@ -221,7 +302,8 @@ export default {
 
   methods: {
     ...mapActions('CustomField', {
-      createCustomField: 'create'
+      createCustomField: 'create',
+      saveCustomField: 'save'
     }),
 
     ...mapActions('AdminProcedure', {
@@ -232,8 +314,57 @@ export default {
       getProcedureTemplateWithFields: 'get'
     }),
 
+    ...mapMutations('CustomField', {
+      updateCustomField: 'set'
+    }),
+
+    async saveEditedFields () {
+      if (this.$refs.confirmDialog?.open) {
+        const isConfirmed = await this.$refs.confirmDialog.open()
+
+        if (isConfirmed) {
+          //TODO: Send data
+          //update store item
+          const storeField = this.customFields[this.newRowData.id]
+          storeField.attributes.options = this.newRowData.options
+          this.updateCustomField(storeField)
+          // send patch
+          await this.saveCustomField(storeField.id).catch()
+
+          //Beispiel Update ohne Store
+          // dpApi.patch(Routing.generate('api_resource_update', { resourceType: 'AnnotatedStatementPdf', resourceId: this.documentId }), {}, payload)
+          //   .then(response => {
+          //     if (response.ok) {
+          //       dplan.notify.confirm(Translator.trans('statement.save.quickSave.success'))
+          //     } else {
+          //       dplan.notify.error(Translator.trans('error.api.generic'))
+          //     }
+          //   })
+          //   .catch((err) => {
+          //     console.error(err)
+          //     dplan.notify.error(Translator.trans('error.api.generic'))
+          //   })
+        }
+      }
+    },
+
+    abortFieldEdit (rowData) {
+      rowData.description = this.initialRowData.description
+      rowData.name = this.initialRowData.name
+      rowData.options = this.initialRowData.options
+
+      this.newRowData = {}
+
+      this.setEditMode(rowData, false)
+      this.hideOptions(rowData)
+    },
+
     addOptionInput () {
       this.newFieldOptions.push('')
+    },
+
+    addOptionInputOnEdit () {
+      this.newRowData.options.push('')
     },
 
     /**
@@ -255,6 +386,47 @@ export default {
       const identicalNames = options.filter(optionName => optionName === name)
 
       return identicalNames.length <= 1
+    },
+
+    deleteOptionOnEdit (index) {
+      this.newRowData.options.splice(index, 1)
+    },
+
+    editCustomField (rowData) {
+      console.log(this.customFieldItems)
+      const editingCustomField = this.customFieldItems.find(customFieldItem => customFieldItem.edit === true)
+
+      console.log(editingCustomField)
+      console.log(rowData)
+      if (editingCustomField) {
+        // Reset row which was in editing state before
+        editingCustomField.description = this.initialRowData.description
+        editingCustomField.edit = false
+        editingCustomField.name = this.initialRowData.name
+        editingCustomField.open = false
+        editingCustomField.options = this.initialRowData.options
+        console.log(this.newRowData)
+        this.newRowData = {}
+      }
+
+      // Save initial state of currently edited row
+      this.initialRowData.description = rowData.description
+      this.initialRowData.name = rowData.name
+      this.initialRowData.options = rowData.options
+
+      this.newRowData.description = rowData.description
+      this.newRowData.name = rowData.name
+
+      if (!this.newRowData.options) {
+        this.newRowData.options = [...rowData.options]//rowData.options.map((option) => option)
+
+        // rowData.options.forEach((option) => {
+        //   this.newRowData.options.push(option)
+        // })
+      }
+      //this.showOptions(rowData)
+
+      this.setEditMode(rowData)
     },
 
     /**
@@ -280,12 +452,22 @@ export default {
         include: ['segmentCustomFields'].join()
       }
 
-      this.getCustomFields(payload)
+      this.getCustomFields(payload).then(() => {
+        this.customFieldsReduced()
+      })
         .catch(err => console.error(err))
     },
 
     hideOptions (rowData) {
+      const idx = this.getIndexOfRowData(rowData)
+
+      this.customFieldItems[idx].open = false
+
       delete this.expandedFields[rowData.id]
+    },
+
+    getIndexOfRowData (rowData) {
+      return this.customFieldItems.findIndex(el => el.id === rowData.id)
     },
 
     removeOptionInput (index) {
@@ -356,7 +538,49 @@ export default {
           })
     },
 
+    /**
+     * CustomFields reduced to the format we need in the FE
+     */
+    customFieldsReduced () {
+      const fieldsReduced = Object.values(this.customFields)
+        .map(field => {
+          if (field) {
+            const { id, attributes } = field
+            const { description, name, options } = attributes
+
+            return {
+              id,
+              name,
+              description,
+              options,
+              open: this.expandedFields[id] || false,
+              edit: false,
+            }
+          }
+        })
+        .filter(field => field !== undefined)
+
+      if (this.customFieldItems.length > 0) {
+        this.customFieldItems = []
+      }
+
+      fieldsReduced.forEach((field) => {
+        this.customFieldItems.push(field)
+      })
+    },
+
+    setEditMode (rowData, editState = true) {
+      const idx = this.customFieldItems.findIndex(el => el.id === rowData.id)
+
+      this.customFieldItems[idx].open = editState
+      this.customFieldItems[idx].edit = editState
+    },
+
     showOptions (rowData) {
+      const idx = this.customFieldItems.findIndex(el => el.id === rowData.id)
+      console.log(rowData)
+      console.log(this.expandedFields)
+      this.customFieldItems[idx].open = true
       this.expandedFields[rowData.id] = true
     },
 
