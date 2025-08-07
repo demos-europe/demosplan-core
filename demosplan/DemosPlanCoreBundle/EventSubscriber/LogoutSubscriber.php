51c4a852149b7780dcb415a3d2f30b9fd4a77b85
Demos-CI <devops-system+github@demos-deutschland.de>
2025-08-07 11:53:16 +0000

    style: Apply php-cs-fixer

--- demosplan/DemosPlanCoreBundle/EventSubscriber/LogoutSubscriber.php
+++ demosplan/DemosPlanCoreBundle/EventSubscriber/LogoutSubscriber.php
@@ -38,7 +38,8 @@ class LogoutSubscriber implements EventSubscriberInterface
 
     /**
      * Set this listener with priority 1 to execute before Symfony's default LogoutListener:
-     * @link \Symfony\Component\Security\Http\EventListener\SessionLogoutListener
+     *
+     * @see \Symfony\Component\Security\Http\EventListener\SessionLogoutListener
      * This prevents the session from being invalidated prematurely,
      * as we need the session to access the stored Keycloak ID token for logout.
      * The token is detected on Keycloak side,
