90d61f805225137e94c466449cf39693e899f825
Catalina Sierra Zamudio <sierra-zamudio@demos-deutschland.de>
2025-08-08 10:27:19 +0200

    feat (refs: DPLAN-14969): Adjust comment

--- demosplan/DemosPlanCoreBundle/Logic/User/OzgKeycloakLogoutManager.php
+++ demosplan/DemosPlanCoreBundle/Logic/User/OzgKeycloakLogoutManager.php
@@ -30,7 +30,7 @@ class OzgKeycloakLogoutManager
     private const POST_LOGOUT_REDIRECT_URI = 'post_logout_redirect_uri=https://';
     private const ID_TOKEN_HINT = 'id_token_hint=';
 
-    /** @var int Session expiration time for testing (120 minutes) */
+    /** @var int Session expiration time for testing (15 minutes) */
     private const TEST_SESSION_LIFETIME_SECONDS = 900;
 
     public function __construct(
