e5c2c77026a6c75b94bafa453c268a7f40e26320
huellnerdemos <huellner@demos-deutschland.de>
2025-08-20 08:14:01 +0000

    fix(refs DPLAN-16399): initialize custom field values on stmt segment (5075)
    fix(refs DPLAN-16399): initialize custom field values on statement segment

--- client/js/components/procedure/StatementSegmentsList/StatementSegment.vue
+++ client/js/components/procedure/StatementSegmentsList/StatementSegment.vue
@@ -737,6 +737,18 @@ export default {
       dplan.notify.notify('confirm', Translator.trans('recommendation.pasted'))
     },
 
+    getCurrentSelectedOption (fieldId) {
+      const selectedOption = this.customFields[fieldId].attributes.options?.find(
+        option => option.id === this.segment.attributes.customFields?.find(
+          customFieldIdValue => customFieldIdValue.id === fieldId)?.value)
+
+      if (!selectedOption) {
+        return
+      }
+
+      return selectedOption
+    },
+
     handleTabChange (id) {
       this.activeId = id
     },
@@ -770,6 +782,20 @@ export default {
         .then(() => {
           this.setSelectedAssignee()
         })
+    },
+
+    initCustomFieldValues () {
+      Object.values(this.customFields).map((field) => {
+        const selectedOption = this.getCurrentSelectedOption(field.id)
+
+        if (selectedOption) {
+          this.customFieldValues[field.id] = {
+            fieldId: field.id,
+            id: selectedOption.id + ":" + selectedOption.label,
+            name: selectedOption.label,
+          }
+        }
+      })
     },
 
     initPlaces () {
@@ -1139,6 +1165,7 @@ export default {
   mounted () {
     this.initPlaces()
     this.initAssignableUsers()
+    this.initCustomFieldValues()
 
     if (hasPermission('field_segments_custom_fields') && this.segment.attributes.customFields?.length > 0) {
       this.setInitiallySelectedCustomFieldValues()
