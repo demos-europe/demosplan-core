3531b6d9e929aaec6a5c25e66c745ae64cd78329
Heidrun Wiemold <wiemold@demos-deutschland.de>
2025-08-22 13:34:19 +0200

    chore: remove duplicate import

--- client/js/components/procedure/StatementSegmentsList/StatementMeta/StatementMeta.vue
+++ client/js/components/procedure/StatementSegmentsList/StatementMeta/StatementMeta.vue
@@ -129,7 +129,6 @@ import {
   hasAnyPermissions
 } from '@demos-europe/demosplan-ui'
 import { mapActions, mapMutations, mapState } from 'vuex'
-import { dpValidateMixin } from '@demos-europe/demosplan-ui'
 import StatementEntry from './StatementEntry'
 import StatementMetaAttachments from './StatementMetaAttachments'
 import StatementMetaFinalEmail from './StatementMetaFinalEmail'
