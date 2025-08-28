e8a1fcf3f0a733707d9a660bdfa41220186d1a96
Catalina Sierra Zamudio <sierra-zamudio@demos-deutschland.de>
2025-08-28 14:56:28 +0200

    feat (refs: DPLAN-15947) Create Strategy to remove customField values given a customField id and a segment Id

---
+++ demosplan/DemosPlanCoreBundle/Utils/CustomField/Strategy/SegmentCustomFieldUsageRemovalStrategy.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
+namespace demosplan\DemosPlanCoreBundle\Utils\CustomField\Strategy;
+
+use demosplan\DemosPlanCoreBundle\CustomField\CustomFieldValue;
+use demosplan\DemosPlanCoreBundle\CustomField\CustomFieldValuesList;
+use demosplan\DemosPlanCoreBundle\Entity\Statement\Segment;
+use demosplan\DemosPlanCoreBundle\Repository\SegmentRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+class SegmentCustomFieldUsageRemovalStrategy implements EntityCustomFieldUsageRemovalStrategyInterface
+{
+    public function __construct(
+        private readonly SegmentRepository $segmentRepository,
+        private readonly EntityManagerInterface $entityManager,
+    ) {
+    }
+
+    public function removeUsages(string $customFieldId): void
+    {
+        $segments = $this->segmentRepository->findSegmentsWithCustomField($customFieldId);
+
+        foreach ($segments as $segment) {
+            $this->removeCustomFieldFromSegment($segment, $customFieldId);
+        }
+
+        $this->entityManager->flush();
+    }
+
+    public function supports(string $targetEntityClass): bool
+    {
+        return Segment::class === $targetEntityClass;
+    }
+
+    private function removeCustomFieldFromSegment(Segment $segment, string $customFieldId): void
+    {
+        $customFields = $segment->getCustomFields();
+        if ($customFields instanceof CustomFieldValuesList) {
+            $customFieldValue = $customFields->findById($customFieldId);
+            if ($customFieldValue instanceof CustomFieldValue) {
+                $customFields->removeCustomFieldValue($customFieldValue);
+                $customFields->reindexValues();
+                $segment->setCustomFields($customFields);
+            }
+        }
+    }
+}
