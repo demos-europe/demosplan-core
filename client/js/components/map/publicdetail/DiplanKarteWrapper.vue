90c37c8990a93ec4d50d99fee76a2d06be6b32fb
Ruth Meissner <meissner@demos-deutschland.de>
2025-09-04 09:04:14 +0200

    feat(refs:DPLAN-16443): trying the configs
    this is a mess. dont use this.

--- client/js/components/map/publicdetail/DiplanKarteWrapper.vue
+++ client/js/components/map/publicdetail/DiplanKarteWrapper.vue
@@ -18,12 +18,13 @@
     />
 
     <diplan-karte
-      v-if="isStoreAvailable"
-      :geojson="drawing"
+      v-if="portalConfig && layerConfig"
+      :fit-to-extent="extent"
+      :portal-config="portalConfig"
+      :layer-config="layerConfig"
+      :jwt-access-token="authToken"
       enable-searchbar
-      enable-toolbar
-      profile="beteiligung"
-      @diplan-karte:geojson-update="handleDrawing"
+      enabled-toolbar-group="none"
     />
   </div>
 </template>
@@ -38,7 +39,7 @@ import { useStore } from 'vuex'
 const { activeStatement, initDrawing, loginPath, styleNonce } = defineProps({
   activeStatement: {
     type: Boolean,
-    required: true
+    required: true,
   },
 
   initDrawing: {
@@ -46,25 +47,91 @@ const { activeStatement, initDrawing, loginPath, styleNonce } = defineProps({
     required: false,
     default: () => ({
       type: 'FeatureCollection',
-      features: []
-    })
+      features: [],
+    }),
   },
 
   loginPath: {
     type: String,
-    required: true
+    required: true,
   },
 
   styleNonce: {
     type: String,
-    required: true
+    required: true,
+  },
+})
+
+// From readme diplankarte
+
+const api = 'https://geodienste.develop.diplanung.de/api/karte-backend'
+const authToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3NTY5MDIxMTcsImV4cCI6MTc1NzE2MTMxNywicm9sZXMiOlsiUkNJVElaIl0sInVzZXJuYW1lIjoiYm9iLXNoNDNAZGVtb3MtZGV1dHNjaGxhbmQuZGUifQ.lGQC5ID_Xxkk8_P2RJq-EdtetbWTw22W1Udg0exJ0xE'
+
+const portalConfig = ref()
+const layerConfig = ref()
+
+/**
+ * BEGIN helper functions you could also move to a helper file
+ */
+function getHeaders (authToken) {
+  if (!authToken) {
+    throw new Error('There was no jwtAuthToken set!')
+  }
+  return {
+    Authorization: `Bearer ${authToken}`,
+  }
+}
+
+async function getLayerConfig (authToken) {
+  const headers = getHeaders(authToken)
+  const response = await fetch(`${api}/config/layer`, {
+    method: 'GET',
+    headers: headers,
+  })
+
+  if (!response.ok) {
+    throw new Error(
+      `Failed to fetch layerConfig: ${response.status} ${response.statusText}`,
+    )
+  }
+
+  return await response.json()
+}
+
+async function getPortalConfig (authToken) {
+  const headers = getHeaders(authToken)
+  const response = await fetch(`${api}/config/portal`, {
+    method: 'GET',
+    headers: headers,
+  })
+
+  if (!response.ok) {
+    throw new Error(
+      `Failed to fetch portalConfig: ${response.status} ${response.statusText}`,
+    )
+  }
+
+  return await response.json()
+}
+/**
+ * END helper functions you could also move to a helper file
+ */
+
+onMounted(async () => {
+  try {
+    portalConfig.value = await getPortalConfig(authToken)
+    layerConfig.value = await getLayerConfig(authToken)
+  } catch (error) {
+    console.error('Error loading configs:', error.message)
   }
 })
 
+// ///end readme
+
 const drawing = computed(() => {
-  return initDrawing
-    ? transformFeatureCollection(JSON.parse(initDrawing), 'EPSG:3857', 'EPSG:4326')
-    : ''
+  return initDrawing ?
+    transformFeatureCollection(JSON.parse(initDrawing), 'EPSG:3857', 'EPSG:4326') :
+    ''
 })
 const emit = defineEmits(['locationDrawing'])
 
@@ -72,6 +139,64 @@ const instance = getCurrentInstance()
 const store = useStore()
 
 instance.appContext.app.mixin(prefixClassMixin)
+
+const extent = [10.3, 53.2347, 10.4, 53.25]
+
+const customLayerList = [
+  {
+    id: '3333',
+    name: 'HafenCity14 - Vektor',
+    url: 'https://hh.xplanungsplattform.de/xplan-wms/services/wms',
+    typ: 'WMS',
+    layers: 'BP_Planvektor',
+    format: 'image/png',
+    version: '1.1.1',
+    singleTile: false,
+    transparent: true,
+    transparency: 0,
+    gutter: 0,
+    minScale: '0',
+    maxScale: '2500000',
+    tilesize: 512,
+    visibleOnLoad: true,
+  },
+  {
+    id: '4444',
+    name: 'HafenCity14 - Raster',
+    url: 'https://hh.xplanungsplattform.de/xplan-wms/services/wms',
+    typ: 'WMS',
+    layers: 'BP_Planraster',
+    format: 'image/png',
+    version: '1.1.1',
+    singleTile: false,
+    transparent: true,
+    transparency: 0,
+    gutter: 0,
+    minScale: '0',
+    maxScale: '2500000',
+    tilesize: 512,
+    visibleOnLoad: true,
+  },
+]
+
+const customLayerConfigurationList = [
+  {
+    Titel: 'Vektor Layer',
+    Layer: [
+      {
+        id: '3333',
+      },
+    ],
+  },
+  {
+    Titel: 'Raster Layer',
+    Layer: [
+      {
+        id: '4444',
+      },
+    ],
+  },
+]
 
 const isStoreAvailable = computed(() => {
   return store.state.PublicStatement.storeInitialised
@@ -97,7 +222,7 @@ const handleDrawing = (event) => {
       r_location: 'notLocated',
       r_location_geometry: '',
       r_location_point: '',
-      location_is_set: ''
+      location_is_set: '',
     }
   } else {
     payload = {
@@ -106,7 +231,7 @@ const handleDrawing = (event) => {
       r_location_priority_area_key: '',
       r_location_priority_area_type: '',
       r_location_point: '',
-      location_is_set: 'geometry'
+      location_is_set: 'geometry',
     }
   }
 
@@ -135,11 +260,8 @@ const toggleStatementModal = (updateStatementPayload) => {
 
 onMounted(() => {
   registerWebComponent({
-    nonce: styleNonce
+    nonce: styleNonce,
   })
-})
-
-onMounted(() => {
   store.commit('PublicStatement/update', { key: 'activeActionBoxTab', val: 'talk' })
 })
 
