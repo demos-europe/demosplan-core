4d88535c2707ace1872db148e04e5e4357959c4a
Catalina Sierra Zamudio <sierra-zamudio@demos-deutschland.de>
2025-09-05 09:59:58 +0200

    feat (refs: DPLAN-16348) Fix duplicated code

--- demosplan/DemosPlanCoreBundle/Utils/CustomField/MultiSelectFieldValidator.php
+++ demosplan/DemosPlanCoreBundle/Utils/CustomField/MultiSelectFieldValidator.php
@@ -10,41 +10,18 @@
 
 namespace demosplan\DemosPlanCoreBundle\Utils\CustomField;
 
-use demosplan\DemosPlanCoreBundle\Entity\Procedure\Procedure;
-use demosplan\DemosPlanCoreBundle\Entity\Statement\Statement;
-
 class MultiSelectFieldValidator extends CustomFieldValidator
 {
-    private const FIELD_TYPE = 'multiSelect';
-
-    protected const SOURCE_TO_TARGET_MAPPING = [
-        'PROCEDURE'          => 'STATEMENT',
-        'PROCEDURE_TEMPLATE' => 'STATEMENT',
-    ];
-
-    protected const CLASS_NAME_TO_CLASS_PATH_MAP = [
-        'PROCEDURE'          => Procedure::class,
-        'PROCEDURE_TEMPLATE' => Procedure::class,
-        'STATEMENT'          => Statement::class,
-    ];
-
-    public function supports(string $fieldType): bool
-    {
-        return self::FIELD_TYPE === $fieldType;
-    }
-
     public function getFieldType(): string
     {
-        return self::FIELD_TYPE;
+        return 'multiSelect';
     }
 
     public function getSourceToTargetMapping(): array
     {
-        return self::SOURCE_TO_TARGET_MAPPING;
-    }
-
-    public function getClassNameToClassPathMap(): array
-    {
-        return self::CLASS_NAME_TO_CLASS_PATH_MAP;
+        return [
+            'PROCEDURE'          => 'STATEMENT',
+            'PROCEDURE_TEMPLATE' => 'STATEMENT',
+        ];
     }
 }
