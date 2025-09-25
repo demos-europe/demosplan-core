307e8b009dad59d0361bfdabe128aab4e429fb29
Lars Hüllner <lars@demos-international.com>
2025-09-25 16:35:33 +0200

    feat(refs DPLAN-16594): solve v-html lint warning

--- client/js/components/map/publicdetail/diplankarte/DiplanKarteWrapper.vue
+++ client/js/components/map/publicdetail/diplankarte/DiplanKarteWrapper.vue
@@ -34,8 +34,9 @@
     <div
       v-if="copyright"
       :class="prefixClass('left-0 bottom-[10px] !absolute z-above-zero bg-white/80 px-1 py-0.5 text-xs text-gray-600 rounded max-w-2/3')"
-      v-html="copyright"
-    />
+    >
+      {{ copyright }}
+    </div>
   </div>
 </template>
 
