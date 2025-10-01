e64cf8c4b52d59aa2c182ab2b863fa6566695a90
Sami Mussbach <mussbach@demos-deutschland.de>
2025-10-01 15:00:18 +0200

    chore: include ADO_NPM_AUTH_IDENT as secret

--- docker/build.sh
+++ docker/build.sh
@@ -58,9 +58,9 @@ rsync --exclude-from=rsyncExcludeProject.txt -az "../projects/$PROJECT_NAME" $CO
 cp -r "$FOLDER"/* $CONTEXT_DIR
 cp -r "$FOLDER"/.dockerignore $CONTEXT_DIR
 
-docker_build "$IMAGE_NAME" fpm --secret id=envlocal,src=../.env.local
-docker_build "$IMAGE_NAME/nginx" nginx
-docker_build "$IMAGE_NAME-nginx" nginx
+docker_build "$IMAGE_NAME" fpm --secret id=envlocal,src=../.env.local --secret id=ado,env=ADO_NPM_AUTH_IDENT
+docker_build "$IMAGE_NAME/nginx" nginx --secret id=envlocal,src=../.env.local --secret id=ado,env=ADO_NPM_AUTH_IDENT
+docker_build "$IMAGE_NAME-nginx" nginx --secret id=envlocal,src=../.env.local --secret id=ado,env=ADO_NPM_AUTH_IDENT
 
 rm -rf $CONTEXT_DIR
 
