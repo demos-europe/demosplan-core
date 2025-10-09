fa68224f65ed9b31e3b9b5e170662593029f15a4
Ruth Meissner <meissner@demos-deutschland.de>
2025-10-09 13:56:12 +0200

    feat(refs:DPLAN-16563): allow save without wms layers

--- demosplan/DemosPlanCoreBundle/Logic/Map/ServiceStorage.php
+++ demosplan/DemosPlanCoreBundle/Logic/Map/ServiceStorage.php
@@ -108,7 +108,8 @@ class ServiceStorage implements MapServiceStorageInterface
             ];
         }
 
-        if ((!array_key_exists('r_layers', $data) || '' === trim((string) $data['r_layers']))
+        $isXtrasse = array_key_exists('r_serviceType', $data) && 'xtrasse' === strtolower(trim((string) $data['r_serviceType']));
+        if (!$isXtrasse && (!array_key_exists('r_layers', $data) || '' === trim((string) $data['r_layers']))
             && !array_key_exists('r_xplanDefaultlayers', $data)) {
             $mandatoryErrors[] = [
                 'type'    => 'error',
@@ -347,7 +348,8 @@ class ServiceStorage implements MapServiceStorageInterface
             ];
         }
 
-        if (!$isGlobalLayer && (!array_key_exists('r_layers', $data) || '' === trim((string) $data['r_layers']))) {
+        $isXtrasse = array_key_exists('r_serviceType', $data) && 'xtrasse' === strtolower(trim((string) $data['r_serviceType']));
+        if (!$isGlobalLayer && !$isXtrasse && (!array_key_exists('r_layers', $data) || '' === trim((string) $data['r_layers']))) {
             $mandatoryErrors[] = [
                 'type'    => 'error',
                 'message' => $this->legacyFlashMessageCreator->createFlashMessage(
