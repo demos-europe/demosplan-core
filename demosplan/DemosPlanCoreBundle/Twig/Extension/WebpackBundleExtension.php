31a17ca55ec4444d35d75b3349cb6ddbc4a29fa8
mussbach <57546580+mussbach@users.noreply.github.com>
2025-11-14 09:27:51 +0100

    chore: optimize asset build and loading (5383)
    * chore: optimize asset build and loading
    
    * style: Apply php-cs-fixer
    
    * refactor: improve code quality in webpack build
    
    - Refactor renderTag method to eliminate duplication of defer attribute logic
    - Add missing error logging for unknown webpack failures
    - Improve code maintainability and debugging visibility
    
    * style: Apply php-cs-fixer
    
    * chore: remove useless comment
    
    ---------
    
    Co-authored-by: Demos-CI <devops-system+github@demos-deutschland.de>

--- demosplan/DemosPlanCoreBundle/Twig/Extension/WebpackBundleExtension.php
+++ demosplan/DemosPlanCoreBundle/Twig/Extension/WebpackBundleExtension.php
@@ -44,6 +44,14 @@ class WebpackBundleExtension extends ExtensionBase
     ];
 
     /**
+     * These bundles should not have defer attribute as they are dependencies
+     * that must be loaded synchronously for inline scripts.
+     */
+    private const NON_DEFER_BUNDLES = [
+        'jquery-3.5.1.min.js',
+    ];
+
+    /**
      * The webpack manifest.
      *
      * This is the combination of `dplan.manifest.json` and `styles.manifest.json`.
@@ -183,18 +191,37 @@ class WebpackBundleExtension extends ExtensionBase
      */
     protected function renderTag($bundleSrc, bool $legacy, string $bundleName, string $dataBundle): string
     {
-        $tagTemplate = '<script src="%s" '.$this->addNonce('script').'></script>';
+        // Handle CSS files
+        if (strpos($bundleName, '.css') > 0) {
+            return sprintf(
+                '<link rel="stylesheet" href="%s" %s>',
+                $this->formatBundlePath($bundleSrc),
+                $this->addNonce('style')
+            );
+        }
 
+        // Build script tag attributes (defer + nonce)
+        $shouldDefer = !in_array($bundleName, self::NON_DEFER_BUNDLES, true);
+        $attributes = ($shouldDefer ? 'defer ' : '').$this->addNonce('script');
+
+        // Script with data-bundle attribute
         if (!$legacy && !in_array($bundleName, self::NON_DATA_BUNDLES, true)) {
             $dataBundle = explode('.', $dataBundle)[0];
-            $tagTemplate = '<script src="%s" data-bundle="%s" '.$this->addNonce('script').'></script>';
+
+            return sprintf(
+                '<script src="%s" data-bundle="%s" %s></script>',
+                $this->formatBundlePath($bundleSrc),
+                $dataBundle,
+                $attributes
+            );
         }
 
-        if (strpos($bundleName, '.css') > 0) {
-            $tagTemplate = '<link rel="stylesheet" href="%s" '.$this->addNonce('style').'>';
-        }
-
-        return sprintf($tagTemplate, $this->formatBundlePath($bundleSrc), $dataBundle);
+        // Regular script tag
+        return sprintf(
+            '<script src="%s" %s></script>',
+            $this->formatBundlePath($bundleSrc),
+            $attributes
+        );
     }
 
     /**
