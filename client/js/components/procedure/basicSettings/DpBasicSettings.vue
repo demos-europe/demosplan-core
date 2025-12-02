89278c985f65e04082ad60c29d382cd9c4368478
Ania Rafeld <rafeld@demos-deutschland.de>
2025-12-02 17:24:22 +0100

    feat(refs BEAA2-27): Remove comments, fix indentation

--- client/js/components/procedure/basicSettings/DpBasicSettings.vue
+++ client/js/components/procedure/basicSettings/DpBasicSettings.vue
@@ -190,9 +190,7 @@ export default {
     },
 
     isPublicParticipationPhaseActive () {
-      // Check current public phase
       const currentPhaseIsPublic = this.publicParticipationPhases.includes(this.selectedPublicPhase)
-      // Check auto-switch phase ONLY if auto-switch is enabled and it's for public phases
       const autoSwitchPhaseIsPublic = this.addonCheckAutoSwitchEnabled &&
         this.publicParticipationPhases.includes(this.addonCheckAutoSwitchPhase)
 
@@ -204,11 +202,14 @@ export default {
     },
 
     shouldShowInterfaceWarningModal () {
-      // Check if checkbox is not disabled
       const checkbox = document.getElementById('interfaceFieldsToTransmit-checkbox')
-      const isCheckboxDisabled = checkbox?.disabled ?? true
+      const isInterfaceCheckboxEnabled = !(checkbox?.disabled ?? true)
 
-      return this.isAddonLoaded && !this.isAddonInterfaceActivated && this.isPublicParticipationPhaseActive && !this.bypassAddonWarningModal && !isCheckboxDisabled
+      return this.isAddonLoaded &&
+        !this.isAddonInterfaceActivated &&
+        this.isPublicParticipationPhaseActive &&
+        !this.bypassAddonWarningModal &&
+        isInterfaceCheckboxEnabled
     },
   },
 
