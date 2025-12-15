85d22d1709543f857ec49419d1b3ac9d49a528b7
riechedemos <carolin@demos-international.com>
2025-12-15 10:02:32 +0100

    fix(DPLAN-12758): change single label for 2 datepicker inputs into a fieldset

--- client/js/components/procedure/admin/DpNewProcedure/DpNewProcedure.vue
+++ client/js/components/procedure/admin/DpNewProcedure/DpNewProcedure.vue
@@ -175,15 +175,11 @@
         reduced-height
       />
 
-      <div class="mb-4">
-        <dp-label
-          class="mb-0"
-          for="startdate"
-          :hint="Translator.trans('explanation.date.procedure')"
-          :required="hasPermission('field_required_procedure_end_date')"
-          :text="Translator.trans('period')"
-          :tooltip="Translator.trans('explanation.date.format')"
-        />
+      <fieldset class="mb-4">
+        <legend class="weight--bold">
+          {{ Translator.trans('period') }}
+          <dp-contextual-help :text="Translator.trans('explanation.date.format')" />
+        </legend>
 
         <dp-date-range-picker
           class="w-1/2"
@@ -191,6 +187,9 @@
           start-name="r_startdate"
           end-id="enddate"
           end-name="r_enddate"
+          :start-label="Translator.trans('start')"
+          :end-label="Translator.trans('end')"
+          aria-labels="true"
           data-cy="newProcedureForm"
           :data-dp-validate-error-fieldname="Translator.trans('period')"
           :required="hasPermission('field_required_procedure_end_date')"
@@ -203,7 +202,7 @@
           id="js__statusBox"
           class="sr-only flash"
         />
-      </div>
+      </fieldset>
 
       <div
         v-if="hasPermission('feature_procedure_couple_by_token')"
@@ -248,6 +247,7 @@
 import {
   dpApi,
   DpButton,
+  DpContextualHelp,
   DpDateRangePicker,
   DpInlineNotification,
   DpInput,
@@ -267,6 +267,7 @@ export default {
     AddonWrapper,
     CoupleTokenInput,
     DpButton,
+    DpContextualHelp,
     DpDateRangePicker,
     DpInput,
     DpLabel,
