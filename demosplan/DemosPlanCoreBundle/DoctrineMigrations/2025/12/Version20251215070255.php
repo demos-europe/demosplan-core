58615b2c745cce0481b805efa311478a01342fd2
Lamia Gülsah Elmas <elmas@demos-deutschland.de>
2025-12-15 08:29:45 +0100

    Fix(refs DPLAN-16442): Soft delete organizations with NULL or undefined names

---
+++ demosplan/DemosPlanCoreBundle/DoctrineMigrations/2025/12/Version20251215070255.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Platforms\MySQLPlatform;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+class Version20251215070255 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Soft delete organizations with NULL or undefined names';
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function up(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+
+        $this->addSql("UPDATE _orga SET _o_deleted = 1 WHERE (_o_name IS NULL OR _o_name = 'undefined') AND _o_deleted = 0");
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function down(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function abortIfNotMysql(): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof MySQLPlatform,
+            "Migration can only be executed safely on 'mysql'."
+        );
+    }
+}
