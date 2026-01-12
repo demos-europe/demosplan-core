c77f90361b48d8149a7737dc81e8a5ff2c4836d0
Catalina Sierra Zamudio <sierra-zamudio@demos-deutschland.de>
2026-01-12 18:53:52 +0100

    feat (DPLAN-16352): Add custom fields to draft statements

---
+++ demosplan/DemosPlanCoreBundle/DoctrineMigrations/2026/01/Version20260108203256.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types = 1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Platforms\MySQLPlatform;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+class Version20260108203256 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'refs DPLAN-16352: Add custom fields value column to draft statement table to enable storing custom field values in public view for draft statements';
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function up(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+
+        $this->addSql('ALTER TABLE _draft_statement ADD custom_fields JSON DEFAULT NULL COMMENT \'(DC2Type:dplan.custom_fields_value)\'');
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function down(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+
+        $this->addSql('ALTER TABLE _draft_statement DROP custom_fields');
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function abortIfNotMysql(): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof MySQLPlatform,
+            "Migration can only be executed safely on 'mysql'."
+        );
+    }
+}
