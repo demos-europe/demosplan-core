bf3fd6e32bebf37fcef042392a4c93a12703cbd9
Demos-CI <devops-system+github@demos-deutschland.de>
2026-01-16 13:17:14 +0000

    style: Apply php-cs-fixer

--- demosplan/DemosPlanCoreBundle/Utils/CustomField/Enum/CustomFieldPropertyName.php
+++ demosplan/DemosPlanCoreBundle/Utils/CustomField/Enum/CustomFieldPropertyName.php
@@ -3,16 +3,17 @@
 declare(strict_types=1);
 
 /**
-* This file is part of the package demosplan.
-*
-* (c) 2010-present DEMOS plan GmbH, for more information see the license file.
-*
-* All rights reserved
-*/
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
 
 namespace demosplan\DemosPlanCoreBundle\Utils\CustomField\Enum;
+
 enum CustomFieldPropertyName: string
 {
-    case ColumnName  = 'custom_fields';
-    case twigRequestName  = 'customFields';
+    case ColumnName = 'custom_fields';
+    case twigRequestName = 'customFields';
 }
