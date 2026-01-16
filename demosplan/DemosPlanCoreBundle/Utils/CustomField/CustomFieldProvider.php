8da6698803d322348a129bcd042ab8f1486e5d45
Demos-CI <devops-system+github@demos-deutschland.de>
2026-01-16 14:14:31 +0000

    style: Apply php-cs-fixer

--- demosplan/DemosPlanCoreBundle/Utils/CustomField/CustomFieldProvider.php
+++ demosplan/DemosPlanCoreBundle/Utils/CustomField/CustomFieldProvider.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
 namespace demosplan\DemosPlanCoreBundle\Utils\CustomField;
 
 use demosplan\DemosPlanCoreBundle\Repository\CustomFieldConfigurationRepository;
@@ -14,9 +22,8 @@ class CustomFieldProvider
     ) {
     }
 
-
     public function getCustomFieldsByCriteria(string $sourceEntity, string $sourceEntityId, string $targetEntity): ArrayCollection
     {
-        return $this->customFieldConfigurationRepository->getCustomFields($sourceEntity,$sourceEntityId, $targetEntity);
+        return $this->customFieldConfigurationRepository->getCustomFields($sourceEntity, $sourceEntityId, $targetEntity);
     }
 }
