16762f98ef307fc6bab99d8ef1924121447a6f18
Catalina Sierra Zamudio <sierra-zamudio@demos-deutschland.de>
2026-01-20 09:00:35 +0100

    feat (DPLAN-16352): Remove unneeded params

--- demosplan/DemosPlanCoreBundle/Repository/CustomFieldJsonRepository.php
+++ demosplan/DemosPlanCoreBundle/Repository/CustomFieldJsonRepository.php
@@ -30,7 +30,6 @@ class CustomFieldJsonRepository implements RepositoryInterface
     public function __construct(
         protected readonly EntityManagerInterface $entityManager,
         protected readonly ConditionFactoryInterface $conditionFactory,
-        private readonly Reindexer $reindexer,
         private readonly CustomFieldConfigurationRepository $customFieldConfigurationRepository,
     ) {
     }
@@ -54,7 +53,7 @@ class CustomFieldJsonRepository implements RepositoryInterface
         throw new InvalidArgumentException();
     }
 
-    public function getEntities(array $conditions, array $sortMethods, int $offset = 0, ?int $limit = null): array
+    public function getEntities(array $conditions, array $sortMethods): array
     {
         $customFieldConfigurations = $this->customFieldConfigurationRepository->getEntities($conditions, $sortMethods);
 
