861fa8d74929e56d4ccff440ea76d5c83f03c1ab
Ania Rafeld <rafeld@demos-deutschland.de>
2026-01-26 14:18:06 +0100

    fix(refs BEEA2-38): Simplify the checkbox check

--- client/js/components/procedure/basicSettings/DpBasicSettings.vue
+++ client/js/components/procedure/basicSettings/DpBasicSettings.vue
@@ -234,7 +234,7 @@ export default {
 
     shouldShowInterfaceWarningModal () {
       const checkbox = document.getElementById('interfaceFieldsToTransmit-checkbox')
-      const isInterfaceCheckboxEnabled = !(checkbox?.checked ?? true)
+      const isInterfaceCheckboxEnabled = !checkbox?.checked
 
       return this.isAddonLoaded &&
         !this.isAddonInterfaceActivated &&
