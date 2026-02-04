ae7ed421f563bceefda7be5556640bb13b63a434
Stefan Graupner <graupner@demos-deutschland.de>
2026-02-04 13:18:43 +0100

    chore: Backport export format changes to release_blp

--- demosplan/DemosPlanCoreBundle/Controller/Report/DemosPlanReportController.php
+++ demosplan/DemosPlanCoreBundle/Controller/Report/DemosPlanReportController.php
@@ -88,7 +88,7 @@ class DemosPlanReportController extends BaseController
         $response = new StreamedResponse(
             static function () use ($procedureId, $reportMeta, $reportService, $permissions) {
                 $reportInfo = $reportService->getReportInfo($procedureId, $permissions);
-                $pdfReport = $reportService->generateProcedureReport($reportInfo, $reportMeta);
+                $pdfReport = $reportService->generateProcedureReport($procedureId, $reportInfo, $reportMeta);
                 $pdfReport->save('php://output');
             }
         );
