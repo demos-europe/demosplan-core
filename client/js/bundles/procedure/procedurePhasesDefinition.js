54939a48a95a4b50c4ef9dd6470e22627ac1ab28
Ania Rafeld <rafeld@demos-deutschland.de>
2026-02-27 09:24:59 +0100

    feat(refs DPLAN-16766): Add VUe component, JS entry point and the twig template

---
+++ client/js/bundles/procedure/procedurePhasesDefinition.js
@@ -0,0 +1,12 @@
+/**
+ * This is the entrypoint for procedure_phases_definition.html.twig
+ */
+
+import { initialize } from '@DpJs/InitVue'
+import ProcedurePhasesDefinition from '@DpJs/components/procedure/admin/ProcedurePhasesDefinition'
+
+const components = {
+  ProcedurePhasesDefinition,
+}
+
+initialize(components)
---
+++ client/js/components/procedure/admin/ProcedurePhasesDefinition.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>{{ Translator.trans('phases.currently.defined') }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'ProcedurePhases',
+}
+</script>
