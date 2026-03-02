b9f7b87e2f8232bab223754b524d11cc97221a1b
Demos-CI <devops-system+github@demos-deutschland.de>
2026-03-02 11:16:09 +0000

    style: Apply php-cs-fixer

--- demosplan/DemosPlanCoreBundle/Logic/Segment/SegmentsExporter.php
+++ demosplan/DemosPlanCoreBundle/Logic/Segment/SegmentsExporter.php
@@ -134,9 +134,8 @@ abstract class SegmentsExporter
             && ([] !== $exportFilteredByTags || [] !== $exportTagTitles)
             && $this->currentUser->hasPermission('feature_adjust_export_file_name')) {
             $filteredExportPreamble = $this->translator->trans('docx.export.filtered');
-            foreach ($exportTagTitles as $tagTopicContainer)
-            {
-                $appendToVariable = "Schlagwort: ".$tagTopicContainer[0].' [Thema: '.$tagTopicContainer[1].']'.', ';
+            foreach ($exportTagTitles as $tagTopicContainer) {
+                $appendToVariable = 'Schlagwort: '.$tagTopicContainer[0].' [Thema: '.$tagTopicContainer[1].'], ';
                 $filteredExportPreamble .= $appendToVariable;
             }
             Html::addHtml($header, $this->htmlHelper->getHtmlValidText($filteredExportPreamble), false, false);
