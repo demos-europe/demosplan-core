ba27f4fd08507a5b7276777804e63023bda85380
Ania Rafeld <rafeld@demos-deutschland.de>
2026-03-03 15:22:14 +0100

    feat(refs DPLAN-16766): Add createPhase method and render create form
    - add translations
    - render success notification after create
    - reset form after new phases fetch

--- client/js/components/procedurePhasesDefinition/ProcedurePhasesDefinition.vue
+++ client/js/components/procedurePhasesDefinition/ProcedurePhasesDefinition.vue
@@ -3,6 +3,65 @@
     <h1>{{ Translator.trans('phases.currently.defined') }}</h1>
 
     <div class="space-stack-m mt-4">
+      <div
+        v-if="!isCreating"
+        class="text-right"
+      >
+        <dp-button
+          :text="Translator.trans('phase.create')"
+          @click="isCreating = true"
+        />
+      </div>
+
+      <div
+        v-if="isCreating"
+        class="border rounded-sm space-stack-m space-inset-m relative"
+        data-dp-validate="phaseForm"
+      >
+        <dp-loading
+          v-if="isLoading"
+          overlay
+        />
+
+        <dp-input
+          id="phaseName"
+          v-model="newPhase.name"
+          :label="{ text: Translator.trans('phase.name') }"
+          required
+        />
+
+        <dp-select
+          id="phaseAudience"
+          v-model="newPhase.audience"
+          :label="{ text: Translator.trans('audience') }"
+          :options="audienceOptions"
+          required
+        />
+
+        <dp-select
+          id="phasePermissionSet"
+          v-model="newPhase.permissionSet"
+          :label="{ text: Translator.trans('status') }"
+          :options="permissionSetOptions"
+          required
+        />
+
+        <dp-select
+          id="phaseParticipationState"
+          v-model="newPhase.participationState"
+          :label="{ text: Translator.trans('participation.state') }"
+          :options="participationStateOptions"
+        />
+
+        <dp-button-row
+          :busy="isLoading"
+          primary
+          secondary
+          @primary-action="dpValidateAction('phaseForm', () => createPhase(), false)"
+          @secondary-action="resetForm()"
+        />
+      </div>
+
       <dp-accordion
         v-if="!isInitiallyLoading"
         :is-open="true"
@@ -37,8 +96,13 @@
 import {
   DpAccordion,
   dpApi,
+  DpButton,
+  DpButtonRow,
   DpDataTable,
+  DpInput,
   DpLoading,
+  DpSelect,
+  dpValidateMixin,
 } from '@demos-europe/demosplan-ui'
 
 export default {
@@ -46,18 +110,34 @@ export default {
 
   components: {
     DpAccordion,
+    DpButton,
+    DpButtonRow,
     DpDataTable,
+    DpInput,
     DpLoading,
+    DpSelect,
   },
+
+  mixins: [dpValidateMixin],
 
   data () {
     return {
+      isCreating: false,
       isInitiallyLoading: true,
+      isLoading: false,
+      newPhase: { name: '', audience: 'external', permissionSet: 'hidden', participationState: null },
       phases: [],
     }
   },
 
   computed: {
+    audienceOptions () {
+      return [
+        { label: Translator.trans('audience.external'), value: 'external' },
+        { label: Translator.trans('audience.internal'), value: 'internal' },
+      ]
+    },
+
     externalPhases () {
       return this.phases
         .filter(phase => phase.audience === 'external')
@@ -78,14 +158,6 @@ export default {
         .map(phase => this.mapPhaseForDisplay(phase))
     },
 
-    permissionSetLabels () {
-      return {
-        hidden: Translator.trans('permissionset.hidden'),
-        read: Translator.trans('permissionset.read'),
-        write: Translator.trans('permissionset.write'),
-      }
-    },
-
     participationStateOptions () {
       return [
         { label: Translator.trans('participation.state.none'), value: '' },
@@ -93,9 +165,56 @@ export default {
         { label: Translator.trans('participation.state.token'), value: 'participateWithToken' },
       ]
     },
+
+    permissionSetOptions () {
+      return [
+        { label: Translator.trans('permissionset.hidden'), value: 'hidden' },
+        { label: Translator.trans('permissionset.read'), value: 'read' },
+        { label: Translator.trans('permissionset.write'), value: 'write' },
+      ]
+    },
   },
 
   methods: {
+    createPhase () {
+      this.isLoading = true
+
+      dpApi.post(Routing.generate('api_resource_create', { resourceType: 'ProcedurePhaseDefinition' }), {}, {
+        data: {
+          type: 'ProcedurePhaseDefinition',
+          attributes: {
+            audience: this.newPhase.audience,
+            name: this.newPhase.name.trim(),
+            participationState: this.newPhase.participationState || null,
+            permissionSet: this.newPhase.permissionSet,
+          },
+        },
+      })
+        .then(({ data }) => {
+          const item = data.data
+          this.phases.push({
+            audience: item.attributes.audience,
+            id: item.id,
+            name: item.attributes.name,
+            orderInAudience: item.attributes.orderInAudience,
+            participationState: item.attributes.participationState,
+            permissionSet: item.attributes.permissionSet,
+          })
+
+          dplan.notify.confirm(Translator.trans('phase.created.success'))
+
+          this.fetchPhases()
+          this.resetForm()
+        })
+        .catch(err => {
+          console.error(err)
+          dplan.notify.error(Translator.trans('error.generic'))
+        })
+        .finally(() => {
+          this.isLoading = false
+        })
+    },
+
     fetchPhases () {
       this.isInitiallyLoading = true
 
@@ -138,8 +257,13 @@ export default {
         name: phase.name,
         orderInAudience: phase.orderInAudience,
         participationStateLabel: this.participationStateOptions.find(option => option.value === (phase.participationState ?? ''))?.label || phase.participationState,
-        permissionSetLabel: this.permissionSetLabels[phase.permissionSet] || phase.permissionSet,
+        permissionSetLabel: this.permissionSetOptions.find(option => option.value === phase.permissionSet)?.label || phase.permissionSet,
       }
+    },
+
+    resetForm () {
+      this.isCreating = false
+      this.newPhase = { name: '', audience: 'external', permissionSet: 'hidden', participationState: null }
     },
   },
 
