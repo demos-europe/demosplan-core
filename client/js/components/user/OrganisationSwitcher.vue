db20a334c189ca409eb8ab00736a3c1565baa853
riechedemos <carolin@demos-international.com>
2026-03-05 10:34:59 +0100

    feat(DPLAN-17451): create a vue component OrganisationSwitcher - refactor twig - create a new bundle as entrypoint

---
+++ client/js/components/user/OrganisationSwitcher.vue
@@ -0,0 +1,48 @@
+<license>
+  (c) 2010-present DEMOS plan GmbH.
+
+  This file is part of the package demosplan,
+  for more information see the license file.
+
+  All rights reserved
+</license>
+
+<template>
+  <div>
+    <dp-radio
+      v-for="organisation in organisations"
+      :id="`organisation-${organisation.id}`"
+      :key="organisation.id"
+      :checked="organisation.id === currentOrganisationId"
+      :label="{ text: organisation.name, hint: organisation.gwId ? `(${organisation.gwId})` : '' }"
+      class="mb-1"
+      name="organisation_id"
+      :value="organisation.id"
+      required
+    />
+  </div>
+</template>
+
+<script>
+import { DpRadio } from '@demos-europe/demosplan-ui'
+
+export default {
+  name: 'OrganisationSwitcher',
+
+  components: {
+    DpRadio,
+  },
+
+  props: {
+    organisations: {
+      type: Array,
+      required: true,
+    },
+
+    currentOrganisationId: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
