f69fd7812516de474a99417561b3f27637380bd3
Sami Mussbach <mussbach@demos-deutschland.de>
2026-03-09 19:10:53 +0100

    chore: use type safety in getCitizenOrga

--- demosplan/DemosPlanCoreBundle/Logic/OzgKeycloakDataMapper/PrivateCitizenMapper.php
+++ demosplan/DemosPlanCoreBundle/Logic/OzgKeycloakDataMapper/PrivateCitizenMapper.php
@@ -11,6 +11,7 @@
 namespace demosplan\DemosPlanCoreBundle\Logic\OzgKeycloakDataMapper;
 
 use DemosEurope\DemosplanAddon\Contracts\Entities\RoleInterface;
+use DemosEurope\DemosplanAddon\Contracts\Entities\UserInterface;
 use demosplan\DemosPlanCoreBundle\Entity\User\Orga;
 use demosplan\DemosPlanCoreBundle\Entity\User\Role;
 use demosplan\DemosPlanCoreBundle\Entity\User\User;
@@ -37,8 +38,13 @@ class PrivateCitizenMapper
         return false;
     }
 
-    public function getCitizenOrga(): ?Orga
+    public function getCitizenOrga(): Orga
     {
-        return $this->orgaRepository->findOneBy(['id' => User::ANONYMOUS_USER_ORGA_ID]);
+        $orga = $this->orgaRepository->findOneBy(['id' => UserInterface::ANONYMOUS_USER_ORGA_ID]);
+        if (null === $orga) {
+            throw new \RuntimeException(sprintf('Orga with id %d not found', UserInterface::ANONYMOUS_USER_ORGA_ID));
+        }
+
+        return $orga;
     }
 }
