eee1460bbf5e086639f0cfe241e42e07eb654020
Sami Mussbach <mussbach@demos-deutschland.de>
2026-03-09 19:27:46 +0100

    chore: fix sonarcloud issue
     Conflicts:
    	demosplan/DemosPlanCoreBundle/Logic/OzgKeycloakDataMapper/PrivateCitizenMapper.php

--- demosplan/DemosPlanCoreBundle/Logic/OzgKeycloakDataMapper/PrivateCitizenMapper.php
+++ demosplan/DemosPlanCoreBundle/Logic/OzgKeycloakDataMapper/PrivateCitizenMapper.php
@@ -14,8 +14,8 @@ use DemosEurope\DemosplanAddon\Contracts\Entities\RoleInterface;
 use DemosEurope\DemosplanAddon\Contracts\Entities\UserInterface;
 use demosplan\DemosPlanCoreBundle\Entity\User\Orga;
 use demosplan\DemosPlanCoreBundle\Entity\User\Role;
+use demosplan\DemosPlanCoreBundle\Exception\OrgaNotFoundException;
 use demosplan\DemosPlanCoreBundle\Repository\OrgaRepository;
-use RuntimeException;
 
 class PrivateCitizenMapper
 {
@@ -42,7 +42,7 @@ class PrivateCitizenMapper
     {
         $orga = $this->orgaRepository->findOneBy(['id' => UserInterface::ANONYMOUS_USER_ORGA_ID]);
         if (null === $orga) {
-            throw new RuntimeException(sprintf('Orga with id %d not found', UserInterface::ANONYMOUS_USER_ORGA_ID));
+            throw new OrgaNotFoundException(sprintf('Orga with id %s not found', UserInterface::ANONYMOUS_USER_ORGA_ID));
         }
 
         return $orga;
