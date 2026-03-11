3cf25ec4e909c6b7c66ee05c612494a3f211cb36
Ania Rafeld <rafeld@demos-deutschland.de>
2026-03-11 14:50:35 +0100

    feat(refs DPLAN-16766): Fix linting error

--- client/js/components/procedurePhasesDefinition/ProcedurePhasesDefinition.vue
+++ client/js/components/procedurePhasesDefinition/ProcedurePhasesDefinition.vue
@@ -165,7 +165,7 @@ export default {
         name: '',
         audience: '',
         permissionSet: '',
-        participationState: null
+        participationState: null,
       },
       phases: [],
     }
