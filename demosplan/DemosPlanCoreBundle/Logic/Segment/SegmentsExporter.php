2bf412488276aedcce1ecba1389511eef349c493
Vera Kijewski <kijewski@demos-deutschland.de>
2026-03-23 15:01:26 +0100

    f_DPLAN-17475: code adjustment for code review

--- demosplan/DemosPlanCoreBundle/Logic/Segment/SegmentsExporter.php
+++ demosplan/DemosPlanCoreBundle/Logic/Segment/SegmentsExporter.php
@@ -138,12 +138,13 @@ abstract class SegmentsExporter
         $section = $phpWord->addSection($this->styles['globalSection']);
         $this->addHeader($section, $procedure, Footer::FIRST, $exportTagTitles);
         $this->addHeader($section, $procedure, null, $exportTagTitles);
-        $exportDate = (new DateTime())->format('d.m.Y');
+        $exportDate = (new DateTime('now', new \DateTimeZone('Europe/Berlin')))->format('d.m.Y');
         $userName = $this->currentUser->getUser()->getFullName();
-        $pageInfoText = $this->translator->trans('export.user').': '.$userName.' am '.$exportDate.'<br>'.$this->translator->trans('docx.export.filtered');
+        $pageInfoText = $this->translator->trans('export.user').': '.$userName.' am '.$exportDate.'<br>';
 
         if ([] !== $exportTagTitles
             && $this->currentUser->hasPermission('feature_adjust_preamble_export_file')) {
+            $pageInfoText .= $this->translator->trans('docx.export.filtered');
             foreach ($exportTagTitles as $tagTopicContainer) {
                 $pageInfoText .= '- '.$tagTopicContainer[0].' [Thema: '.$tagTopicContainer[1].'] <br>';
             }
