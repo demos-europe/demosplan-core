c089750e724cab87a708bce4d3ed6e5d070ca2b3
Sami Mussbach <mussbach@demos-deutschland.de>
2026-03-23 16:44:39 +0100

    fix: guard getCustomFields with permission check in SegmentsList (6008)

--- client/js/components/procedure/SegmentsList/SegmentsList.vue
+++ client/js/components/procedure/SegmentsList/SegmentsList.vue
@@ -1094,7 +1094,9 @@ export default {
       })
     }
     this.initPagination()
-    this.getCustomFields()
+    if (hasPermission('field_segments_custom_fields')) {
+      this.getCustomFields()
+    }
     this.applyQuery(this.pagination.currentPage)
 
     this.fetchPlaces()
