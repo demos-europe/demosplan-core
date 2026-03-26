057c3cae21e53d97e2d27bf6efdb75786f7b56a3
Vera Kijewski <kijewski@demos-deutschland.de>
2026-03-26 14:43:26 +0100

    f_DPLAN-17524: added UserPasswordHistory.php and UserPasswordHistoryRepository.php

---
+++ demosplan/DemosPlanCoreBundle/Entity/User/UserPasswordHistory.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
+namespace  demosplan\DemosPlanCoreBundle\Entity\User;
+
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+class UserPasswordHistory
+{
+    private string $id;
+    private User $user;
+    private string $hashedPassword;
+    private DateTime $createdDate;
+
+    public function __construct(User $user, string $hashedPassword)
+    {
+        $this->id = \Ramsey\Uuid\Uuid::uuid4()->toString();
+        $this->user = $user;
+        $this->hashedPassword = $hashedPassword;
+    }
+    public function getHashedPassword(): string {return $this->hashedPassword;}
+    public function getUser(): User {return $this->user;}
+    public function getCreatedDate(): DateTime { return $this->createdDate;}
+}
---
+++ demosplan/DemosPlanCoreBundle/Repository/UserPasswordHistoryRepository.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
+namespace demosplan\DemosPlanCoreBundle\Repository;
+
+use demosplan\DemosPlanCoreBundle\Entity\User\User;
+use demosplan\DemosPlanCoreBundle\Entity\User\UserPasswordHistory;
+use Doctrine\ORM\EntityRepository;
+
+class UserPasswordHistoryRepository extends EntityRepository
+{
+    /**
+     * @return UserPasswordHistory[]
+     */
+    public function findByUser(User $user): array
+    {
+        return $this->findBy(['user' => $user]);
+    }
+}
