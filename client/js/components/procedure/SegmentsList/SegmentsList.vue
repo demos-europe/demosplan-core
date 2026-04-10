08a55380c69e1c82ab259e47ede9b400a467ef2e
riechedemos <carolin@demos-international.com>
2026-04-10 10:55:28 +0200

    fix(refs: DPLAN-17509) refactor is-columns-draggable

--- client/js/components/procedure/SegmentsList/SegmentsList.vue
+++ client/js/components/procedure/SegmentsList/SegmentsList.vue
@@ -162,7 +162,7 @@
             has-borders
             has-sticky-header
             :header-fields="availableHeaderFields"
-            :is-columns-draggable
+            is-columns-draggable
             is-resizable
             is-selectable
             :items="items"
