81d200ac42d4d8d61d271b708cc3e5c5939f3300
breierDemos <breier@demos-deutschland.de>
2026-04-17 13:06:45 +0200

    feat(refs: DPLAN-17646): extract portraitWithPrioritization template name into constant

--- demosplan/DemosPlanCoreBundle/Logic/Export/DocxExporter.php
+++ demosplan/DemosPlanCoreBundle/Logic/Export/DocxExporter.php
@@ -63,6 +63,7 @@ class DocxExporter
     final public const EXPORT_SORT_BY_PARAGRAPH_FRAGMENTS_ONLY = 'byParagraphFragmentsOnly';
     final public const EXPORT_SORT_BY_PARAGRAPH = 'byParagraph';
     final public const EXPORT_SORT_DEFAULT = 'default';
+    final public const TEMPLATE_PORTRAIT_WITH_PRIORITIZATION = 'portraitWithPrioritization';
     /**
      * @var array Style, wie Tabelle im gesamten aussehen soll
      */
@@ -1359,7 +1360,7 @@ class DocxExporter
                 }
 
                 // Address
-                if ('portraitWithPrioritization' !== $templateName && $this->isAddressExportable($organisationData, $exportConfig, $statement, $anonym)) {
+                if (self::TEMPLATE_PORTRAIT_WITH_PRIORITIZATION !== $templateName && $this->isAddressExportable($organisationData, $exportConfig, $statement, $anonym)) {
                     $cell2->addText(
                         htmlspecialchars($organisationData['postalAddressPartsOfAuthor']),
                         null,
@@ -1367,7 +1368,7 @@ class DocxExporter
                     );
                 }
 
-                if ('portraitWithPrioritization' !== $templateName && $this->exportFieldDecider->isExportable(FieldDecider::FIELD_SUBMITTER_NAME,
+                if (self::TEMPLATE_PORTRAIT_WITH_PRIORITIZATION !== $templateName && $this->exportFieldDecider->isExportable(FieldDecider::FIELD_SUBMITTER_NAME,
                     $exportConfig,
                     $statement,
                     $organisationData,
@@ -1418,7 +1419,7 @@ class DocxExporter
                     );
                 }
 
-                if ('portraitWithPrioritization' !== $templateName && $this->exportFieldDecider->isExportable(
+                if (self::TEMPLATE_PORTRAIT_WITH_PRIORITIZATION !== $templateName && $this->exportFieldDecider->isExportable(
                     FieldDecider::FIELD_SUBMITTER_NAME,
                     $exportConfig,
                     $statement,
@@ -1433,7 +1434,7 @@ class DocxExporter
                     );
                 }
 
-                if ('portraitWithPrioritization' !== $templateName && $this->isAddressExportable($citizenDetails, $exportConfig, $statement, $anonym)) {
+                if (self::TEMPLATE_PORTRAIT_WITH_PRIORITIZATION !== $templateName && $this->isAddressExportable($citizenDetails, $exportConfig, $statement, $anonym)) {
                     // Adresse
                     $cell2->addText(
                         htmlspecialchars((string) $citizenDetails['postalAddressPartsOfAuthor']),
@@ -1540,8 +1541,8 @@ class DocxExporter
                     });
             }
 
-            // Priorität (redundant in portraitWithPrioritization as it is already shown in the group heading)
-            if ('portraitWithPrioritization' !== $templateName && $this->exportFieldDecider->isExportable(FieldDecider::FIELD_PRIORITY, $exportConfig, $statement)) {
+            // Priorität (redundant in TEMPLATE_PORTRAIT_WITH_PRIORITIZATION as it is already shown in the group heading)
+            if (self::TEMPLATE_PORTRAIT_WITH_PRIORITIZATION !== $templateName && $this->exportFieldDecider->isExportable(FieldDecider::FIELD_PRIORITY, $exportConfig, $statement)) {
                 $textRun2 = $cell2->addTextRun($cellHCentered);
                 $textRun2AddText = $this->containerAddTextFunctionConstructor($textRun2, null, null);
                 $textRun2AddText('priority', '');
