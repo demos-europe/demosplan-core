763ac364bf6c2e55d9b43fb4459a92136bb5fa5f
Catalina Sierra Zamudio <sierra-zamudio@demos-deutschland.de>
2026-04-21 15:29:03 +0200

    feat (DPLAN-17619): Add TextFieldValueValidationStrategy for textField values

---
+++ demosplan/DemosPlanCoreBundle/Utils/CustomField/TextFieldValidator.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
+namespace demosplan\DemosPlanCoreBundle\Utils\CustomField;
+
+class TextFieldValidator extends CustomFieldValidator
+{
+    public function getFieldType(): string
+    {
+        return 'text';
+    }
+
+    public function getSourceToTargetMapping(): array
+    {
+        return [
+            'PROCEDURE'          => 'STATEMENT',
+            'PROCEDURE_TEMPLATE' => 'STATEMENT',
+        ];
+    }
+}
