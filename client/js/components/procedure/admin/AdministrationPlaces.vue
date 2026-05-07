112033c66887128df5c254c5340c978ab528785b
Ruth Meissner <meissner@demos-deutschland.de>
2026-05-07 14:03:13 +0200

    feat(DPLAN-17492): send locked when place marked solved

--- client/js/components/procedure/admin/AdministrationPlaces.vue
+++ client/js/components/procedure/admin/AdministrationPlaces.vue
@@ -392,6 +392,11 @@ export default {
           solved: this.newPlace.solved,
         },
       }
+
+      if (hasPermission('feature_administrate_segment_lock')) {
+        payload.attributes.locked = this.newPlace.solved
+      }
+
       dpApi.post(Routing.generate('api_resource_create', { resourceType: 'Place' }), {}, { data: payload })
         .then(response => {
           /**
@@ -454,6 +459,10 @@ export default {
             solved: this.newRowData.solved,
           },
         },
+      }
+
+      if (hasPermission('feature_administrate_segment_lock')) {
+        payload.data.attributes.locked = this.newRowData.solved
       }
 
       dpApi.patch(Routing.generate('api_resource_update', { resourceType: 'Place', resourceId: rowData.id }), {}, payload)
