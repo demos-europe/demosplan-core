d7fd62537960defbd8ab173fe65f657f4f2769ad
Sami Mussbach <mussbach@demos-deutschland.de>
2026-05-18 13:07:37 +0200

    chore: keep former comparisons

--- demosplan/DemosPlanCoreBundle/Logic/Statement/StatementService.php
+++ demosplan/DemosPlanCoreBundle/Logic/Statement/StatementService.php
@@ -1326,7 +1326,19 @@ class StatementService implements StatementServiceInterface
             }
         }
 
-        return $statement instanceof Statement && ($statement->getAuthorName() !== $currentAuthorName || $statement->getSubmitterName() !== $currentSubmitterName || $statement->getMeta()->getOrgaDepartmentName() !== $currentDepartmentName || $statement->getMeta()->getOrgaName() !== $currentSubmitterType || $statement->getSubmitterEmailAddress() !== $currentSubmitterEmailAddress || $statement->getOrgaPostalCode() !== $currentOrgaPostalCode || $statement->getOrgaCity() !== $currentOrgaCity || $statement->getOrgaStreet() !== $currentOrgaStreet || $statement->getOrgaEmail() != $currentOrgaEmail || $statement->getAuthoredDate() != $currentAuthoredDateTimeStamp || $statement->getSubmit() !== $currentSubmittedDateTimeStamp);
+        return $statement instanceof Statement && (
+                $statement->getAuthorName() != $currentAuthorName
+                || $statement->getSubmitterName() != $currentSubmitterName
+                || $statement->getMeta()->getOrgaDepartmentName() != $currentDepartmentName
+                || $statement->getMeta()->getOrgaName() != $currentSubmitterType
+                || $statement->getSubmitterEmailAddress() != $currentSubmitterEmailAddress
+                || $statement->getOrgaPostalCode() != $currentOrgaPostalCode
+                || $statement->getOrgaCity() != $currentOrgaCity
+                || $statement->getOrgaStreet() != $currentOrgaStreet
+                || $statement->getOrgaEmail() != $currentOrgaEmail
+                || $statement->getAuthoredDate() != $currentAuthoredDateTimeStamp
+                || $statement->getSubmit() != $currentSubmittedDateTimeStamp
+            );
     }
 
     /**
