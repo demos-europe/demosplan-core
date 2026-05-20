33814ae6958574ba546584e2b429f4ecbb1f040a
Vera Kijewski <kijewski@demos-deutschland.de>
2026-05-20 14:55:41 +0200

    f_DPLAN-17486: added migration file

---
+++ demosplan/DemosPlanCoreBundle/DoctrineMigrations/2026/05/Version20260519084605.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types = 1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Platforms\MySQLPlatform;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+class Version20260519084605 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'refs T: DPLAN-17486 add deadline_date column to _statement';
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function up(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+
+        $this->addSql('ALTER TABLE _statement ADD deadline_date DATE DEFAULT NULL');
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function down(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+
+        $this->addSql('ALTER TABLE _statement DROP deadline_date');
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function abortIfNotMysql(): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof MySQLPlatform,
+            "Migration can only be executed safely on 'mysql'."
+        );
+    }
+}
