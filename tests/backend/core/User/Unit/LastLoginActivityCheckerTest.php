2cfa412dd52de2251a33b7225b4a346d9c2cd748
Moritz Mandler <mandler@demos-deutschland.de>
2026-05-20 11:20:26 +0200

    feat(refs: DPLAN-17697): Mock PiiAwareLogger in LastLoginActivityCheckerTest

--- tests/backend/core/User/Unit/LastLoginActivityCheckerTest.php
+++ tests/backend/core/User/Unit/LastLoginActivityCheckerTest.php
@@ -12,8 +12,8 @@ namespace Tests\Core\User\Unit;
 
 use DateTimeImmutable;
 use DemosEurope\DemosplanAddon\Contracts\Entities\UserInterface;
+use demosplan\DemosPlanCoreBundle\Logger\PiiAwareLogger;
 use demosplan\DemosPlanCoreBundle\Logic\User\LastLoginActivityChecker;
-use Psr\Log\NullLogger;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
 use Tests\Base\UnitTestCase;
 
@@ -21,10 +21,13 @@ class LastLoginActivityCheckerTest extends UnitTestCase
 {
     protected $sut;
 
+    protected ?PiiAwareLogger $piiLogger = null;
+
     protected function setUp(): void
     {
         parent::setUp();
-        $this->sut = new LastLoginActivityChecker(new ParameterBag(), new NullLogger(), 30);
+        $this->piiLogger = $this->createMock(PiiAwareLogger::class);
+        $this->sut = new LastLoginActivityChecker(new ParameterBag(), $this->piiLogger, 30);
     }
 
     public function testIsUserActiveReturnsTrueWhenUserLoggedInWithinThreshold(): void
@@ -109,7 +112,7 @@ class LastLoginActivityCheckerTest extends UnitTestCase
     public function testGetActivityDescriptionWithCustomThreshold(): void
     {
         // Arrange
-        $checker = new LastLoginActivityChecker(new ParameterBag(), new NullLogger(), 60);
+        $checker = new LastLoginActivityChecker(new ParameterBag(), $this->piiLogger, 60);
 
         // Act
         $description = $checker->getActivityDescription();
@@ -170,7 +173,7 @@ class LastLoginActivityCheckerTest extends UnitTestCase
     public function testIsUserActiveWithOneDayThreshold(): void
     {
         // Arrange
-        $checker = new LastLoginActivityChecker(new ParameterBag(), new NullLogger(), 1);
+        $checker = new LastLoginActivityChecker(new ParameterBag(), $this->piiLogger, 1);
         $user = $this->createMock(UserInterface::class);
         // Login from yesterday should be active with 1-day threshold
         $yesterdayLogin = new DateTimeImmutable('-12 hours');
@@ -186,7 +189,7 @@ class LastLoginActivityCheckerTest extends UnitTestCase
     public function testIsUserActiveWithVeryLargeDayThreshold(): void
     {
         // Arrange
-        $checker = new LastLoginActivityChecker(new ParameterBag(), new NullLogger(), 3650); // ~10 years
+        $checker = new LastLoginActivityChecker(new ParameterBag(), $this->piiLogger, 3650); // ~10 years
         $user = $this->createMock(UserInterface::class);
         $veryOldLogin = new DateTimeImmutable('-5 years');
         $user->method('getLastLogin')->willReturn($veryOldLogin);
