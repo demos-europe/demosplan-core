2918a372e276b6b6c549fd89751e852e1e0208c6
riechedemos <carolin@demos-international.com>
2026-05-21 17:38:57 +0200

    feat(DPLAN-17747): add new bundle

---
+++ client/js/bundles/statement/groupStatements.js
@@ -0,0 +1,31 @@
+/**
+ * (c) 2010-present DEMOS plan GmbH.
+ *
+ * This file is part of the package demosplan,
+ * for more information see the license file.
+ *
+ * All rights reserved
+ */
+
+/**
+ * This is the entrypoint for assessment_statement.html.twig and cluster_detail.html.twig
+ */
+
+import BoilerplatesStore from '@DpJs/store/procedure/Boilerplates'
+import { hasPermission } from '@demos-europe/demosplan-ui'
+import { initialize } from '@DpJs/InitVue'
+import StatementGroupForm from '@DpJs/components/statement/StatementGroupForm'
+
+const components = { StatementGroupForm }
+const stores = {}
+let apiStores = ['StatementSegment', 'Tag', 'TagTopic']
+
+if (hasPermission('area_admin_boilerplates')) {
+  stores.boilerplates = BoilerplatesStore
+}
+
+if (hasPermission('field_segments_custom_fields')) {
+  apiStores = [...apiStores, 'AdminProcedure', 'CustomField']
+}
+
+initialize(components, stores, apiStores)
