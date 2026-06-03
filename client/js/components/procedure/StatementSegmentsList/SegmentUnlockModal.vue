497142d5f2472a4a290ac1e2ab658c2300d4b66d
Ruth Meissner <meissner@demos-deutschland.de>
2026-06-03 10:23:33 +0200

    fix(refs: DPLAN-17492): implement new design for SegmentUnlockModal

--- client/js/components/procedure/StatementSegmentsList/SegmentUnlockModal.vue
+++ client/js/components/procedure/StatementSegmentsList/SegmentUnlockModal.vue
@@ -3,52 +3,45 @@
     ref="unlockModal"
     content-classes="w-1/4 px-2 pb-4"
   >
-    <h3
-      class="font-semibold mb-4 mt-2"
-    >
+    <template v-slot:header>
       {{ Translator.trans('segment.unlock') }}
-    </h3>
-    <p class="mb-2">
-      {{ Translator.trans('segment.lock.hint.admin') }}
-    </p>
-    <p class="mb-2">
-      {{ '* ' + Translator.trans('field.required') }}
-    </p>
+    </template>
+    <dp-inline-notification
+      :message="Translator.trans('segment.lock.hint.admin')"
+      class="mb-4 mt-2"
+      dismissible-key="segmentUnlockModalHint"
+      type="info"
+      dismissible
+    />
     <dp-label
-      class="mb-0 mt-4"
       :text="Translator.trans('place')"
-      :hint="Translator.trans('place.set')"
-      :bold="false"
+      class="mb-0 mt-4"
       for="segmentUnlockPlace"
       required
     />
     <dp-multiselect
       id="segmentUnlockPlace"
       v-model="localPlace"
-      class="mb-2"
       :allow-empty="false"
-      label="name"
       :options="places"
+      class="mb-2"
+      label="name"
       track-by="id"
       required
     />
     <dp-label
-      class="mb-0 mt-4"
       :text="Translator.trans('assignee')"
-      :hint="Translator.trans('assignee.assign')"
-      :bold="false"
+      class="mb-0 mt-4"
       for="segmentUnlockAssignee"
-      required
     />
     <dp-multiselect
       id="segmentUnlockAssignee"
       v-model="localAssignee"
-      class="mb-6"
       :allow-empty="false"
-      label="name"
       :options="assignableUsers"
+      class="mb-6"
+      label="name"
       track-by="id"
-      required
     />
     <dp-button-row
       primary
@@ -60,10 +53,10 @@
 </template>
 
 <script setup>
-import { DpButtonRow, DpLabel, DpModal, DpMultiselect } from '@demos-europe/demosplan-ui'
+import { DpButtonRow, DpInlineNotification, DpLabel, DpModal, DpMultiselect } from '@demos-europe/demosplan-ui'
 import { ref } from 'vue'
 
-defineProps({
+const { assignableUsers, places } = defineProps({
   assignableUsers: {
     type: Object,
     required: true,
@@ -77,17 +70,17 @@ defineProps({
 
 const emit = defineEmits(['unlock'])
 
-defineExpose({ toggle })
-
-const localAssignee = ref(null)
+// Default the assignee to "not assigned"
+const localAssignee = ref(assignableUsers.find(user => user.id === 'noAssigneeId'))
 const localPlace = ref(null)
 const unlockModal = ref()
+
+const toggle = () => unlockModal.value.toggle()
 
 const save = () => {
   emit('unlock', { assignee: localAssignee.value, place: localPlace.value })
   toggle()
 }
-function toggle () { unlockModal.value.toggle() }
 
+defineExpose({ toggle })
 </script>
-
