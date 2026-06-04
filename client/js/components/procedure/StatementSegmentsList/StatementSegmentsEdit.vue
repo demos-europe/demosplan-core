22753d1fbcf321e7c8cca06727e1a41a87cad302
Ruth Meissner <meissner@demos-deutschland.de>
2026-06-04 08:01:35 +0200

    feat(refs: DPLAN-17492): add unlocking for admins to StatementSegmentsEdit

--- client/js/components/procedure/StatementSegmentsList/StatementSegmentsEdit.vue
+++ client/js/components/procedure/StatementSegmentsList/StatementSegmentsEdit.vue
@@ -38,16 +38,28 @@
         class="px-1 hover:bg-interactive-secondary-subtle-hover"
       >
         <div class="inline-block w-[5%]">
-          <dp-tooltip
-            v-if="isSegmentLocked(segment)"
-            :text="lockTooltip"
-          >
-            <dp-icon
+          <template v-if="isSegmentLocked(segment)">
+            <dp-button
+              v-if="hasPermission('feature_administrate_segment_lock')"
+              :text="lockTooltip"
               class="text-interactive inline-block ml-1"
               icon="prohibit"
-              weight="fill"
+              icon-weight="fill"
+              variant="subtle"
+              hide-text
+              @click="openUnlockModal(segment)"
             />
-          </dp-tooltip>
+            <dp-tooltip
+              v-else
+              :text="lockTooltip"
+            >
+              <dp-icon
+                class="text-interactive inline-block ml-1"
+                icon="prohibit"
+                weight="fill"
+              />
+            </dp-tooltip>
+          </template>
           <dp-claim
             v-else
             class="c-at-item__row-icon inline-block"
@@ -148,6 +160,13 @@
         <div v-cleanhtml="statement.attributes.fullText || ''" />
       </div>
     </template>
+    <segment-unlock-modal
+      v-if="hasPermission('feature_administrate_segment_lock')"
+      ref="unlockModal"
+      :assignable-users="assignableUsers"
+      :places="places"
+      @unlock="unlockSegment"
+    />
   </div>
 </template>
 
@@ -155,6 +174,7 @@
 import {
   CleanHtml,
   dpApi,
+  DpButton,
   DpButtonRow,
   DpIcon,
   DpInlineNotification,
@@ -170,12 +190,14 @@ import DpEditField from '@DpJs/components/statement/assessmentTable/DpEditField'
 import { handleSegmentNavigation } from '@DpJs/lib/segment/handleSegmentNavigation'
 import paginationMixin from '@DpJs/components/shared/mixins/paginationMixin'
 import { scrollTo } from 'vue-scrollto'
+import SegmentUnlockModal from '@DpJs/components/procedure/StatementSegmentsList/SegmentUnlockModal'
 import TextContentRenderer from '@DpJs/components/shared/TextContentRenderer'
 
 export default {
   name: 'StatementSegmentsEdit',
 
   components: {
+    DpButton,
     DpButtonRow,
     DpClaim,
     DpEditField,
@@ -188,6 +210,7 @@ export default {
       return DpEditor
     }),
     DpInlineNotification,
+    SegmentUnlockModal,
     TextContentRenderer,
   },
 
@@ -239,6 +262,7 @@ export default {
       pagination: {},
       storageKeyPagination: `segmentsEdit_${this.statementId}_pagination`,
       segmentNavigation: null,
+      segmentToUnlock: null,
     }
   },
 
@@ -254,6 +278,19 @@ export default {
     ...mapState('Place', {
       placeItems: 'items',
     }),
+
+    ...mapState('AssignableUser', {
+      assignableUsersObject: 'items',
+    }),
+
+    assignableUsers () {
+      const users = Object.values(this.assignableUsersObject).map(user => ({
+        name: user.attributes.firstname + ' ' + user.attributes.lastname,
+        id: user.id,
+      }))
+
+      return [{ name: Translator.trans('not.assigned'), id: 'noAssigneeId' }, ...users]
+    },
 
     assigneeBySegment () {
       return segmentId => {
@@ -285,13 +322,22 @@ export default {
 
     lockTooltip () {
       return hasPermission('feature_administrate_segment_lock') ?
-        Translator.trans('segment.lock.hint.admin.change.view') :
+        Translator.trans('segment.unlock.click.hint') :
         Translator.trans('segment.lock.hint')
+    },
+
+    places () {
+      return Object.values(this.placeItems).map(place => ({
+        name: place.attributes.name,
+        id: place.id,
+        locked: place.attributes.locked,
+      }))
     },
 
     statement () {
       return this.statements[this.statementId] || null
     },
+
   },
 
   methods: {
@@ -309,6 +355,10 @@ export default {
 
     ...mapActions('Statement', {
       restoreStatementAction: 'restoreFromInitial',
+    }),
+
+    ...mapActions('AssignableUser', {
+      fetchAssignableUsers: 'list',
     }),
 
     ...mapMutations('Statement', {
@@ -394,6 +444,42 @@ export default {
       const placeId = segment?.relationships?.place?.data?.id
 
       return !!this.placeItems[placeId]?.attributes?.locked
+    },
+
+    openUnlockModal (segment) {
+      this.segmentToUnlock = segment
+      this.$refs.unlockModal.toggle()
+    },
+
+    unlockSegment ({ assignee, place }) {
+      const assigneeRel = assignee.id === 'noAssigneeId' ?
+        { data: null } :
+        { data: { id: assignee.id, type: 'AssignableUser' } }
+
+      const payload = {
+        data: {
+          id: this.segmentToUnlock.id,
+          type: 'StatementSegment',
+          relationships: {
+            assignee: assigneeRel,
+            place: { data: { id: place.id, type: 'Place' } },
+          },
+        },
+      }
+
+      return dpApi.patch(
+        Routing.generate('api_resource_update', { resourceType: 'StatementSegment', resourceId: this.segmentToUnlock.id }),
+        {},
+        payload,
+      )
+        .then(() => {
+          dplan.notify.notify('confirm', Translator.trans('confirm.saved'))
+          this.fetchSegments(this.pagination?.currentPage || 1)
+        })
+        .catch((err) => {
+          console.error(err)
+          dplan.notify.notify('error', Translator.trans('error.api.generic'))
+        })
     },
 
     reset (segmentId) {
@@ -672,6 +758,11 @@ export default {
        * default to 1st page
        */
       this.fetchSegments(this.pagination?.currentPage || 1)
+
+      // Assignable users are needed to populate the unlock modal
+      if (hasPermission('feature_administrate_segment_lock')) {
+        this.fetchAssignableUsers()
+      }
     }
   },
 
