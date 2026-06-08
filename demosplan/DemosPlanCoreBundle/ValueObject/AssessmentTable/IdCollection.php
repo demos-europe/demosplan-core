0042d5b23e03ed5c5d2bce316b8ca69a89e202fd
Demos-CI <devops-system+github@demos-deutschland.de>
2026-06-08 08:27:03 +0000

    style: Apply php-cs-fixer

--- demosplan/DemosPlanCoreBundle/ValueObject/AssessmentTable/IdCollection.php
+++ demosplan/DemosPlanCoreBundle/ValueObject/AssessmentTable/IdCollection.php
@@ -15,10 +15,10 @@ namespace demosplan\DemosPlanCoreBundle\ValueObject\AssessmentTable;
 use demosplan\DemosPlanCoreBundle\ValueObject\ValueObject;
 
 /**
- * @method array  getFragmentIds()
- * @method        setFragmentIds(array $fragmentIds)
- * @method null|array getStatementIds()
- * @method        setStatementIds(array $statementIds)
+ * @method array      getFragmentIds()
+ * @method            setFragmentIds(array $fragmentIds)
+ * @method array|null getStatementIds()
+ * @method            setStatementIds(array $statementIds)
  */
 class IdCollection extends ValueObject
 {
