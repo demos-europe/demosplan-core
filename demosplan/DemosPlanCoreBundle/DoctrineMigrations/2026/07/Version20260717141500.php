21168e88b9c7d438b2c97128a2f728ecdb36cf08
Catalina Sierra Zamudio <sierra-zamudio@demos-deutschland.de>
2026-07-17 14:17:37 +0200

    feat (DPLAN-17302): Add allow_uninvited_institutions column to procedure settings to support per-procedure opt-out of the invited-institutions-only restriction.

---
+++ demosplan/DemosPlanCoreBundle/DoctrineMigrations/2026/07/Version20260717141500.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types = 1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Platforms\MySQLPlatform;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+class Version20260717141500 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'refs DPLAN-17302: Add allow_uninvited_institutions column to _procedure_settings';
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function up(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+
+        $this->addSql('ALTER TABLE _procedure_settings ADD allow_uninvited_institutions TINYINT(1) DEFAULT 0 NOT NULL');
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function down(Schema $schema): void
+    {
+        $this->abortIfNotMysql();
+
+        $this->addSql('ALTER TABLE _procedure_settings DROP allow_uninvited_institutions');
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function abortIfNotMysql(): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof MySQLPlatform,
+            "Migration can only be executed safely on 'mysql'."
+        );
+    }
+}
