00f86c13f530b994c7cf45996d1e6d4421a720e7
Heidrun Wiemold <wiemold@demos-deutschland.de>
2026-07-28 14:05:46 +0200

    style(refs DPLAN-18225): sort imports alphabetically

--- client/js/components/procedure/SegmentsBulkEdit/SegmentsBulkEdit.vue
+++ client/js/components/procedure/SegmentsBulkEdit/SegmentsBulkEdit.vue
@@ -397,11 +397,11 @@ import {
   reformatDateString,
 } from '@demos-europe/demosplan-ui'
 import { mapActions, mapState } from 'vuex'
-import { defineAsyncComponent } from 'vue'
 import ActionStepper from '@DpJs/components/procedure/SegmentsBulkEdit/ActionStepper/ActionStepper'
 import ActionStepperAction from '@DpJs/components/procedure/SegmentsBulkEdit/ActionStepper/ActionStepperAction'
 import ActionStepperResponse from '@DpJs/components/procedure/SegmentsBulkEdit/ActionStepper/ActionStepperResponse'
 import { apiUrl } from '@DpJs/store/core/VuexApiRoutes'
+import { defineAsyncComponent } from 'vue'
 import DpBoilerPlateModal from '@DpJs/components/statement/DpBoilerPlateModal'
 import lscache from 'lscache'
 import RecommendationModal from '../Shared/RecommendationModal'
