4bb4bc59a36ab902cc27fd1339f1f34dbe5e7f81
Stefan Graupner <graupner@demos-deutschland.de>
2026-08-17 15:01:49 +0200

    fix: Make sure the tabs are actually initialized
    The import tabs addons were loaded, however, they were not visible in the UI.
    This commit also sneaks in two additional long standing cleanups:
    
    Instead of rolling it's own, AdministrationImport now also uses the centralized
    addon loading loagic.
    
    The import tabs are sorted by their title instead of being sorted sort-of randomly.
    (The random sorting was internal, then addons in load order. But it seems random
    from a user's perspective)

--- client/js/components/procedure/AdministrationImport/AdministrationImport.vue
+++ client/js/components/procedure/AdministrationImport/AdministrationImport.vue
@@ -25,7 +25,7 @@
         <slot>
           <keep-alive>
             <component
-              :is="option.name"
+              :is="option.component || option.name"
               class="u-mt"
               :demosplan-ui="demosplanUi"
               :csrf-token="csrfToken"
@@ -44,9 +44,15 @@
 
 <script>
 import * as demosplanUi from '@demos-europe/demosplan-ui'
-import { DpLoading, dpRpc, DpTab, DpTabs, hasAnyPermissions } from '@demos-europe/demosplan-ui'
+import {
+  DpLoading,
+  DpTab,
+  DpTabs,
+  hasAnyPermissions,
+} from '@demos-europe/demosplan-ui'
 import AdministrationImportNone from './AdministrationImportNone'
 import ExcelImport from './ExcelImport/ExcelImport'
+import loadAddonComponents from '@DpJs/lib/addon/loadAddonComponents'
 import ParticipationImport from './ParticipationImport/ParticipationImport'
 import { shallowRef } from 'vue'
 import StatementFormImport from './StatementFormImport/StatementFormImport'
@@ -144,7 +150,10 @@ export default {
       return [
         {
           name: ExcelImport.name,
-          permissions: ['feature_statements_import_excel', 'feature_segments_import_excel'],
+          permissions: [
+            'feature_statements_import_excel',
+            'feature_segments_import_excel',
+          ],
           title: 'import.options.xls',
         },
         {
@@ -157,9 +166,14 @@ export default {
           permissions: ['feature_statements_participation_import_excel'],
           title: 'import.options.participation',
         },
-      ].filter((component) => {
-        return hasAnyPermissions(component.permissions)
-      }).concat(this.asyncComponents)
+      ]
+        .filter((component) => {
+          return hasAnyPermissions(component.permissions)
+        })
+        .concat(this.asyncComponents)
+        .sort((a, b) => {
+          return Translator.trans(a.title).localeCompare(Translator.trans(b.title))
+        })
     },
   },
 
@@ -170,49 +184,28 @@ export default {
       }
 
       if (window.localStorage.getItem('importCenterActiveTabId')) {
-        this.activeTabId = window.localStorage.getItem('importCenterActiveTabId')
+        this.activeTabId = window.localStorage.getItem(
+          'importCenterActiveTabId',
+        )
       }
-    },
-
-    loadComponents (hookName) {
-      const params = {
-        hookName,
-      }
-
-      return dpRpc('addons.assets.load', params)
-        .then(({ data }) => {
-          const result = data[0].result
-
-          for (const key of Object.keys(result)) {
-            const addon = result[key]
-            const contentKey = addon.entry + '.umd.js'
-            const content = addon.content[contentKey]
-
-            /**
-             * The evaluation of the response content automatically binds the vue component
-             * to the window object. This way we can implement it in vue's internals to render
-             * the component.
-             */
-            eval(content)
-            this.$options.components[addon.entry] = window[addon.entry].default
-
-            this.asyncComponents.push({
-              name: addon.entry,
-              title: addon.options.title,
-            })
-          }
-        })
     },
   },
 
   mounted () {
-    const promises = [this.loadComponents('import.tabs')]
+    const promises = [
+      loadAddonComponents('import.tabs').then((addons) => {
+        this.asyncComponents = addons.map((addon) => ({
+          component: shallowRef(window[addon.name].default),
+          name: addon.name,
+          title: addon.options.title,
+        }))
+      }),
+    ]
 
-    Promise.allSettled(promises)
-      .then(() => {
-        this.allComponentsLoaded = true
-        this.setActiveTabId()
-      })
+    Promise.allSettled(promises).then(() => {
+      this.allComponentsLoaded = true
+      this.setActiveTabId()
+    })
   },
 }
 </script>
