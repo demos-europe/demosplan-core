339b392668de0dc60a995800f5932c1a0784ace1
Stefan Graupner <graupner@demos-deutschland.de>
2026-08-18 15:42:11 +0200

    refactor: Extract methods

--- client/js/lib/addon/loadAddonComponents.js
+++ client/js/lib/addon/loadAddonComponents.js
@@ -1,6 +1,60 @@
 import { dpRpc } from '@demos-europe/demosplan-ui'
 
-export default async function loadAddonComponents (hookName) {
+/**
+ * Legacy path for addons still built as UMD bundles. While eval is generally a BAD IDEA,
+ * older addon builds ship no ES module to import(), only a global to eval into existence.
+ * Remove once every addon has migrated to the ESM builder output!
+ *
+ * @param {object} addon
+ */
+function legacyLoadAddon (addon) {
+  const contentKey = addon.entry + '.umd.js'
+
+  eval(addon.content[contentKey])
+
+  return window[addon.entry].default
+}
+
+/**
+ * Handle loading the frontend assets for an addon
+ *
+ * @param {object} addon
+ * @returns {Promise<void>}
+ */
+async function loadAddon (addon) {
+  if (addon === undefined) {
+    /*
+     * If for some reason we don't receive a valid response object from the backend
+     * we'll just skip it.
+     */
+    throw Error('Addon is undefined')
+  }
+
+  let component
+
+  if (addon.urls) {
+    const urlKey = `${addon.entry}.esm.js`
+    const module = await import(/* webpackIgnore: true */ addon.urls[urlKey])
+
+    component = module.default
+  } else {
+    component = legacyLoadAddon(addon)
+  }
+
+  return {
+    component,
+    name: addon.entry,
+    options: addon.options ?? '',
+  }
+}
+
+/**
+ * Process addon registration and loading for a given hook
+ *
+ * @param {string} hookName
+ * @returns {Promise<*|*[]>}
+ */
+async function loadAddonComponents (hookName) {
   while (window.dplan.loadedAddons[hookName] === 'pending') {
     await new Promise(resolve => setTimeout(resolve, 250))
   }
@@ -23,39 +77,13 @@ export default async function loadAddonComponents (hookName) {
       for (const key of Object.keys(result)) {
         const addon = result[key]
 
-        if (addon === undefined) {
-          /*
-           * If for some reason we don't receive a valid response object from the backend
-           * we'll just skip it.
-           */
-          console.debug('Skipping addon hook response evaluation for ' + key)
-          continue
+        try {
+          let loadedAddon = await loadAddon(addon, key, addons)
+
+          addons.push(loadedAddon)
+        } catch (error) {
+          console.debug(`An error occurred while loading addon ${key}`, error)
         }
-
-        let component
-
-        if (addon.urls) {
-          const urlKey = addon.entry + '.esm.js'
-          const module = await import(/* webpackIgnore: true */ addon.urls[urlKey])
-
-          component = module.default
-        } else {
-          /*
-           * Legacy path for addons still built as UMD bundles. While eval is generally a BAD IDEA,
-           * older addon builds ship no ES module to import(), only a global to eval into existence.
-           * Remove once every addon has migrated to the ESM builder output.
-           */
-          const contentKey = addon.entry + '.umd.js'
-
-          eval(addon.content[contentKey])
-          component = window[addon.entry].default
-        }
-
-        addons.push({
-          component,
-          name: addon.entry,
-          options: addon.options ?? '',
-        })
       }
 
       window.dplan.loadedAddons[hookName] = addons
@@ -63,3 +91,5 @@ export default async function loadAddonComponents (hookName) {
       return addons
     })
 }
+
+export default loadAddonComponents
