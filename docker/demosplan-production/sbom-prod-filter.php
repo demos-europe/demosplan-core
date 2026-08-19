161b03e1aa801d8707e5f239e5f72af4fd541c7d
Demos-CI <devops-system+github@demos-deutschland.de>
2026-08-19 18:50:03 +0000

    style: Apply php-cs-fixer

--- docker/demosplan-production/sbom-prod-filter.php
+++ docker/demosplan-production/sbom-prod-filter.php
@@ -14,6 +14,14 @@
 
 declare(strict_types=1);
 
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
 function fail(string $message): never
 {
     fwrite(STDERR, "refusing to filter: $message\n");
