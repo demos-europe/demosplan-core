819ebeeab00c98ea19828db769ee0f9fd5384798
Demos-CI <devops-system+github@demos-deutschland.de>
2026-09-03 16:44:24 +0000

    style: Apply php-cs-fixer

--- tests/backend/core/Procedure/Functional/BoilerplateDeletionServiceTest.php
+++ tests/backend/core/Procedure/Functional/BoilerplateDeletionServiceTest.php
@@ -292,5 +292,4 @@ class BoilerplateDeletionServiceTest extends FunctionalTestCase
         }
         static::assertCount(3, $this->boilerplateUsageRepository->getUsagesForBoilerplate($boilerplateId));
     }
-
 }
