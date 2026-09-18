892ecd3e235364a54d924353cd573c6a4f27173a
Hamza Bakri <bakri@demos-deutschland.de>
2026-09-18 10:46:55 +0200

    test HDDP-16 : cover Customer rich-text file references in FileInUseChecker
    Adds coverage for files embedded via hash or ident in Customer fields
    (e.g. overview description, imprint, terms of use), plus a regression
    check that unreferenced files are still correctly detected as unused.
    
    Signed-off-by: Hamza Bakri <bakri@demos-deutschland.de>

---
+++ tests/backend/core/Logic/FileInUseCheckerTest.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the package demosplan.
+ *
+ * (c) 2010-present DEMOS plan GmbH, for more information see the license file.
+ *
+ * All rights reserved
+ */
+
+namespace Tests\Core\Logic;
+
+use demosplan\DemosPlanCoreBundle\DataGenerator\Factory\FileFactory;
+use demosplan\DemosPlanCoreBundle\DataGenerator\Factory\User\CustomerFactory;
+use demosplan\DemosPlanCoreBundle\Logic\FileInUseChecker;
+use Tests\Base\FunctionalTestCase;
+
+class FileInUseCheckerTest extends FunctionalTestCase
+{
+    protected $sut = null;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->sut = self::getContainer()->get(FileInUseChecker::class);
+    }
+
+    public function testFileReferencedByHashInCustomerFieldIsInUse(): void
+    {
+        $hash = 'c1839dbf0b5c8e3c92a902f33b32490e';
+        $file = FileFactory::createOne(['hash' => $hash]);
+        CustomerFactory::createOne([
+            'overviewDescriptionInSimpleLanguage' => '<img src="/file/'.$hash.'">',
+        ]);
+
+        $isInUse = $this->sut->isFileInUse($file->getId());
+
+        self::assertTrue($isInUse);
+    }
+
+    public function testFileReferencedByIdentInCustomerFieldIsInUse(): void
+    {
+        $file = FileFactory::createOne(['hash' => 'unrelated-hash']);
+        CustomerFactory::createOne([
+            'imprint' => '<img src="/file/'.$file->getId().'">',
+        ]);
+
+        $isInUse = $this->sut->isFileInUse($file->getId());
+
+        self::assertTrue($isInUse);
+    }
+
+    public function testFileNotReferencedAnywhereIsNotInUse(): void
+    {
+        $file = FileFactory::createOne(['hash' => 'orphaned-hash']);
+        CustomerFactory::createOne();
+
+        $isInUse = $this->sut->isFileInUse($file->getId());
+
+        self::assertFalse($isInUse);
+    }
+}
